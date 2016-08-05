cc98eb6b30b01541edbcc93bcd3b122a32f81f81
Björn Buchhold <b.buchhold@gmail.com>
2016-08-05 15:50:15 +0200

    fix to python script that creates the nt file from broccoli input

--- misc/broccoli_ontology_txt_to_nt.py
+++ misc/broccoli_ontology_txt_to_nt.py
@@ -15,7 +15,7 @@ def handleSubject(element):
     if not element:
         return element
     new = '<'
-    for c in element[1:-1]:
+    for c in element:
         if c == '<':
             new += '&lt'
         elif c == '>':
@@ -27,7 +27,7 @@ def handleSubject(element):
 
 
 def handleObject(element):
-    if element and element[0] == '"':
+    if element and element[0] != '"':
         new = '<'
         for c in element[1:-1]:
             if c == '<':
