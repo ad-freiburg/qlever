179802ae0f1b24c7de833681023b2eddde44d3bd
Björn Buchhold <b.buchhold@gmail.com>
2016-08-05 15:43:29 +0200

    Added experiments folder and a python script to transform broccoli KBs to NT format

---
+++ misc/broccoli-ontology-txt-to-nt.py
@@ -0,0 +1,62 @@
+import argparse
+import sys
+
+__author__ = 'buchholb'
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--ontologytxt',
+                    type=str,
+                    help='Broccoli ontology.txt file.',
+                    required=True)
+
+
+def handleSubject(element):
+    if not element:
+        return element
+    new = '<'
+    for c in element[1:-1]:
+        if c == '<':
+            new += '&lt'
+        elif c == '>':
+            new += '&gt'
+        else:
+            new += c
+    new += '>'
+    return new
+
+
+def handleObject(element):
+    if element and element[0] == '"':
+        new = '<'
+        for c in element[1:-1]:
+            if c == '<':
+                new += '&lt'
+            elif c == '>':
+                new += '&gt'
+            else:
+                new += c
+        new += '>'
+        return new
+    else:
+        return element
+
+
+def writeNtFileToStdout(ontologytxt):
+    for line in open(ontologytxt):
+        cols = line.strip('\n').split('\t')
+        if len(cols) != 4 or cols[3] != '.':
+            sys.err.write('Ignoring malformed line: ' + line)
+        else:
+            print '\t'.join([handleSubject(cols[0]), handleSubject(cols[1]),
+                             handleObject(cols[2]), '.'])
+
+
+def main():
+    args = vars(parser.parse_args())
+    ontologytxt = args['ontologytxt']
+    writeNtFileToStdout(ontologytxt)
+
+
+if __name__ == '__main__':
+    main()
