1fff97c2c8e18011984dcf84fd37a1579a8289c3
Björn Buchhold <b.buchhold@gmail.com>
2016-08-08 14:35:14 +0200

    changed (and improved??) heuristic for joins. hopefully very easy joins are preferred more reliably now. needs performance evaluation.

--- src/engine/Join.h
+++ src/engine/Join.h
@@ -7,66 +7,99 @@
 #include <unordered_map>
 #include "./Operation.h"
 #include "./QueryExecutionTree.h"
+#include "IndexScan.h"
 
 using std::list;
 using std::unordered_map;
 
 
 class Join : public Operation {
-  public:
+public:
 
-    Join(QueryExecutionContext *qec, const QueryExecutionTree& t1,
-         const QueryExecutionTree& t2, size_t t1JoinCol, size_t t2JoinCol,
-         bool keepJoinColumn = true);
+  Join(QueryExecutionContext* qec, const QueryExecutionTree& t1,
+       const QueryExecutionTree& t2, size_t t1JoinCol, size_t t2JoinCol,
+       bool keepJoinColumn = true);
 
-    Join(const Join& other);
+  Join(const Join& other);
 
-    Join& operator=(const Join& other);
+  Join& operator=(const Join& other);
 
-    virtual ~Join();
+  virtual ~Join();
 
-    virtual string asString() const;
+  virtual string asString() const;
 
-    virtual size_t getResultWidth() const;
+  virtual size_t getResultWidth() const;
 
-    virtual size_t resultSortedOn() const;
+  virtual size_t resultSortedOn() const;
 
-    unordered_map<string, size_t> getVariableColumns() const;
+  unordered_map<string, size_t> getVariableColumns() const;
 
-    virtual void setTextLimit(size_t limit) {
-      _left->setTextLimit(limit);
-      _right->setTextLimit(limit);
-    }
+  virtual void setTextLimit(size_t limit) {
+    _left->setTextLimit(limit);
+    _right->setTextLimit(limit);
+  }
 
-    bool isSelfJoin() const;
+  bool isSelfJoin() const;
 
-    virtual size_t getSizeEstimate() const {
-      // return std::min(_left->getSizeEstimate(), _right->getSizeEstimate()) / 2;
-      // Self joins generallay increase the size
-      if (isSelfJoin()) {
-        return std::max(
-            size_t(1),
-            (_left->getSizeEstimate() + _right->getSizeEstimate()) * 10);
+  virtual size_t getSizeEstimate() const {
+    // Check if there are easy sides, i.e. a scan with only one
+    // variable.
+    // As a very basic heuristic, we expect joins with those to be even more
+    // restrictive. Obvious counter examples are stuff like "?x <is-a> <Topic>",
+    // i.e. very large lists, but at least we certainly account for size
+    // already and such joins are still very nice
+    // (no sorting, certainly restrictive).
+    // Without any easy side, we assume the worst, i.e. that the join actually
+    // increases the result size over the sum of two subtree sizes.
+    size_t easySides = 0;
+    if (_left->getType() == QueryExecutionTree::SCAN) {
+      if (static_cast<const IndexScan*>(
+              _left->getRootOperation())->getResultWidth() == 1) {
+        ++easySides;
       }
+    }
+    if (_right->getType() == QueryExecutionTree::SCAN) {
+      if (static_cast<const IndexScan*>(
+              _right->getRootOperation())->getResultWidth() == 1) {
+        ++easySides;
+      }
+    }
+    // return std::min(_left->getSizeEstimate(), _right->getSizeEstimate()) / 2;
+    // Self joins generally increase the size significantly.
+    if (isSelfJoin()) {
+      return std::max(
+          size_t(1),
+          (_left->getSizeEstimate() + _right->getSizeEstimate()) * 10);
+    }
+    if (easySides == 0) {
       return std::max(
           size_t(1),
           (_left->getSizeEstimate() + _right->getSizeEstimate()) / 4);
+    } else if (easySides == 1) {
+      return std::max(
+          size_t(1),
+          (_left->getSizeEstimate() + _right->getSizeEstimate()) / 4);
+    } else {
+      return std::max(
+          size_t(1),
+          (_left->getSizeEstimate() + _right->getSizeEstimate()) / 10);
     }
+  }
 
-    virtual size_t getCostEstimate() const {
-      return getSizeEstimate() +
-             _left->getSizeEstimate() + _left->getCostEstimate() +
-             _right->getSizeEstimate() + _right->getCostEstimate();
-    }
+  virtual size_t getCostEstimate() const {
+    return getSizeEstimate() +
+           _left->getSizeEstimate() + _left->getCostEstimate() +
+           _right->getSizeEstimate() + _right->getCostEstimate();
+  }
 
-  private:
-    QueryExecutionTree *_left;
-    QueryExecutionTree *_right;
+private:
+  QueryExecutionTree* _left;
+  QueryExecutionTree* _right;
 
-    size_t _leftJoinCol;
-    size_t _rightJoinCol;
+  size_t _leftJoinCol;
+  size_t _rightJoinCol;
 
-    bool _keepJoinColumn;
+  bool _keepJoinColumn;
 
-    virtual void computeResult(ResultTable *result) const;
+  virtual void computeResult(ResultTable* result) const;
 };
