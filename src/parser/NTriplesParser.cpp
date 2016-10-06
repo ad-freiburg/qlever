521db856bf14f188d7f724c7de0b4a7da1be4e0a
Buchhold <b.buchhold@gmail.com>
2016-10-06 14:59:45 +0200

    Fixed a bug with escaped backslashes vs escaped quotes when looking for the end of literals in the ntriples parser.

--- src/parser/NTriplesParser.cpp
+++ src/parser/NTriplesParser.cpp
@@ -24,54 +24,62 @@ bool NTriplesParser::getLine(array<string, 3>& res) {
   string line;
   if (std::getline(_in, line)) {
     size_t i = 0;
-    while (i < line.size() && (line[i] == ' ' || line[i] == '\t')) {++i;}
+    while (i < line.size() && (line[i] == ' ' || line[i] == '\t')) { ++i; }
     assert(i < line.size());
     size_t j = i + 1;
-    while (j < line.size() && line[j] != '\t' && line[j] != ' ') {++j;}
+    while (j < line.size() && line[j] != '\t' && line[j] != ' ') { ++j; }
     assert(j < line.size());
     if (!(line[i] == '<' && line[j - 1] == '>')) {
       AD_THROW(ad_semsearch::Exception::BAD_INPUT, "Illegal URI in : " + line);
     }
     res[0] = line.substr(i, j - i);
     i = j;
-    while (i < line.size() && (line[i] == ' ' || line[i] == '\t')) {++i;}
+    while (i < line.size() && (line[i] == ' ' || line[i] == '\t')) { ++i; }
     assert(i < line.size());
     j = i + 1;
-    while (j < line.size() && line[j] != '\t' && line[j] != ' ') {++j;}
+    while (j < line.size() && line[j] != '\t' && line[j] != ' ') { ++j; }
     if (!(line[i] == '<' && line[j - 1] == '>')) {
       AD_THROW(ad_semsearch::Exception::BAD_INPUT, "Illegal URI in : " + line);
     }
     res[1] = line.substr(i, j - i);
     i = j;
-    while (i < line.size() && (line[i] == ' ' || line[i] == '\t')) {++i;}
+    while (i < line.size() && (line[i] == ' ' || line[i] == '\t')) { ++i; }
     assert(i < line.size());
     if (line[i] == '<') {
       // URI
       j = line.find('>', i + 1);
       if (j == string::npos) {
         AD_THROW(ad_semsearch::Exception::BAD_INPUT,
-            "Illegal URI in : " + line);
+                 "Illegal URI in : " + line);
       }
       ++j;
     } else {
       // Literal
       j = line.find('\"', i + 1);
-      while (j != string::npos && line[j - 1] == '\\') {
-        j = line.find('\"', j + 1);
-      }
-      if (j == string::npos) {
-        AD_THROW(ad_semsearch::Exception::BAD_INPUT,
-            "Illegal literal in : " + line);
+      bool escape = false;
+      while (j < line.size()) {
+        if (!escape && line[j] == '\\') {
+          escape = true;
+        } else if (!escape && line[j] == '\"') {
+          break;
+        } else {
+          escape = false;
+        }
+        ++j;
       }
 
+      if (j == line.size()) {
+        AD_THROW(ad_semsearch::Exception::BAD_INPUT,
+                 "Illegal literal in : " + line);
+      }
       ++j;
-      while (j < line.size() && line[j] != ' ' && line[j] != '\t') {++j;}
+      while (j < line.size() && line[j] != ' ' && line[j] != '\t') { ++j; }
     }
     assert(j < line.size());
     res[2] = line.substr(i, j - i);
     if (!(line[j] == ' ' || line[j] == '\t')) {
       AD_THROW(ad_semsearch::Exception::BAD_INPUT,
-          "Object not followed by space in : " + line);
+               "Object not followed by space in : " + line);
     }
     return true;
   }
