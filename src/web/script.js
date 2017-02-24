9b9d3bd5bc23e423d9126e434e75e3824ccc361a
Björn Buchhold <b.buchhold@gmail.com>
2017-02-24 17:47:14 +0100

    removed unnecessary js snippet

--- src/web/script.js
+++ src/web/script.js
@@ -70,20 +70,6 @@ $(document).ready(function () {
     });
 });
 
-// Copy&paste from:
-// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript/
-// TODO: use consistently, so far it has been added later to comfortably get stats.
-function getParameterByName(name, url) {
-    if (!url) {
-        url = window.location.href;
-    }
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
 
 function htmlEscape(str) {
 //    return str.replace(/&/g, "&amp;")
