2c1c8331462156c92f950eae3fe5dbd4b8ecd379
Axel Lehmann <lehmann.4178656ch@googlemail.com>
2018-12-18 17:46:26 +0100

    Allow TurtleParser output into file (#166)
    *  Allow TurtleParser output into file and use NTriple format

--- src/TurtleParserMain.cpp
+++ src/TurtleParserMain.cpp
@@ -3,18 +3,35 @@
 // Author: Johannes Kalmbach(joka921) <johannes.kalmbach@gmail.com>
 
 #include <array>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include "./parser/TurtleParser.h"
 
-int main(int argc, char** argv) {
-  if (argc != 2) {
-    std::cerr << "Usage: ./TurtleParserMain <turtleInput>";
-    exit(1);
-  }
-  TurtleParser p(argv[1]);
+void writeNT(std::ostream& out, const std::string& filename) {
+  TurtleParser p(filename);
   std::array<std::string, 3> triple;
   while (p.getLine(&triple)) {
-    std::cout << triple[0] << " " << triple[1] << " " << triple[2] << '\n';
+    out << triple[0] << " " << triple[1] << " " << triple[2] << " .\n";
+  }
+}
+
+int main(int argc, char** argv) {
+  if (argc < 2 || argc > 3) {
+    std::cerr << "Usage: ./TurtleParserMain <turtleInput> [<nTripleOutput>]"
+              << std::endl;
+    exit(1);
+  }
+
+  if (argc == 3) {
+    std::ofstream of(argv[2]);
+    if (!of) {
+      std::cerr << "Error opening '" << argv[2] << "'" << std::endl;
+      exit(1);
+    }
+    writeNT(of, argv[1]);
+    of.close();
+  } else {
+    writeNT(std::cout, argv[1]);
   }
 }
