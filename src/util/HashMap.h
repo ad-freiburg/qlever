07e08ef7c9992f664aba22fc0a32779c77734319
Florian Kramer <flo.kramer@arcor.de>
2020-12-04 12:07:55 +0100

    Turned the hash map into a type definition.

--- src/util/HashMap.h
+++ src/util/HashMap.h
@@ -7,71 +7,13 @@
 
 #include <absl/container/flat_hash_map.h>
 
-#include "./DefaultKeyProvider.h"
-
 namespace ad_utility {
 // Wrapper for HashMaps to be used everywhere throughout code for the semantic
 // search. This wrapper interface is not designed to be complete from the
 // beginning. Feel free to extend it at need.
-//
-// The current version uses google's dense_hash_map and directly exports its
-// functions. It only adds a constructor which automatically sets the empty and
-// deleted keys.  This may be changed in the future.
 template <class K, class V,
           class HashFcn = absl::container_internal::hash_default_hash<K>,
           class EqualKey = absl::container_internal::hash_default_eq<K>,
           class Alloc = std::allocator<std::pair<const K, V>>>
-class HashMap : private absl::flat_hash_map<K, V, HashFcn, EqualKey, Alloc> {
-  using Base = typename absl::flat_hash_map<K, V, HashFcn, EqualKey, Alloc>;
-
- public:
-  // Iterator type of this map, it.first is the key, it.second the value
-  using typename Base::iterator;
-
-  // Const Iterator type of this map, it.first is the key, it.second the value
-  using typename Base::const_iterator;
-
-  // Returns an iterator to the first element of the map if it exists and an
-  // iterator equal to end() otherwise
-  using Base::begin;
-
-  // Returns an iterator one past the end so that `it != m.end()` is a viable
-  // stopping condition in a for loop
-  using Base::end;
-
-  // Finds an element for a given key. Returns an iterator to the element or
-  // end() if it is not found
-  using Base::find;
-
-  // Erases an element for a given key
-  using Base::erase;
-
-  // Access an element for the given key, creating by default construction if it
-  // does not exist
-  using Base::operator[];
-
-  // Counts the number of occurences (0 or 1) of the given key
-  using Base::count;
-
-  // Returns the size of the map
-  using Base::size;
-
-  using Base::empty;
-
-  // Clears the contents of the map
-  using Base::clear;
-
-  // determine if a key is contained
-  using Base::contains;
-
-  // Access an element and throw if not present (needed for const access)
-  using Base::at;
-
-  // Inserts a std::pair<K, V> pair or a range of pairs from a compatible
-  // container. Note: Such a pair can also be constructed using brace
-  // initializer lists.
-  //       Therefore syntax like m.insert({"answer", 42}); can be used in
-  //       a HashMap<string, int>
-  using Base::insert;
-};
+using HashMap = absl::flat_hash_map<K, V, HashFcn, EqualKey, Alloc>;
 }  // namespace ad_utility
