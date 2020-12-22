a67b6aa6270227ed5a4a26187b67ebfee47d95b8
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2020-12-22 17:16:11 +0100

    Trying to make it work...

--- src/engine/CancelableSort/CancelableSequentialSort.h
+++ src/engine/CancelableSort/CancelableSequentialSort.h
@@ -706,22 +706,7 @@ struct TimeoutedAlgorithms {
       std::istreambuf_iterator<_CharT, std::char_traits<_CharT>>, _Size,
       _CharT*);
 
-  template <typename _InputIterator, typename _Size, typename _OutputIterator>
-  _GLIBCXX20_CONSTEXPR _OutputIterator __copy_n(_InputIterator __first,
-                                                _Size __n,
-                                                _OutputIterator __result,
-                                                std::input_iterator_tag) {
-    return std::__niter_wrap(
-        __result, __copy_n_a(__first, __n, std::__niter_base(__result)));
-  }
 
-  template <typename _RandomAccessIterator, typename _Size,
-            typename _OutputIterator>
-  _GLIBCXX20_CONSTEXPR inline _OutputIterator __copy_n(
-      _RandomAccessIterator __first, _Size __n, _OutputIterator __result,
-      std::random_access_iterator_tag) {
-    return std::copy(__first, __first + __n, __result);
-  }
 
   /**
    *  @brief Copies the range [first,first+n) into [result,result+n).
