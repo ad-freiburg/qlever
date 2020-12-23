a8ee109234b0646d5f810d20d965c9a3ac37e065
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2020-12-23 11:56:33 +0100

    clang-formatted.

--- src/global/Constants.h
+++ src/global/Constants.h
@@ -6,7 +6,6 @@
 #include <stdexcept>
 #include <string>
 
-
 static const size_t STXXL_MEMORY_TO_USE = 1024L * 1024L * 1024L * 2L;
 static const size_t STXXL_DISK_SIZE_INDEX_BUILDER = 1000 * 1000;
 static const size_t STXXL_DISK_SIZE_INDEX_TEST = 10;
