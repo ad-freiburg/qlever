dcc21458f31f5763622cafa09378fc3d6ed05f27
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2021-05-28 09:07:09 +0200

    Clang-Format.

--- src/parser/SparqlExpression.h
+++ src/parser/SparqlExpression.h
@@ -110,7 +110,8 @@ class SparqlExpression {
 
   using EvaluationInput = sparqlExpression::EvaluationInput;
   using VariableColumnMap = sparqlExpression::VariableColumnMap;
-  using VariableColumnMapWithResultTypes = sparqlExpression::VariableColumnMapWithResultTypes;
+  using VariableColumnMapWithResultTypes =
+      sparqlExpression::VariableColumnMapWithResultTypes;
 
   /// The result of an epxression variable can either be a constant of type
   /// bool/double/int (same value for all result rows), a vector of one of those
