9ef9e93d60d1536f6574f8f5349c70750635abeb
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2021-09-08 17:51:45 +0200

    Refactored the ParallelBuffer stuff into a separate .cpp file.

--- src/index/ParallelBuffer.cpp
+++
@@ -1,2 +0,0 @@
-//  Copyright 2021, University of Freiburg, Chair of Algorithms and Data Structures.
-//  Author: Johannes Kalmbach <kalmbacj@cs.uni-freiburg.de>
--- src/parser/ParallelBuffer.h
+++ src/parser/ParallelBuffer.h
@@ -64,37 +64,11 @@ class ParallelFileBuffer : public ParallelBuffer {
   ParallelFileBuffer() : ParallelBuffer(){};
   ParallelFileBuffer(size_t blocksize) : ParallelBuffer(blocksize) {}
 
-  virtual void open(const string& filename) override {
-    _file.open(filename, "r");
-    _eof = false;
-    _buf.resize(_blocksize);
-    auto task = [&file = this->_file, bs = this->_blocksize,
-                 &buf = this->_buf]() { return file.read(buf.data(), bs); };
-    _fut = std::async(task);
-  }
+  // _________________________________________________________________________
+  virtual void open(const string& filename) override;
 
   // _____________________________________________________
-  virtual std::optional<std::vector<char>> getNextBlock() override {
-    if (_eof) {
-      return std::nullopt;
-    }
-
-    AD_CHECK(_file.isOpen() && _fut.valid());
-    auto numBytesRead = _fut.get();
-    if (numBytesRead == 0) {
-      _eof = true;
-      return std::nullopt;
-    }
-    _buf.resize(numBytesRead);
-    std::optional<std::vector<char>> ret = std::move(_buf);
-
-    _buf.resize(_blocksize);
-    auto task = [&file = this->_file, bs = this->_blocksize,
-                 &buf = this->_buf]() { return file.read(buf.data(), bs); };
-    _fut = std::async(task);
-
-    return ret;
-  }
+  virtual std::optional<std::vector<char>> getNextBlock() override;
 
  private:
   ad_utility::File _file;
@@ -108,41 +82,9 @@ class ParallelBufferWithEndRegex : public ParallelBuffer {
  public:
   ParallelBufferWithEndRegex(size_t blocksize, std::string endRegex)
       : ParallelBuffer{blocksize}, _endRegex{endRegex} {}
-  std::optional<std::vector<char>> getNextBlock() override {
-    auto rawInput = _rawBuffer.getNextBlock();
-    if (!rawInput || _exhausted) {
-      _exhausted = true;
-      if (_remainder.empty()) {
-        return std::nullopt;
-      }
-      auto copy = std::move(_remainder);
-      // The C++ standard does not require that _remainder is empty after the move, but we need it to be empty to make the logic above work.
-      _remainder.clear();
-      return copy;
-    }
 
-    auto endPosition = findRegexNearEnd(rawInput.value(), _endRegex);
-    if (!endPosition) {
-      if (_rawBuffer.getNextBlock()) {
-        throw std::runtime_error(
-            "The regex which marks the end of a statement was not found at "
-            "all within a single batch that was not the last one. Please increase the FILE_BUFFER_SIZE "
-            "or choose a different parser");
-      }
-      // This was the last (possibly incomplete) block, simply concatenate
-      endPosition = rawInput->size();
-      _exhausted = true;
-    }
-    std::vector<char> result;
-    result.reserve(_remainder.size() + *endPosition);
-    result.insert(result.end(), _remainder.begin(), _remainder.end());
-    result.insert(result.end(), rawInput->begin(),
-                  rawInput->begin() + *endPosition);
-    _remainder.clear();
-    _remainder.insert(_remainder.end(), rawInput->begin() + *endPosition,
-                      rawInput->end());
-    return result;
-  }
+  // __________________________________________________________________________
+  std::optional<std::vector<char>> getNextBlock() override;
 
   // Open the file from which the blocks are read.
   void open(const string& filename) override { _rawBuffer.open(filename); }
@@ -151,36 +93,7 @@ class ParallelBufferWithEndRegex : public ParallelBuffer {
   // Find `regex` near the end of `vec` by searching in blocks of 1000, 2000, 4000... bytes. We have to do this, because "reverse" regex matching is not trivial. Returns the st
   // Returns the number of bytes in `vec` until the end of the regex match, or std::nullopt if the regex was not found at all.
   static std::optional<size_t> findRegexNearEnd(const std::vector<char>& vec,
-                                                const re2::RE2& regex) {
-    size_t chunkSize = 1000;
-    size_t inputSize = vec.size();
-    re2::StringPiece regexResult;
-    bool match = false;
-    while (true) {
-      if (chunkSize >= inputSize) {
-        break;
-      }
-
-      auto startIdx = inputSize - chunkSize;
-      auto regexInput = re2::StringPiece{vec.data() + startIdx, chunkSize};
-
-      match = RE2::PartialMatch(regexInput, regex, &regexResult);
-      if (match) {
-        break;
-      }
-
-      if (chunkSize == inputSize - 1) {
-        break;
-      }
-      chunkSize = std::min(chunkSize * 2, inputSize - 1);
-    }
-    if (!match) {
-      return std::nullopt;
-    }
-
-    // regexResult.data() is a pointer to the beginning of the match, vec.data() is a pointer to the beginning of the total input.
-    return regexResult.data() + regexResult.size() - vec.data();
-  }
+                                                const re2::RE2& regex);
   ParallelFileBuffer _rawBuffer{_blocksize};
   std::vector<char> _remainder;
   re2::RE2 _endRegex;
