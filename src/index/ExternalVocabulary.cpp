0b3d7e9bed9251854e726534f299ea9269c0e2d9
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2021-09-16 10:41:59 +0200

    clang-format.

--- src/index/ExternalVocabulary.cpp
+++ src/index/ExternalVocabulary.cpp
@@ -8,8 +8,8 @@
 
 #include "../parser/RdfEscaping.h"
 #include "../parser/Tokenizer.h"
-#include "../util/Log.h"
 #include "../util/BufferedVector.h"
+#include "../util/Log.h"
 
 // _____________________________________________________________________________
 template <class Comp>
@@ -72,7 +72,8 @@ void ExternalVocabulary<Comp>::buildFromTextFile(const string& textFileName,
   _file.open(outFileName.c_str(), "w");
   std::ifstream infile(textFileName);
   AD_CHECK(infile.is_open());
-  ad_utility::BufferedVector<off_t> offsets(1'000'000'000, textFileName + ".offset.tmp.buffer");
+  ad_utility::BufferedVector<off_t> offsets(
+      1'000'000'000, textFileName + ".offset.tmp.buffer");
   off_t currentOffset = 0;
   _size = 0;
   std::string word;
