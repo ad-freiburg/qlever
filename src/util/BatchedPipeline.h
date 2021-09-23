bbf6d2e9539ed1a50127d0121057cc8bdde3c67a
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2021-09-23 10:09:51 +0200

    missing newlines.

--- src/util/BatchedPipeline.h
+++ src/util/BatchedPipeline.h
@@ -261,7 +261,7 @@ class BatchedPipeline {
       results.push_back(futures[i].get());
     }
     t.stop();
-    LOG(TIMING) << "Waiting for inner futures ms: " << t.msecs();
+    LOG(TIMING) << "Waiting for inner futures ms: " << t.msecs() << '\n';
     t.start();
     for (auto& vec : results) {
       result.m_content.insert(result.m_content.end(),
@@ -269,7 +269,7 @@ class BatchedPipeline {
                               std::make_move_iterator(vec.end()));
     }
     t.stop();
-    LOG(TIMING) << "Waiting for merging result" << t.msecs();
+    LOG(TIMING) << "Waiting for merging result" << t.msecs() << '\n';
     return result;
   }
 
