85b7ee7f594713105730f6a559240828bde82693
joka921 <joka921@users.noreply.github.com>
2021-09-25 13:30:34 +0200

    Added a template alias for a memory-limited HashSet (#479)

--- src/util/HashSet.h
+++ src/util/HashSet.h
@@ -8,15 +8,27 @@
 
 #include <string>
 
+#include "./AllocatorWithLimit.h"
+
 using std::string;
 
 namespace ad_utility {
-// Wrapper for HashSets to be used everywhere throughout code for
-// the semantic search. This wrapper interface is not designed to
-// be complete from the beginning. Feel free to extend it at need.
-template <class K,
-          class HashFcn = absl::container_internal::hash_default_hash<K>,
-          class EqualKey = absl::container_internal::hash_default_eq<K>,
-          class Alloc = std::allocator<K>>
-using HashSet = absl::flat_hash_set<K, HashFcn, EqualKey, Alloc>;
+// Wrapper for HashSets (with elements of type T) to be used everywhere
+// throughout code for the semantic search. This wrapper interface is not
+// designed to be complete from the beginning. Feel free to extend it at need.
+// T is the element type.
+template <class T,
+          class HashFct = absl::container_internal::hash_default_hash<T>,
+          class EqualElem = absl::container_internal::hash_default_eq<T>,
+          class Alloc = std::allocator<T>>
+using HashSet = absl::flat_hash_set<T, HashFct, EqualElem, Alloc>;
+
+// A hash set (with elements of type T) with a memory Limit.
+template <class T,
+          class HashFct = absl::container_internal::hash_default_hash<T>,
+          class EqualElem = absl::container_internal::hash_default_eq<T>,
+          class Alloc = ad_utility::AllocatorWithLimit<T>>
+using HashSetWithMemoryLimit =
+    absl::flat_hash_set<T, HashFct, EqualElem, Alloc>;
+
 }  // namespace ad_utility
