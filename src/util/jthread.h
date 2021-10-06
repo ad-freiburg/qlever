bfe14f41ca9986de754be791bfda6e739ec4f1a7
joka921 <johannes.kalmbach@gmail.com>
2021-10-06 10:46:15 +0000

    Included a jthread replacement, and integrated the gcc10 and clang13 toolchains into the

--- src/util/jthread.h
+++ src/util/jthread.h
@@ -4,15 +4,17 @@
 
 #ifndef QLEVER_JTHREAD_H
 
-#include <version>
 #include <thread>
+#include <version>
 
 // While libc++ has no std::jthread, we ship our own
 #ifdef _LIBCPP_VERSION
 namespace ad_utility {
 struct JThread : public std::thread {
 #ifdef _cpp_lib_jthread
-  static_assert(false, "std::jthread is now supported by libc++, get rid of ad_utility::Jthread");
+  static_assert(false,
+                "std::jthread is now supported by libc++, get rid of "
+                "ad_utility::Jthread");
 #endif
   using Base = std::thread;
   using Base::Base;
@@ -23,12 +25,14 @@ struct JThread : public std::thread {
     }
   }
 };
-}
+}  // namespace ad_utility
 
 #else
 // libstdc++ already supports jthread, simply use it
 #ifndef _cpp_lib_jthread
-  static_assert(false, "std::jthread is not supported by the version of libstdc++ you are using, please update or using QLever inside of Docker");
+static_assert(false,
+              "std::jthread is not supported by the version of libstdc++ you "
+              "are using, please update or using QLever inside of Docker");
 #endif
 namespace ad_utility {
 using JThread = std::jthread;
