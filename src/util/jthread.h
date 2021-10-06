01f4272f8f4d1f4a58250d719a35f00f3f7d89f9
joka921 <johannes.kalmbach@gmail.com>
2021-10-06 17:05:14 +0000

    Small changes from Hannah's review

--- src/util/jthread.h
+++ src/util/jthread.h
@@ -1,13 +1,14 @@
-//
-// Created by projector-user on 10/6/21.
-//
+// Copyright 2021, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Johannes Kalmbach (johannes.kalmbach@gmail.com)
 
 #ifndef QLEVER_JTHREAD_H
 
 #include <thread>
 #include <version>
 
-// While libc++ has no std::jthread, we ship our own
+// While libc++ has no std::jthread (threads that automatically join on
+// destruction), we ship our own.
 #ifdef _LIBCPP_VERSION
 namespace ad_utility {
 struct JThread : public std::thread {
@@ -28,11 +29,11 @@ struct JThread : public std::thread {
 }  // namespace ad_utility
 
 #else
-// libstdc++ already supports jthread, simply use it
+// libstdc++ already supports jthread, simply use it.
 #ifndef __cpp_lib_jthread
 static_assert(false,
               "std::jthread is not supported by the version of libstdc++ you "
-              "are using, please update or using QLever inside of Docker");
+              "are using, please update or use QLever inside of Docker");
 #endif
 namespace ad_utility {
 using JThread = std::jthread;
