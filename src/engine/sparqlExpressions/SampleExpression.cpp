e419b1e0cc19c2ff6495d9b32b1f191108f0a340
joka921 <joka921@users.noreply.github.com>
2021-10-28 11:21:23 +0200

    Implement a  `SampleExpression`  (#504)
    This will be used to compute expressions like SAMPLE(<something>) as ?somethingElse

---
+++ src/engine/sparqlExpressions/SampleExpression.cpp
@@ -0,0 +1,43 @@
+//  Copyright 2021, University of Freiburg, Chair of Algorithms and Data
+//  Structures. Author: Johannes Kalmbach <kalmbacj@cs.uni-freiburg.de>
+
+//
+// Created by johannes on 15.09.21.
+//
+
+#include "./SampleExpression.h"
+
+#include "./SparqlExpressionGenerators.h"
+
+using namespace sparqlExpression;
+using namespace sparqlExpression::detail;
+
+// ____________________________________________________________________________
+ExpressionResult SampleExpression::evaluate(EvaluationContext* context) const {
+  auto evaluator =
+      [context]<typename T>(const T& childResult) -> ExpressionResult {
+    if constexpr (std::is_same_v<T, ad_utility::SetOfIntervals>) {
+      // If there exists an element that is true, return true.
+      return Bool{!childResult._intervals.empty()};
+    } else if constexpr (isVectorResult<T>) {
+      AD_CHECK(!childResult.empty());
+      return childResult[0];
+    } else if constexpr (std::is_same_v<T, Variable>) {
+      // TODO<joka921> Can't this be a simpler function (getIdAt)
+      AD_CHECK(context->_endIndex > context->_beginIndex);
+      EvaluationContext contextForSingleValue = *context;
+      contextForSingleValue._endIndex = contextForSingleValue._beginIndex + 1;
+      auto idOfFirstAsVector =
+          detail::getIdsFromVariable(childResult, &contextForSingleValue);
+      return StrongIdWithResultType{
+          idOfFirstAsVector[0],
+          context->_variableToColumnAndResultTypeMap.at(childResult._variable)
+              .second};
+    } else {
+      static_assert(isConstantResult<T>);
+      return childResult;
+    }
+  };
+
+  return std::visit(evaluator, _child->evaluate(context));
+}
