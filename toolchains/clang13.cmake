d8f7a286681343cdea7587b73d6bd092d912063f
joka921 <johannes.kalmbach@gmail.com>
2021-11-17 15:33:51 +0000

    Try to make this all compile with clang.

--- toolchains/clang13.cmake
+++ toolchains/clang13.cmake
@@ -1,5 +1,6 @@
 # CMake toolchain file for using `clang++-13` together with `libc++`
 set(CMAKE_C_COMPILER clang-13)
 set(CMAKE_CXX_COMPILER clang++-13)
+set(CMAKE_AR llvm-ar-13)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lc++abi")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lc++abi -fuse-ld=lld-13")
