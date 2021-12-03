4138017a5da68345adc880c971a809ee32dfb439
Robin TF <83676088+RobinTF@users.noreply.github.com>
2021-12-03 22:25:52 +0100

    Apply formatter

--- src/util/ostringstream.h
+++ src/util/ostringstream.h
@@ -25,41 +25,40 @@ concept HasView = requires(T t) {
 };
 
 namespace implementation {
-  using base_buffer =
-      std::basic_stringbuf<char, std::char_traits<char>, std::allocator<char>>;
+using base_buffer =
+    std::basic_stringbuf<char, std::char_traits<char>, std::allocator<char>>;
 
+/**
+ * A rudimentary re-implementation of std::basic_stringbuf guaranteed to have
+ * a view() member function.
+ */
+class CustomBuffer : public base_buffer {
+ public:
   /**
-   * A rudimentary re-implementation of std::basic_stringbuf guaranteed to have
-   * a view() member function.
+   * If this method exists for the base class this implementation
+   * would hide it, but in that case this function is unused so
+   * it doesn't matter. This method works for libc++ and stdlibc++
+   * but shouldn't be viewed as a general solution.
    */
-  class CustomBuffer : public base_buffer {
-   public:
-    /**
-     * If this method exists for the base class this implementation
-     * would hide it, but in that case this function is unused so
-     * it doesn't matter. This method works for libc++ and stdlibc++
-     * but shouldn't be viewed as a general solution.
-     */
-    std::string_view view() const {
-      return {pbase(),
-              static_cast<size_t>(std::max(pptr(), egptr()) - pbase())};
-    }
-  };
+  std::string_view view() const {
+    return {pbase(), static_cast<size_t>(std::max(pptr(), egptr()) - pbase())};
+  }
+};
 
-  using base = std::basic_ostream<char, std::char_traits<char>>;
+using base = std::basic_ostream<char, std::char_traits<char>>;
 
-  /**
-   * A rudimentary re-implementation of std::osstringstream guaranteed to have a
-   * view() member function.
-   */
-  class ostringstream : public base {
-    std::conditional_t<HasView<base_buffer>, base_buffer, CustomBuffer> _buf;
+/**
+ * A rudimentary re-implementation of std::osstringstream guaranteed to have a
+ * view() member function.
+ */
+class ostringstream : public base {
+  std::conditional_t<HasView<base_buffer>, base_buffer, CustomBuffer> _buf;
 
-   public:
-    ostringstream() : base{&_buf} {}
-    auto view() const { return _buf.view(); }
-    void str(const std::string& string) { _buf.str(string); }
-  };
+ public:
+  ostringstream() : base{&_buf} {}
+  auto view() const { return _buf.view(); }
+  void str(const std::string& string) { _buf.str(string); }
+};
 }  // namespace implementation
 
 /**
