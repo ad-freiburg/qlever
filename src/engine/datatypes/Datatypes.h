acdfd253fb911bf8e3404b705e457c318d18b5c4
joka921 <johannes.kalmbach@gmail.com>
2022-01-12 13:07:56 +0000

    Fixed the int datatype, and started implementing dates.
    ToDO: Really test EVERYTHING exhaustively.

--- src/engine/datatypes/Datatypes.h
+++ src/engine/datatypes/Datatypes.h
@@ -5,50 +5,184 @@
 #ifndef QLEVER_DATATYPES_H
 #define QLEVER_DATATYPES_H
 
-#include <variant>
+#include <Exceptions.h>
+
 #include <bit>
+#include <variant>
+
 #include "../../util/AllocatorWithLimit.h"
 
-namespace ad_utility {
+namespace ad_utility::datatypes {
 enum struct Datatype {
-  Undefined, Int, Bool, Decimal, Double, Float, Date,
-  Vocab, LocalVocab
+  Undefined,
+  Int,
+  Bool,
+  Decimal,
+  Double,
+  Float,
+  Date,
+  Vocab,
+  LocalVocab
 };
 
+class DateOutOfRangeException : public std::runtime_error {
+  using std::runtime_error::runtime_error;
+};
+namespace detail {
+static void check(const auto& element, const auto& min, const auto& max,
+                  std::string_view name) {
+  if (element >= min || element < max) {
+    std::stringstream s;
+    s << name << " "
+      << "is out of range.";
+    throw DateOutOfRangeException{s.str()};
+  }
+}
+}  // namespace detail
 
+class Date {
+ private:
+  short _year;
+  signed char _month;
+  signed char _day;
+
+  struct NoShiftOrCheckTag{};
+
+ public:
+  constexpr Date(short year, signed char month, signed char day)
+      : _year{year}, _month{month}, _day{day} {
+    detail::check(year, -9999, +10000, "year");
+    // Shift the year into the positive range.
+    _year += 9999;
+    detail::check(month, 1, 13, "month");
+    detail::check(day, 1, 32, "day");
+  }
+
+ private:
+  constexpr Date(short year, signed char month, signed char day, NoShiftOrCheckTag)
+      : _year{year}, _month{month}, _day{day} {}
+
+
+  static constexpr uint64_t onlyLastBits(uint64_t input, uint64_t numBytes) {
+    return ~(std::numeric_limits<uint64_t>::max() << numBytes) & input;
+  }
+ public:
+
+  static constexpr Date fromBytes(uint64_t bytes) {
+    auto day = onlyLastBits(bytes, 5);
+    auto month = onlyLastBits(bytes >> 5, 4);
+    auto year = onlyLastBits(bytes >> 9, 15);
+    return Date(year, month, day, NoShiftOrCheckTag{});
+  }
+  // 15 bits for the year, 4 bits for the month, 5 bits for the day
+  static constexpr uint64_t numBitsRequired = 24;
+
+  [[nodiscard]] auto year() const {return _year;}
+  [[nodiscard]] auto month() const {return _month;}
+  [[nodiscard]] auto day() const {return _day;}
+};
+
+class Time {
+ private:
+  signed char _hour;
+  signed char _minute;
+  // a fixed point representation;
+  unsigned int _seconds;
+  // Timezone is currently only supported as an hour;
+  signed char _timezone = 0;
+
+ public:
+  constexpr Time(signed char hour, signed char minute, float seconds)
+      : _hour{hour}, _minute{minute} {
+    detail::check(hour, 0, 24, "hour");
+    detail::check(minute, 0, 60, "minute");
+    detail::check(seconds, 0.0f, 60.0f, "seconds");
+
+    constexpr auto numBitsForFraction = numBitsForSeconds - 6;
+    _seconds = static_cast<unsigned>(seconds * pow(2, numBitsForFraction));
+  }
+  constexpr Time(signed char hour, signed char minute, float seconds,
+                 signed char timezone)
+      : Time(hour, minute, seconds) {
+    detail::check(timezone, -23, 25, "timezone");
+    // The minimal timezone is -23 which will become 1, so
+    // 0 stands for "undefined timezone"
+    _timezone = timezone + static_cast<signed char>(24);
+  }
+
+  [[nodiscard]] constexpr uint64_t toBytes() const {
+    // timezone has 5 bits
+    constexpr auto secShift = 5;
+    constexpr auto minShift = numBitsForSeconds + secShift;
+    constexpr auto hourShift = minShift + 6;
+    return (static_cast<uint64_t>(_hour) << hourShift) |
+           (static_cast<uint64_t>(minShift) << 5) | (_seconds << secShift) |
+           _timezone;
+  }
+  // 15 bits for the year, 4 bits for the month, 5 bits for the day
+  static constexpr uint64_t numBitsForSeconds = 20;
+  static constexpr uint64_t numBitsForRequired = numBitsForSeconds + 6 + 5;
+
+ private:
+  static constexpr size_t pow(size_t base, size_t exp) {
+    size_t res = 1;
+    while (exp--) {
+      res *= base;
+    }
+    return res;
+  }
+};
+
+class DateTime {
+  Date _date;
+  Time _time;
+};
 
 class FancyId {
   using T = uint64_t;
+
  public:
-  static FancyId Undefined() {return {0ul};}
-  static FancyId Double(double d) {
+  // static FancyId Undefined() {return {0ul};}
+  static constexpr FancyId Double(double d) {
     auto asBits = std::bit_cast<T>(d);
     asBits = asBits >> MASK_SIZE;
     asBits |= DoubleMask;
     return FancyId{asBits};
   }
 
-  static FancyId Integer(int64_t i) {
-    return {static_cast<T>(i)};
+  static constexpr FancyId Integer(int64_t i) {
+    // propagate the sign.
+    constexpr int64_t signBit = 1l << MASK_SHIFT;
+    auto highestBitShifted =
+        ((signBit & i) << (MASK_SIZE - 1)) >> (MASK_SIZE - 1);
+    constexpr T mask = std::numeric_limits<T>::max() >> MASK_SIZE;
+    return {static_cast<T>((i & mask) | highestBitShifted)};
+  }
+  static constexpr FancyId MaxInteger() {
+    // propagate the sign.
+    return {std::numeric_limits<T>::max() >> MASK_SIZE};
+  }
+  static constexpr FancyId MinInteger() {
+    constexpr int64_t signBit = 1l << MASK_SHIFT;
+    auto highestBitShifted = ((signBit) << (MASK_SIZE - 1)) >> (MASK_SIZE - 1);
+    return {static_cast<T>(highestBitShifted)};
   }
 
-  [[nodiscard]] double getDoubleUnchecked() const {
+  [[nodiscard]] constexpr double getDoubleUnchecked() const {
     return std::bit_cast<double>(_data << MASK_SIZE);
   }
 
-  [[nodiscard]] double getDoubleUnchecked() const {
-    return std::bit_cast<double>(_data << MASK_SIZE);
+  [[nodiscard]] constexpr int64_t getIntegerUnchecked() const {
+    return static_cast<int64_t>(_data);
   }
 
-  [[nodiscard]] T data() const {
-    return _data;
-  }
+  [[nodiscard]] constexpr T data() const { return _data; }
 
  private:
   T _data;
   static constexpr size_t MASK_SIZE = 4;
   static constexpr size_t MASK_SHIFT = 8 * sizeof(T) - MASK_SIZE;
-  FancyId(uint64_t data): _data{data}{};
+  constexpr FancyId(uint64_t data) : _data{data} {};
 
   constexpr static uint64_t DoubleMask = 1ul << MASK_SHIFT;
   constexpr static uint64_t DecimalMask = 3ul << MASK_SHIFT;
@@ -61,12 +195,16 @@ class FancyId {
   constexpr static uint64_t VocabMask = 4ul << MASK_SHIFT;
   constexpr static uint64_t LocalVocabMask = 8ul << MASK_SHIFT;
   constexpr static uint64_t DateMask = 12ul << MASK_SHIFT;
-
-
-
-
 };
 
-}
+namespace fancyIdLimits {
+
+static constexpr int64_t maxInteger =
+    FancyId::MaxInteger().getIntegerUnchecked();
+static constexpr int64_t minInteger =
+    FancyId::MinInteger().getIntegerUnchecked();
+}  // namespace fancyIdLimits
+
+}  // namespace ad_utility::datatypes
 
 #endif  // QLEVER_DATATYPES_H
--- test/DatatypesTest.cpp
+++ test/DatatypesTest.cpp
@@ -3,14 +3,20 @@
 //  Author: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
 
 #include <gtest/gtest.h>
-#include "../../src/engine/datatypes/Datatypes.h"
+
 #include <bitset>
 #include <cmath>
 
-using ad_utility::FancyId;
+#include "../../src/engine/datatypes/Datatypes.h"
+
+using namespace ad_utility::datatypes;
+static auto print = [](auto d) {
+  auto b = std::bit_cast<std::bitset<64>>(d);
+  std::cout << b << std::endl;
+};
 
 TEST(FancyId, Double) {
-  auto test = [](double d, bool testExact=false) {
+  auto test = [](double d, bool testExact = false) {
     auto result = FancyId::Double(d).getDoubleUnchecked();
 
     if (std::isnan(d)) {
@@ -19,27 +25,13 @@ TEST(FancyId, Double) {
       ASSERT_EQ(d, FancyId::Double(d).getDoubleUnchecked());
     } else {
       ASSERT_FLOAT_EQ(d, FancyId::Double(d).getDoubleUnchecked());
-
     }
-
   };
 
-  auto testAll = [&](const std::vector<double>& ds, bool testExact=false) {
+  auto testAll = [&](const std::vector<double>& ds, bool testExact = false) {
     for (auto d : ds) {
       test(d, testExact);
     }
-  };
-
-  auto print = [](auto d) {
-    auto b = std::bit_cast<std::bitset<64>>(d);
-    std::cout << b << std::endl;
-  };
-
-  auto printBeforeAfter = [&](double d) {
-    print(d);
-    print(FancyId::Double(d).data());
-    print(FancyId::Double(d).getDoubleUnchecked());
-    std::cout << std::endl;
   };
 
   using limits = std::numeric_limits<double>;
@@ -52,7 +44,7 @@ TEST(FancyId, Double) {
   auto denorm = limits::denorm_min();
 
   testAll({0.0, 1.0, -1.0, -17}, true);
-  testAll({1.2345, 1254.123 -51234.2, 2.239e-12});
+  testAll({1.2345, 1254.123 - 51234.2, 2.239e-12});
 
   testAll({0.1});
   test(0.5, true);
@@ -61,4 +53,56 @@ TEST(FancyId, Double) {
   testAll({min, max, denorm});
 }
 
+TEST(FancyId, Int) {
+  auto inOut = [](int64_t i) {
+    return FancyId::Integer(i).getIntegerUnchecked();
+  };
 
+  auto testEq = [&](int64_t i) { ASSERT_EQ(i, inOut(i)); };
+
+  auto testAll = [&](const std::vector<int64_t>& is) {
+    for (auto i : is) {
+      testEq(i);
+    }
+  };
+
+  testAll({0, 1, -1});
+
+  auto min = fancyIdLimits::minInteger;
+  auto max = fancyIdLimits::maxInteger;
+
+  testAll({min, min + 1, max, max - 1});
+
+  ASSERT_EQ(inOut(min - 1), max);
+  ASSERT_EQ(inOut(max + 1), min);
+
+  for (auto i = 1; i < 1000000; ++i) {
+    if (inOut(min - i) != max - (i - 1)) {
+      std::cout << (min - i) << std::endl;
+      print(min - i);
+      print(inOut(min - i));
+
+      print(max - (i - 1));
+      print(inOut(max - (i - 1)));
+    }
+    ASSERT_EQ(inOut(min - i), max - (i - 1));
+  }
+
+  for (auto i = max - 1000000; i < max + 100000; ++i) {
+    ASSERT_EQ(inOut(min - i), max - (i - 1));
+  }
+
+  for (auto i = max - 1000000; i < max + 100000; ++i) {
+    ASSERT_EQ(inOut(min - i), max - (i - 1));
+  }
+
+  auto outerMax = std::numeric_limits<int64_t>::max();
+  auto outerMin = std::numeric_limits<int64_t>::min();
+
+  ASSERT_EQ(inOut(outerMax + 1), inOut(outerMax) + inOut(1));
+  ASSERT_EQ(inOut(outerMin + 1), inOut(outerMin) + inOut(1));
+}
+
+TEST(Date, FirstTests) {
+
+}
