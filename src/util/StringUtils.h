4cd55f7c5b2032056e92b3b706d3eef1c6351440
Robin TF <83676088+RobinTF@users.noreply.github.com>
2022-01-26 01:59:35 +0100

    Remove test-only functions

--- src/util/StringUtils.h
+++ src/util/StringUtils.h
@@ -47,23 +47,11 @@ inline string getUppercase(const string& orig);
 
 inline string getLowercaseUtf8(std::string_view s);
 
-inline string getUppercaseUtf8(const std::string& s);
-
-inline string firstCharToUpperUtf8(const string& s);
-
 inline std::pair<size_t, std::string> getUTF8Prefix(std::string_view s,
                                                     size_t prefixLength);
 
 //! Gets the last part of a string that is somehow split by the given separator.
 inline string getLastPartOfString(const string& text, const char separator);
-
-inline string removeSpaces(const string& orig);
-
-//! Converts an optional string to a JSON string value.
-//! I.e. it escapes illegal characters and adss quotes around the string.
-//! Taking a std::optional<string> it takes normal strings (which are implicitly
-//! converted or std::nullopt which it converts to 'null' (without quotes)
-inline string toJson(const std::optional<string>& orig);
 
 //! Strips any sequence of characters in s from the left and right of the text.
 //! The type T can be anything convertible to a std::string_view<char> so in
@@ -71,12 +59,6 @@ inline string toJson(const std::optional<string>& orig);
 //! Additionally it can also be a single char as in ' '
 template <class T>
 inline string strip(const string& text, const T& s);
-//! Strips any sequence of characters from s from the left of the text.
-//! The type T can be anything convertible to a std::string_view<char> so in
-//! particular s can be a string literal, std::string, std::string_view.
-//! Additionally it can also be a single char as in ' '
-template <class T>
-inline string lstrip(const string& text, const T& s);
 //! Strips any sequence of characters from s from the right of the text.
 //! The type T can be anything convertible to a std::string_view<char> so in
 //! particular s can be a string literal, std::string, std::string_view.
@@ -86,25 +68,6 @@ inline string rstrip(const string& text, const T& s);
 
 //! Splits a string at the separator, kinda like python.
 inline vector<string> split(const string& orig, const char sep);
-
-//! Splits a string at any maximum length sequence of whitespace
-inline vector<string> splitWs(const string& orig);
-
-//! Splits a string at any maximum length sequence of whitespace, ignoring
-//! whitespace within an escaped sequence. Left char begins an escaped
-//! sequence right ends it. If left == right the char toggles an escaped
-//! sequence, otherwise the depth of opening chars is tracked and whitespace
-//! is ignored as long as that depth is larger than 0.
-inline vector<string> splitWsWithEscape(const string& orig, const char left,
-                                        const char right);
-
-//! Splits a string at any character found in the separators string.
-//! The type T can be anything convertible to a std::string_view so in
-//! particular seps can be a string literal, a std::string or
-//! a std::string_view.
-//! This is analogous to how std::string::find_first_of() works
-template <class T>
-inline vector<string> splitAny(const string& orig, const T& separators);
 
 //! Similar to Python's ",".join(somelist) this joins elements of the given
 //! vector to_join, separating them by joiner.
@@ -177,18 +140,6 @@ std::string getLowercaseUtf8(std::string_view s) {
   return result;
 }
 
-// ____________________________________________________________________________
-string getUppercaseUtf8(const std::string& s) {
-  std::string result;
-  icu::StringByteSink<std::string> sink(&result);
-  UErrorCode err = U_ZERO_ERROR;
-  icu::CaseMap::utf8ToUpper("", 0, s, sink, nullptr, err);
-  if (U_FAILURE(err)) {
-    throw std::runtime_error(u_errorName(err));
-  }
-  return result;
-}
-
 /**
  * @brief get a prefix of a utf-8 string of a specified length
  *
@@ -225,15 +176,6 @@ std::pair<size_t, std::string> getUTF8Prefix(std::string_view sv,
 }
 
 // ____________________________________________________________________________
-inline string firstCharToUpperUtf8(const string& s) {
-  auto [numCodepoints, prefix] = getUTF8Prefix(s, 1);
-  (void)numCodepoints;
-  auto prefixSize = prefix.size();
-  prefix = getUppercaseUtf8(prefix);
-  return prefix + s.substr(prefixSize);
-}
-
-// ____________________________________________________________________________
 string getLastPartOfString(const string& text, const char separator) {
   size_t pos = text.rfind(separator);
   if (pos != text.npos) {
@@ -241,79 +183,6 @@ string getLastPartOfString(const string& text, const char separator) {
   } else {
     return text;
   }
-}
-
-// ____________________________________________________________________________
-string removeSpaces(const string& orig) {
-  string ret;
-  ret.reserve(orig.size());
-  for (size_t i = 0; i < orig.size(); ++i) {
-    if (orig[i] != ' ') {
-      ret += orig[i];
-    }
-  }
-  return ret;
-}
-
-// ____________________________________________________________________________
-string toJson(const std::optional<string>& input) {
-  if (!input) {
-    return "null";
-  }
-  const auto& orig = input.value();
-  // First we should determine the size of the result.
-  // As a linear scan this should almost always be faster than
-  // having to realloc. + 2 for quotes
-  size_t rsize = orig.size() + 2;
-  for (const auto& c : orig) {
-    switch (c) {
-      case '\t':
-      case '\v':
-      case '\0':
-      case '\f':
-      case '\b':
-      case '\n':
-      case '\"':
-      case '\\':
-        rsize++;
-        continue;
-      default:
-        continue;
-    }
-  }
-  string ret;
-  ret.reserve(rsize);
-  ret += '"';
-  for (const auto& c : orig) {
-    switch (c) {
-      case '\t':
-        ret += "\\t";
-        continue;
-      case '\v':
-        ret += "\\v";
-        continue;
-      case '\0':
-        ret += "\\0";
-        continue;
-      case '\f':
-        ret += "\\f";
-        continue;
-      case '\b':
-        ret += "\\b";
-        continue;
-      case '\n':
-        ret += "\\n";
-        continue;
-      case '\"':
-      case '\\':
-        ret += '\\';
-        [[fallthrough]];
-      default:
-        ret += c;
-    }
-  }
-  ret += '"';
-  return ret;
 }
 
 // _____________________________________________________________________________
@@ -333,105 +202,6 @@ vector<string> split(const string& orig, const char sep) {
 }
 
 // _____________________________________________________________________________
-vector<string> splitWs(const string& orig) {
-  vector<string> result;
-  if (orig.size() > 0) {
-    size_t start = 0;
-    size_t pos = 0;
-    while (pos < orig.size()) {
-      if (::isspace(static_cast<unsigned char>(orig[pos]))) {
-        if (start != pos) {
-          result.emplace_back(orig.substr(start, pos - start));
-        }
-        // skip any whitespace
-        while (pos < orig.size() &&
-               ::isspace(static_cast<unsigned char>(orig[pos]))) {
-          pos++;
-        }
-        start = pos;
-      }
-      pos++;
-    }
-    // avoid adding whitespace at the back of the string
-    if (start != orig.size()) {
-      result.emplace_back(orig.substr(start));
-    }
-  }
-  return result;
-}
-
-// _____________________________________________________________________________
-inline vector<string> splitWsWithEscape(const string& orig, const char left,
-                                        const char right) {
-  vector<string> result;
-  if (orig.size() > 0) {
-    size_t start = 0;
-    size_t pos = 0;
-    int depth = 0;
-    while (pos < orig.size()) {
-      if (depth <= 0 && ::isspace(static_cast<unsigned char>(orig[pos]))) {
-        if (start != pos) {
-          result.emplace_back(orig.substr(start, pos - start));
-        }
-        // skip any whitespace
-        while (pos < orig.size() &&
-               ::isspace(static_cast<unsigned char>(orig[pos]))) {
-          pos++;
-        }
-        start = pos;
-      }
-      if (orig[pos] == left) {
-        depth++;
-        if (left == right) {
-          depth %= 2;
-        }
-      } else if (orig[pos] == right) {
-        depth--;
-      }
-      pos++;
-    }
-    // avoid adding whitespace at the back of the string
-    if (start != orig.size()) {
-      result.emplace_back(orig.substr(start));
-    }
-  }
-  return result;
-}
-
-// _____________________________________________________________________________
-template <class T>
-vector<string> splitAny(const string& orig, const T& separators) {
-  // Converting the input into a std::string_view gives us a size() method and
-  // iterators. It works with both const char* and std::string without needing
-  // a copy
-  std::string_view seps(separators);
-  // 256 bytes should fit on almost any stack
-  // note the {}, it initializes with 0 = false
-  array<bool, std::numeric_limits<unsigned char>::max() + 1> chars{};
-  for (size_t i = 0; i < seps.size(); ++i) {
-    chars[seps[i]] = true;
-  }
-  vector<string> result;
-  if (orig.size() > 0) {
-    size_t from = 0;
-    size_t i = 0;
-    while (i < orig.size()) {
-      if (chars[static_cast<unsigned char>(orig[i])]) {
-        if (from < i) {
-          result.emplace_back(orig.substr(from, i - from));
-        }
-        from = i + 1;
-      }
-      ++i;
-    }
-    if (from < orig.size()) {
-      result.emplace_back(orig.substr(from));
-    }
-  }
-  return result;
-}
-
-// _____________________________________________________________________________
 template <typename J, typename S>
 J join(const vector<J>& to_join, const S& joiner) {
   J res{};  // {} does zero initialization
@@ -445,16 +215,6 @@ J join(const vector<J>& to_join, const S& joiner) {
     res += *it;
   }
   return res;
-}
-
-// _____________________________________________________________________________
-template <class T>
-inline string lstrip(const string& text, const T& s) {
-  auto pos = text.find_first_not_of(s);
-  if (pos == string::npos) {
-    return string{};
-  }
-  return text.substr(pos);
 }
 
 // _____________________________________________________________________________
