6759a1aeff3ec823ac22eedc95fea8ebcaf30e15
joka921 <johannes.kalmbach@gmail.com>
2022-02-04 11:19:34 +0000

    Fix CI pipeline.

--- test/CompactStringVectorTest.cpp
+++ test/CompactStringVectorTest.cpp
@@ -51,6 +51,7 @@ TEST(CompactVectorOfStrings, Iterator) {
 
     auto it = s.begin();
     using std::ranges::equal;
+
     ASSERT_TRUE(equal(input[0], *it));
     ASSERT_TRUE(equal(input[0], *it++));
     ASSERT_TRUE(equal(input[1], *it));
