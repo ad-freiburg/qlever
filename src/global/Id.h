84e8d4bcc6db8f6f76dcf4bf508702dbf6620f9b
joka921 <johannes.kalmbach@gmail.com>
2022-02-09 17:53:26 +0000

    Included first tests, and added some comments.

--- src/global/Id.h
+++ src/global/Id.h
@@ -19,23 +19,24 @@ namespace ad_utility {
 
 // Manages two kinds of IDs: unrounded IDs (unsigned 64-bit integers), and
 // rounded IDs (unsigned 64-bit integers, where the least significant
-// `numBytesRounded` bytes are all 0rounded IDs (unsigned 64-bit integers, where
-// the least significant `numBytesRounded` bytes are all 0. Has functionality to
-// give out ascending Ids, and to detect and convert rounded IDs
+// `numBytesRounded` bytes are all `rounded IDs` (unsigned 64-bit integers,
+// where the least significant `numBytesRounded` bytes are all 0. Has
+// functionality to give out ascending Ids, and to detect and convert rounded
+// IDs
 template <size_t numBytesRounded>
 requires(numBytesRounded < 8) class RoundedIdManager {
  private:
-  using T = uint64_t;
-  T nextId{0};
+  constexpr static size_t numBitsRounded = numBytesRounded * 8;
+  // The next free unrounded ID;
+  uint64_t nextId{0};
+  // The last ID that was assigned. Used for overflow detection.
+  uint64_t lastId{0};
 
  public:
   RoundedIdManager() = default;
-  constexpr static uint64_t maxRoundedId =
-      uint64_t{(1ull << ((8 - numBytesRounded) * 8)) - 1};
-  constexpr static uint64_t maxExternalIdPerBlock{
-      (1ull << (numBytesRounded * 8)) - 1};
-  constexpr static uint64_t maxId = std::numeric_limits<uint64_t>::max();
 
+  // Get the smallest ID that is larger than all previously obtained IDs and
+  // has the lower `numBytesRounded` bytes set to zero.
   uint64_t getNextRoundedId() {
     if (!isRoundedId(nextId)) {
       nextId = (fromRoundedId(toRoundedId(nextId) + 1));
@@ -43,24 +44,38 @@ requires(numBytesRounded < 8) class RoundedIdManager {
     return getNextUnroundedId();
   }
 
+  // Get the smallest ID that is larger than all previously obtained IDs.
   uint64_t getNextUnroundedId() {
-    AD_CHECK(nextId <= maxId);
-    return nextId++;
+    if (nextId < lastId) {
+      // TODO<joka921> Proper exception type.
+      throw std::runtime_error{"Overflow while rounding Ids"};
+    }
+    lastId = nextId;
+    nextId++;
+    return lastId;
   }
 
-  constexpr static uint64_t toRoundedId(uint64_t id) {
-    return id >> (numBytesRounded * 8);
-  }
-
-  constexpr static uint64_t fromRoundedId(uint64_t id) {
-    return id << (numBytesRounded * 8);
-  }
+  // Is this ID a "rounded ID", equivalently: Are the lower `numBytesRounded`
+  // bytes all zero.
   constexpr static bool isRoundedId(uint64_t id) {
     // Bits set to one in the range where the external part is stored.
-    constexpr uint64_t mask = (~uint64_t(0)) >> (64 - numBytesRounded * 8);
+    constexpr uint64_t mask = (~uint64_t(0)) >> (64 - numBitsRounded);
     return !(mask & id);
+  }
+
+  // Convert a rounded ID to its "actual value" by shifting it to the right by
+  // `numBytesRounded` (The i-th rounded ID will become `i`).
+  // TODO:: Naming!!!
+  constexpr static uint64_t toRoundedId(uint64_t id) {
+    return id >> numBitsRounded;
+  }
+
+  // TODO::Naming!
+  // Convert `i` to the `i-th` rounded ID by shifting it to the left by
+  // `numBytesRounded`.
+  constexpr static uint64_t fromRoundedId(uint64_t id) {
+    return id << numBitsRounded;
   }
 };
 
-using DefaultIdManager = RoundedIdManager<3>;
 }  // namespace ad_utility
---
+++ test/IdTest.cpp
@@ -0,0 +1,34 @@
+//  Copyright 2022, University of Freiburg,
+//  Chair of Algorithms and Data Structures.
+//  Author: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
+
+#include <gtest/gtest.h>
+
+#include <ranges>
+
+#include "../src/global/Id.h"
+#include "../src/util/Log.h"
+
+namespace views = std::views;
+
+TEST(Id, RoundedIds) {
+  ad_utility::RoundedIdManager<1> m;
+  for (size_t i = 0; i < 256; ++i) {
+    auto id = m.getNextRoundedId();
+    ASSERT_EQ(i * 256, id);
+    ASSERT_TRUE(m.isRoundedId(id));
+    ASSERT_EQ(i, m.toRoundedId(id));
+  }
+}
+
+TEST(Id, UnroundedIds) {
+  ad_utility::RoundedIdManager<1> m;
+  for (size_t i = 0; i < 256; ++i) {
+    auto id = m.getNextUnroundedId();
+    ASSERT_EQ(i, id);
+  }
+}
+
+TEST(Id, MixedIds) {
+  // TODO<joka921> implement this and extend all others.
+}
