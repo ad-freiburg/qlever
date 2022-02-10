102230bbd025a631ffdb3bfc0bd9be9e2c33ddc5
joaomarques90 <36512701+joaomarques90@users.noreply.github.com>
2022-02-10 01:41:03 +0100

    yaml tidy & yaml format-checker

---
+++ e2e/yaml-format-check.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+printf "Checking sources for code style\n"
+SOURCE_FILES=()
+find ./e2e/ -regextype egrep -regex '.*\.(yaml)?$' -print0 > sourcelist
+while IFS=  read -r -d $'\0'; do
+    SOURCE_FILES+=("$REPLY")
+done <sourcelist
+
+ERROR=0
+for source in "${SOURCE_FILES[@]}" ;do
+	yamllint $source | grep -E 'error|warning'  &> /dev/null
+	HAS_WRONG_FILES=$?
+	if [ $HAS_WRONG_FILES -ne 1 ] ; then
+		# Print an error and exit
+		printf "\x1b[31mError: "
+		printf "The source file \x1b[m$source\x1b[31m does not match the code style\n"
+		printf "\x1b[m"
+		ERROR=1
+	fi
+done
+
+if [ $ERROR -eq 0 ]; then
+	printf "\x1b[32mCongratulations! All sources match the code style\x1b[m\n"
+else
+	exit 1
+fi
