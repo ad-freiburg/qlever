d8c53893a13bc5af789cdf4608c68038ca3e7d38
Robin TF <83676088+RobinTF@users.noreply.github.com>
2022-03-05 17:24:57 +0100

    Adjust test cases

--- test/AsyncStreamTest.cpp
+++ test/AsyncStreamTest.cpp
@@ -4,6 +4,8 @@
 
 #include <gtest/gtest.h>
 
+#include <algorithm>
+#include <atomic>
 #include <chrono>
 #include <semaphore>
 
@@ -11,8 +13,8 @@
 
 using ad_utility::streams::runStreamAsync;
 
-cppcoro::generator<std::string> generateNChars(size_t n,
-                                               size_t& totalProcessed) {
+cppcoro::generator<std::string> generateNChars(
+    size_t n, std::atomic_size_t& totalProcessed) {
   for (size_t i = 0; i < n; i++) {
     co_yield "A";
     totalProcessed = i + 1;
@@ -20,7 +22,7 @@ cppcoro::generator<std::string> generateNChars(size_t n,
 }
 
 TEST(AsyncStream, EnsureMaximumBufferLimitWorks) {
-  size_t totalProcessed = 0;
+  std::atomic_size_t totalProcessed = 0;
   size_t bufferLimit = 10;
   auto stream = runStreamAsync<cppcoro::generator<std::string>>(
       generateNChars(bufferLimit + 2, totalProcessed), bufferLimit);
@@ -44,21 +46,9 @@ TEST(AsyncStream, EnsureMaximumBufferLimitWorks) {
 }
 
 TEST(AsyncStream, EnsureBuffersArePassedCorrectly) {
-  auto generator =
-      runStreamAsync<std::vector<std::string>>({"Abc", "Def", "Ghi"}, 3);
+  std::vector<std::string> testData{"Abc", "Def", "Ghi"};
+  auto generator = runStreamAsync<std::vector<std::string>>(testData, 2);
 
-  auto iterator = generator.begin();
-  ASSERT_NE(iterator, generator.end());
-  ASSERT_EQ(*iterator, "Abc");
-
-  ++iterator;
-  ASSERT_NE(iterator, generator.end());
-  ASSERT_EQ(*iterator, "Def");
-
-  ++iterator;
-  ASSERT_NE(iterator, generator.end());
-  ASSERT_EQ(*iterator, "Ghi");
-
-  ++iterator;
-  ASSERT_EQ(iterator, generator.end());
+  ASSERT_TRUE(std::ranges::equal(testData.begin(), testData.end(),
+                                 generator.begin(), generator.end()));
 }
