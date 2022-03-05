6841db5b921b0767aeb3a2c1f37dd770828c91af
RobinTF <83676088+RobinTF@users.noreply.github.com>
2022-03-05 23:16:09 +0100

    Asynchronous creation, compression, and sending of query results (#626)

--- src/util/HttpServer/ContentEncodingHelper.h
+++ src/util/HttpServer/ContentEncodingHelper.h
@@ -4,6 +4,9 @@
 
 #pragma once
 
+#include <absl/strings/ascii.h>
+#include <absl/strings/str_split.h>
+
 #include "./beast.h"
 
 namespace ad_utility::content_encoding {
@@ -15,11 +18,15 @@ namespace detail {
 constexpr std::string_view DEFLATE = "deflate";
 constexpr std::string_view GZIP = "gzip";
 
-constexpr CompressionMethod getCompressionMethodFromAcceptEncodingHeader(
-    const std::string_view acceptEncodingHeader) {
-  if (acceptEncodingHeader.find(DEFLATE) != std::string::npos) {
+inline CompressionMethod getCompressionMethodFromAcceptEncodingHeader(
+    std::vector<std::string_view> acceptedEncodings) {
+  const auto& contains = [&acceptedEncodings](std::string_view value) {
+    return std::find(acceptedEncodings.begin(), acceptedEncodings.end(),
+                     value) != acceptedEncodings.end();
+  };
+  if (contains(DEFLATE)) {
     return CompressionMethod::DEFLATE;
-  } else if (acceptEncodingHeader.find(GZIP) != std::string::npos) {
+  } else if (contains(GZIP)) {
     return CompressionMethod::GZIP;
   }
   return CompressionMethod::NONE;
@@ -31,10 +38,19 @@ using boost::beast::http::field;
 template <typename Body>
 CompressionMethod getCompressionMethodForRequest(
     const boost::beast::http::request<Body>& request) {
-  std::string_view acceptEncodingHeader =
-      request.base()[field::accept_encoding];
-  return detail::getCompressionMethodFromAcceptEncodingHeader(
-      acceptEncodingHeader);
+  auto acceptEncodingHeaders =
+      request.base().equal_range(field::accept_encoding);
+  std::vector<std::string_view> acceptedHeaders;
+  for (auto& current = acceptEncodingHeaders.first;
+       current != acceptEncodingHeaders.second; current++) {
+    std::string_view headerContent = current->value();
+    std::vector<std::string_view> tokens =
+        absl::StrSplit(headerContent, absl::ByChar(','));
+    for (auto token : tokens) {
+      acceptedHeaders.push_back(absl::StripAsciiWhitespace(token));
+    }
+  }
+  return detail::getCompressionMethodFromAcceptEncodingHeader(acceptedHeaders);
 }
 
 template <bool isRequest, typename Fields>
@@ -42,9 +58,9 @@ void setContentEncodingHeaderForCompressionMethod(
     CompressionMethod method,
     boost::beast::http::header<isRequest, Fields>& header) {
   if (method == CompressionMethod::DEFLATE) {
-    header.set(field::content_encoding, detail::DEFLATE);
+    header.insert(field::content_encoding, detail::DEFLATE);
   } else if (method == CompressionMethod::GZIP) {
-    header.set(field::content_encoding, detail::GZIP);
+    header.insert(field::content_encoding, detail::GZIP);
   }
 }
 
--- test/StreamableGeneratorTest.cpp
+++ test/StreamableGeneratorTest.cpp
@@ -4,10 +4,9 @@
 
 #include <gtest/gtest.h>
 
-#include "../src/util/streamable_generator.h"
+#include "../src/util/stream_generator.h"
 
-using namespace ad_utility::stream_generator;
-namespace io = boost::iostreams;
+using namespace ad_utility::streams;
 
 constexpr size_t TEST_BUFFER_SIZE = 10;
 
@@ -18,23 +17,20 @@ stream_generator generateException() {
 
 TEST(StreamableGeneratorTest, TestGeneratorExceptionResultsInException) {
   auto generator = generateException();
-  ASSERT_TRUE(generator.hasNext());
   try {
-    generator.next();
+    generator.begin();
     FAIL() << "Generator should throw Exception";
   } catch (const std::exception& e) {
     ASSERT_STREQ(e.what(), "Test Exception");
   }
-  ASSERT_FALSE(generator.hasNext());
 }
 
 stream_generator generateNothing() { co_return; }
 
 TEST(StreamableGeneratorTest, TestEmptyGeneratorReturnsEmptyResult) {
   auto generator = generateNothing();
-  ASSERT_TRUE(generator.hasNext());
-  ASSERT_EQ(generator.next(), "");
-  ASSERT_FALSE(generator.hasNext());
+  auto iterator = generator.begin();
+  ASSERT_EQ(iterator, generator.end());
 }
 
 const std::string MAX_TEST_BUFFER_STRING(TEST_BUFFER_SIZE, 'A');
@@ -48,76 +44,20 @@ basic_stream_generator<TEST_BUFFER_SIZE> generateMultipleElements() {
 TEST(StreamableGeneratorTest, TestGeneratorReturnsBufferedResults) {
   auto generator = generateMultipleElements();
 
-  ASSERT_TRUE(generator.hasNext());
-  ASSERT_EQ(generator.next(), MAX_TEST_BUFFER_STRING);
+  auto iterator = generator.begin();
+  ASSERT_NE(iterator, generator.end());
+  ASSERT_EQ(*iterator, MAX_TEST_BUFFER_STRING);
+  ++iterator;
 
-  ASSERT_TRUE(generator.hasNext());
-  ASSERT_EQ(generator.next(), std::string("1Abc"));
+  ASSERT_NE(iterator, generator.end());
+  ASSERT_EQ(*iterator, std::string("1Abc"));
+  ++iterator;
 
-  ASSERT_FALSE(generator.hasNext());
+  ASSERT_EQ(iterator, generator.end());
 }
 
 TEST(StreamableGeneratorTest, TestGeneratorDefaultInitialisesWithNoOp) {
   stream_generator generator;
-
-  ASSERT_TRUE(generator.hasNext());
-  std::string_view view = generator.next();
-  ASSERT_TRUE(view.empty());
-  ASSERT_FALSE(generator.hasNext());
+  auto iterator = generator.begin();
+  ASSERT_EQ(iterator, generator.end());
 }
-
-TEST(StreamableGeneratorTest, TestGeneratorNextThrowsExceptionWhenDone) {
-  auto generator = generateNothing();
-
-  generator.next();
-  try {
-    generator.next();
-    FAIL() << "next() should throw an exception";
-  } catch (const std::exception& e) {
-    ASSERT_STREQ(e.what(), "Coroutine is not active");
-  }
-}
-
-class StreamableGeneratorTestFixture
-    : public ::testing::TestWithParam<CompressionMethod> {
- public:
-  [[nodiscard]] static std::string decompressData(
-      std::string_view compressedData) {
-    std::string result;
-    io::filtering_ostream filterStream;
-    if (GetParam() == CompressionMethod::GZIP) {
-      filterStream.push(io::gzip_decompressor());
-    } else if (GetParam() == CompressionMethod::DEFLATE) {
-      filterStream.push(io::zlib_decompressor());
-    } else {
-      // Unsupported decompression
-      AD_CHECK(false);
-    }
-    filterStream.push(io::back_inserter(result));
-
-    filterStream.write(compressedData.data(),
-                       static_cast<std::streamsize>(compressedData.size()));
-    return result;
-  }
-};
-
-TEST_P(StreamableGeneratorTestFixture, TestGeneratorAppliesCompression) {
-  auto generator = generateMultipleElements();
-  generator.setCompressionMethod(GetParam());
-
-  ASSERT_TRUE(generator.hasNext());
-  auto compressedData = generator.next();
-
-  // compression introduces another buffer, which is why co_yield
-  // is not suspended after TEST_BUFFER_SIZE bytes
-  ASSERT_EQ(decompressData(compressedData), MAX_TEST_BUFFER_STRING + "1Abc");
-
-  ASSERT_FALSE(generator.hasNext());
-}
-
-using ad_utility::content_encoding::CompressionMethod;
-
-INSTANTIATE_TEST_SUITE_P(CompressionMethodParameters,
-                         StreamableGeneratorTestFixture,
-                         ::testing::Values(CompressionMethod::DEFLATE,
-                                           CompressionMethod::GZIP));
