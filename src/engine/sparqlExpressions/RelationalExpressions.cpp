a7626cc7af1fdf47d88de337436393729c86765e
joka921 <johannes.kalmbach@gmail.com>
2022-03-14 14:39:51 +0000

    Added an empty relationalExpression

---
+++ src/engine/sparqlExpressions/RelationalExpressions.cpp
@@ -0,0 +1,7 @@
+
+
+//  Copyright 2022, University of Freiburg,
+//  Chair of Algorithms and Data Structures.
+//  Author: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
+
+#include "RelationalExpressions.h"
---
+++ src/engine/sparqlExpressions/RelationalExpressions.h
@@ -0,0 +1,10 @@
+//  Copyright 2022, University of Freiburg,
+//  Chair of Algorithms and Data Structures.
+//  Author: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
+
+#ifndef QLEVER_RELATIONALEXPRESSIONS_H
+#define QLEVER_RELATIONALEXPRESSIONS_H
+
+
+
+#endif  // QLEVER_RELATIONALEXPRESSIONS_H
