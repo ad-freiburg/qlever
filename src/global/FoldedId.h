4443b16a6d6206ba17156b74a6f8aa8e9b6c3fdb
joka921 <johannes.kalmbach@gmail.com>
2022-04-06 14:56:21 +0000

    Added serialization and hashing, as well as tests for these.

--- src/global/FoldedId.h
+++ src/global/FoldedId.h
@@ -11,16 +11,37 @@
 #include "../util/BitUtils.h"
 #include "../util/NBitInteger.h"
 
+/// The different Datatypes that a `FoldedId` (see below) can encode.
 enum struct Datatype {
   Undefined = 0,
   Int,
   Double,
-  Date,
   Vocab,
   LocalVocab,
   Text,
+  // TODO<joka921> At least "date" is missing and not yet folded.
 };
 
+/// Convert the `Datatype` enum to the corresponding string
+constexpr std::string_view toString(Datatype type) {
+  switch (type) {
+    case Datatype::Undefined:
+      return "Undefined";
+    case Datatype::Double:
+      return "Double";
+    case Datatype::Int:
+      return "Int";
+    case Datatype::Vocab:
+      return "Vocab";
+    case Datatype::LocalVocab:
+      return "LocalVocab";
+    case Datatype::Text:
+      return "Text";
+  }
+}
+
+/// Encode values of different types (the types from the `Datatype` enum above)
+/// using 4 bits for the datatype and 60 bits for the value.
 class FoldedId {
  public:
   using T = uint64_t;
@@ -29,42 +50,82 @@ class FoldedId {
 
   using IntegerType = ad_utility::NBitInteger<numDataBits>;
 
-  static constexpr T maxId = 1ull << (numDataBits - 1);
+  /// The maximum value for the unsigned "index" types (currently Vocab,
+  /// LocalVocab and Text).
+  static constexpr T maxIndex = 1ull << (numDataBits - 1);
 
-  struct IdTooLargeException : public std::exception {};
+  /// This exception is thrown if we try to store a value of an index type
+  /// (Vocab, LocalVocab, Text) that is larger than `maxIndex`.
+  struct IndexTooLargeException : public std::exception {};
+
+  /// A struct that represents the single undefined value. This is required for
+  /// generic code like in the `visit` method.
+  struct UndefinedT {};
 
  private:
+  // The actual bits.
   T _bits;
 
  public:
+  /// Default construction of an uninitialized id.
+  FoldedId() {}
+
+  /// Equality comparison is performed directly on the underlying representation
   constexpr bool operator==(const FoldedId&) const = default;
+
+  /// Comparison is performed directly on the underlying representation. This
+  /// means that all values of the same `Datatype` will be adjacent to each
+  /// other. Unsigned index types are also ordered correctly.
+  /// TODO<joka921> Should we give additional guarantees on the other types?
   constexpr auto operator<=>(const FoldedId& other) const {
     return _bits <=> other._bits;
   }
 
+  /// Get the datatype.
   [[nodiscard]] constexpr Datatype getDatatype() const noexcept {
     return static_cast<Datatype>(_bits >> numDataBits);
   }
 
-  static FoldedId Undefined() { return {0}; }
+  /// Create a `FoldedId` of the `Undefined` type. There is only one such ID and
+  /// it is guaranteed to be smaller than all Ids of other types.
+  constexpr static FoldedId Undefined() noexcept { return {0}; }
+
+  /// Returns an object of `UndefinedT`. In many scenarios this function is
+  /// unnecessary because `getDatatype() == Undefined` already identifies the
+  /// single undefined value correctly, but it is very useful for generic code
+  /// like the `visit` member function.
+  [[nodiscard]] UndefinedT getUndefined() const noexcept { return {}; }
+
+  /// Create a `FoldedId` for a double value. The conversion will reduce the
+  /// precision of the mantissa of an IEEE double precision floating point
+  /// number from 53 to 49 significant bits.
   static FoldedId Double(double d) {
     auto shifted = std::bit_cast<T>(d) >> numTypeBits;
     return addMask(shifted, Datatype::Double);
   }
-
+  /// Obtain the `double` that this `FoldedId` encodes. If `getDatatype() !=
+  /// Double` then the result is unspecified.
   [[nodiscard]] double getDouble() const noexcept {
     return std::bit_cast<double>(_bits << numTypeBits);
   }
 
+  /// Create a `FoldedId` for a signed integer value. Integers in the range
+  /// [-2^59, 2^59-1] can be represented. Integers outside of this range will
+  /// overflow according to the semantics of `NBitInteger<60>`.
   static FoldedId Int(int64_t i) noexcept {
     auto nbit = IntegerType::toNBit(i);
     return addMask(nbit, Datatype::Int);
   }
 
+  /// Obtain the signed integer that this `FoldedId` encodes. If `getDatatype()
+  /// != Int` then the result is unspecified.
   [[nodiscard]] int64_t getInt() const noexcept {
     return IntegerType::fromNBit(_bits);
   }
 
+  /// Create a `FoldedId` for an unsigned index of type `Vocab|Text|LocalVocab`.
+  /// These types can represent values in the range [0, 2^60]. When `index` is
+  /// outside of this range, and `IndexTooLargeException` is thrown.
   static FoldedId Vocab(T index) {
     return makeUnsigned(index, Datatype::Vocab);
   }
@@ -73,6 +134,8 @@ class FoldedId {
     return makeUnsigned(index, Datatype::LocalVocab);
   }
 
+  /// Obtain the unsigned index that this `FoldedId` encodes. If `getDatatype()
+  /// != [Vocab|Text|LocalVocab]` then the result is unspecified.
   [[nodiscard]] constexpr T getVocab() const noexcept {
     return removeMask(_bits);
   }
@@ -85,21 +148,81 @@ class FoldedId {
 
   // TODO<joka921> implement dates
 
+  /// Enable hashing in abseil for `FoldedId` (required by `ad_utility::HashSet`
+  /// and `ad_utility::HashMap`
+  template <typename H>
+  friend H AbslHashValue(H h, const FoldedId& id) {
+    return H::combine(std::move(h), id._bits);
+  }
+
+  /// Enable the serialization of `FoldedId` in the `ad_utility::serialization`
+  /// framework.
+  template <typename Serializer>
+  friend void serialize(Serializer& serializer, FoldedId& id) {
+    serializer | id._bits;
+  }
+
+  /// Similar to `std::visit` for `std::variant`. First gets the datatype and
+  /// then calls `visitor(getTYPE)` where `getTYPE` is the correct getter method
+  /// for the datatype (e.g. `getDouble` for `Datatype::Double`). Visitor must
+  /// be callable with all of the possible return types of the `getTYPE`
+  /// functions.
+  template <typename Visitor>
+  decltype(auto) visit(Visitor&& visitor) const {
+    switch (getDatatype()) {
+      case Datatype::Undefined:
+        return std::invoke(visitor, getUndefined());
+      case Datatype::Double:
+        return std::invoke(visitor, getDouble());
+      case Datatype::Int:
+        return std::invoke(visitor, getInt());
+      case Datatype::Vocab:
+        return std::invoke(visitor, getVocab());
+      case Datatype::LocalVocab:
+        return std::invoke(visitor, getLocalVocab());
+      case Datatype::Text:
+        return std::invoke(visitor, getText());
+    }
+  }
+
+  /// This operator is only for debugging and testing. It returns a
+  /// human-readable representation.
+  friend std::ostream& operator<<(std::ostream& ostr, const FoldedId& id) {
+    ostr << toString(id.getDatatype()) << ':';
+    auto visitor = [&ostr]<typename T>(T&& value) {
+      if constexpr (ad_utility::isSimilar<T, FoldedId::UndefinedT>) {
+        ostr << "Undefined";
+      } else {
+        ostr << std::to_string(value);
+      }
+    };
+    id.visit(visitor);
+    return ostr;
+  }
+
  private:
+  // Private constructor that implicitly converts from the underlying
+  // representation. Used in the implementation of the static factory methods
+  // `Double()`, `Int()` etc.
   constexpr FoldedId(T bits) : _bits{bits} {}
 
+  // Set the first 4 bits of `bits` to a 4-bit representation of `type`.
+  // Requires that the first four bits of `bits` are all zero.
   static constexpr T addMask(T bits, Datatype type) {
     auto mask = static_cast<T>(type) << numDataBits;
     return bits | mask;
   }
+
+  // Set the first 4 bits of `bits` to zero.
   static constexpr T removeMask(T bits) noexcept {
     auto mask = ad_utility::bitMaskForLowerBits(numDataBits);
     return bits & mask;
   }
 
+  // Helper function for the implementation of the unsigned index types
   static constexpr FoldedId makeUnsigned(T id, Datatype type) {
-    if (id > maxId) {
-      throw IdTooLargeException();
+    if (id > maxIndex) {
+      throw IndexTooLargeException();
     }
     return addMask(id, type);
   }
--- test/FoldedIdTest.cpp
+++ test/FoldedIdTest.cpp
@@ -5,12 +5,14 @@
 #include <gtest/gtest.h>
 
 #include "../src/global/FoldedId.h"
+#include "../src/util/HashSet.h"
 #include "../src/util/Random.h"
+#include "../src/util/Serializer/Serializer.h"
 
 auto doubleGenerator = RandomDoubleGenerator();
-auto idGenerator = SlowRandomIntGenerator<uint64_t>(0, FoldedId::maxId);
+auto idGenerator = SlowRandomIntGenerator<uint64_t>(0, FoldedId::maxIndex);
 auto invalidIdGenerator = SlowRandomIntGenerator<uint64_t>(
-    FoldedId::maxId, std::numeric_limits<uint64_t>::max());
+    FoldedId::maxIndex, std::numeric_limits<uint64_t>::max());
 
 auto nonOverflowingNBitGenerator = SlowRandomIntGenerator<int64_t>(
     FoldedId::IntegerType::min(), FoldedId::IntegerType::max());
@@ -63,11 +65,11 @@ TEST(FoldedId, Indices) {
       testSingle(idGenerator());
     }
     testSingle(0);
-    testSingle(FoldedId::maxId);
+    testSingle(FoldedId::maxIndex);
 
     for (size_t idx = 0; idx < 10000; ++idx) {
       auto value = invalidIdGenerator();
-      ASSERT_THROW(makeId(value), FoldedId::IdTooLargeException);
+      ASSERT_THROW(makeId(value), FoldedId::IndexTooLargeException);
     }
   };
 
@@ -82,6 +84,100 @@ TEST(FoldedId, Undefined) {
   ASSERT_EQ(id.getDatatype(), Datatype::Undefined);
 }
 
+auto makeRandomIds = []() {
+  std::vector<FoldedId> ids;
+
+  auto makeIdsFromGenerator = [&ids](auto& generator, auto makeIds) {
+    for (size_t i = 0; i < 10000; ++i) {
+      ids.push_back(makeIds(generator()));
+    }
+  };
+
+  makeIdsFromGenerator(doubleGenerator, &FoldedId::Double);
+  makeIdsFromGenerator(idGenerator, &FoldedId::Vocab);
+  makeIdsFromGenerator(idGenerator, &FoldedId::Text);
+  makeIdsFromGenerator(idGenerator, &FoldedId::LocalVocab);
+  makeIdsFromGenerator(nonOverflowingNBitGenerator, &FoldedId::Int);
+  makeIdsFromGenerator(overflowingNBitGenerator, &FoldedId::Int);
+  makeIdsFromGenerator(underflowingNBitGenerator, &FoldedId::Int);
+
+  for (size_t i = 0; i < 10000; ++i) {
+    ids.push_back(FoldedId::Undefined());
+  }
+
+  randomShuffle(ids.begin(), ids.end());
+  return ids;
+};
+
 TEST(FoldedId, Ordering) {
-  // TODO<joka921> implement tests.
+  auto ids = makeRandomIds();
+  std::sort(ids.begin(), ids.end());
+
+  auto sortedByDatatypeAndIndexTypes = [](FoldedId a, FoldedId b) {
+    if (a.getDatatype() != b.getDatatype()) {
+      return a.getDatatype() < b.getDatatype();
+    }
+    static ad_utility::HashSet<Datatype> indexTypes{
+        Datatype::LocalVocab, Datatype::Vocab, Datatype::Text};
+    if (indexTypes.contains(a.getDatatype())) {
+      return a.getVocab() < b.getVocab();
+    }
+    return false;
+  };
+  ASSERT_TRUE(
+      std::is_sorted(ids.begin(), ids.end(), sortedByDatatypeAndIndexTypes));
+}
+
+TEST(FoldedId, Serialization) {
+  auto ids = makeRandomIds();
+
+  for (auto id : ids) {
+    ad_utility::serialization::ByteBufferWriteSerializer writer;
+    writer << id;
+    ad_utility::serialization::ByteBufferReadSerializer reader{
+        std::move(writer).data()};
+    FoldedId serializedId;
+    reader >> serializedId;
+    ASSERT_EQ(id, serializedId);
+  }
+}
+
+TEST(FoldedId, Hashing) {
+  auto ids = makeRandomIds();
+  ad_utility::HashSet<FoldedId> duplicates;
+  duplicates.reserve(2 * ids.size());
+  for (size_t i = 0; i < 2; ++i) {
+    for (auto id : ids) {
+      duplicates.insert(id);
+    }
+  }
+  std::vector<FoldedId> duplicatesAsVector;
+  duplicatesAsVector.reserve(duplicates.size());
+  for (auto id : duplicates) {
+    duplicatesAsVector.push_back(id);
+  }
+
+  std::sort(duplicatesAsVector.begin(), duplicatesAsVector.end());
+  std::sort(ids.begin(), ids.end());
+  ids.erase(std::unique(ids.begin(), ids.end()), ids.end());
+
+  ASSERT_EQ(ids, duplicatesAsVector);
+}
+
+TEST(FoldedId, toDebugString) {
+  auto test = [](FoldedId id, std::string_view expected) {
+    std::stringstream stream;
+    stream << id;
+    ASSERT_EQ(stream.str(), expected);
+  };
+  test(FoldedId::Undefined(), "Undefined:Undefined");
+  test(FoldedId::Int(-42), "Int:-42");
+  test(FoldedId::Double(42.0), "Double:42.000000");
+  test(FoldedId::Vocab(15), "Vocab:15");
+  test(FoldedId::LocalVocab(25), "LocalVocab:25");
+  test(FoldedId::Text(37), "Text:37");
+}
+
+TEST(FoldedId, TriviallyCopyable) {
+  static_assert(std::is_trivially_copyable_v<FoldedId>);
 }
