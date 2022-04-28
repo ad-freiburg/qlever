59f795f4a05e2ec5bdd5e4867242fc2487f37a40
joka921 <johannes.kalmbach@gmail.com>
2022-04-28 10:21:27 +0000

    Started writing generic unit tests.

--- test/ValueIdComparatorsTest.cpp
+++ test/ValueIdComparatorsTest.cpp
@@ -64,3 +64,66 @@ TEST(ValueIdComparators, Int) {
   ASSERT_EQ(ranges[1].first->getDatatype(), Datatype::Double);
   ASSERT_DOUBLE_EQ(ranges[1].first->getDouble(), 42.0);
 }
+
+TEST(ValueIdComparators, GetRangeForDatatype) {
+  std::vector<Datatype> datatypes {Datatype::Int, Datatype::Double, Datatype::VocabIndex, Datatype::Undefined, Datatype::LocalVocabIndex, Datatype::TextIndex};
+  auto ids = makeAndSortIds();
+  for (auto datatype : datatypes) {
+    auto [begin, end] = getRangeForDatatype(ids.begin(), ids.end(), datatype);
+    for (auto it = ids.begin(); it < begin; ++it) {
+      ASSERT_NE(it->getDatatype(), datatype);
+    }
+    for (auto it = begin; it < end; ++it) {
+      ASSERT_EQ(it->getDatatype(), datatype);
+    }
+    for (auto it = end; it < ids.end(); ++it) {
+      ASSERT_NE(it->getDatatype(), datatype);
+    }
+  }
+}
+
+template<Comparison comparison>
+auto toFunctor() {
+#define RETURN(comp, func)                         \
+  if constexpr (comparison == comp) {                    \
+    return func<>();                               \
+  }
+
+  RETURN(Comparison::LT, std::less)
+  RETURN(Comparison::LE, std::less_equal)
+  RETURN(Comparison::EQ, std::equal_to)
+  RETURN(Comparison::NE, std::not_equal_to)
+  RETURN(Comparison::GE, std::greater_equal)
+  RETURN(Comparison::GT, std::greater)
+#undef RETURN
+}
+
+auto testGetRangesForId(auto begin, auto end, ValueId id, auto getFromId) {
+  auto valueFromId = std::invoke(getFromId, id);
+
+  auto impl = [&]<Comparison comparison>() {
+    auto ranges = getRangesForId(begin, end, id, comparison);
+    auto comparator = toFunctor<comparison>;
+    for (auto [singleBegin, singleEnd] : ranges) {
+      for (auto it = singleBegin; it != singleEnd; ++it) {
+        auto valueFromRange = std::invoke(getFromId, *it);
+        ASSERT_TRUE(comparator(valueFromRange, valueFromId));
+      }
+    }
+  };
+
+  impl.template operator()<Comparison::LT>();
+  impl.template operator()<Comparison::LE>();
+  impl.template operator()<Comparison::EQ>();
+  impl.template operator()<Comparison::NE>();
+  impl.template operator()<Comparison::GE>();
+  impl.template operator()<Comparison::GT>();
+}
+
+
+
+
+
+
+
+
