22c478cdacc76c400b15f4eddff8c024eb1dcd9c
joka921 <johannes.kalmbach@gmail.com>
2022-06-09 18:25:57 +0000

    Added some unit tests for the triple parsing.

--- test/TurtleParserTest.cpp
+++ test/TurtleParserTest.cpp
@@ -400,8 +400,8 @@ TEST(TurtleParserTest, numericLiteralErrorBehavior) {
         "<a> <b> \"123\"^^xsd:integer",
         "<a> <b> 456",
         "<a> <b> \"-9999.0\"^^xsd:integer",
-        "<a> <b> \"9999.0\"^^xsd:int",
-        "<a> <b> \"9999E4\"^^xsd:integer",
+        "<a> <b> \"9999.0\"^^xsd:short",
+        "<a> <b> \"9999E4\"^^xsd:nonNegativeInteger",
         "<a> <b> \"9999E4\"^^xsd:int",
         "<a> <b> 99999999999999999999999",
         "<a> <b> \"99999999999999999999\"^^xsd:integer",
@@ -443,6 +443,26 @@ TEST(TurtleParserTest, numericLiteralErrorBehavior) {
         TurtleParserIntegerOverflowBehavior::OverflowingToDouble;
     auto result = parseAllTriples(parser, input);
     ASSERT_EQ(result, expected);
+  }
+}
+
+TEST(TurtleParserTest, DateLiterals) {
+  std::vector<std::string> dateLiterals{
+      R"("2000-10-15"^^<)"s + XSD_DATE_TYPE + ">",
+      R"("-2014-03-16T12:13:52"^^<)"s + XSD_DATETIME_TYPE + ">",
+      R"("2084"^^<)"s + XSD_GYEAR_TYPE + ">",
+      R"("2083-12"^^<)"s + XSD_GYEARMONTH_TYPE + ">"};
+  std::vector<std::string> expected{
+      ":v:date:0000000000000002000-10-15T00:00:00",
+      ":v:date:-999999999999997985-03-16T12:13:52",
+      ":v:date:0000000000000002084-00-00T00:00:00",
+      ":v:date:0000000000000002083-12-00T00:00:00"};
+
+  for (size_t i = 0; i < dateLiterals.size(); ++i) {
+    auto object =
+        TurtleStringParser<Tokenizer>::parseTripleObject(dateLiterals[i]);
+    ASSERT_TRUE(object.isString());
+    EXPECT_EQ(object.getString(), expected[i]);
   }
 }
 
