8ec8a7aeea7b14099873f3d2e260d18efb63461d
Julian <Qup42@users.noreply.github.com>
2022-06-24 23:32:51 +0200

    Enable parallel execution of tests (#672)
    So far, parallel execution of our tests failed because of shared file names, etc. Now filenames are as distinct as possible and test cases that must not be executed in parallel with any other tests (these are currently the tests involving STXLL, which share a common STXLL disk file) are explicitly marked as such.

--- test/VocabularyOnDiskTest.cpp
+++ test/VocabularyOnDiskTest.cpp
@@ -5,121 +5,157 @@
 #include <gtest/gtest.h>
 
 #include "../src/index/VocabularyOnDisk.h"
+#include "../src/util/Forward.h"
 #include "./VocabularyTestHelpers.h"
 
 using namespace vocabulary_test;
 
-const std::string vocabFilename = "vocabulary.tmp.test.dat";
+// A common suffix for all files to reduce the probability of colliding file
+// names, when other tests are run in parallel.
+std::string suffix = ".vocabularyOnDiskTest.dat";
 
-// Create and return a `VocabularyOnDisk` from words and ids. `words` and `ids`
-// must have the same size.
-auto createVocabularyImpl(
-    const std::vector<std::string>& words,
-    std::optional<std::vector<uint64_t>> ids = std::nullopt) {
-  VocabularyOnDisk vocabulary;
-  if (!ids.has_value()) {
-    vocabulary.buildFromVector(words, vocabFilename);
-  } else {
-    AD_CHECK(words.size() == ids.value().size());
-    std::vector<std::pair<std::string, uint64_t>> wordsAndIds;
-    for (size_t i = 0; i < words.size(); ++i) {
-      wordsAndIds.emplace_back(words[i], ids.value()[i]);
-    }
-    vocabulary.buildFromStringsAndIds(wordsAndIds, vocabFilename);
+// Store a VocabularyOnDisk and read it back from file. For each instance of
+// `VocabularyCreator` that exists at the same time, a different filename has to
+// be chosen.
+class VocabularyCreator {
+ private:
+  std::string vocabFilename_;
+
+ public:
+  explicit VocabularyCreator(std::string filename)
+      : vocabFilename_{std::move(filename)} {
+    ad_utility::deleteFile(vocabFilename_, false);
   }
-  return vocabulary;
+  ~VocabularyCreator() { ad_utility::deleteFile(vocabFilename_); }
+
+  // Create and return a `VocabularyOnDisk` from words and ids. `words` and
+  // `ids` must have the same size.
+  auto createVocabularyImpl(
+      const std::vector<std::string>& words,
+      std::optional<std::vector<uint64_t>> ids = std::nullopt) {
+    VocabularyOnDisk vocabulary;
+    if (!ids.has_value()) {
+      vocabulary.buildFromVector(words, vocabFilename_);
+    } else {
+      AD_CHECK(words.size() == ids.value().size());
+      std::vector<std::pair<std::string, uint64_t>> wordsAndIds;
+      for (size_t i = 0; i < words.size(); ++i) {
+        wordsAndIds.emplace_back(words[i], ids.value()[i]);
+      }
+      vocabulary.buildFromStringsAndIds(wordsAndIds, vocabFilename_);
+    }
+    return vocabulary;
+  }
+
+  // Create and return a `VocabularyOnDisk` from words and ids. `words` and
+  // `ids` must have the same size. Note: The resulting vocabulary will be
+  // destroyed and re-initialized from disk before it is returned.
+  auto createVocabularyFromDiskImpl(
+      const std::vector<std::string>& words,
+      std::optional<std::vector<uint64_t>> ids = std::nullopt) {
+    { createVocabularyImpl(words, std::move(ids)); }
+    VocabularyOnDisk vocabulary;
+    vocabulary.open(vocabFilename_);
+    return vocabulary;
+  }
+
+  // Create and return a `VocabularyOnDisk` from words. The ids will be [0, ..
+  // words.size()).
+  auto createVocabulary(const std::vector<std::string>& words) {
+    return createVocabularyImpl(words);
+  }
+
+  // Create and return a `VocabularyOnDisk` from words. The ids will be [0, ..
+  // words.size()). Note: The resulting vocabulary will be destroyed and
+  // re-initialized from disk before it is returned.
+  auto createVocabularyFromDisk(const std::vector<std::string>& words) {
+    return createVocabularyFromDiskImpl(words);
+  }
+};
+
+auto createVocabulary(std::string filename) {
+  return [c = VocabularyCreator{std::move(filename)}](auto&&... args) mutable {
+    return c.createVocabulary(AD_FWD(args)...);
+  };
 }
 
-// Create and return a `VocabularyOnDisk` from words and ids. `words` and `ids`
-// must have the same size. Note: The resulting vocabulary will be destroyed and
-// re-initialized from disk before it is returned.
-auto createVocabularyFromDiskImpl(
-    const std::vector<std::string>& words,
-    std::optional<std::vector<uint64_t>> ids = std::nullopt) {
-  { createVocabularyImpl(words, std::move(ids)); }
-  VocabularyOnDisk vocabulary;
-  vocabulary.open(vocabFilename);
-  return vocabulary;
+auto createVocabularyFromDisk(std::string filename) {
+  return [c = VocabularyCreator{std::move(filename)}](auto&&... args) mutable {
+    return c.createVocabularyFromDisk(AD_FWD(args)...);
+  };
 }
 
-// Create and return a `VocabularyOnDisk` from words. The ids will be [0, ..
-// words.size()).
-auto createVocabulary(const std::vector<std::string>& words) {
-  return createVocabularyImpl(words);
-}
-
-// Create and return a `VocabularyOnDisk` from words. The ids will be [0, ..
-// words.size()). Note: The resulting vocabulary will be destroyed and
-// re-initialized from disk before it is returned.
-auto createVocabularyFromDisk(const std::vector<std::string>& words) {
-  return createVocabularyFromDiskImpl(words);
+auto createVocabularyFromDiskImpl(std::string filename) {
+  return [c = VocabularyCreator{std::move(filename)}](auto&&... args) mutable {
+    return c.createVocabularyFromDiskImpl(AD_FWD(args)...);
+  };
 }
 
 TEST(VocabularyOnDisk, LowerUpperBoundStdLess) {
-  ad_utility::deleteFile(vocabFilename);
-  testUpperAndLowerBoundWithStdLess(createVocabulary);
-  ad_utility::deleteFile(vocabFilename);
-  testUpperAndLowerBoundWithStdLess(createVocabularyFromDisk);
-  ad_utility::deleteFile(vocabFilename);
+  testUpperAndLowerBoundWithStdLess(
+      createVocabulary("lowerUpperBoundStdLess1"));
+  testUpperAndLowerBoundWithStdLess(
+      createVocabularyFromDisk("lowerUpperBoundStdLess2"));
 }
 
 TEST(VocabularyOnDisk, LowerUpperBoundStdLessNonContiguousIds) {
   std::vector<std::string> words{"alpha", "betta", "chimes", "someVery123Word"};
   std::vector<uint64_t> ids{2, 4, 8, 42};
-  ad_utility::deleteFile(vocabFilename);
+  VocabularyCreator creator1{"lowerUppperBoundStdLessNonContiguousIds1"};
   testUpperAndLowerBoundWithStdLessFromWordsAndIds(
-      createVocabularyImpl(words, ids), words, ids);
-  ad_utility::deleteFile(vocabFilename);
+      creator1.createVocabularyImpl(words, ids), words, ids);
+
+  VocabularyCreator creator2{"lowerUppperBoundStdLessNonContiguousIds2"};
   testUpperAndLowerBoundWithStdLessFromWordsAndIds(
-      createVocabularyFromDiskImpl(words, ids), words, ids);
-  ad_utility::deleteFile(vocabFilename);
+      creator2.createVocabularyFromDiskImpl(words, ids), words, ids);
 }
 
 TEST(VocabularyOnDisk, LowerUpperBoundNumeric) {
-  ad_utility::deleteFile(vocabFilename);
-  testUpperAndLowerBoundWithNumericComparator(createVocabulary);
-  ad_utility::deleteFile(vocabFilename);
-  testUpperAndLowerBoundWithNumericComparator(createVocabularyFromDisk);
-  ad_utility::deleteFile(vocabFilename);
+  testUpperAndLowerBoundWithNumericComparator(
+      createVocabulary("lowerUpperBoundNumeric1"));
+  testUpperAndLowerBoundWithNumericComparator(
+      createVocabularyFromDisk("lowerUpperBoundNumeric2"));
 }
 
 TEST(VocabularyOnDisk, LowerUpperBoundNumericNonContiguousIds) {
   std::vector<std::string> words{"4", "33", "222", "1111"};
   std::vector<uint64_t> ids{2, 4, 8, 42};
-  ad_utility::deleteFile(vocabFilename);
+
+  VocabularyCreator creator1{"lowerUpperBoundNumericNonContiguousIds1"};
   testUpperAndLowerBoundWithNumericComparatorFromWordsAndIds(
-      createVocabularyImpl(words, ids), words, ids);
-  ad_utility::deleteFile(vocabFilename);
+      creator1.createVocabularyImpl(words, ids), words, ids);
+  VocabularyCreator creator2{"lowerUpperBoundNumericNonContiguousIds2"};
   testUpperAndLowerBoundWithNumericComparatorFromWordsAndIds(
-      createVocabularyFromDiskImpl(words, ids), words, ids);
-  ad_utility::deleteFile(vocabFilename);
+      creator2.createVocabularyFromDiskImpl(words, ids), words, ids);
 }
 
 TEST(VocabularyOnDisk, AccessOperator) {
-  ad_utility::deleteFile(vocabFilename);
-  testAccessOperatorForUnorderedVocabulary(createVocabulary);
-  ad_utility::deleteFile(vocabFilename);
-  testAccessOperatorForUnorderedVocabulary(createVocabularyFromDisk);
+  testAccessOperatorForUnorderedVocabulary(createVocabulary("AccessOperator1"));
+  testAccessOperatorForUnorderedVocabulary(
+      createVocabularyFromDisk("AccessOperator2"));
 }
 
 TEST(VocabularyOnDisk, AccessOperatorWithNonContiguousIds) {
   std::vector<std::string> words{"game",  "4",      "nobody", "33",
                                  "alpha", "\n\1\t", "222",    "1111"};
   std::vector<uint64_t> ids{2, 4, 8, 16, 17, 19, 42, 42 * 42 + 7};
-  ad_utility::deleteFile(vocabFilename);
-  testAccessOperatorForUnorderedVocabulary(createVocabulary);
-  ad_utility::deleteFile(vocabFilename);
-  testAccessOperatorForUnorderedVocabulary(createVocabularyFromDisk);
+  testAccessOperatorForUnorderedVocabulary(
+      createVocabulary("AccessOperatorWithNonContiguousIds1"));
+  testAccessOperatorForUnorderedVocabulary(
+      createVocabularyFromDisk("AccessOperatorWithNonContiguousIds2"));
 }
 
 TEST(VocabularyOnDisk, ErrorOnNonAscendingIds) {
   std::vector<std::string> words{"game", "4", "nobody"};
   std::vector<uint64_t> ids{2, 4, 3};
-  ASSERT_THROW(createVocabularyImpl(words, ids), ad_semsearch::Exception);
-  ASSERT_THROW(createVocabularyFromDiskImpl(words, ids),
+  VocabularyCreator creator1{"ErrorOnNonAscendingIds1"};
+  ASSERT_THROW(creator1.createVocabularyImpl(words, ids),
+               ad_semsearch::Exception);
+  VocabularyCreator creator2{"ErrorOnNonAscendingIds2"};
+  ASSERT_THROW(creator2.createVocabularyFromDiskImpl(words, ids),
                ad_semsearch::Exception);
 }
 
 TEST(VocabularyOnDisk, EmptyVocabulary) {
-  testEmptyVocabulary(createVocabulary);
+  testEmptyVocabulary(createVocabulary("EmptyVocabulary"));
 }
