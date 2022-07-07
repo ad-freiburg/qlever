ad-freiburg/qlever

commit 0b2d01d23ad68a5b110ded98e7e51ccbf3798a4e

Consistently use Value IDs via new `TripleComponent` class (#686)
The Value IDs are now also used in the SPARQL parser. The conversion of a value to a Value ID now happens via a new `TripleComponent` class, which is consistently used throughout the code. Via this change, the code also becomes much simpler in many places.

src/parser/TurtleParser.cpp
@@ -5,8 +5,9 @@
 
 #include "./TurtleParser.h"
 
-#include <string.h>
+#include <cstring>
 
+#include "../util/Conversions.h"
 #include "../util/TaskQueue.h"
 #include "./RdfEscaping.h"
 
@@ -127,7 +128,6 @@ template <class T>
 bool TurtleParser<T>::objectList() {
   if (object()) {
     while (skip<TurtleTokenId::Comma>() && check(object())) {
-      continue;
     }
     return true;
   } else {
@@ -238,7 +238,7 @@ void TurtleParser<T>::parseDoubleConstant(const std::string& input) {
   size_t position;
 
   bool errorOccured = false;
-  TripleObject result;
+  TripleComponent result;
   try {
     // We cannot directly store this in `_lastParseResult` because this might
     // overwrite `input`.
@@ -264,7 +264,7 @@ void TurtleParser<T>::parseIntegerConstant(const std::string& input) {
   size_t position = 0;
 
   bool errorOccured = false;
-  TripleObject result;
+  TripleComponent result;
   try {
     // We cannot directly store this in `_lastParseResult` because this might
     // overwrite `input`.
@@ -349,13 +349,28 @@ bool TurtleParser<T>::rdfLiteral() {
     const auto& typeIri = _lastParseResult.getString();
     auto type = stripAngleBrackets(typeIri);
     std::string strippedLiteral{stripDoubleQuotes(literalString)};
-    if (type == XSD_INT_TYPE || type == XSD_INTEGER_TYPE) {
+    // TODO<joka921> clean this up by moving the check for the types to a
+    // separate module.
+    if (type == XSD_INT_TYPE || type == XSD_INTEGER_TYPE ||
+        type == XSD_NON_POSITIVE_INTEGER_TYPE ||
+        type == XSD_NEGATIVE_INTEGER_TYPE || type == XSD_LONG_TYPE ||
+        type == XSD_SHORT_TYPE || type == XSD_BYTE_TYPE ||
+        type == XSD_NON_NEGATIVE_INTEGER_TYPE ||
+        type == XSD_UNSIGNED_LONG_TYPE || type == XSD_UNSIGNED_INT_TYPE ||
+        type == XSD_UNSIGNED_SHORT_TYPE || type == XSD_POSITIVE_INTEGER_TYPE ||
+        type == XSD_BOOLEAN_TYPE) {
       parseIntegerConstant(strippedLiteral);
     } else if (type == XSD_DECIMAL_TYPE || type == XSD_DOUBLE_TYPE ||
                type == XSD_FLOAT_TYPE) {
       parseDoubleConstant(strippedLiteral);
     } else {
       _lastParseResult = literalString + "^^" + _lastParseResult.getString();
+      // TODO: remove this once the dates become value IDs, too.
+      if (type == XSD_DATETIME_TYPE || type == XSD_DATE_TYPE ||
+          type == XSD_GYEAR_TYPE || type == XSD_GYEARMONTH_TYPE) {
+        _lastParseResult = ad_utility::convertValueLiteralToIndexWord(
+            _lastParseResult.getString());
+      }
     }
     return true;
   } else {

e2e/scientists_queries.yaml
@@ -703,7 +703,7 @@ queries:
       }
       GROUP BY ?p
     checks:
-      - num_rows: 109 
+      - num_rows: 109
       - num_cols: 2
       - selected: ["?p", "?count"]
       - contains_row: ["<Film_appeared_in>", 56] 
@@ -719,7 +719,7 @@ queries:
       }
       GROUP BY ?p
     checks:
-      - num_rows: 36 
+      - num_rows: 36
       - num_cols: 2
       - selected: ["?p", "?count"]
       - contains_row: ["<Hall_of_fame_induction>", 1] 
@@ -1108,7 +1108,7 @@ queries:
         }
       }
     checks:
-      #The query returns to many rows, the current limit is 4096
+      #The query returns too many rows, the current limit is 4096
       - num_cols: 2
       - selected: ["?a", "?r"]
       - contains_row: ["<Aafia_Siddiqui>","<Religion>"]
@@ -1135,3 +1135,43 @@ queries:
       - contains_row: ["<Jacques_Curie>", "Jacques_Curie"]
       - contains_row: ["<Marie_Curie>", "Marie_Curie"]
       - contains_row: ["<Pierre_Curie>", "Pierre_Curie"]
+
+
+  - query : datetime_object-find-justin-boyan
+    type: no-text
+    sparql: |
+      PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+      SELECT ?s WHERE {
+        ?s <Date_of_birth> "1970-05-28T17:55Z"^^xsd:dateTime
+      }
+    checks:
+      - num_cols: 1
+      - selected: ["?s"]
+      - contains_row: ["<Justin_Boyan>"]
+
+
+  - query : datetime_filter-find-justin-boyan
+    type: no-text
+    sparql: |
+      PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+      SELECT ?s WHERE {
+        ?s <Date_of_birth> ?d .
+        FILTER (?d = "1970-05-28T17:55Z"^^xsd:dateTime)
+      }
+    checks:
+      - num_cols: 1
+      - selected: ["?s"]
+      - contains_row: ["<Justin_Boyan>"]
+  - query : xsd-value-in-property-path
+    type: no-text
+    sparql: |
+      PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+      SELECT ?x WHERE {
+        ?x ^<is-a>/<Structure_Count> "1"^^xsd:int
+      }
+    checks:
+      - num_cols: 1
+      - num_rows: 31
+      - selected: ["?x"]
+      - contains_row: ["<Architect>"]
+      - contains_row: ["<Visual_Artist>"]

src/engine/CountAvailablePredicates.cpp
@@ -27,11 +27,11 @@ CountAvailablePredicates::CountAvailablePredicates(
       _countVarName("count") {}
 
 CountAvailablePredicates::CountAvailablePredicates(QueryExecutionContext* qec,
-                                                   std::string entityName)
+                                                   TripleComponent entityName)
     : Operation(qec),
       _subtree(nullptr),
       _subjectColumnIndex(0),
-      _subjectEntityName(entityName),
+      _subjectEntityName(entityName.getString()),
       _predicateVarName("predicate"),
       _countVarName("count") {}
 

src/engine/CountAvailablePredicates.h
@@ -45,7 +45,8 @@ class CountAvailablePredicates : public Operation {
    * @brief Creates a new CountAvailblePredicates operation that returns
    * predicates and their counts for the entity given by the entityName.
    */
-  CountAvailablePredicates(QueryExecutionContext* qec, std::string entityName);
+  CountAvailablePredicates(QueryExecutionContext* qec,
+                           TripleComponent entityName);
 
  protected:
   [[nodiscard]] string asStringImpl(size_t indent) const override;

src/engine/Filter.cpp
@@ -530,20 +530,17 @@ void Filter::computeResultFixedValue(
         // all other filters (e.g. regexes) don't use the `Id` in `rhs`
         break;
       }
-      TripleObject rhsObject =
+      TripleComponent rhsObject =
           TurtleStringParser<TokenizerCtre>::parseTripleObject(rhs_string);
       if (rhsObject.isInt()) {
         rhs = Id::makeFromInt(rhsObject.getInt());
       } else if (rhsObject.isDouble()) {
         rhs = Id::makeFromDouble(rhsObject.getDouble());
       } else {
-        // TODO<joka921> give the `TripleObject` a visit function such that this
-        // becomes a compile time check.
+        // TODO<joka921> give the `TripleComponent` a visit function such that
+        // this becomes a compile time check.
         AD_CHECK(rhsObject.isString());
-        // We still need this conversion for dates
-        if (ad_utility::isXsdValue(rhs_string)) {
-          rhs_string = ad_utility::convertValueLiteralToIndexWord(rhs_string);
-        }
+        rhs_string = rhsObject.getString();
         // TODO: This is not standard conform, but currently required due to
         // our vocabulary storing iris with the greater than and
         // literals with their quotation marks.

src/engine/HasPredicateScan.cpp
@@ -396,12 +396,22 @@ void HasPredicateScan::computeSubqueryS(
   *dynResult = result.moveToDynamic();
 }
 
-void HasPredicateScan::setSubject(const std::string& subject) {
-  _subject = subject;
+void HasPredicateScan::setSubject(const TripleComponent& subject) {
+  if (!subject.isString()) {
+    throw ParseException{absl::StrCat(
+        "The object of a ql:has-predicate triple must be an IRI, but was \"",
+        subject.toString(), "\"")};
+  }
+  _subject = subject.getString();
 }
 
-void HasPredicateScan::setObject(const std::string& object) {
-  _object = object;
+void HasPredicateScan::setObject(const TripleComponent& object) {
+  if (!object.isString()) {
+    throw ParseException{absl::StrCat(
+        "The object of a ql:has-predicate triple must be an IRI, but was \"",
+        object.toString(), "\"")};
+  }
+  _object = object.getString();
 }
 
 const std::string& HasPredicateScan::getObject() const { return _object; }

src/engine/HasPredicateScan.h
@@ -50,8 +50,8 @@ class HasPredicateScan : public Operation {
 
   virtual size_t getCostEstimate() override;
 
-  void setSubject(const std::string& subject);
-  void setObject(const std::string& object);
+  void setSubject(const TripleComponent& subject);
+  void setObject(const TripleComponent& object);
   void setSubtree(std::shared_ptr<QueryExecutionTree> subtree);
   void setSubtreeSubjectColumn(size_t colIndex);
   ScanType getType() const;

src/engine/IndexScan.cpp
@@ -23,12 +23,12 @@ string IndexScan::asStringImpl(size_t indent) const {
          << "\"";
       break;
     case POS_BOUND_O:
-      os << "SCAN POS with P = \"" << _predicate << "\", O = \"" << _object
-         << "\"";
+      os << "SCAN POS with P = \"" << _predicate << "\", O = \""
+         << _object.toRdfLiteral() << "\"";
       break;
     case SOP_BOUND_O:
-      os << "SCAN SOP with S = \"" << _subject << "\", O = \"" << _object
-         << "\"";
+      os << "SCAN SOP with S = \"" << _subject << "\", O = \""
+         << _object.toRdfLiteral() << "\"";
       break;
     case PSO_FREE_S:
       os << "SCAN PSO with P = \"" << _predicate << "\"";
@@ -43,10 +43,10 @@ string IndexScan::asStringImpl(size_t indent) const {
       os << "SCAN SOP with S = \"" << _subject << "\"";
       break;
     case OPS_FREE_P:
-      os << "SCAN OPS with O = \"" << _object << "\"";
+      os << "SCAN OPS with O = \"" << _object.toRdfLiteral() << "\"";
       break;
     case OSP_FREE_S:
-      os << "SCAN OSP with O = \"" << _object << "\"";
+      os << "SCAN OSP with O = \"" << _object.toRdfLiteral() << "\"";
       break;
     case FULL_INDEX_SCAN_SPO:
       os << "SCAN FOR FULL INDEX SPO (DUMMY OPERATION)";
@@ -72,7 +72,8 @@ string IndexScan::asStringImpl(size_t indent) const {
 
 // _____________________________________________________________________________
 string IndexScan::getDescriptor() const {
-  return "IndexScan " + _subject + " " + _predicate + " " + _object;
+  return "IndexScan " + _subject.toString() + " " + _predicate + " " +
+         _object.toString();
 }
 
 // _____________________________________________________________________________
@@ -132,91 +133,62 @@ ad_utility::HashMap<string, size_t> IndexScan::getVariableColumns() const {
   ad_utility::HashMap<string, size_t> res;
   size_t col = 0;
 
+  // Helper lambdas that add the respective triple component as the next column.
+  auto addSubject = [&]() {
+    if (_subject.isVariable()) {
+      res[_subject.getString()] = col++;
+    }
+  };
+  auto addPredicate = [&]() {
+    if (_predicate[0] == '?') {
+      res[_predicate] = col++;
+    }
+  };
+  auto addObject = [&]() {
+    if (_object.isVariable()) {
+      res[_object.getString()] = col++;
+    }
+  };
+
   switch (_type) {
     case SPO_FREE_P:
     case FULL_INDEX_SCAN_SPO:
-      if (_subject[0] == '?') {
-        res[_subject] = col++;
-      }
-      if (_predicate[0] == '?') {
-        res[_predicate] = col++;
-      }
-
-      if (_object[0] == '?') {
-        res[_object] = col++;
-      }
+      addSubject();
+      addPredicate();
+      addObject();
       return res;
     case SOP_FREE_O:
     case SOP_BOUND_O:
     case FULL_INDEX_SCAN_SOP:
-      if (_subject[0] == '?') {
-        res[_subject] = col++;
-      }
-
-      if (_object[0] == '?') {
-        res[_object] = col++;
-      }
-
-      if (_predicate[0] == '?') {
-        res[_predicate] = col++;
-      }
+      addSubject();
+      addObject();
+      addPredicate();
       return res;
     case PSO_BOUND_S:
     case PSO_FREE_S:
     case FULL_INDEX_SCAN_PSO:
-      if (_predicate[0] == '?') {
-        res[_predicate] = col++;
-      }
-      if (_subject[0] == '?') {
-        res[_subject] = col++;
-      }
-
-      if (_object[0] == '?') {
-        res[_object] = col++;
-      }
+      addPredicate();
+      addSubject();
+      addObject();
       return res;
     case POS_BOUND_O:
     case POS_FREE_O:
     case FULL_INDEX_SCAN_POS:
-      if (_predicate[0] == '?') {
-        res[_predicate] = col++;
-      }
-
-      if (_object[0] == '?') {
-        res[_object] = col++;
-      }
-
-      if (_subject[0] == '?') {
-        res[_subject] = col++;
-      }
+      addPredicate();
+      addObject();
+      addSubject();
       return res;
     case OPS_FREE_P:
     case FULL_INDEX_SCAN_OPS:
-      if (_object[0] == '?') {
-        res[_object] = col++;
-      }
-
-      if (_predicate[0] == '?') {
-        res[_predicate] = col++;
-      }
-
-      if (_subject[0] == '?') {
-        res[_subject] = col++;
-      }
+      addObject();
+      addPredicate();
+      addSubject();
       return res;
     case OSP_FREE_S:
     case FULL_INDEX_SCAN_OSP:
-      if (_object[0] == '?') {
-        res[_object] = col++;
-      }
-
-      if (_subject[0] == '?') {
-        res[_subject] = col++;
-      }
-
-      if (_predicate[0] == '?') {
-        res[_predicate] = col++;
-      }
+      addObject();
+      addSubject();
+      addPredicate();
       return res;
     default:
       AD_THROW(ad_semsearch::Exception::CHECK_FAILED, "Should be unreachable.");
@@ -329,16 +301,26 @@ size_t IndexScan::computeSizeEstimate() {
         return getResult()->size();
       }
     }
+    // TODO<joka921> Should be a oneliner
+    // getIndex().cardinality(getPermutation(), getFirstKey());
     if (_type == SPO_FREE_P || _type == SOP_FREE_O) {
-      return getIndex().sizeEstimate(_subject, "", "");
+      return getIndex().subjectCardinality(_subject);
     } else if (_type == POS_FREE_O || _type == PSO_FREE_S) {
-      return getIndex().sizeEstimate("", _predicate, "");
+      return getIndex().relationCardinality(_predicate);
     } else if (_type == OPS_FREE_P || _type == OSP_FREE_S) {
-      return getIndex().sizeEstimate("", "", _object);
+      return getIndex().objectCardinality(_object);
     }
-    return getIndex().sizeEstimate("", "", "");
+    // The triple consists of three variables.
+    return getIndex().getNofTriples();
   } else {
-    return 1000 + _subject.size() + _predicate.size() + _object.size();
+    // Only for test cases. The handling of the objects is to make the
+    // strange query planner tests pass.
+    // TODO<joka921> Code duplication.
+    std::string objectStr =
+        _object.isString() ? _object.getString() : _object.toString();
+    std::string subjectStr =
+        _subject.isString() ? _subject.getString() : _subject.toString();
+    return 1000 + subjectStr.size() + _predicate.size() + objectStr.size();
   }
 }
 

commit 1bc6c823ee1ca196a85c2465e2ce2e9320c9d87b

Quick fix for parsing bools properly again
Fixes a bug introduced by PR #686

src/parser/TurtleParser.cpp
@@ -357,8 +357,8 @@ bool TurtleParser<T>::rdfLiteral() {
         type == XSD_SHORT_TYPE || type == XSD_BYTE_TYPE ||
         type == XSD_NON_NEGATIVE_INTEGER_TYPE ||
         type == XSD_UNSIGNED_LONG_TYPE || type == XSD_UNSIGNED_INT_TYPE ||
-        type == XSD_UNSIGNED_SHORT_TYPE || type == XSD_POSITIVE_INTEGER_TYPE ||
-        type == XSD_BOOLEAN_TYPE) {
+        type == XSD_UNSIGNED_SHORT_TYPE || type == XSD_POSITIVE_INTEGER_TYPE) {
+      // type == XSD_BOOLEAN_TYPE) {
       parseIntegerConstant(strippedLiteral);
     } else if (type == XSD_DECIMAL_TYPE || type == XSD_DOUBLE_TYPE ||
                type == XSD_FLOAT_TYPE) {