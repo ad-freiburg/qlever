3efdd4cc6ebdfcec32cfac2e7f6db73f97ebb531
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2022-08-17 15:28:45 +0000

    Remove the redundant -l option.

--- src/index/Index.h
+++ src/index/Index.h
@@ -341,8 +341,6 @@ class Index {
 
   void setLoadAllPermutations(bool loadAllPermutations);
 
-  void setOnDiskLiterals(bool onDiskLiterals);
-
   void setKeepTempFiles(bool keepTempFiles);
 
   uint64_t& stxxlMemoryInBytes() { return _stxxlMemoryInBytes; }
@@ -506,7 +504,6 @@ class Index {
   TurtleParserIntegerOverflowBehavior _turtleParserIntegerOverflowBehavior =
       TurtleParserIntegerOverflowBehavior::Error;
   bool _turtleParserSkipIllegalLiterals = false;
-  bool _onDiskLiterals = false;
   bool _keepTempFiles = false;
   uint64_t _stxxlMemoryInBytes = DEFAULT_STXXL_MEMORY_IN_BYTES;
   json _configurationJson;
