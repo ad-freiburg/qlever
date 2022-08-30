ac55fb6c5daf90df34d379e2c9e2667c0949db1d
Julian Mundhahs <Qup42@users.noreply.github.com>
2022-08-30 20:11:56 +0200

    Generalize ParseExceptionTest testcases

--- test/ParseExceptionTest.cpp
+++ test/ParseExceptionTest.cpp
@@ -16,35 +16,27 @@ TEST(ParseException, coloredError) {
             "SELECT A \x1B[1m\x1B[4m\x1B[31m?var\x1B[0m WHERE");
 }
 
+void expectParseExceptionWithMetadata(
+    const string& input, const std::optional<ExceptionMetadata>& metadata) {
+  try {
+    SparqlParser(input).parse();
+    FAIL();  // Should be unreachable.
+  } catch (const ParseException& e) {
+    // The constructor has to be bracketed because EXPECT_EQ is a macro.
+    EXPECT_EQ(e.metadata(), metadata);
+  }
+}
+
 TEST(ParseException, MetadataGeneration) {
   // The SparqlLexer changes the input that has already been read into a token
   // when it is outputted with getUnconsumedInput (which is used for ANtLR).
-  {
-    try {
-      SparqlParser("SELECT A ?a WHERE { ?a ?b ?c }").parse();
-    } catch (const ParseException& e) {
-      EXPECT_TRUE(e.metadata().has_value());
-      EXPECT_EQ(e.metadata().value(),
-                (ExceptionMetadata{"select   A ?a WHERE { ?a ?b ?c }", 9, 9}));
-    }
-  }
+  expectParseExceptionWithMetadata(
+      "SELECT A ?a WHERE { ?a ?b ?c }",
+      {{"select   A ?a WHERE { ?a ?b ?c }", 9, 9}});
   // The ANTLR Parser currently doesn't always have the whole query.
-  {
-    try {
-      SparqlParser("SELECT * WHERE { ?a a:b ?b }").parse();
-    } catch (const ParseException& e) {
-      EXPECT_TRUE(e.metadata().has_value());
-      EXPECT_EQ(e.metadata().value(),
-                (ExceptionMetadata{"where  { ?a a:b ?b }", 12, 14}));
-    }
-  }
+  expectParseExceptionWithMetadata("SELECT * WHERE { ?a a:b ?b }",
+                                   {{"where  { ?a a:b ?b }", 12, 14}});
   // "%" doesn't match any valid token. So in this case we will get an Error
   // from the Lexer. There is no ExceptionMetadata available for Lexer Errors.
-  {
-    try {
-      SparqlParser("SELECT * WHERE { % }").parse();
-    } catch (const ParseException& e) {
-      EXPECT_FALSE(e.metadata().has_value());
-    }
-  }
+  expectParseExceptionWithMetadata("SELECT * WHERE { % }", std::nullopt);
 }
