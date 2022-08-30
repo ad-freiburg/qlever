02beaa47ce56861271bd7fd76aebd28e89827172
Julian Mundhahs <Qup42@users.noreply.github.com>
2022-08-30 22:17:05 +0200

    re-add ExceptionMetadata for Lexer Error

--- src/util/antlr/ANTLRErrorHandling.cpp
+++ src/util/antlr/ANTLRErrorHandling.cpp
@@ -2,22 +2,47 @@
 //  Chair of Algorithms and Data Structures.
 //  Author: Julian Mundhahs (mundhahj@informatik.uni-freiburg.de)
 
-#include <util/antlr/ANTLRErrorHandling.h>
+#include "ANTLRErrorHandling.h"
 
-#include "CharStream.h"
-#include "CommonTokenStream.h"
-#include "TokenSource.h"
+#include <Lexer.h>
+#include <Parser.h>
+
+#include "util/Exception.h"
+
+// _____________________________________________________________________________
+ExceptionMetadata generateMetadata(antlr4::Parser* parser,
+                                   antlr4::Token* offendingToken) {
+  return {
+      parser->getTokenStream()->getTokenSource()->getInputStream()->toString(),
+      offendingToken->getStartIndex(), offendingToken->getStopIndex()};
+}
+
+// _____________________________________________________________________________
+ExceptionMetadata generateMetadata(antlr4::Lexer* lexer) {
+  return {lexer->getInputStream()->toString(), lexer->tokenStartCharIndex,
+          lexer->tokenStartCharIndex};
+}
 
 // _____________________________________________________________________________
 ExceptionMetadata generateMetadata(antlr4::Recognizer* recognizer,
                                    antlr4::Token* offendingToken,
                                    [[maybe_unused]] size_t line,
                                    [[maybe_unused]] size_t charPositionInLine) {
-  return {((antlr4::CommonTokenStream*)recognizer->getInputStream())
-              ->getTokenSource()
-              ->getInputStream()
-              ->toString(),
-          offendingToken->getStartIndex(), offendingToken->getStopIndex()};
+  // The `antlr4::Recognizer` interface does not provide enough information to
+  // obtain a copy of the input string. Do this switching here to obtain that.
+  // As of ANTLRv4 `antlr4::Parser` and `antlr4::Lexer` are the only subclasses
+  // of `antlr4::Recognizer`.
+  if (auto* parser = dynamic_cast<antlr4::Parser*>(recognizer)) {
+    AD_CHECK(offendingToken != nullptr);
+    return generateMetadata(parser, offendingToken);
+  } else if (auto* lexer = dynamic_cast<antlr4::Lexer*>(recognizer)) {
+    // If the recognizer is a Lexer this means that the error was a Lexer error.
+    // In that case the tokens are not yet available and `offendingToken` is
+    // NULL.
+    return generateMetadata(lexer);
+  } else {
+    AD_FAIL();  // Should be unreachable.
+  }
 }
 
 // _____________________________________________________________________________
@@ -26,20 +51,23 @@ ExceptionMetadata generateMetadata(antlr4::ParserRuleContext* ctx) {
           ctx->getStart()->getStartIndex(), ctx->getStop()->getStopIndex()};
 }
 
+std::string generateExceptionMessage(antlr4::Token* offendingSymbol,
+                                     size_t line, size_t charPositionInLine,
+                                     const std::string& msg) {
+  return (offendingSymbol)
+             ? absl::StrCat("Token \"", offendingSymbol->getText(),
+                            "\" at line ", line, ":", charPositionInLine, " ",
+                            msg)
+             : absl::StrCat("line ", line, ":", charPositionInLine, " ", msg);
+}
+
 // _____________________________________________________________________________
 void ThrowingErrorListener::syntaxError(antlr4::Recognizer* recognizer,
                                         antlr4::Token* offendingSymbol,
                                         size_t line, size_t charPositionInLine,
                                         const std::string& msg,
                                         [[maybe_unused]] std::exception_ptr e) {
-  if (offendingSymbol) {
-    throw ParseException{
-        absl::StrCat("Token \"", offendingSymbol->getText(), "\" at line ",
-                     line, ":", charPositionInLine, " ", msg),
-        generateMetadata(recognizer, offendingSymbol, line,
-                         charPositionInLine)};
-  } else {
-    throw ParseException{
-        absl::StrCat("line ", line, ":", charPositionInLine, " ", msg)};
-  }
+  throw ParseException{
+      generateExceptionMessage(offendingSymbol, line, charPositionInLine, msg),
+      generateMetadata(recognizer, offendingSymbol, line, charPositionInLine)};
 }
