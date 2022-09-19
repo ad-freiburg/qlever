c48719495cc204c353cfab5663960d857e5bae65
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2022-09-19 16:44:38 +0200

    Completed tests for constant against vector. Added tests for strings (constant-constant and constant-vector).

--- test/RelationalExpressionTest.cpp
+++ test/RelationalExpressionTest.cpp
@@ -2,6 +2,8 @@
 //                  Chair of Algorithms and Data Structures.
 //  Author: Johannes Kalmbach <kalmbacj@cs.uni-freiburg.de>
 
+#include <string>
+
 #include "./SparqlParserTestHelpers.h"
 #include "./util/GTestHelpers.h"
 #include "engine/sparqlExpressions/RelationalExpressions.h"
@@ -9,6 +11,7 @@
 
 using namespace sparqlExpression;
 using ad_utility::source_location;
+using namespace std::literals;
 
 using enum valueIdComparators::Comparison;
 
@@ -94,32 +97,64 @@ TEST(RelationalExpression, DoubleAndDouble) {
                                        {-12.83, -12.83});
 }
 
-auto testNumericAndString = [](auto numeric, std::string s,
+TEST(RelationalExpression, StringAndString) {
+  testNumericConstants<std::string, std::string>(
+      {"alpha", "beta"}, {"sigma", "delta"}, {"epsilon", "epsilon"});
+  // TODO<joka921> These tests only work, when we actually use unicode
+  // comparisons for the string based expressions. TODDO<joka921> Add an example
+  // for strings that are bytewise different but equal on the unicode level
+  // (e.g.`ä` vs `a + dots`.
+  // testNumericConstants<std::string, std::string>({"Alpha", "beta"}, {"beta",
+  // "äpfel"}, {"xxx", "xxx"});
+}
+
+auto testNumericAndString = [](auto numeric, auto s,
                                source_location l = source_location::current()) {
   auto trace = generateLocationTrace(l, "testNumericAndString was called here");
   auto True = expectTrueBoolean;
   auto False = expectFalseBoolean;
 
-  False(makeExpression<LT>(numeric, s));
-  False(makeExpression<LE>(numeric, s));
-  False(makeExpression<EQ>(numeric, s));
-  True(makeExpression<NE>(numeric, s));
-  False(makeExpression<GT>(numeric, s));
-  False(makeExpression<GE>(numeric, s));
+  auto clone = [](const auto& input) {
+    if constexpr (requires { input.clone(); }) {
+      return input.clone();
+    } else {
+      return input;
+    }
+  };
 
-  False(makeExpression<LT>(s, numeric));
-  False(makeExpression<LE>(s, numeric));
-  False(makeExpression<EQ>(s, numeric));
-  True(makeExpression<NE>(s, numeric));
-  False(makeExpression<GT>(s, numeric));
-  False(makeExpression<GE>(s, numeric));
+  False(makeExpression<LT>(clone(numeric), clone(s)));
+  False(makeExpression<LE>(clone(numeric), clone(s)));
+  False(makeExpression<EQ>(clone(numeric), clone(s)));
+  True(makeExpression<NE>(clone(numeric), clone(s)));
+  False(makeExpression<GT>(clone(numeric), clone(s)));
+  False(makeExpression<GE>(clone(numeric), clone(s)));
+
+  False(makeExpression<LT>(clone(s), clone(numeric)));
+  False(makeExpression<LE>(clone(s), clone(numeric)));
+  False(makeExpression<EQ>(clone(s), clone(numeric)));
+  True(makeExpression<NE>(clone(s), clone(numeric)));
+  False(makeExpression<GT>(clone(s), clone(numeric)));
+  False(makeExpression<GE>(clone(s), clone(numeric)));
 };
 
 TEST(RelationalExpression, NumericAndStringAreNeverEqual) {
-  testNumericAndString(3, "hallo");
-  testNumericAndString(3, "3");
-  testNumericAndString(-12.0, "hallo");
-  testNumericAndString(-12.0, "-12.0");
+  ad_utility::AllocatorWithLimit<Id> alloc{
+      ad_utility::makeAllocationMemoryLeftThreadsafeObject(1000)};
+  auto stringVec = VectorWithMemoryLimit<std::string>({"hallo",
+                                                       "by"
+                                                       ""},
+                                                      alloc);
+  auto intVec = VectorWithMemoryLimit<int64_t>({-12365, 0, 12}, alloc);
+  auto doubleVec = VectorWithMemoryLimit<double>({-12.365, 0, 12.1e5}, alloc);
+  testNumericAndString(3, "hallo"s);
+  testNumericAndString(3, "3"s);
+  testNumericAndString(-12.0, "hallo"s);
+  testNumericAndString(-12.0, "-12.0"s);
+  testNumericAndString(intVec.clone(), "someString"s);
+  testNumericAndString(doubleVec.clone(), "someString"s);
+  testNumericAndString(3, stringVec.clone());
+  testNumericAndString(intVec.clone(), stringVec.clone());
+  testNumericAndString(doubleVec.clone(), stringVec.clone());
 }
 
 // The vector must consist of 9 elements: The first three must be less than
@@ -142,25 +177,60 @@ auto testNumericConstantAndVector(
   AD_CHECK(vector.size() == 9);
   context._beginIndex = 0;
   context._endIndex = 9;
-  auto expression = makeExpression<LT>(constant, vector.clone());
-  auto resultAsVariant = expression.evaluate(&context);
-  const auto& result = std::get<VectorWithMemoryLimit<Bool>>(resultAsVariant);
+  auto m = [&]<auto comp>() {
+    auto expression = makeExpression<comp>(constant, vector.clone());
+    auto resultAsVariant = expression.evaluate(&context);
+    auto expressionInverted = makeExpression<comp>(vector.clone(), constant);
+    auto resultAsVariantInverted = expressionInverted.evaluate(&context);
+    auto& result = std::get<VectorWithMemoryLimit<Bool>>(resultAsVariant);
+    auto& resultInverted =
+        std::get<VectorWithMemoryLimit<Bool>>(resultAsVariantInverted);
+    return std::pair{std::move(result), std::move(resultInverted)};
+  };
+  auto [resultLT, invertedLT] = m.template operator()<LT>();
+  auto [resultLE, invertedLE] = m.template operator()<LE>();
+  auto [resultEQ, invertedEQ] = m.template operator()<EQ>();
+  auto [resultNE, invertedNE] = m.template operator()<NE>();
+  auto [resultGE, invertedGE] = m.template operator()<GE>();
+  auto [resultGT, invertedGT] = m.template operator()<GT>();
 
-  EXPECT_EQ(result.size(), 9);
+  EXPECT_EQ(resultLT.size(), 9);
+  EXPECT_EQ(resultLE.size(), 9);
+  EXPECT_EQ(resultEQ.size(), 9);
+  EXPECT_EQ(resultNE.size(), 9);
+  EXPECT_EQ(resultGE.size(), 9);
+  EXPECT_EQ(resultGT.size(), 9);
   for (size_t i = 0; i < 3; ++i) {
-    EXPECT_FALSE(result[i])
-        << "Constant: " << constant << "vector element" << vector[i];
+    EXPECT_FALSE(resultLT[i]);
+    EXPECT_FALSE(resultLE[i]);
+    EXPECT_FALSE(resultEQ[i]);
+    EXPECT_TRUE(resultNE[i]);
+    EXPECT_TRUE(resultGE[i]);
+    EXPECT_TRUE(resultGT[i]);
+
+    EXPECT_TRUE(invertedLT[i]);
+    EXPECT_TRUE(invertedLE[i]);
+    EXPECT_FALSE(invertedEQ[i]);
+    EXPECT_TRUE(invertedNE[i]);
+    EXPECT_FALSE(invertedGE[i]);
+    EXPECT_FALSE(invertedGT[i]);
   }
   for (size_t i = 3; i < 6; ++i) {
-    EXPECT_TRUE(result[i]);
+    EXPECT_TRUE(resultLT[i]);
+    EXPECT_TRUE(resultLE[i]);
+    EXPECT_FALSE(resultEQ[i]);
+    EXPECT_TRUE(resultNE[i]);
+    EXPECT_FALSE(resultGE[i]);
+    EXPECT_FALSE(resultGT[i]);
   }
   for (size_t i = 6; i < 9; ++i) {
-    EXPECT_FALSE(result[i]);
+    EXPECT_FALSE(resultLT[i]);
+    EXPECT_TRUE(resultLE[i]);
+    EXPECT_TRUE(resultEQ[i]);
+    EXPECT_FALSE(resultNE[i]);
+    EXPECT_TRUE(resultGE[i]);
+    EXPECT_FALSE(resultGT[i]);
   }
-
-  // TODO<joka921> Continue here with the other expressions (not only less
-  // than).
-  // TODO<joka921> Also check the inverse (`vector` as lhs of expression).
 }
 
 TEST(RelationalExpression, NumericConstantAndNumericVector) {
@@ -173,6 +243,25 @@ TEST(RelationalExpression, NumericConstantAndNumericVector) {
                                       alloc};
   testNumericConstantAndVector(-2.0, ints.clone());
   testNumericConstantAndVector(int64_t{-2}, ints.clone());
+}
+
+TEST(RelationalExpression, StringConstantsAndStringVector) {
+  ad_utility::AllocatorWithLimit<Id> alloc{
+      ad_utility::makeAllocationMemoryLeftThreadsafeObject(1000)};
+  VectorWithMemoryLimit<std::string> vec(
+      {"alpha", "alpaka", "bertram", "sigma", "zeta", "kaulquappe", "caesar",
+       "caesar", "caesar"},
+      alloc);
+  testNumericConstantAndVector("caesar"s, vec.clone());
+
+  // TODO<joka921> These tests only work, when we actually use unicode
+  // comparisons for the string based expressions. TODDO<joka921> Add an example
+  // for strings that are bytewise different but equal on the unicode level
+  // (e.g.`ä` vs `a + dots`.
+  // VectorWithMemoryLimit<std::string> vec2({"AlpHa", "älpaka", "Æ", "sigma",
+  // "Eta", "kaulQuappe", "Caesar", "Caesar", "Caesare"}, alloc);
+  // testNumericConstants<std::string, std::string>({"Alpha", "beta"}, {"beta",
+  // "äpfel"}, {"xxx", "xxx"});
 }
 
 TEST(SparqlExpression, LessThan) {
