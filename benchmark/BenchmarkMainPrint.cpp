d8d8b17555bc48d80e544dd132f6fcb92b8d9e19
Andre <hiroandre@gmx.de>
2022-12-27 09:16:11 +0100

    Wrote a main function for benchmarks, that prints the time measured.

---
+++ benchmark/BenchmarkMainPrint.cpp
@@ -0,0 +1,25 @@
+// Copyright 2022, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Andre Schlegel (November of 2022, schlegea@informatik.uni-freiburg.de)
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+#include "../benchmark/Benchmark.h"
+
+int main() {
+  BenchmarkRecords benchmarksTime;
+  
+  for (const auto& benchmarkFunction: BenchmarkRegister::getRegisterdBenchmarks()) {
+    benchmarkFunction(&benchmarksTime);
+  }
+  
+  std::stringstream visualization;
+  visualization << "##############\n# Benchmarks #\n##############\n";
+  for (const BenchmarkRecords::RecordEntry& entry: benchmarksTime.getRecords()) {
+    visualization << "\nBenchmark '" << entry.descriptor << "' took " << entry.measuredTime << " usecs.";
+  }
+
+  std::cout << visualization.str() << "\n";
+};
