31efcc7cbf97ec437baaef66195f2313e3a51e15
Andre <hiroandre@gmx.de>
2022-12-29 11:33:10 +0100

    Benchmark: Added a new main function for printing the average execution time.

---
+++ benchmark/MainPrintAverageTime.cpp
@@ -0,0 +1,53 @@
+// Copyright 2022, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Andre Schlegel (November of 2022, schlegea@informatik.uni-freiburg.de)
+
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+
+#include "../benchmark/Benchmark.h"
+#include "../benchmark/util/MainFunctionHelperFunction.h"
+
+/*
+ * @brief Goes through the registerd benchmarks N, here 100, times, and prints
+ *  the average execution time of every benchmark.
+ */
+int main() {
+  const size_t N = 100;
+
+  // The first execution is copied, so that we can easier calculate the
+  // average.
+  std::vector<BenchmarkRecords::RecordEntry>
+    averageExecutionTime(measureTimeForAllBenchmarks());
+
+  for (size_t i = 0; i < N - 1; i++) {
+    // The descriptors and measured times of all the register benchmarks.
+    const std::vector<BenchmarkRecords::RecordEntry> records = measureTimeForAllBenchmarks();
+
+    // The items in measureTimeForAllBenchmarks() always have the same order.
+    // So we can just go by position.
+    for (size_t entryNumber = 0; entryNumber < records.size(); entryNumber++) {
+      // Adding all the execution times of a single benchmark up.
+      averageExecutionTime[entryNumber].measuredTime +=
+        records[entryNumber].measuredTime;
+    }
+  }
+
+  // Calculating the average.
+  for (size_t i = 0; i < averageExecutionTime.size(); i++) {
+    averageExecutionTime[i].measuredTime /= N;
+  }
+ 
+  std::stringstream visualization;
+  visualization << "##############\n# Benchmarks #\n##############\n\n" <<
+    "Number of executions per benchmark: " << N;
+
+  for (const BenchmarkRecords::RecordEntry& entry: averageExecutionTime) {
+    visualization << "\nBenchmark '" << entry.descriptor <<
+      "' has an average execution time of " << entry.measuredTime << " seconds.";
+  }
+
+  std::cout << visualization.str() << "\n\n";
+};
