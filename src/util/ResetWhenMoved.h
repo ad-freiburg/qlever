f16de5db7635eb6079fbfaa8dd989a00c6d16fdb
joka921 <joka921@users.noreply.github.com>
2023-01-11 18:22:43 +0100

    Enable IdTable with arbitrary value type and underlying storage (#850)
    The IdTable class now has two additional template parameters: the value type (so far: Id) and the underlying storage (so far: std::vector<Id, Allocator>, where the Allocator was a template parameter). Add tests, in particular for two possible storage classes: MmapVector and BufferedVector.

---
+++ src/util/ResetWhenMoved.h
@@ -0,0 +1,45 @@
+//  Copyright 2022, University of Freiburg,
+//                  Chair of Algorithms and Data Structures.
+//  Author: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
+
+#pragma once
+#include <utility>
+
+namespace ad_utility {
+// This class stores a single value of type `T` ( and is implicitly convertible
+// to and from `T`. Each time an object of type `ResetWhenMoved<T>` is moved
+// into another `ResetWhenMoved<T>` or into a plain `T`, its value changes back
+// to the `DefaultValue`. This class can be used as a simplification to
+// implement data structures that store raw integers or pointers that have to be
+// reset when the data structure is moved. When using `ResetWhenMoved` for such
+// members, then often the defaulted move constructors and move assignment
+// operators will have the correct semantics. For example usages see
+// `MmapVector.h` and `IdTable.h`.
+template <typename T, T DefaultValue>
+struct ResetWhenMoved {
+  T value_ = DefaultValue;
+  ResetWhenMoved() = default;
+
+  // Implicit conversion to and from `T`.
+  ResetWhenMoved(T t) : value_{std::move(t)} {}
+  operator T() const& { return value_; }
+  operator T&() & { return value_; }
+  // When moving into a `T`, the `value_` is reset to the `DefaultValue`.
+  operator T() && { return std::exchange(value_, DefaultValue); }
+
+  // The copy constructor and copy assignment both just copy the value.
+  ResetWhenMoved(const ResetWhenMoved&) = default;
+  ResetWhenMoved& operator=(const ResetWhenMoved&) = default;
+
+  constexpr static bool isNoexcept = std::is_nothrow_move_constructible_v<T> &&
+                                     std::is_nothrow_move_assignable_v<T>;
+
+  // The move constructor and move assignment operators reset the `value_`
+  ResetWhenMoved(ResetWhenMoved&& other) noexcept(isNoexcept)
+      : value_{std::exchange(other.value_, DefaultValue)} {}
+  ResetWhenMoved& operator=(ResetWhenMoved&& other) noexcept(isNoexcept) {
+    value_ = std::exchange(other.value_, DefaultValue);
+    return *this;
+  }
+};
+}  // namespace ad_utility
--- test/BufferedVectorTest.cpp
+++ test/BufferedVectorTest.cpp
@@ -12,7 +12,7 @@ using ad_utility::BufferedVector;
 
 // ______________________________________________________
 TEST(BufferedVectorTest, Constructor) {
-  BufferedVector<int> b(15, "_testBuf.dat");
+  BufferedVector<int> b(15, "_testBufConstructor.dat");
   ASSERT_EQ(b.threshold(), 15u);
   ASSERT_TRUE(b.isInternal());
   ASSERT_EQ(b.size(), 0u);
@@ -20,7 +20,7 @@ TEST(BufferedVectorTest, Constructor) {
 
 // ______________________________________________________
 TEST(BufferedVectorTest, PushBackSmall) {
-  BufferedVector<int> b(15, "_testBuf.dat");
+  BufferedVector<int> b(15, "_testBufPushBackSmall.dat");
   for (int i = 0; i < 13; ++i) {
     b.push_back(i);
   }
@@ -47,7 +47,7 @@ TEST(BufferedVectorTest, PushBackSmall) {
 
 // ______________________________________________________
 TEST(BufferedVectorTest, PushBackBig) {
-  BufferedVector<int> b(15, "_testBuf.dat");
+  BufferedVector<int> b(15, "_testBufPushBackBig.dat");
   for (int i = 0; i < 25; ++i) {
     b.push_back(i);
   }
@@ -74,7 +74,7 @@ TEST(BufferedVectorTest, PushBackBig) {
 
 // ______________________________________________________
 TEST(BufferedVectorTest, Clear) {
-  BufferedVector<int> b(15, "_testBuf.dat");
+  BufferedVector<int> b(15, "_testBufClear.dat");
   for (int i = 0; i < 25; ++i) {
     b.push_back(i);
   }
@@ -110,5 +110,45 @@ TEST(BufferedVectorTest, Clear) {
     ASSERT_EQ(el++, idx + 42);
     ASSERT_EQ(idx + 1 + 42, b[idx]);
     ++idx;
+  }
+}
+
+TEST(BufferedVectorTest, Resize) {
+  BufferedVector<int> b{5, "_testBufResize.dat"};
+  b.push_back(0);
+  b.push_back(1);
+  b.resize(4);
+  ASSERT_EQ(4, b.size());
+  ASSERT_TRUE(b.isInternal());
+  ASSERT_EQ(0, b[0]);
+  ASSERT_EQ(1, b[1]);
+  b.resize(5);
+  ASSERT_FALSE(b.isInternal());
+  ASSERT_EQ(5, b.size());
+  ASSERT_EQ(0, b[0]);
+  ASSERT_EQ(1, b[1]);
+  b[2] = 2;
+  b[3] = 3;
+  b[4] = 4;
+  b.resize(3000);
+  ASSERT_FALSE(b.isInternal());
+  ASSERT_EQ(3000, b.size());
+  for (size_t i = 0; i < 5; ++i) {
+    ASSERT_EQ(i, b[i]);
+  }
+  b[5] = 5;
+  b[6] = 6;
+  b.resize(7);
+  ASSERT_FALSE(b.isInternal());
+  ASSERT_EQ(7, b.size());
+  for (size_t i = 0; i < 7; ++i) {
+    ASSERT_EQ(i, b[i]);
+  }
+  // Resizing back from the external to the internal vector
+  b.resize(4);
+  ASSERT_TRUE(b.isInternal());
+  ASSERT_EQ(4, b.size());
+  for (size_t i = 0; i < 4; ++i) {
+    ASSERT_EQ(i, b[i]);
   }
 }
