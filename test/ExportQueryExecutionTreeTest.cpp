6921cdfb6f70d870d862717050130644251abb6d
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2023-01-11 10:28:08 +0100

    Started writing an infrastructure for *Reasonable* unit tests.

--- test/ExportQueryExecutionTreeTest.cpp
+++ test/ExportQueryExecutionTreeTest.cpp
@@ -4,22 +4,60 @@
 
 #include <gtest/gtest.h>
 
-#include "engine/ExportQueryExecutionTrees.h"
 #include "./IndexTestHelpers.h"
+#include "engine/ExportQueryExecutionTrees.h"
 #include "engine/IndexScan.h"
 #include "engine/QueryPlanner.h"
 #include "parser/SparqlParser.h"
 
-
-TEST(ExportQueryExecutionTree, SelectQueryTSV) {
-  auto qec = ad_utility::testing::getQec();
+std::string runTSVQuery(const std::string& kg, const std::string& query) {
+  auto qec = ad_utility::testing::getQec(kg);
   QueryPlanner qp{qec};
-  auto pq = SparqlParser::parseQuery("SELECT ?s ?p ?o WHERE {?s ?p ?o } ORDER BY ?s ?p ?o");
+  auto pq = SparqlParser::parseQuery(query);
   auto qet = qp.createExecutionTree(pq);
-  auto tsvGenerator = ExportQueryExecutionTrees::queryToStreamableGenerator(pq, qet, ad_utility::MediaType::tsv);
+  auto tsvGenerator = ExportQueryExecutionTrees::queryToStreamableGenerator(
+      pq, qet, ad_utility::MediaType::tsv);
   std::string result;
   for (const auto& block : tsvGenerator) {
     result += block;
   }
-  ASSERT_EQ(result, "SomeResult");
+  return result;
+}
+
+// TODO<joka921> This is a lot of duplication from the TSV function
+std::string runCSVQuery(const std::string& kg, const std::string& query) {
+  auto qec = ad_utility::testing::getQec(kg);
+  QueryPlanner qp{qec};
+  auto pq = SparqlParser::parseQuery(query);
+  auto qet = qp.createExecutionTree(pq);
+  auto generator = ExportQueryExecutionTrees::queryToStreamableGenerator(
+      pq, qet, ad_utility::MediaType::csv);
+  std::string result;
+  for (const auto& block : generator) {
+    result += block;
+  }
+  return result;
+}
+
+std::string kgInt =
+    "<s> <p> 42 . <s> <p> -42019234865781 . <s> <p> 4012934858173560";
+std::string objectQuery = "SELECT ?o WHERE {?s ?p ?o} ORDER BY ?o";
+
+struct ExportTestCase {
+  std::string kg;
+  std::string query;
+  std::string resultTsv;
+  std::string resultCsv;
+  // TODO<joka921> Add the otherFormats.
+};
+
+ExportTestCase testCaseInt{kgInt, objectQuery,
+                           "?o\n42\n4012934858173560\n-42019234865781\n",
+                           "?o\n42\n4012934858173560\n-42019234865781\n"};
+
+TEST(ExportQueryExecutionTree, SelectQueryTSV) {
+  ASSERT_EQ(runTSVQuery(testCaseInt.kg, testCaseInt.query),
+            testCaseInt.resultTsv);
+  ASSERT_EQ(runCSVQuery(testCaseInt.kg, testCaseInt.query),
+            testCaseInt.resultCsv);
 }
--- test/IndexTestHelpers.h
+++ test/IndexTestHelpers.h
@@ -23,23 +23,30 @@ inline Index makeIndexWithTestSettings() {
   return index;
 }
 
-// Create an `Index`, the vocabulary of which contains the literals `"alpha",
+// Create an `Index` from the given `turtleInput`. If the `turtleInput` is not
+// specified, a default input will be used and the resulting index will have the
+// following properties: Its vocabulary contains the literals `"alpha",
 // "älpha", "A", "Beta"`. These vocabulary entries are expected by the tests
 // for the subclasses of `SparqlExpression`.
 // The concrete triple contents are currently used in `GroupByTest.cpp`.
-inline Index makeTestIndex() {
+inline Index makeTestIndex(std::string turtleInput = "") {
   // Ignore the (irrelevant) log output of the index building and loading during
   // these tests.
   static std::ostringstream ignoreLogStream;
   ad_utility::setGlobalLoggingStream(&ignoreLogStream);
   std::string filename = "relationalExpressionTestIndex.ttl";
-  std::string dummyKb =
-      "<x> <label> \"alpha\" . <x> <label> \"älpha\" . <x> <label> \"A\" . <x> "
-      "<label> \"Beta\". <x> <is-a> <y>. <y> <is-a> <x>. <z> <label> \"zz\"@en";
+  if (turtleInput.empty()) {
+    turtleInput =
+
+        "<x> <label> \"alpha\" . <x> <label> \"älpha\" . <x> <label> \"A\" . "
+        "<x> "
+        "<label> \"Beta\". <x> <is-a> <y>. <y> <is-a> <x>. <z> <label> "
+        "\"zz\"@en";
+  }
 
   FILE_BUFFER_SIZE() = 1000;
   std::fstream f(filename, std::ios_base::out);
-  f << dummyKb;
+  f << turtleInput;
   f.close();
   std::string indexBasename = "_staticGlobalTestIndex";
   {
@@ -55,21 +62,29 @@ inline Index makeTestIndex() {
 // Return a static  `QueryExecutionContext` that refers to an index that was
 // build using `makeTestIndex()` (see above). The index (most notably its
 // vocabulary) is the only part of the `QueryExecutionContext` that is actually
-// relevant for these tests, so the other members are defaultet.
-static inline QueryExecutionContext* getQec() {
+// relevant for these tests, so the other members are defaulted.
+static inline QueryExecutionContext* getQec(std::string turtleInput = "") {
   static ad_utility::AllocatorWithLimit<Id> alloc{
       ad_utility::makeAllocationMemoryLeftThreadsafeObject(100'000)};
-  static const Index index = makeTestIndex();
+  static ad_utility::HashMap<std::string, Index> turtleToIndexMap;
+  static ad_utility::HashMap<std::string, QueryExecutionContext>
+      turtleToContextMap;
   static const Engine engine{};
   static QueryResultCache cache{};
-  static QueryExecutionContext qec{
-      index,
-      engine,
-      &cache,
-      ad_utility::AllocatorWithLimit<Id>{
-          ad_utility::makeAllocationMemoryLeftThreadsafeObject(100'000)},
-      {}};
-  return &qec;
+  if (!turtleToIndexMap.contains(turtleInput)) {
+    AD_CHECK(!turtleToContextMap.contains(turtleInput));
+    turtleToIndexMap.emplace(turtleInput, makeTestIndex(turtleInput));
+    turtleToContextMap.emplace(
+        turtleInput,
+        QueryExecutionContext{
+            turtleToIndexMap.at(turtleInput),
+            engine,
+            &cache,
+            ad_utility::AllocatorWithLimit<Id>{
+                ad_utility::makeAllocationMemoryLeftThreadsafeObject(100'000)},
+            {}});
+  }
+  return &turtleToContextMap.at(turtleInput);
 }
 
 // Create an unlimited allocator.
