de6c9c9ff5efec6be7b372799cc0551973a65d70
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2023-01-16 12:29:35 +0100

    It seems to work with GCC-11. Compatibility between lcov and gcc versions has to be checked...

--- toolchains/gcc12-coverage.cmake
+++ toolchains/gcc12-coverage.cmake
@@ -1,6 +1,6 @@
 # Toolchain that uses G++ 12
 set(CMAKE_C_COMPILER gcc-12)
-set(CMAKE_CXX_COMPILER g++-12)
+set(CMAKE_CXX_COMPILER g++-11)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Og -g --coverage -fkeep-inline-functions -fkeep-static-functions")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --coverage")
 set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --coverage")
