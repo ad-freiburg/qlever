e3afbe6c8fdc4ace802d10d8bfeaed63e3f338c2
Andre <hiroandre@gmx.de>
2023-01-17 11:35:27 +0100

    Wrote a basic benchmark, that uses all the features of the infrastructure.

---
+++ benchmark/BasicBenchmark.cpp
@@ -0,0 +1,85 @@
+// Copyright 2023, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Andre Schlegel (January of 2023, schlegea@informatik.uni-freiburg.de)
+#include "../benchmark/Benchmark.h"
+
+// Single Measurments
+void BM_SingeMeasurments(BenchmarkRecords* records) {
+  // Setup.
+  const size_t number = 10;
+  auto exponentiate = [](const size_t number){
+    return number*number;
+  };
+
+  // Measuring functions.
+  records->addSingleMeasurment("Exponentiate once", [&](){exponentiate(number);});
+  records->addSingleMeasurment("Recursivly exponentiate ten times", [&](){
+        size_t to_exponentiate = number;
+        for (int i = 0; i < 10; i++) {
+          to_exponentiate = exponentiate(to_exponentiate);
+        }
+      });
+}
+
+// Groups
+void BM_Groups(BenchmarkRecords* records) {
+  // Setup.
+  auto loopAdd = [](const size_t a, const size_t b) {
+    size_t to_return = a;
+    for (size_t i = 0; i < b; i++) {
+      to_return += 1;
+    }
+    return to_return;
+  };
+
+  auto loopMultiply = [](const size_t a, const size_t b) {
+    size_t to_return = a;
+    for (size_t i = 0; i < b; i++) {
+      to_return += a;
+    }
+    return to_return;
+  };
+
+  // Measuring functions.
+  records->addGroup("loopAdd");
+  records->addGroup("loopMultiply");
+
+  records->addToExistingGroup("loopAdd", "1+1", [&](){loopAdd(1,1);});
+  records->addToExistingGroup("loopAdd", "42+69", [&](){loopAdd(42,69);});
+  records->addToExistingGroup("loopAdd", "10775+24502",
+      [&](){loopAdd(10775, 24502);});
+
+  records->addToExistingGroup("loopMultiply", "1*1", [&](){loopMultiply(1,1);});
+  records->addToExistingGroup("loopMultiply", "42*69",
+      [&](){loopMultiply(42,69);});
+  records->addToExistingGroup("loopMultiply", "10775*24502",
+      [&](){loopMultiply(10775, 24502);});
+}
+
+// Tables
+void BM_Tables(BenchmarkRecords* records) {
+  // Setup.
+  auto exponentiateNTimes = [](const size_t number, const size_t n){
+    size_t to_return = 1;
+    for (size_t i = 0; i < n; i++) {
+      to_return *= number;
+    }
+    return to_return;
+  };
+
+  // Measuring functions.
+  records->addTable("Exponents with basis 2", {"2"}, {"0", "1", "2", "3", "4"});
+  records->addTable("Exponents with basis 3", {"3"}, {"0", "1", "2", "3", "4"});
+
+  for (int i = 0; i < 5; i++) {
+    records->addToExistingTable("Exponents with basis 2", 0, i,
+        [&](){exponentiateNTimes(2,i);});
+  }
+
+  for (int i = 0; i < 5; i++) {
+    records->addToExistingTable("Exponents with basis 3", 0, i,
+        [&](){exponentiateNTimes(3,i);});
+  }
+}
+
+BenchmarkRegister temp{{BM_SingeMeasurments, BM_Groups, BM_Tables}};
