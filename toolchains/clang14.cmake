2a0b617901fb4d53bded0390a6056c7e59005574
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2023-01-17 14:14:58 +0100

    Trying to make it work with llvm-cov

--- toolchains/clang14.cmake
+++ toolchains/clang14.cmake
@@ -2,3 +2,6 @@
 set(CMAKE_C_COMPILER clang-14)
 set(CMAKE_CXX_COMPILER clang++-14)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address -fno-omit-frame-pointer")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Og -g -fprofile-instr-generate -fcoverage-mapping")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
--- toolchains/gcc12-coverage.cmake
+++ toolchains/gcc12-coverage.cmake
@@ -1,9 +1,7 @@
 # Toolchain that uses G++ 12
 set(CMAKE_C_COMPILER gcc-12)
-set(CMAKE_CXX_COMPILER clang++-15)
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Og -g -fprofile-instr-generate -fcoverage-mapping")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
-set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
-set(PROFDATA_TOOL "llvm-profdata-15")
-set(PROFGEN_TOOL "llvm-profgen-15")
+set(CMAKE_CXX_COMPILER g++-12)
+#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Og -g -fprofile-instr-generate -fcoverage-mapping")
+#set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
+#set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fprofile-instr-generate -fcoverage-mapping")
 
