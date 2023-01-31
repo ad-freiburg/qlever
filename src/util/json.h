f5d06440246190bc2a22143d387b813ee22a2b00
Andre <hiroandre@gmx.de>
2023-01-31 16:49:34 +0100

    Added json support for std::optional<T>.

--- src/util/json.h
+++ src/util/json.h
@@ -2,7 +2,8 @@
 //  Chair of Algorithms and Data Structures.
 //  Author: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
 
-// Convenience header for Nlohmann::Json that sets the default options.
+// Convenience header for Nlohmann::Json that sets the default options. Also
+// adds support for std::optional.
 
 #ifndef QLEVER_JSON_H
 #define QLEVER_JSON_H
@@ -11,5 +12,26 @@
 // most notably to std::string.
 #define JSON_USE_IMPLICIT_CONVERSIONS 0
 #include <nlohmann/json.hpp>
+#include <optional>
+
+// Adding third party type support.
+namespace nlohmann {
+    template <typename T>
+    struct adl_serializer<std::optional<T>> {
+        static void to_json(nlohmann::json& j, const std::optional<T>& opt) {
+          j = nlohmann::json{{"has_value", opt.has_value()}};
+          // Only add, if there is value.
+          if (opt.has_value()) {j["value"] = opt.value();}
+        }
+
+        static void from_json(const nlohmann::json& j, std::optional<T>& opt) {
+          if (j.is_null() || !j["has_value"].get<bool>()){
+            opt = std::nullopt;
+          } else {
+            opt = j["value"].get<T>();
+          }
+        }
+    };
+}
 
 #endif  // QLEVER_JSON_H
