bf339f0df3d0f00ec39cd7f1efffd3acbd75d97f
Hannah Bast <bast@informatik.uni-freiburg.de>
2023-02-07 09:34:51 +0100

    Update author info in `RandomExpression.h`

--- src/engine/sparqlExpressions/RandomExpression.h
+++ src/engine/sparqlExpressions/RandomExpression.h
@@ -1,6 +1,7 @@
-//  Copyright 2023, University of Freiburg,
-//                  Chair of Algorithms and Data Structures.
-//  Author: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
+//  Copyright 2023, University of Freiburg
+//  Chair of Algorithms and Data Structures
+//  Authors: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
+//           Hannah Bast <bast@cs.uni-freiburg.de>
 
 #pragma once
 
