70196692c25671279c4bacfbd407aaab4600e015
Andre <hiroandre@gmx.de>
2023-03-03 17:44:37 +0100

    benchmark: Second draft of RecordTables, where you can manually set entries. It seems to work now.

--- src/util/json.h
+++ src/util/json.h
@@ -16,6 +16,7 @@
 #include <optional>
 #include <variant>
 #include <utility>
+#include <concepts>
 
 // Added support for serializing `std::optional` using `nlohmann::json`.
 namespace nlohmann {
@@ -61,36 +62,85 @@ namespace nlohmann {
     };
 }
 
+/*
+ * @brief A compile time for loop, which passes the loop index to the
+ *  given loop body.
+ *
+ * @tparam Function The loop body should be a templated function, with one
+ *  size_t template argument and no more. It also shouldn't take any function
+ *  arguments. Should be passed per deduction.
+ * @tparam ForLoopIndexes The indexes, that the for loop goes over. Should be
+ *  passed per deduction.
+ *
+ * @param loopBody The body of the for loop.
+ */
+template <typename Function, size_t... ForLoopIndexes>
+void ConstExprForLoop(const std::index_sequence<ForLoopIndexes...>&,
+    const Function& loopBody){
+  ((loopBody.template operator()<ForLoopIndexes>()), ...);
+}
+
+/*
+ * @brief 'Converts' a run time value of size_t to a compile time value, as
+ *  long as the maximal value of this value is known at compile time, and
+ *  passes it to a given function. However, it doesn't really convert anything,
+ *  but simply creates all templated version in `[0, MaxValue]` and only
+ *  executes the one, where the template parameter and given run time value are
+ *  the same.
+ *
+ * @tparam MaxValue The maximal value, that the function parameter value could
+ *  take.
+ * @tparam Function The given function be a templated function, with one
+ *  size_t template argument and no more. It also shouldn't take any function
+ *  arguments. This parameter should be passed per deduction.
+ *
+ * @param value Value that you need as a compile time value.
+ * @param functionBody The templated function, which you wish to execute.
+ */
+template <size_t MaxValue, typename Function>
+void RuntimeValueToCompileTimeValue(const size_t& value, const Function& functionBody){
+  ConstExprForLoop(std::make_index_sequence<MaxValue>{}, [&functionBody, &value]
+      <size_t Index>(){
+        if (Index == value) {functionBody.template operator()<Index>();}
+      }
+  );
+}
+
 // Added support for serializing `std::variant` using `nlohmann::json`.
 namespace nlohmann {
     template <typename... Types>
-    struct adl_serializer<std::optional<Types...>> {
+    struct adl_serializer<std::variant<Types...>> {
         static void to_json(nlohmann::json& j,
             const std::variant<Types...>& var) {
           // We need to save, which of the types the std::variant actually
           // uses.
-          j["index"] = var.index();
-          j["value"] = std::get<var.index()>(var);
+          size_t index = var.index();
+          j["index"] = index;
+
+          // 'Translate' the runtime variable index to a compile time value,
+          // so that we can get the currently used value. This is needed,
+          // because `var` is only known at compile time. And by extension, so
+          // is `var.index()`.
+          RuntimeValueToCompileTimeValue<sizeof...(Types)>(index, [&j, &var]
+              <size_t Index>(){
+                j["value"] = std::get<Index>(var);
+              }
+          );
         }
 
         static void from_json(const nlohmann::json& j,
             std::variant<Types...>& var) {
+          // Which of the `sizeof...(Types)` possible value types, was the
+          // serialized std::variant using?
           size_t index = j["index"].get<size_t>();
 
-          auto setValueBasedOnJson = [&j, &var]<size_t Index>(const size_t& i){
-            if (Index == i) {
-              var = j["value"].get<std::variant_alternative_t<Index,
+          // Interpreting the value based on its type.
+          RuntimeValueToCompileTimeValue<sizeof...(Types)>(index, [&j, &var]
+              <size_t Index>(){
+                var = j["value"].get<std::variant_alternative_t<Index,
                   std::variant<Types...>>>();
-            }
-          };
-
-          auto tryAllValues = [&j, &var,
-               &setValueBasedOnJson]<typename T, T... ints>(const size_t& i,
-                   const std::integer_sequence<T, ints...>&){
-            ((setValueBasedOnJson<ints>(i)), ...);
-          };
-
-          tryAllValues(index, std::index_sequence_for<Types...>{});
+              }
+          );
         }
     };
 }
