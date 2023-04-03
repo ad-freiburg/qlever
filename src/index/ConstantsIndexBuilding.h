e719c849ef67e0b8a9d55d6a9a0e58c8596dc460
Hannah Bast <bast@informatik.uni-freiburg.de>
2023-04-03 21:03:06 +0200

    Fix comment

--- src/index/ConstantsIndexBuilding.h
+++ src/index/ConstantsIndexBuilding.h
@@ -86,5 +86,5 @@ constexpr size_t QUEUE_SIZE_AFTER_PARALLEL_PARSING = 10;
 // triples). With 100K, the total space for all the `CompressedBlockMetadata` is
 // still small compared to the rest of the index. However, with 100K, and single
 // block is just 10K compresse, which might result in sub-optimal IO-efficiency
-// when reading many blocks. We take 1M as a compromise.
+// when reading many blocks. We take 500K as a compromise.
 constexpr size_t BLOCKSIZE_COMPRESSED_METADATA = 500'000;
