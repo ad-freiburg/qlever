e40454e300152d19bd48231c444b031c7912307c
Robin TF <83676088+RobinTF@users.noreply.github.com>
2023-04-21 14:41:55 +0200

    Fix macro error

--- src/util/http/beast.h
+++ src/util/http/beast.h
@@ -27,14 +27,17 @@
 // Don't set header for boost beast 1.81 and forward, because it is noop there.
 #if defined BOOST_BEAST_VERSION && BOOST_BEAST_VERSION < 345
 #define BOOST_BEAST_USE_STD_STRING_VIEW
-using beast_string_view = std::string_view;
-#else
-using beast_string_view = boost::core::string_view;
 #endif
 
 #include <boost/asio.hpp>
 #include <boost/asio/ssl/stream.hpp>
 #include <boost/beast.hpp>
+
+#if defined BOOST_BEAST_VERSION && BOOST_BEAST_VERSION < 345
+using beast_string_view = std::string_view;
+#else
+using beast_string_view = boost::core::string_view;
+#endif
 
 // For boost versions prior to 1.81 this should be no-op
 inline std::string_view toStd(beast_string_view view) { return view; }
