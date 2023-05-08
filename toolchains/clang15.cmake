c989c36c7ebb8cf96425631026bbbd53cc577e22
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2023-05-08 18:42:36 +0200

    Debugging the strange issues in Clang 14 and 15.

--- toolchains/clang15.cmake
+++ toolchains/clang15.cmake
@@ -1,4 +1,3 @@
 # CMake toolchain file for using `clang++-15`
 set(CMAKE_C_COMPILER clang-15)
 set(CMAKE_CXX_COMPILER clang++-15)
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -lc++abi")
