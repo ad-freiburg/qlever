ac526d19c4a6129d6c8d3f1421abd8a72edfa554
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2023-05-11 10:39:22 +0200

    Allow for templating of the `IndexType` of a cvocabulary.

--- src/index/Index.h
+++ src/index/Index.h
@@ -100,11 +100,15 @@ class Index {
   // Read necessary metadata into memory and open file handles.
   void addTextFromOnDiskIndex();
 
-  using Vocab = Vocabulary<CompressedString, TripleComponentComparator>;
+  using EntityVocabIndex = VocabIndex;
+  using Vocab =
+      Vocabulary<CompressedString, TripleComponentComparator, EntityVocabIndex>;
   [[nodiscard]] const Vocab& getVocab() const;
   Vocab& getNonConstVocabForTesting();
 
-  using TextVocab = Vocabulary<std::string, SimpleStringComparator>;
+  using WordVocabIndex = VocabIndex;
+  using TextVocab =
+      Vocabulary<std::string, SimpleStringComparator, WordVocabIndex>;
   [[nodiscard]] const TextVocab& getTextVocab() const;
 
   // --------------------------------------------------------------------------
