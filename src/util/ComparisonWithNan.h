6697bce1f38daf7e062e5b0173e252d093df0e41
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2023-05-23 13:27:03 +0200

    Added many unit tests and fixed the behavior to something remotely consistent.

--- src/util/ComparisonWithNan.h
+++ src/util/ComparisonWithNan.h
@@ -7,27 +7,59 @@
 #include <cmath>
 #include <type_traits>
 
+#include "util/TypeTraits.h"
+
 namespace ad_utility {
+namespace detail {
+template <typename T>
+static constexpr bool isTransparentComparator = isTypeContainedIn<
+    T, std::tuple<std::less<>, std::less_equal<>, std::equal_to<>,
+                  std::not_equal_to<>, std::greater<>, std::greater_equal<>>>;
+}
+
+// Convert one of the transparent comparators from the standard library
+// (`std::less<>, std::equal_to<>, etc.`) into a comparator that imposes a
+// partial ordering on elements even for not a number (NaN) values. These
+// comparators can be for example safely used in combination with `std::sort`,
+// which has undefined behavior for the "normal" comparisons as soon as the
+// input contains NaN values. The semantics are that of the `comparator` with
+// the following changes:
+// 1. NaN values are always at the end of the ordering, so  `3 < Nan` and
+//    `!(Nan < 3)`, but also `3 > Nan` and `!(Nan > 3)`.
+//    Note that for this reason `<` is NOT the exact opposite of `>=` because of
+//    the position of the `NaN` values, but
+// 2. Nan == Nan (other than the default comparison where Nan != Nan).
+// For detailed examples see the corresponding tests which contain all relevant
+// corner cases.
 template <typename Comparator>
-inline auto makeComparatorForNans(Comparator comparator) {
+inline auto makeComparatorForNans(Comparator comparator)
+    requires detail::isTransparentComparator<Comparator> {
   return [comparator]<typename A, typename B>(const A& a, const B& b)
       requires std::is_invocable_r_v<bool, Comparator, A, B> {
+    static constexpr bool isNotEqual =
+        ad_utility::isSimilar<Comparator, std::not_equal_to<>>;
+    static constexpr bool isEqual =
+        ad_utility::isSimilar<Comparator, std::equal_to<>>;
     if constexpr (std::is_floating_point_v<A> && std::is_floating_point_v<B>) {
       bool aNan = std::isnan(a);
       bool bNan = std::isnan(b);
       if (aNan && bNan) {
-        return false;
+        return comparator(0.0, 0.0);
       } else if (aNan) {
-        return false;
+        return isNotEqual;
       } else if (bNan) {
-        return true;
+        return !isEqual;
       } else {
         return comparator(a, b);
       }
     } else if constexpr (std::is_floating_point_v<A>) {
-      return !std::isnan(a) && comparator(a, b);
+      if constexpr (isNotEqual) {
+        return std::isnan(a) || comparator(a, b);
+      } else {
+        return !std::isnan(a) && comparator(a, b);
+      }
     } else if constexpr (std::is_floating_point_v<B>) {
-      return std::isnan(b) || comparator(a, b);
+      return (std::isnan(b) && !isEqual) || comparator(a, b);
     } else {
       return comparator(a, b);
     }
--- test/ComparisonWithNanTest.cpp
+++ test/ComparisonWithNanTest.cpp
@@ -10,6 +10,7 @@
 
 static constexpr double NaN = std::numeric_limits<double>::quiet_NaN();
 
+// ___________________________________________________________
 TEST(ComparisonWithNan, NoFloatingPoint) {
   auto comp = ad_utility::makeComparatorForNans(std::less{});
   ASSERT_TRUE(comp(3, 4));
@@ -18,6 +19,7 @@ TEST(ComparisonWithNan, NoFloatingPoint) {
   ASSERT_FALSE(comp(3, -2));
 }
 
+// ___________________________________________________________
 TEST(ComparisonWithNan, FirstFloatingPoint) {
   auto comp = ad_utility::makeComparatorForNans(std::less{});
   ASSERT_TRUE(comp(3.0, 4));
@@ -30,4 +32,81 @@ TEST(ComparisonWithNan, FirstFloatingPoint) {
 
   auto compEq = ad_utility::makeComparatorForNans(std::equal_to{});
   auto compNe = ad_utility::makeComparatorForNans(std::not_equal_to{});
+
+  ASSERT_TRUE(compEq(3.0, 3));
+  ASSERT_FALSE(compEq(3.0, 5));
+  ASSERT_FALSE(compEq(NaN, 5));
+
+  ASSERT_FALSE(compNe(3.0, 3));
+  ASSERT_TRUE(compNe(3.0, 5));
+
+  ASSERT_TRUE(compNe(NaN, 5));
+
+  auto compLe = ad_utility::makeComparatorForNans(std::less_equal{});
+  ASSERT_TRUE(compLe(3.0, 3));
+  ASSERT_TRUE(compLe(3.0, 4));
+  ASSERT_FALSE(compLe(NaN, 4));
+}
+
+// ___________________________________________________________
+TEST(ComparisonWithNan, SecondFloatingPoint) {
+  auto comp = ad_utility::makeComparatorForNans(std::less{});
+  ASSERT_TRUE(comp(3, 4.0));
+  ASSERT_TRUE(comp(3, 15.2));
+  ASSERT_FALSE(comp(3, 3.0));
+  ASSERT_FALSE(comp(3, -2.3));
+
+  ASSERT_TRUE(comp(6, NaN));
+  ASSERT_TRUE(comp(-7432, NaN));
+
+  auto compEq = ad_utility::makeComparatorForNans(std::equal_to{});
+  auto compNe = ad_utility::makeComparatorForNans(std::not_equal_to{});
+
+  ASSERT_TRUE(compEq(3, 3.0));
+  ASSERT_FALSE(compEq(3, 5.2));
+  ASSERT_FALSE(compEq(5, NaN));
+
+  ASSERT_FALSE(compNe(3, 3.0));
+  ASSERT_TRUE(compNe(3, 5.2));
+
+  ASSERT_TRUE(compNe(5, NaN));
+
+  auto compLe = ad_utility::makeComparatorForNans(std::less_equal{});
+  ASSERT_TRUE(compLe(3, 3.0));
+  ASSERT_TRUE(compLe(3, 3.1));
+  ASSERT_TRUE(compLe(3, NaN));
+}
+
+// ___________________________________________________________
+TEST(ComparisonWithNan, BothFloatingPoint) {
+  auto comp = ad_utility::makeComparatorForNans(std::less{});
+  ASSERT_TRUE(comp(3.0, 4.0));
+  ASSERT_TRUE(comp(3.8, 15.2));
+  ASSERT_FALSE(comp(3.0, 3.0));
+  ASSERT_FALSE(comp(3.3, -2.3));
+
+  ASSERT_TRUE(comp(6.2, NaN));
+  ASSERT_TRUE(comp(-7432.5, NaN));
+  ASSERT_FALSE(comp(NaN, 6.2));
+  ASSERT_FALSE(comp(NaN, -7432.5));
+
+  auto compEq = ad_utility::makeComparatorForNans(std::equal_to{});
+  auto compNe = ad_utility::makeComparatorForNans(std::not_equal_to{});
+
+  ASSERT_TRUE(compEq(3.0, 3.0));
+  ASSERT_FALSE(compEq(3.0, 5.2));
+  ASSERT_FALSE(compEq(5.0, NaN));
+  ASSERT_TRUE(compEq(NaN, NaN));
+
+  ASSERT_FALSE(compNe(3.0, 3.0));
+  ASSERT_TRUE(compNe(3.4, 5.2));
+
+  ASSERT_TRUE(compNe(5.0, NaN));
+  ASSERT_FALSE(compNe(NaN, NaN));
+
+  auto compLe = ad_utility::makeComparatorForNans(std::less_equal{});
+  ASSERT_TRUE(compLe(3, 3.0));
+  ASSERT_TRUE(compLe(3, 3.1));
+  ASSERT_TRUE(compLe(3, NaN));
+  ASSERT_TRUE(compLe(NaN, NaN));
 }
