f72c972eac055680b7b90b4e0cdbd77259266610
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2023-07-06 13:07:15 +0200

    Started to write tests for the index scan class.

--- test/engine/IndexScanTest.cpp
+++ test/engine/IndexScanTest.cpp
@@ -10,9 +10,32 @@
 
 using namespace ad_utility::testing;
 
-std::string kg = "<a> <p> <A>. <b> <p> <B>. <a> <x> <xa>. <b> <x> <xb>.";
+using IndexPair = std::pair<size_t, size_t>;
+void testLazyScan(auto lazyScan, IndexScan& scanOp,
+                  const std::vector<IndexPair>& expectedRows) {
+  auto alloc = ad_utility::makeUnlimitedAllocator<Id>();
+  IdTable lazyScanRes{2, alloc};
+  size_t numBlocks = 0;
+  for (const auto& block : lazyScan) {
+    lazyScanRes.insertAtEnd(block.begin(), block.end());
+    ++numBlocks;
+  }
 
-TEST(IndexScan, lazyScanForJoinOfTwoScans) {
+  auto resFullScan = scanOp.getResult()->idTable().clone();
+  IdTable expected{resFullScan.numColumns(), alloc};
+
+  for (auto [lower, upper] : expectedRows) {
+    for (auto index : std::views::iota(lower, upper)) {
+      expected.push_back(resFullScan.at(index));
+    }
+  }
+
+  EXPECT_EQ(lazyScanRes, expected);
+}
+
+void testLazyScanForJoinOfTwoScans(const std::string& kg,
+                                   const std::vector<IndexPair>& leftRows,
+                                   const std::vector<IndexPair>& rightRows) {
   using Tc = TripleComponent;
   using Var = Variable;
   auto qec = getQec(kg);
@@ -22,20 +45,32 @@ TEST(IndexScan, lazyScanForJoinOfTwoScans) {
                SparqlTriple{Tc{Var{"?x"}}, "<x>", Tc{Var{"?z"}}}};
   auto [scan1, scan2] = (IndexScan::lazyScanForJoinOfTwoScans(s1, s2));
 
-  IdTable res1{2, ad_utility::makeUnlimitedAllocator<Id>()};
-  IdTable res2{2, ad_utility::makeUnlimitedAllocator<Id>()};
+  testLazyScan(std::move(scan1), s1, leftRows);
+  testLazyScan(std::move(scan2), s2, rightRows);
+}
 
-  for (const auto& block : scan1) {
-    res1.insertAtEnd(block.begin(), block.end());
+TEST(IndexScan, lazyScanForJoinOfTwoScans) {
+  {
+    // In the tests we have a blocksize of two triples per block, and a new
+    // block is started for a new relation. That explains the spacing of the
+    // following example knowledge graphs.
+    std::string kg =
+        "<a> <p> <A>. <a> <p> <A2>. "
+        "<a> <p> <A3> . <b> <p> <B>. "
+        "<b> <p> <B2> ."
+        "<b> <x> <xb>. <b> <x> <xb2> .";
+
+    // When joining the <p> and <x> relations, we only need to read the last two
+    // blocks of the <p> relation, as <a> never appears as a subject in <x>.
+    // This means that the lazy partial scan can skip the first two triples.
+    testLazyScanForJoinOfTwoScans(kg, {{2, 5}}, {{0, 2}});
   }
-  for (const auto& block : scan2) {
-    res2.insertAtEnd(block.begin(), block.end());
+  {
+    // No triple for relation <p>, so both scans can be empty.
+    std::string kg =
+        "<a> <p2> <A>. <a> <p2> <A2>. "
+        "<a> <p2> <A3> . <b> <p2> <B>. "
+        "<b> <x> <xb>. <b> <x> <xb2> .";
+    testLazyScanForJoinOfTwoScans(kg, {}, {});
   }
-
-  EXPECT_EQ(res1.size(), 2u);
-  EXPECT_EQ(res2.size(), 2u);
-
-  // TODO<joka921> We need additional tests that are not only dummys. To make
-  // this work, we need to have the blocksize of the index class configurable, I
-  // will split this into a separate PR.
 }
