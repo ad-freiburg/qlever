2e11a476ee5d1238e71b5a2ad9375b4ded082707
Andre <hiroandre@gmx.de>
2023-07-09 13:24:29 +0200

    src/util/json.h: Prepared blank file for testing general json utility functions.

---
+++ test/JsonUtilTest.cpp
@@ -0,0 +1,7 @@
+// Copyright 2023, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Andre Schlegel (July of 2023, schlegea@informatik.uni-freiburg.de)
+
+#include <gtest/gtest.h>
+
+#include "util/json.h"
