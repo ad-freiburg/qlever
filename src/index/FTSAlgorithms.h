ab237b5ab1868cc251183463bc91abf2e3ec2b31
NickG-1 <nick.goeckel@web.de>
2023-08-05 13:07:15 +0200

    deletion of unused functions

--- src/index/FTSAlgorithms.h
+++ src/index/FTSAlgorithms.h
@@ -21,12 +21,6 @@ using ad_utility::HashSet;
 
 class FTSAlgorithms {
  public:
-  typedef vector<array<Id, 1>> WidthOneList;
-  typedef vector<array<Id, 2>> WidthTwoList;
-  typedef vector<array<Id, 3>> WidthThreeList;
-  typedef vector<array<Id, 4>> WidthFourList;
-  typedef vector<array<Id, 5>> WidthFiveList;
-  typedef vector<vector<Id>> VarWidthList;
 
   static Index::WordEntityPostings filterByRange(
       const IdRange<WordVocabIndex>& idRange,
@@ -50,10 +44,6 @@ class FTSAlgorithms {
   static void multVarsAggScoresAndTakeTopContext(
       const Index::WordEntityPostings& wep, size_t nofVars, IdTable* dynResult);
 
-  template <typename Row>
-  static void aggScoresAndTakeTopKContexts(vector<Row>& nonAggRes, size_t k,
-                                           vector<Row>& res);
-
   // Special case where k == 1.
   template <int WIDTH>
   static void aggScoresAndTakeTopContext(const Index::WordEntityPostings& wep,
@@ -67,145 +57,6 @@ class FTSAlgorithms {
   static Index::WordEntityPostings crossIntersectKWay(
       const vector<Index::WordEntityPostings>& wepVecs,
       vector<Id>* lastListEids);
-
-  // Constructs the cross-product between entity postings of this
-  // context and matching subtree result tuples.
-  template <size_t I>
-  static inline void appendCrossProduct(
-      const Index::WordEntityPostings& wep, size_t from, size_t toExclusive,
-      const ad_utility::HashMap<Id, vector<array<Id, I>>>& subRes,
-      vector<array<Id, 3 + I>>& res) {
-    LOG(TRACE) << "Append cross-product called for a context with "
-               << toExclusive - from << " postings.\n";
-    vector<array<Id, I>> contextSubRes;
-    for (size_t i = from; i < toExclusive; ++i) {
-      auto it = subRes.find(wep.eids_[i]);
-      if (it != subRes.end()) {
-        for (auto& tup : it->second) {
-          contextSubRes.push_back(tup);
-        }
-      }
-    }
-    for (size_t i = from; i < toExclusive; ++i) {
-      for (auto& tup : contextSubRes) {
-        res.emplace_back(concatTupleOld(
-            wep.eids_[i], Id::makeFromInt(wep.scores_[i]),
-            Id::makeFromTextRecordIndex(wep.cids_[i]), tup, GenSeq<I>()));
-      }
-    }
-  }
-
-  template <size_t... I>
-  static inline array<Id, 3 + sizeof...(I)> concatTuple(
-      const array<Id, 2>& l, Id cid, const array<Id, sizeof...(I)>& r,
-      IndexSequence<I...>) {
-    return array<Id, 3 + sizeof...(I)>{{l[0], l[1], cid, r[I]...}};
-  }
-
-  template <size_t... I>
-  static inline array<Id, 3 + sizeof...(I)> concatTupleOld(
-      Id eid, Id score, Id cid, const array<Id, sizeof...(I)>& r,
-      IndexSequence<I...>) {
-    return array<Id, 3 + sizeof...(I)>{{eid, score, cid, r[I]...}};
-  }
-
-  template <typename InTup, size_t I>
-  static inline void fillTuple(Id cid, Id score, const InTup& in,
-                               array<Id, I>& out) {
-    out[0] = cid;
-    out[1] = score;
-    size_t n = 2;
-    for (auto e : in) {
-      out[n++] = e;
-    }
-  }
-
-  template <typename InTup>
-  static inline void fillTuple(Id cid, Id score, const InTup& in,
-                               vector<Id>& out) {
-    out.reserve(in.size() + 2);
-    out.push_back(cid);
-    out.push_back(score);
-    out.insert(std::end(out), std::begin(in), std::end(in));
-  }
-
-  template <size_t I>
-  static inline void fillThreeTuple(Id cid, Id score, Id eid,
-                                    array<Id, I>& out) {
-    out[0] = cid;
-    out[1] = score;
-    out[2] = eid;
-  }
-
-  static inline void fillThreeTuple(Id cid, Id score, Id eid, vector<Id>& out) {
-    out.resize(3);
-    out[0] = cid;
-    out[1] = score;
-    out[2] = eid;
-  }
-
-  template <typename Iter1, typename Iter2, size_t I>
-  static inline void fillTuple(Id cid, Id score, Iter1 keyBegin, Iter1 keyEnd,
-                               Iter2 fBegin, Iter2 fEnd, array<Id, I>& out) {
-    out[0] = cid;
-    out[1] = score;
-    size_t n = 2;
-    while (keyBegin != keyEnd) {
-      out[n++] = *keyBegin;
-      ++keyBegin;
-    }
-    while (fBegin != fEnd) {
-      out[n++] = *fBegin;
-      ++fBegin;
-    }
-    assert(n == I);
-  }
-
-  template <typename Iter, size_t I>
-  static inline void fillTuple(Id cid, Id score, Iter keyBegin, Iter keyEnd,
-                               Id eid, array<Id, I>& out) {
-    out[0] = cid;
-    out[1] = score;
-    size_t n = 2;
-    while (keyBegin != keyEnd) {
-      out[n++] = *keyBegin;
-      ++keyBegin;
-    }
-    out[n++] = eid;
-    assert(n == I);
-  }
-
-  template <typename Iter>
-  static inline void fillTuple(Id cid, Id score, Iter keyBegin, Iter keyEnd,
-                               Id eid, vector<Id>& out) {
-    out.push_back(cid);
-    out.push_back(score);
-    while (keyBegin != keyEnd) {
-      out.push_back(*keyBegin);
-      ++keyBegin;
-    }
-    out.push_back(eid);
-  }
-
-  template <typename Iter1, typename Iter2>
-  static inline void fillTuple(Id cid, Id score, Iter1 keyBegin, Iter1 keyEnd,
-                               Iter2 fBegin, Iter2 fEnd, vector<Id>& out) {
-    out.push_back(cid);
-    out.push_back(score);
-    out.insert(out.end(), keyBegin, keyEnd);
-    out.insert(out.end(), fBegin, fEnd);
-  }
-
-  static void appendCrossProduct(const Index::WordEntityPostings& wep,
-                                 size_t from, size_t toExclusive,
-                                 const ad_utility::HashSet<Id>& subRes1,
-                                 const ad_utility::HashSet<Id>& subRes2,
-                                 vector<array<Id, 5>>& res);
-
-  static void appendCrossProduct(
-      const Index::WordEntityPostings& wep, size_t from, size_t toExclusive,
-      const vector<ad_utility::HashMap<Id, vector<vector<Id>>>>&,
-      vector<vector<Id>>& res);
 
   template <int WIDTH>
   static void oneVarFilterAggScoresAndTakeTopKContexts(
