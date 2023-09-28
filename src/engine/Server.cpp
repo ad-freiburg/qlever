ad-freiburg/qlever

commit 90640c33dd1c9e81fadd506876735dfed958c767

Use Boost::Asio::static_thread_pool to limit the number of concurrent queries (#1103)
So far, the number of concurrent queries running in QLever was limited by first acquiring a semaphore and then scheduling the query on a completely new thread using handwritten glue code. Now those queries are simply scheduled onto a thread pool that automatically handles the number of concurrent queries.

src/engine/Server.cpp
@@ -13,7 +13,7 @@
 
 #include "engine/ExportQueryExecutionTrees.h"
 #include "engine/QueryPlanner.h"
-#include "util/BoostHelpers/AsyncWaitForFuture.h"
+#include "util/AsioHelpers.h"
 #include "util/OnDestructionDontThrowDuringStackUnwinding.h"
 #include "util/http/websocket/MessageSender.h"
 
@@ -21,7 +21,7 @@ template <typename T>
 using Awaitable = Server::Awaitable<T>;
 
 // __________________________________________________________________________
-Server::Server(unsigned short port, int numThreads, size_t maxMemGB,
+Server::Server(unsigned short port, size_t numThreads, size_t maxMemGB,
                std::string accessToken, bool usePatternTrick)
     : numThreads_(numThreads),
       port_(port),
@@ -37,7 +37,7 @@ Server::Server(unsigned short port, int numThreads, size_t maxMemGB,
       enablePatternTrick_(usePatternTrick),
       // The number of server threads currently also is the number of queries
       // that can be processed simultaneously.
-      queryProcessingSemaphore_(numThreads) {
+      threadPool_{numThreads} {
   // TODO<joka921> Write a strong type for KB, MB, GB etc and use it
   // in the cache and the memory limit
   // Convert a number of gigabytes to the number of Ids that find in that
@@ -137,8 +137,8 @@ void Server::run(const string& indexBaseName, bool useText, bool usePatterns,
 
   // First set up the HTTP server, so that it binds to the socket, and
   // the "socket already in use" error appears quickly.
-  auto httpServer =
-      HttpServer{port_, "0.0.0.0", numThreads_, std::move(httpSessionHandler)};
+  auto httpServer = HttpServer{port_, "0.0.0.0", static_cast<int>(numThreads_),
+                               std::move(httpSessionHandler)};
   queryHub_ = &httpServer.getQueryHub();
 
   // Initialize the index
@@ -757,13 +757,10 @@ boost::asio::awaitable<void> Server::processQuery(
 // _____________________________________________________________________________
 template <typename Function, typename T>
 Awaitable<T> Server::computeInNewThread(Function function) const {
-  auto acquireComputeRelease = [this, function = std::move(function)] {
-    LOG(DEBUG) << "Acquiring new thread for query processing\n";
-    queryProcessingSemaphore_.acquire();
-    auto cleanup = ad_utility::makeOnDestructionDontThrowDuringStackUnwinding(
-        [this]() { queryProcessingSemaphore_.release(); });
-    return function();
+  auto runOnExecutor = [](auto executor, Function func) -> net::awaitable<T> {
+    co_await net::post(executor, net::use_awaitable);
+    co_return std::invoke(func);
   };
-  co_return co_await ad_utility::asio_helpers::async_on_external_thread(
-      std::move(acquireComputeRelease), boost::asio::use_awaitable);
+  return ad_utility::sameExecutor(
+      runOnExecutor(threadPool_.get_executor(), std::move(function)));
 }

src/ServerMain.cpp
@@ -126,8 +126,8 @@ int main(int argc, char** argv) {
             << qlever::version::GitShortHash() << EMPH_OFF << std::endl;
 
   try {
-    Server server(port, static_cast<int>(numSimultaneousQueries),
-                  memoryMaxSizeGb, std::move(accessToken), !noPatternTrick);
+    Server server(port, numSimultaneousQueries, memoryMaxSizeGb,
+                  std::move(accessToken), !noPatternTrick);
     server.run(indexBasename, text, !noPatterns, !onlyPsoAndPosPermutations);
   } catch (const std::exception& e) {
     // This code should never be reached as all exceptions should be handled

src/engine/Server.h
@@ -30,7 +30,7 @@ using std::vector;
 //! The HTTP Server used.
 class Server {
  public:
-  explicit Server(unsigned short port, int numThreads, size_t maxMemGB,
+  explicit Server(unsigned short port, size_t numThreads, size_t maxMemGB,
                   std::string accessToken, bool usePatternTrick = true);
 
   virtual ~Server() = default;
@@ -52,7 +52,7 @@ class Server {
   const Index& index() const { return index_; }
 
  private:
-  const int numThreads_;
+  const size_t numThreads_;
   unsigned short port_;
   std::string accessToken_;
   QueryResultCache cache_;
@@ -68,9 +68,7 @@ class Server {
   // nullptr once the object is destroyed which only happens on shutdown.
   ad_utility::websocket::QueryHub* queryHub_ = nullptr;
 
-  // Semaphore for the number of queries that can be processed at once.
-  mutable std::counting_semaphore<std::numeric_limits<int>::max()>
-      queryProcessingSemaphore_;
+  mutable net::static_thread_pool threadPool_;
 
   template <typename T>
   using Awaitable = boost::asio::awaitable<T>;

src/util/AsioHelpers.h
@@ -9,20 +9,33 @@
 #include <boost/asio/dispatch.hpp>
 #include <boost/asio/use_awaitable.hpp>
 
+#include "util/Exception.h"
+
 namespace ad_utility {
 
 namespace net = boost::asio;
 
 /// Helper function that ensures that co_await resumes on the same
 /// executor it started with.
-/// IMPORTANT: If the inner awaitable throws, no guarantees are given. Make
-/// sure to keep that in mind when handling errors inside coroutines!
+/// IMPORTANT: If the coroutine is cancelled, no guarantees are given. Make
+/// sure to keep that in mind when handling cancellation errors!
 template <typename T>
 inline net::awaitable<T> sameExecutor(net::awaitable<T> awaitable) {
   auto initialExecutor = co_await net::this_coro::executor;
-  T result = co_await std::move(awaitable);
-  co_await net::dispatch(initialExecutor, net::use_awaitable);
-  co_return result;
+  std::exception_ptr exceptionPtr;
+  try {
+    T result = co_await std::move(awaitable);
+    co_await net::dispatch(initialExecutor, net::use_awaitable);
+    co_return result;
+  } catch (...) {
+    exceptionPtr = std::current_exception();
+  }
+  auto cancellationState = co_await net::this_coro::cancellation_state;
+  if (cancellationState.cancelled() == net::cancellation_type::none) {
+    co_await net::dispatch(initialExecutor, net::use_awaitable);
+  }
+  AD_CORRECTNESS_CHECK(exceptionPtr);
+  std::rethrow_exception(exceptionPtr);
 }
 
 // _____________________________________________________________________________
@@ -31,8 +44,19 @@ inline net::awaitable<T> sameExecutor(net::awaitable<T> awaitable) {
 /// executor it started with. Overload for void.
 inline net::awaitable<void> sameExecutor(net::awaitable<void> awaitable) {
   auto initialExecutor = co_await net::this_coro::executor;
-  co_await std::move(awaitable);
-  co_await net::dispatch(initialExecutor, net::use_awaitable);
+  std::exception_ptr exceptionPtr;
+  try {
+    co_await std::move(awaitable);
+  } catch (...) {
+    exceptionPtr = std::current_exception();
+  }
+  if ((co_await net::this_coro::cancellation_state).cancelled() ==
+      net::cancellation_type::none) {
+    co_await net::dispatch(initialExecutor, net::use_awaitable);
+  }
+  if (exceptionPtr) {
+    std::rethrow_exception(exceptionPtr);
+  }
 }
 }  // namespace ad_utility
 

src/util/BoostHelpers/AsyncWaitForFuture.h
@@ -1,61 +1,0 @@
-//  Copyright 2021, University of Freiburg,
-//  Chair of Algorithms and Data Structures.
-//  Author: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
-
-#ifndef QLEVER_ASYNCWAITFORFUTURE_H
-#define QLEVER_ASYNCWAITFORFUTURE_H
-// Inspired by https://gist.github.com/inetic/dc9081baf45ec4b60037
-
-#include <future>
-
-#include "util/http/beast.h"
-
-namespace ad_utility::asio_helpers {
-
-// An async operation that can be used inside boost::asio.
-// The `function` (which takes no arguments) is started on a new detached
-// thread (unrelated to the executor used by the `completionToken`).
-// As soon as the `function` is finished, the completion handler that is
-// obtained from the `completionToken` is invoked with the result of the
-// `function`. The handler obtained from the `completionToken` must be callable
-// with (std::exception_ptr, returnValueOfFunction).
-template <typename CompletionToken, typename Function>
-auto async_on_external_thread(Function function,
-                              CompletionToken&& completionToken) {
-  namespace asio = boost::asio;
-  namespace system = boost::system;
-
-  using Value = std::invoke_result_t<Function>;
-  using Signature = void(std::exception_ptr, Value);
-
-  auto initiatingFunction = [function =
-                                 std::move(function)](auto&& handler) mutable {
-    auto onRemoteThread =
-        [function = std::move(function),
-         handler = std::forward<decltype(handler)>(handler)]() mutable {
-          try {
-            // If `function()` throws no exception, we have no exception_ptr
-            // and the return value as the second argument.
-            handler(nullptr, function());
-          } catch (...) {
-            // If `function()` throws, we propagate the exception to the
-            // handler, and have no return value.
-            // TODO<C++23/26> When we get networking+executors, we hopefully
-            // have `setError` method which doesn't need a dummy return value.
-            Value* ptr = nullptr;
-            handler(std::current_exception(), std::move(*ptr));
-          }
-        };
-
-    std::thread thread(std::move(onRemoteThread));
-    thread.detach();
-  };
-
-  // This helper function automagically obtains a completionHandler from the
-  // `completionToken` and returns the correct `boost::asio::async_result`.
-  return boost::asio::async_initiate<CompletionToken, Signature>(
-      std::move(initiatingFunction), completionToken);
-}
-}  // namespace ad_utility::asio_helpers
-
-#endif  // QLEVER_ASYNCWAITFORFUTURE_H

test/AsioHelpersTest.cpp
@@ -5,8 +5,11 @@
 #include <gtest/gtest.h>
 
 #include <boost/asio/co_spawn.hpp>
+#include <boost/asio/deadline_timer.hpp>
 #include <boost/asio/detached.hpp>
+#include <boost/asio/experimental/awaitable_operators.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/steady_timer.hpp>
 #include <boost/asio/strand.hpp>
 
 #include "util/AsioHelpers.h"
@@ -14,6 +17,7 @@
 namespace net = boost::asio;
 
 using ad_utility::sameExecutor;
+using namespace boost::asio::experimental::awaitable_operators;
 
 TEST(AsioHelpers, sameExecutor) {
   net::io_context ioContext{};
@@ -31,7 +35,7 @@ TEST(AsioHelpers, sameExecutor) {
   };
 
   auto outerAwaitable = [&sanityCounter, &innerAwaitable,
-                         &strand1]() -> net::awaitable<void> {
+                         strand1]() -> net::awaitable<void> {
     // Sanity check
     EXPECT_TRUE(strand1.running_in_this_thread());
     auto value = co_await sameExecutor(innerAwaitable());
@@ -72,4 +76,168 @@ TEST(AsioHelpers, sameExecutorVoidOverload) {
   ioContext.run();
 
   EXPECT_TRUE(sanityFlag);
+}
+
+// _____________________________________________________________________________
+
+TEST(AsioHelpers, sameExecutorWhenException) {
+  net::io_context ioContext{};
+  auto strand1 = net::make_strand(ioContext);
+  auto strand2 = net::make_strand(ioContext);
+
+  uint32_t sanityCounter = 0;
+
+  auto innerAwaitable = [&sanityCounter, strand2]() -> net::awaitable<int> {
+    co_await net::post(strand2, net::use_awaitable);
+    // sanity check
+    EXPECT_TRUE(strand2.running_in_this_thread());
+    sanityCounter++;
+    throw std::runtime_error{"Expected"};
+  };
+
+  auto outerAwaitable = [&sanityCounter, &innerAwaitable,
+                         strand1]() -> net::awaitable<void> {
+    // Sanity check
+    EXPECT_TRUE(strand1.running_in_this_thread());
+    EXPECT_THROW(co_await sameExecutor(innerAwaitable()), std::runtime_error);
+    // Verify we're back on the same strand
+    EXPECT_TRUE(strand1.running_in_this_thread());
+    sanityCounter++;
+  };
+
+  net::co_spawn(strand1, outerAwaitable(), net::detached);
+
+  ioContext.run();
+
+  EXPECT_EQ(sanityCounter, 2);
+}
+
+// _____________________________________________________________________________
+
+TEST(AsioHelpers, sameExecutorVoidOverloadWhenException) {
+  net::io_context ioContext{};
+  auto strand1 = net::make_strand(ioContext);
+  auto strand2 = net::make_strand(ioContext);
+
+  uint32_t sanityCounter = 0;
+
+  auto innerAwaitable = [&sanityCounter, strand2]() -> net::awaitable<void> {
+    co_await net::post(strand2, net::use_awaitable);
+    // sanity check
+    EXPECT_TRUE(strand2.running_in_this_thread());
+    sanityCounter++;
+    throw std::runtime_error{"Expected"};
+  };
+
+  auto outerAwaitable = [&sanityCounter, &innerAwaitable,
+                         strand1]() -> net::awaitable<void> {
+    // Sanity check
+    EXPECT_TRUE(strand1.running_in_this_thread());
+    EXPECT_THROW(co_await sameExecutor(innerAwaitable()), std::runtime_error);
+    // Verify we're back on the same strand
+    EXPECT_TRUE(strand1.running_in_this_thread());
+    sanityCounter++;
+  };
+
+  net::co_spawn(strand1, outerAwaitable(), net::detached);
+
+  ioContext.run();
+
+  EXPECT_EQ(sanityCounter, 2);
+}
+
+// _____________________________________________________________________________
+
+template <typename T, typename Rep, typename Period>
+net::awaitable<T> cancelAfter(net::awaitable<T> coroutine,
+                              std::chrono::duration<Rep, Period> duration) {
+  net::steady_timer timer{co_await net::this_coro::executor, duration};
+  co_await (std::move(coroutine) || timer.async_wait(net::use_awaitable));
+}
+// _____________________________________________________________________________
+
+// Checks that behavior is consistent for cancellation case
+TEST(AsioHelpers, sameExecutorWhenCancelled) {
+  net::io_context ioContext{};
+  auto strand1 = net::make_strand(ioContext);
+  auto strand2 = net::make_strand(ioContext);
+  auto strand3 = net::make_strand(ioContext);
+  net::deadline_timer infiniteTimer{ioContext,
+                                    static_cast<net::deadline_timer::time_type>(
+                                        boost::posix_time::pos_infin)};
+
+  uint32_t sanityCounter = 0;
+
+  auto innerAwaitable = [&sanityCounter, strand2,
+                         &infiniteTimer]() -> net::awaitable<void> {
+    co_await net::post(strand2, net::use_awaitable);
+    // sanity check
+    EXPECT_TRUE(strand2.running_in_this_thread());
+    sanityCounter++;
+    co_await infiniteTimer.async_wait(net::use_awaitable);
+  };
+
+  auto outerAwaitable = [&sanityCounter, &innerAwaitable, strand1, strand2,
+                         strand3]() -> net::awaitable<void> {
+    co_await net::post(strand1, net::use_awaitable);
+    // Sanity check
+    EXPECT_TRUE(strand1.running_in_this_thread());
+    EXPECT_THROW(co_await sameExecutor(innerAwaitable()),
+                 boost::system::system_error);
+    // Verify we're on the strand the cancellation happened
+    EXPECT_TRUE(strand3.running_in_this_thread());
+    sanityCounter++;
+  };
+
+  net::co_spawn(strand3,
+                cancelAfter(outerAwaitable(), std::chrono::milliseconds(10)),
+                net::detached);
+
+  ioContext.run();
+
+  EXPECT_EQ(sanityCounter, 2);
+}
+
+// _____________________________________________________________________________
+
+// Checks that behavior is consistent for cancellation case
+TEST(AsioHelpers, sameExecutorVoidOverloadWhenCancelled) {
+  net::io_context ioContext{};
+  auto strand1 = net::make_strand(ioContext);
+  auto strand2 = net::make_strand(ioContext);
+  auto strand3 = net::make_strand(ioContext);
+  net::deadline_timer infiniteTimer{ioContext,
+                                    static_cast<net::deadline_timer::time_type>(
+                                        boost::posix_time::pos_infin)};
+
+  uint32_t sanityCounter = 0;
+
+  auto innerAwaitable = [&sanityCounter, strand2,
+                         &infiniteTimer]() -> net::awaitable<void> {
+    co_await net::post(strand2, net::use_awaitable);
+    // sanity check
+    EXPECT_TRUE(strand2.running_in_this_thread());
+    sanityCounter++;
+    co_await infiniteTimer.async_wait(net::use_awaitable);
+  };
+
+  auto outerAwaitable = [&sanityCounter, &innerAwaitable, strand1, strand2,
+                         strand3]() -> net::awaitable<void> {
+    co_await net::post(strand1, net::use_awaitable);
+    // Sanity check
+    EXPECT_TRUE(strand1.running_in_this_thread());
+    EXPECT_THROW(co_await sameExecutor(innerAwaitable()),
+                 boost::system::system_error);
+    // Verify we're on the strand the cancellation happened
+    EXPECT_TRUE(strand3.running_in_this_thread());
+    sanityCounter++;
+  };
+
+  net::co_spawn(strand3,
+                cancelAfter(outerAwaitable(), std::chrono::milliseconds(10)),
+                net::detached);
+
+  ioContext.run();
+
+  EXPECT_EQ(sanityCounter, 2);
 }

commit 046669f0596c86a2305564577739fc211b7667af

Actually spawn queries on new thread (#1108)
Fix a regression introduced in #1103, that caused the query processing to actually happen on the server threads instead of the threads dedicated to query processing.

src/engine/Server.cpp
@@ -760,7 +760,7 @@ boost::asio::awaitable<void> Server::processQuery(
 template <typename Function, typename T>
 Awaitable<T> Server::computeInNewThread(Function function) const {
   auto runOnExecutor = [](auto executor, Function func) -> net::awaitable<T> {
-    co_await net::post(executor, net::use_awaitable);
+    co_await net::post(net::bind_executor(executor, net::use_awaitable));
     co_return std::invoke(func);
   };
   return ad_utility::sameExecutor(