fc76ef4de9e08866e8cae181cdfd989db75b3ea7
RobinTF <83676088+RobinTF@users.noreply.github.com>
2023-10-02 21:41:11 +0200

    Fix deadlocks in the Websocket module caused by heavy load (#1109)
    The destructor of a query session has to synchronously wait until the session is unregistered. This waiting is blocking because destructors can't be asynchronous in C++. Under heavy load it could happen that all available server threads were blocking in such destructors, which lead to a deadlock as there were no threads left which could complete the tasks that the destructors were blocking on.
    This is now fixed as the destructors are also continuing to run server tasks while they are waiting for the unregistering of their session.
    
    Also clear up the interface of the QueryHub class. It is now only possible to have at most one sender per query session, else an exception is thrown. This makes reasoning about the lifetime of such sessions much more explicit.

--- src/util/http/websocket/QueryHub.cpp
+++ src/util/http/websocket/QueryHub.cpp
@@ -8,72 +8,45 @@
 
 namespace ad_utility::websocket {
 
-/// Helper function to check if two weak pointers point to the same control
-/// block. This is the case for all weak pointers created from the same shared
-/// pointer or aliases of that shared pointer.
-template <typename T, typename U>
-bool equals(const std::weak_ptr<T>& t, const std::weak_ptr<U>& u) noexcept {
-  return !t.owner_before(u) && !u.owner_before(t);
-}
-
-// _____________________________________________________________________________
-
-net::awaitable<std::shared_ptr<QueryToSocketDistributor>>
+template <bool isSender>
+net::awaitable<std::shared_ptr<
+    QueryHub::ConditionalConst<isSender, QueryToSocketDistributor>>>
 QueryHub::createOrAcquireDistributorInternal(QueryId queryId) {
-  co_await net::dispatch(globalStrand_, net::use_awaitable);
-  if (socketDistributors_.contains(queryId)) {
-    auto ptr = socketDistributors_.at(queryId).lock();
-    // On all code paths a finished `socketDistributor` is first deleted from
-    // the `QueryHub` and only then destroyed.
-    // This is asserted by the following check.
-    AD_CORRECTNESS_CHECK(ptr);
-    co_return ptr;
+  co_await net::dispatch(net::bind_executor(globalStrand_, net::use_awaitable));
+  while (socketDistributors_.contains(queryId)) {
+    auto& reference = socketDistributors_.at(queryId);
+    if (auto ptr = reference.pointer_.lock()) {
+      if constexpr (isSender) {
+        // Ensure only single sender reference is acquired for a single session
+        AD_CONTRACT_CHECK(!reference.started_);
+        reference.started_ = true;
+      }
+      co_return ptr;
+    }
+    // There's the unlikely case where the reference counter reached zero and
+    // the weak pointer can no longer create a shared pointer, but the
+    // destructor is waiting for execution on `globalStrand_`. In this case
+    // re-schedule this coroutine to be executed after destruction. So it is
+    // crucial to use post over dispatch here.
+    co_await net::post(net::bind_executor(globalStrand_, net::use_awaitable));
   }
 
-  auto pointerHolder =
-      std::make_shared<std::optional<PointerType>>(std::nullopt);
-  auto cleanupAction = [this, queryId, pointerHolder]() {
-    AD_CORRECTNESS_CHECK(pointerHolder->has_value());
-    auto referencePointer = std::move(pointerHolder->value());
-    // Only erase object if we created it, otherwise we have a race condition
-    if (socketDistributors_.contains(queryId) &&
-        equals(referencePointer, socketDistributors_.at(queryId))) {
-      socketDistributors_.erase(queryId);
-    }
-  };
-  auto distributor = std::make_shared<QueryToSocketDistributor>(
-      ioContext_, [this, cleanupAction = std::move(cleanupAction)]() mutable {
-        auto future =
-            net::dispatch(globalStrand_,
-                          std::packaged_task<void()>(std::move(cleanupAction)));
-        // If we blocked until the cleanup action is executed on different
-        // strand, this could theoretically lead to deadlocks when globalStrand_
-        // is on a different execution context. This is currently not the case,
-        // but it might be in the future, resulting in deadlocks at distance
-        // which is bad. Unfortunately destructors can't be coroutines, which
-        // would just allow us to co_await execution without issues. To prevent
-        // deadlocks, we wait at most 1 second (arbitrarily chosen interval)
-        // for the cleanup to complete, then print a warning, and wait for 9
-        // more seconds before we call std::terminate instead of running into
-        // a deadlock.
-        auto status = future.wait_for(std::chrono::seconds(1));
-        if (status != std::future_status::ready) {
-          LOG(WARN)
-              << "Distributor cleanup in the websocket module taking longer "
-                 "than usual. This might be due to heavy load or a deadlock."
-              << std::endl;
-        }
-        status = future.wait_for(std::chrono::seconds(9));
-        if (status != std::future_status::ready) {
-          LOG(FATAL)
-              << "Distributor cleanup in the websocket module taking longer "
-                 "than 10 seconds. Terminating to avoid potential deadlock."
-              << std::endl;
-          std::terminate();
+  auto distributor =
+      std::make_shared<QueryToSocketDistributor>(ioContext_, [this, queryId]() {
+        auto future = net::dispatch(net::bind_executor(
+            globalStrand_, std::packaged_task<void()>([this, &queryId]() {
+              bool wasErased = socketDistributors_.erase(queryId);
+              AD_CORRECTNESS_CHECK(wasErased);
+            })));
+        // Make sure we never block, just run some tasks on the `ioContext_`
+        // until the task is executed
+        while (future.wait_for(std::chrono::seconds(0)) !=
+               std::future_status::ready) {
+          ioContext_.poll_one();
         }
       });
-  *pointerHolder = distributor;
-  socketDistributors_.emplace(queryId, distributor);
+  socketDistributors_.emplace(queryId,
+                              WeakReferenceHolder{distributor, isSender});
   co_return distributor;
 }
 
@@ -81,15 +54,15 @@ QueryHub::createOrAcquireDistributorInternal(QueryId queryId) {
 
 net::awaitable<std::shared_ptr<QueryToSocketDistributor>>
 QueryHub::createOrAcquireDistributorForSending(QueryId queryId) {
-  return sameExecutor(createOrAcquireDistributorInternal(std::move(queryId)));
+  return sameExecutor(
+      createOrAcquireDistributorInternal<true>(std::move(queryId)));
 }
 
 // _____________________________________________________________________________
 
 net::awaitable<std::shared_ptr<const QueryToSocketDistributor>>
 QueryHub::createOrAcquireDistributorForReceiving(QueryId queryId) {
-  auto result = co_await sameExecutor(
-      createOrAcquireDistributorInternal(std::move(queryId)));
-  co_return result;
+  return sameExecutor(
+      createOrAcquireDistributorInternal<false>(std::move(queryId)));
 }
 }  // namespace ad_utility::websocket
