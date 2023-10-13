7d530a1e8a46513e26f07b557ca2e609914c5ef1
RobinTF <83676088+RobinTF@users.noreply.github.com>
2023-10-13 02:40:17 +0200

    Fix formatting

--- src/engine/CartesianProductJoin.h
+++ src/engine/CartesianProductJoin.h
@@ -83,8 +83,8 @@ class CartesianProductJoin : public Operation {
 
   template <size_t StaticGroupSize = 0>
   void writeResultColumn(std::span<Id> targetColumn,
-                                std::span<const Id> inputColumn, size_t groupSize,
-                                size_t offset);
+                         std::span<const Id> inputColumn, size_t groupSize,
+                         size_t offset);
 };
 
 #endif  // QLEVER_CARTESIANPRODUCTJOIN_H
