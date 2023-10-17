5d1278cb1e147173bd42026a54eeff8bb181ddd9
Benedikt Maria Beckermann <benedikt.beckermann@dagstuhl.de>
2023-10-17 14:39:11 +0200

    * less naive implementation of encode_for_uri

--- src/engine/sparqlExpressions/StringExpressions.cpp
+++ src/engine/sparqlExpressions/StringExpressions.cpp
@@ -2,9 +2,10 @@
 //                  Chair of Algorithms and Data Structures.
 //  Author: Johannes Kalmbach <kalmbacj@cs.uni-freiburg.de>
 
+#include <boost/url.hpp>
+
 #include "engine/sparqlExpressions/NaryExpressionImpl.h"
 #include "engine/sparqlExpressions/VariadicExpression.h"
-#include <boost/url.hpp>
 
 namespace sparqlExpression {
 namespace detail::string_expressions {
@@ -159,7 +160,7 @@ class SubstrImpl {
       return std::string{};
     }
 
-    // In SPARQL indices are 1-based, but the implemenetation we use is 0 based,
+    // In SPARQL indices are 1-based, but the implementation we use is 0 based,
     // hence the `- 1`.
     int64_t startInt = std::visit(round, start) - 1;
     int64_t lengthInt = std::visit(round, length);
@@ -369,14 +370,22 @@ class ConcatExpression : public detail::VariadicExpression {
   if (!input.has_value()) {
     return Id::makeUndefined();
   } else {
+    std::string value = input.value();
 
-    //TODO:: issue: "hi"@en -> "%22hi%22%40en", expected "hi"
-    return boost::urls::encode(input.value().data(),
-                               boost::urls::unreserved_chars);
+    if (value.starts_with("\"")) {
+      auto contentEnd = ad_utility::findLiteralEnd(value, "\"");
+      if (contentEnd != string::npos) {
+        std::string content = value.substr(1, contentEnd - 1);
+        std::string encodedContent =
+            boost::urls::encode(content, boost::urls::unreserved_chars);
+        return encodedContent;
+      }
+    }
+    return boost::urls::encode(value, boost::urls::unreserved_chars);
   }
 };
-using EncodeForUriExpression = StringExpressionImpl<1, decltype(encodeForUriImpl)>;
-
+using EncodeForUriExpression =
+    StringExpressionImpl<1, decltype(encodeForUriImpl)>;
 
 }  // namespace detail::string_expressions
 using namespace detail::string_expressions;
