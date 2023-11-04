15cf8a1db5c7184bb47a6be03435f671df12c548
RobinTF <83676088+RobinTF@users.noreply.github.com>
2023-11-05 00:38:02 +0100

    Use double quotes

--- src/util/CompilerExtensions.h
+++ src/util/CompilerExtensions.h
@@ -8,14 +8,13 @@
 // A generic macro that forces inlining during compilation across compilers
 #ifdef __CLANG__
 #define AD_ALWAYS_INLINE [[clang::always_inline]]
-#else
-#ifdef __GNUC__
+#elif __GNUC__
 #define AD_ALWAYS_INLINE [[gnu::always_inline]]
 #else
-#warning For this compiler we don't know how to force the inlining of \
-functions. There might be some performance degradations.
+#warning \
+    "For this compiler we don't know how to force the inlining of functions. \
+There might be some performance degradations."
 #define AD_ALWAYS_INLINE
-#endif
 #endif
 
 #endif  // QLEVER_COMPILEREXTENSIONS_H
