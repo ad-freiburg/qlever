f3ef5a1abc9be5fb8621fbcdc2ca21de4eaa6274
Johannes Herrmann <johannes.r.herrmann@gmail.com>
2023-11-08 17:20:50 +0100

    Reverted accidential change

--- misc/format-check.sh
+++ misc/format-check.sh
@@ -9,7 +9,7 @@ done <sourcelist
 
 ERROR=0
 for source in "${SOURCE_FILES[@]}" ;do
-	clang-format -output-replacements-xml $source | grep "<replacement " &> /dev/null
+	clang-format-16 -output-replacements-xml $source | grep "<replacement " &> /dev/null
 	HAS_WRONG_FILES=$?
 	if [ $HAS_WRONG_FILES -ne 1 ] ; then
 		# Print an error and exit
