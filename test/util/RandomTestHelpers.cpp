d75651f5adf4099740c15e939bfd4be001a03657
Andre <hiroandre@gmx.de>
2023-11-13 10:01:34 +0100

    test/util/RandomTestHelpers: Added random generator for generator seeds.

---
+++ test/util/RandomTestHelpers.cpp
@@ -0,0 +1,13 @@
+// Copyright 2023, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Andre Schlegel (November of 2023,
+// schlegea@informatik.uni-freiburg.de)
+
+#include "../test/util/RandomTestHelpers.h"
+
+#include <functional>
+
+// ____________________________________________________________________________
+ad_utility::RandomSeed RandomSeedGenerator::operator()() {
+  return ad_utility::RandomSeed::make(std::invoke(numberGenerator_));
+}
