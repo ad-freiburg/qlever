c9e4937b71a5bdc1bbad5dd6d907f75ba18b4f2e
NickG-1 <nick.goeckel@web.de>
2023-11-16 21:33:49 +0100

    updated wordIndexScanTest

--- test/engine/TextIndexScanForWordTest.cpp
+++ test/engine/TextIndexScanForWordTest.cpp
@@ -13,21 +13,23 @@ using ad_utility::source_location;
 TEST(TextIndexScanForWord, WordScanBasic) {
   // build qec
   auto qec = getQec(
-      "<a> <p> \"he failed the test\". <a> <p> \"testing can help\". <a> <p> "
-      "\"some other sentence\". <b> <p> \"test but with wrong subject\". <b> "
-      "<x2> <x>. <b> <x2> <xb2> .");
-  // create operation
+      "<a> <p> \"he failed the test\" . <a> <p> \"testing can help\" . <a> <p> "
+      "\"some other sentence\" . <b> <p> \"test but with wrong subject\" . <b> "
+      "<x2> <x> . <b> <x2> <xb2> .", true, true, true, 32UL, true);
+  // create operations
   TextIndexScanForWord s1{qec, Variable{"?a"}, "test*"};
   // compute test results
   auto result = s1.computeResultOnlyForTesting();
-  auto idConverter = makeGetId(qec->getIndex());
-  // IdTable expectedResult =
-  //    makeIdTableFromVector({{idConverter("\"he failed the test\""),
-  //                            idConverter("\"test\""), idConverter("1")}});//
-  //                            make id from int 1
-  // stattdessen idToString in Type
-  // id datentyp checken
-  IdTable expectedResult = makeIdTableFromVector({});
   // compare results
-  ASSERT_EQ(expectedResult, result.idTable());
+  for (int i=0; i<result.idTable().getColumn(0).size(); i++) {
+      TextRecordIndex tridx = result.idTable().getColumn(0)[i].getTextRecordIndex();
+      std::string s = qec->getIndex().getTextExcerpt(tridx);
+      LOG(INFO) << " Text: " << s << " Word: " << qec->getIndex().idToOptionalString(result.idTable().getColumn(1)[i].getWordVocabIndex()).value() << endl;
+  }
+
+  ASSERT_EQ(result.idTable(), makeIdTableFromVector({}));
+  ASSERT_EQ(qec->getIndex().getTextExcerpt(result.idTable().getColumn(0)[0].getTextRecordIndex()), "he failed the test");
+
+  // TODO: add entity scan
+  // TODO: fixed entity case
 }
