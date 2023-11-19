5a8bba40fbfbd0faa35ad334d241dcd1e69e6e10
NickG-1 <nick.goeckel@web.de>
2023-11-19 01:25:47 +0100

    added tests

---
+++ test/engine/EntityIndexScanForWordTest.cpp
@@ -0,0 +1,88 @@
+#include <gtest/gtest.h>
+
+#include "../IndexTestHelpers.h"
+#include "../util/GTestHelpers.h"
+#include "../util/IdTableHelpers.h"
+#include "engine/EntityIndexScanForWord.h"
+#include "engine/IndexScan.h"
+#include "parser/ParsedQuery.h"
+
+using namespace ad_utility::testing;
+using ad_utility::source_location;
+
+string getTextFromResultTable(const QueryExecutionContext* qec,
+                              const ResultTable& result,
+                              const size_t& rowIndex) {
+  return qec->getIndex()
+      .idToOptionalString(
+          result.idTable().getColumn(0)[rowIndex].getVocabIndex())
+      .value();
+}
+
+string getEntityFromResultTable(const QueryExecutionContext* qec,
+                                const ResultTable& result,
+                                const size_t& rowIndex) {
+  return qec->getIndex()
+      .idToOptionalString(
+          result.idTable().getColumn(1)[rowIndex].getVocabIndex())
+      .value();
+}
+
+TEST(EntityIndexScanForWord, EntityScanBasic) {
+  auto qec = getQec(
+      "<a> <p> \"he failed the test\" . <b> <p> \"some other "
+      "sentence\" . <a> <p> \"testing can help\" . <b> <p> \"the test on "
+      "friday was really hard\" . <b> <x2> <x> . <b> <x2> <xb2> .",
+      true, true, true, 32UL, true);
+
+  EntityIndexScanForWord s1{qec, Variable{"?text"}, Variable{"?entityVar"},
+                            "test*"};
+  EntityIndexScanForWord s2{qec, Variable{"?text2"}, Variable{"?entityVar2"},
+                            "test*"};
+  ASSERT_EQ(s1.asStringImpl(0), s2.asStringImpl(0));
+  ASSERT_EQ(s1.getResultWidth(), 3);
+
+  auto result = s1.computeResultOnlyForTesting();
+  ASSERT_EQ(result.width(), 3);
+  ASSERT_EQ(result.size(), 3);
+
+  // NOTE: because of the way the graph above is constructed, the entities are
+  // texts
+  ASSERT_EQ("\"he failed the test\"", getEntityFromResultTable(qec, result, 0));
+  ASSERT_EQ("\"testing can help\"", getEntityFromResultTable(qec, result, 1));
+  ASSERT_EQ("\"the test on friday was really hard\"",
+            getEntityFromResultTable(qec, result, 2));
+
+  using enum ColumnIndexAndTypeInfo::UndefStatus;
+  VariableToColumnMap expectedVariables{
+      {Variable{"?text2"}, {0, AlwaysDefined}},
+      {Variable{"?entityVar2"}, {1, AlwaysDefined}},
+      {Variable{"?ql_score_entityVar2"}, {2, AlwaysDefined}}};
+  EXPECT_THAT(s2.getExternallyVisibleVariableColumns(),
+              ::testing::UnorderedElementsAreArray(expectedVariables));
+
+  // fixed entity case
+  string fixedEntity = "\"some other sentence\"";
+  EntityIndexScanForWord s3{qec, Variable{"?text3"}, std::nullopt, "sentence",
+                            fixedEntity};
+
+  result = s3.computeResultOnlyForTesting();
+  ASSERT_EQ(s3.getResultWidth(), 2);
+  ASSERT_EQ(result.width(), 2);
+  ASSERT_EQ(result.size(), 1);
+
+  ASSERT_TRUE(s1.asStringImpl(0) != s3.asStringImpl(0));
+
+  expectedVariables = {
+      {Variable{"?text3"}, {0, AlwaysDefined}},
+      {Variable{"?ql_score_someothersentence"}, {1, AlwaysDefined}}};
+  EXPECT_THAT(s3.getExternallyVisibleVariableColumns(),
+              ::testing::UnorderedElementsAreArray(expectedVariables));
+
+  ASSERT_EQ(fixedEntity, getTextFromResultTable(qec, result, 0));
+
+  fixedEntity = "\"new entity\"";
+  EntityIndexScanForWord s4{qec, Variable{"?text4"}, std::nullopt, "sentence",
+                            fixedEntity};
+  ASSERT_TRUE(s3.asStringImpl(0) != s4.asStringImpl(0));
+}
--- test/engine/TextIndexScanForWordTest.cpp
+++ test/engine/TextIndexScanForWordTest.cpp
@@ -1,3 +1,4 @@
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "../IndexTestHelpers.h"
@@ -10,26 +11,79 @@
 using namespace ad_utility::testing;
 using ad_utility::source_location;
 
+// NOTE: this function exploits a "lucky accindent" that allows us to
+// obtain the textRecord using idToOptionalString.
+// TODO: Implement a more elegant/stable version
+string getTextRecordFromResultTable(const QueryExecutionContext* qec,
+                                    const ResultTable& result,
+                                    const size_t& rowIndex) {
+  return qec->getIndex()
+      .idToOptionalString(
+          result.idTable().getColumn(0)[rowIndex].getVocabIndex())
+      .value();
+}
+
+string getWordFromResultTable(const QueryExecutionContext* qec,
+                              const ResultTable& result,
+                              const size_t& rowIndex) {
+  return qec->getIndex()
+      .idToOptionalString(
+          result.idTable().getColumn(1)[rowIndex].getWordVocabIndex())
+      .value();
+}
+
+string combineToString(const string& text, const string& word) {
+  std::stringstream ss;
+  ss << "Text: " << text << ", Word: " << word << endl;
+  return ss.str();
+}
+
 TEST(TextIndexScanForWord, WordScanBasic) {
-  // build qec
   auto qec = getQec(
       "<a> <p> \"he failed the test\" . <a> <p> \"testing can help\" . <a> <p> "
-      "\"some other sentence\" . <b> <p> \"test but with wrong subject\" . <b> "
-      "<x2> <x> . <b> <x2> <xb2> .", true, true, true, 32UL, true);
-  // create operations
-  TextIndexScanForWord s1{qec, Variable{"?a"}, "test*"};
-  // compute test results
+      "\"some other sentence\" . <b> <p> \"the test on friday was really "
+      "hard\" . <b> <x2> <x> . <b> <x2> <xb2> .",
+      true, true, true, 32UL, true);
+
+  TextIndexScanForWord s1{qec, Variable{"?text1"}, "test*"};
+  TextIndexScanForWord s2{qec, Variable{"?text2"}, "test*"};
+
+  ASSERT_EQ(s1.asStringImpl(0), s2.asStringImpl(0));
+  ASSERT_EQ(s1.getResultWidth(), 2);
+
   auto result = s1.computeResultOnlyForTesting();
-  // compare results
-  for (int i=0; i<result.idTable().getColumn(0).size(); i++) {
-      TextRecordIndex tridx = result.idTable().getColumn(0)[i].getTextRecordIndex();
-      std::string s = qec->getIndex().getTextExcerpt(tridx);
-      LOG(INFO) << " Text: " << s << " Word: " << qec->getIndex().idToOptionalString(result.idTable().getColumn(1)[i].getWordVocabIndex()).value() << endl;
-  }
+  ASSERT_EQ(result.width(), 2);
+  ASSERT_EQ(result.size(), 3);
+  s2.getExternallyVisibleVariableColumns();
 
-  ASSERT_EQ(result.idTable(), makeIdTableFromVector({}));
-  ASSERT_EQ(qec->getIndex().getTextExcerpt(result.idTable().getColumn(0)[0].getTextRecordIndex()), "he failed the test");
+  using enum ColumnIndexAndTypeInfo::UndefStatus;
+  VariableToColumnMap expectedVariables{
+      {Variable{"?text2"}, {0, AlwaysDefined}},
+      {Variable{"?ql_matchingword_text2_test"}, {1, AlwaysDefined}}};
+  EXPECT_THAT(s2.getExternallyVisibleVariableColumns(),
+              ::testing::UnorderedElementsAreArray(expectedVariables));
 
-  // TODO: add entity scan
-  // TODO: fixed entity case
+  ASSERT_EQ(combineToString("\"he failed the test\"", "test"),
+            combineToString(getTextRecordFromResultTable(qec, result, 0),
+                            getWordFromResultTable(qec, result, 0)));
+  ASSERT_EQ(combineToString("\"testing can help\"", "testing"),
+            combineToString(getTextRecordFromResultTable(qec, result, 1),
+                            getWordFromResultTable(qec, result, 1)));
+  ASSERT_EQ(combineToString("\"the test on friday was really hard\"", "test"),
+            combineToString(getTextRecordFromResultTable(qec, result, 2),
+                            getWordFromResultTable(qec, result, 2)));
+
+  TextIndexScanForWord s3{qec, Variable{"?text1"}, "test"};
+
+  ASSERT_TRUE(s1.asStringImpl(0) != s3.asStringImpl(0));
+  ASSERT_EQ(s3.getResultWidth(), 1);
+
+  result = s3.computeResultOnlyForTesting();
+  ASSERT_EQ(result.width(), 1);
+  ASSERT_EQ(result.size(), 2);
+
+  ASSERT_EQ("\"he failed the test\"",
+            getTextRecordFromResultTable(qec, result, 0));
+  ASSERT_EQ("\"the test on friday was really hard\"",
+            getTextRecordFromResultTable(qec, result, 1));
 }
