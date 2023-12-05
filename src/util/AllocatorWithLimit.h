9eb4131d706240d4690f63adb63583118a7dfef7
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2023-12-05 20:56:34 +0100

    Fix the compilation.

--- src/util/AllocatorWithLimit.h
+++ src/util/AllocatorWithLimit.h
@@ -178,7 +178,7 @@ class AllocatorWithLimit {
   requires(!std::same_as<U, T>)
   AllocatorWithLimit(const AllocatorWithLimit<U>& other)
       : memoryLeft_{other.getMemoryLeft()},
-        clearOnAllocation_(other.clearOnAllocation_){};
+        clearOnAllocation_(other.clearOnAllocation()){};
 
   // Defaulted copy operations.
   AllocatorWithLimit(const AllocatorWithLimit&) = default;
@@ -256,6 +256,7 @@ class AllocatorWithLimit {
   }
 
   const auto& getMemoryLeft() const { return memoryLeft_; }
+  const auto& clearOnAllocation() const {return clearOnAllocation_;}
 
   // The STL needs two allocators to be equal if and only they refer to the same
   // memory pool. For us, they are hence equal if they use the same
