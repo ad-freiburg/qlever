5e62611e667488a95271a2d0fceeb151bf884826
Andre <hiroandre@gmx.de>
2023-12-09 14:51:32 +0100

    src/util/ConfigManager/ConfigManager.cpp: Integrated insertThousandDelimiter into printConfigurationDoc.

--- src/util/ConfigManager/ConfigManager.cpp
+++ src/util/ConfigManager/ConfigManager.cpp
@@ -789,8 +789,9 @@ std::string ConfigManager::printConfigurationDoc(bool detailed) const {
   }
 
   // We always print the configuration doc json.
-  const std::string& configurationDocJsonString{absl::StrCat(
-      "Configuration:\n", generateConfigurationDocJson("").dump(2))};
+  const std::string& configurationDocJsonString{
+      insertThousandDelimiter(absl::StrCat(
+          "Configuration:\n", generateConfigurationDocJson("").dump(2)))};
 
   if (!detailed) {
     return configurationDocJsonString;
@@ -798,7 +799,8 @@ std::string ConfigManager::printConfigurationDoc(bool detailed) const {
 
   return absl::StrCat(
       configurationDocJsonString, "\n\n",
-      generateConfigurationDocDetailedList("", getValidatorAssignment()));
+      insertThousandDelimiter(
+          generateConfigurationDocDetailedList("", getValidatorAssignment())));
 }
 
 // ____________________________________________________________________________
