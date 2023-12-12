f45fece4a4774c62a9cbb87e4c57eb1fd50a35a2
schlegan <43997908+schlegan@users.noreply.github.com>
2023-12-12 13:48:36 +0100

    Benchmark::ResultTable : Add helper functions for column-based operations (#1174)
    Add some helper functions for adding statistics columns (that is columns with results that were not measured but computed from other columns) to the result table of a benchmark run.

---
+++ test/util/BenchmarkMeasurementContainerHelpers.cpp
@@ -0,0 +1,38 @@
+// Copyright 2023, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Andre Schlegel (November of 2023,
+// schlegea@informatik.uni-freiburg.de)
+
+#include "../test/util/BenchmarkMeasurementContainerHelpers.h"
+
+using namespace std::string_literals;
+
+// ____________________________________________________________________________
+template <typename Type>
+requires ad_utility::isTypeContainedIn<Type,
+                                       ad_benchmark::ResultTable::EntryType>
+Type createDummyValueEntryType() {
+  if constexpr (ad_utility::isSimilar<Type, float>) {
+    return 4.2f;
+  } else if constexpr (ad_utility::isSimilar<Type, std::string>) {
+    return "test"s;
+  } else if constexpr (ad_utility::isSimilar<Type, bool>) {
+    return true;
+  } else if constexpr (ad_utility::isSimilar<Type, size_t>) {
+    return 17361644613946UL;
+  } else if constexpr (ad_utility::isSimilar<Type, int>) {
+    return -42;
+  } else {
+    // Not a supported type.
+    AD_FAIL();
+  }
+}
+
+/*
+Explicit instantiation for all types in `ad_benchmark::ResultTable::EntryType`.
+*/
+template bool createDummyValueEntryType<bool>();
+template std::string createDummyValueEntryType<std::string>();
+template int createDummyValueEntryType<int>();
+template size_t createDummyValueEntryType<size_t>();
+template float createDummyValueEntryType<float>();
