16c690428a651dd07f1a2620fd1d4ca913984a0e
Benedikt Maria Beckermann <benedikt.beckermann@dagstuhl.de>
2024-01-18 15:38:27 +0100

    * Rebase

---
+++ src/parser/LiteralType.cpp
@@ -0,0 +1,42 @@
+// Copyright 2023, University of Freiburg,
+// Chair of Algorithms and Data Structures
+// Authors:
+//   2023 -    Benedikt Maria Beckermann <benedikt.beckermann@dagstuhl.de>
+
+#include "LiteralType.h"
+
+LiteralType::LiteralType(std::string content) {
+  this->content = content;
+  this->descriptorType = LiteralDescriptor::NONE;
+}
+
+LiteralType::LiteralType(std::string content, std::string datatypeOrLanguageTag,
+                         LiteralDescriptor type) {
+  this->content = content;
+  this->descriptorType = type;
+  this->descriptorValue = datatypeOrLanguageTag;
+}
+
+bool LiteralType::hasLanguageTag() const {
+  return this->descriptorType == LiteralDescriptor::LANGUAGE_TAG;
+}
+
+bool LiteralType::hasDatatype() const {
+  return this->descriptorType == LiteralDescriptor::DATATYPE;
+}
+
+std::string_view LiteralType::getContent() const { return this->content; }
+
+std::string_view LiteralType::getDatatype() const {
+  if (!hasDatatype()) {
+    AD_THROW("The literal does not have an explicit datatype.");
+  }
+  return this->descriptorValue;
+}
+
+std::string_view LiteralType::getLanguageTag() const {
+  if (!hasLanguageTag()) {
+    AD_THROW("The literal does not have an explicit language tag.");
+  }
+  return this->descriptorValue;
+}
---
+++ src/parser/LiteralType.h
@@ -0,0 +1,44 @@
+// Copyright 2023, University of Freiburg,
+// Chair of Algorithms and Data Structures
+// Authors:
+//   2023 -    Benedikt Maria Beckermann <benedikt.beckermann@dagstuhl.de>
+
+#pragma once
+
+enum LiteralDescriptor { NONE, LANGUAGE_TAG, DATATYPE };
+
+class LiteralType {
+ private:
+  // Stores the string value of the literal
+  std::string content;
+  // Stores the optional language tag or the optional datatype if applicable
+  std::string descriptorValue;
+  // Stores information if the literal has a language tag, a datatype, or non of
+  // these two assinged to it
+  LiteralDescriptor descriptorType;
+
+ public:
+  // Creates a new literal without any descriptor
+  LiteralType(std::string content);
+
+  // Created a new literal with the given descriptor
+  LiteralType(std::string content, std::string datatypeOrLanguageTag,
+              LiteralDescriptor type);
+
+  // Returns true if the literal has an assigned language tag
+  bool hasLanguageTag() const;
+
+  // Returns true if the literal has an assigned datatype
+  bool hasDatatype() const;
+
+  // Returns the value of the literal, without any datatype or language tag
+  std::string_view getContent() const;
+
+  // Returns the language tag of the literal if available.
+  // Throws an exception if the literal has no language tag.
+  std::string_view getLanguageTag() const;
+
+  // Returns the datatype of the literal if available.
+  // Throws an exception if the literal has no datatype.
+  std::string_view getDatatype() const;
+};
