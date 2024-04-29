6d0f42a8b4c70bc906135e9cc54478d4c43136e2
realHannes <realhannes@outlook.com>
2024-04-29 16:07:40 +0200

    made clang-format for NaryExpressionImpl.h

--- src/engine/sparqlExpressions/NaryExpressionImpl.h
+++ src/engine/sparqlExpressions/NaryExpressionImpl.h
@@ -6,8 +6,8 @@
 
 #include <ranges>
 
-#include "absl/strings/charconv.h"
 #include "absl/strings/ascii.h"
+#include "absl/strings/charconv.h"
 #include "engine/sparqlExpressions/NaryExpression.h"
 #include "engine/sparqlExpressions/SparqlExpressionGenerators.h"
 #include "engine/sparqlExpressions/SparqlExpressionValueGetters.h"
