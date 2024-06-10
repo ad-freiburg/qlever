fdb93533e25717773611eaf317540d1051586fe2
realHannes <realhannes@outlook.com>
2024-06-10 18:31:32 +0200

    clang formatting

--- src/engine/sparqlExpressions/RdfTermExpressions.cpp
+++ src/engine/sparqlExpressions/RdfTermExpressions.cpp
@@ -29,7 +29,7 @@ inline auto getDatatype = [](OptLiteral input) -> IdOrLiteralOrIri {
   }
 };
 
-  using GetDatatype = NARY<1, FV<decltype(getDatatype), LiteralValueGetter>>;
+using GetDatatype = NARY<1, FV<decltype(getDatatype), LiteralValueGetter>>;
 }  //  namespace detail
 
 using namespace detail;
