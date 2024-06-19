1be8381aa1d3d162182c97b316b30b59555403e0
Hannes Baumann <116301375+realHannes@users.noreply.github.com>
2024-06-19 21:04:06 +0200

    Implemented function `NOW()` (#1377)
    * Added conversion str to int
    
    * templated function for toNumeric, add declaration to NaryExpression.h
    
    * str to num for SparqlExpression implemented + added test
    
    * Update src/engine/sparqlExpressions/StringExpressions.cpp
    
    Co-authored-by: Johannes Kalmbach <joka921@users.noreply.github.com>
    
    * Update src/engine/sparqlExpressions/StringExpressions.cpp
    
    Co-authored-by: Johannes Kalmbach <joka921@users.noreply.github.com>
    
    * Update src/engine/sparqlExpressions/StringExpressions.cpp
    
    Co-authored-by: Johannes Kalmbach <joka921@users.noreply.github.com>
    
    * Update src/engine/sparqlExpressions/StringExpressions.cpp
    
    Co-authored-by: Johannes Kalmbach <joka921@users.noreply.github.com>
    
    * using now absl::from_chars() and stripping whitespaces for string to number conv.
    
    * added new functions to processIriFuntionCall() (for string to number)
    
    * renaming to: toIntExpression and toDoubleExpression for later more general implementation
    
    * made format (clang-format-16)
    
    * Update src/parser/sparqlParser/SparqlQleverVisitor.cpp
    
    Co-authored-by: Johannes Kalmbach <joka921@users.noreply.github.com>
    
    * Update src/parser/sparqlParser/SparqlQleverVisitor.cpp
    
    Co-authored-by: Johannes Kalmbach <joka921@users.noreply.github.com>
    
    * renaming in NaryExpression.h for accordance with other function, adding correct prefix in Constants.h
    
    * added test coverage for function calls makeIntExpression and make DoubleExpression
    
    * toNumeric has now correct behavior and uses absl::from_chars() and std::from_chars()
    
    * made clang-format for NaryExpressionImpl.h
    
    * first implementation NOW() expression
    
    * fix spelling error
    
    * pass by reference
    
    * changes w.r.t. pull-request comments
    
    * last changes for #1377
    
    ---------
    
    Co-authored-by: Johannes Kalmbach <joka921@users.noreply.github.com>

--- src/parser/sparqlParser/SparqlQleverVisitor.cpp
+++ src/parser/sparqlParser/SparqlQleverVisitor.cpp
@@ -13,16 +13,20 @@
 #include <string>
 #include <vector>
 
+#include "absl/time/time.h"
+#include "engine/sparqlExpressions/GroupConcatExpression.h"
 #include "engine/sparqlExpressions/LangExpression.h"
+#include "engine/sparqlExpressions/LiteralExpression.h"
+#include "engine/sparqlExpressions/NowDatetimeExpression.h"
 #include "engine/sparqlExpressions/RandomExpression.h"
 #include "engine/sparqlExpressions/RegexExpression.h"
 #include "engine/sparqlExpressions/RelationalExpressions.h"
+#include "engine/sparqlExpressions/SampleExpression.h"
 #include "engine/sparqlExpressions/UuidExpressions.h"
 #include "parser/SparqlParser.h"
 #include "parser/TokenizerCtre.h"
 #include "parser/TurtleParser.h"
 #include "parser/data/Variable.h"
-#include "util/OnDestructionDontThrowDuringStackUnwinding.h"
 #include "util/StringUtils.h"
 #include "util/TransparentFunctors.h"
 #include "util/antlr/GenerateAntlrExceptionMetadata.h"
@@ -53,6 +57,12 @@ std::string Visitor::getOriginalInputForContext(
   // exists when the result of this call is used. Not performance-critical.
   return std::string{
       ad_utility::getUTF8Substring(fullInput, posBeg, posEnd - posBeg + 1)};
+}
+
+// _____________________________________________________________________________
+std::string Visitor::currentTimeAsXsdString() {
+  return absl::FormatTime("%Y-%m-%dT%H:%M:%E3S%Ez", absl::Now(),
+                          absl::LocalTimeZone());
 }
 
 // ___________________________________________________________________________
@@ -2001,6 +2011,9 @@ ExpressionPtr Visitor::visit([[maybe_unused]] Parser::BuiltInCallContext* ctx) {
     return createUnary(&makeDayExpression);
   } else if (functionName == "tz") {
     return createUnary(&makeTimezoneStrExpression);
+  } else if (functionName == "now") {
+    AD_CONTRACT_CHECK(argList.empty());
+    return std::make_unique<NowDatetimeExpression>(startTime_);
   } else if (functionName == "hours") {
     return createUnary(&makeHoursExpression);
   } else if (functionName == "minutes") {
--- test/SparqlAntlrParserTest.cpp
+++ test/SparqlAntlrParserTest.cpp
@@ -18,6 +18,7 @@
 #include "SparqlAntlrParserTestHelpers.h"
 #include "engine/sparqlExpressions/LangExpression.h"
 #include "engine/sparqlExpressions/LiteralExpression.h"
+#include "engine/sparqlExpressions/NowDatetimeExpression.h"
 #include "engine/sparqlExpressions/RandomExpression.h"
 #include "engine/sparqlExpressions/RegexExpression.h"
 #include "engine/sparqlExpressions/UuidExpressions.h"
@@ -1385,6 +1386,7 @@ TEST(SparqlParser, builtInCall) {
   expectBuiltInCall("month(?x)", matchUnary(&makeMonthExpression));
   expectBuiltInCall("tz(?x)", matchUnary(&makeTimezoneStrExpression));
   expectBuiltInCall("day(?x)", matchUnary(&makeDayExpression));
+  expectBuiltInCall("NOW()", matchPtr<NowDatetimeExpression>());
   expectBuiltInCall("hours(?x)", matchUnary(&makeHoursExpression));
   expectBuiltInCall("minutes(?x)", matchUnary(&makeMinutesExpression));
   expectBuiltInCall("seconds(?x)", matchUnary(&makeSecondsExpression));
