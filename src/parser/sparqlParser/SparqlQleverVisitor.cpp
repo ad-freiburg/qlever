833b95192d084edd964575fc9bd65f7a39e7a2ba
Johannes Kalmbach <joka921@users.noreply.github.com>
2024-07-05 15:16:00 +0200

    Proper handling of (all kinds of) empty queries (#1383)
    Empty queries were properly handled so far (before the parsing), but queries that consisted only of whitespace, comments, or prefix declarations were not. The first two produced a segmentation fault, the last produced a confusing error message. Now none of these queries lead to a segmentation fault anymore, and in each case, the same proper error message is returned.

--- src/parser/sparqlParser/SparqlQleverVisitor.cpp
+++ src/parser/sparqlParser/SparqlQleverVisitor.cpp
@@ -184,7 +184,17 @@ ParsedQuery Visitor::visit(Parser::QueryContext* ctx) {
 // ____________________________________________________________________________________
 ParsedQuery Visitor::visit(Parser::QueryOrUpdateContext* ctx) {
   if (ctx->update()) {
-    reportNotSupported(ctx->update(), "SPARQL 1.1 Update");
+    // An empty query currently matches the `update()` rule. We handle this
+    // case manually to get a better error message. If an update query doesn't
+    // have an `update1()`, then it consists of a (possibly empty) prologue, but
+    // has not actual content, see the grammar in `SparqlAutomatic.g4` for
+    // details.
+    if (!ctx->update()->update1()) {
+      reportError(ctx->update(),
+                  "Empty query (this includes queries that only consist "
+                  "of comments or prefix declarations).");
+    }
+    reportNotSupported(ctx->update(), "SPARQL 1.1 Update is");
   } else {
     return visit(ctx->query());
   }
