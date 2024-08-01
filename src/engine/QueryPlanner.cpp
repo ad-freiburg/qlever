ad-freiburg/qlever

commit bb6e2f9e0063a13d04c575f11f417928d34934fe

Add a VALUES clause to the query of a SERVICE clause to simplify the execution (#1341)
If the query tree, with which the result of a `SERVICE` clause is joined, leads to a small result (The definition of "small" can be set by a runtime parameter, the current default is 100), then this result is added to the query that is sent to the endpoint of the `SERVICE`, making it much easier to process, and the result of the `SERVICE` query potentially much smaller.

src/engine/QueryPlanner.cpp
@@ -1642,6 +1642,13 @@ std::vector<QueryPlanner::SubtreePlan> QueryPlanner::createJoinCandidates(
     return {makeSubtreePlan<OptionalJoin>(_qec, a._qet, b._qet)};
   }
 
+  // Check if one of the two Operations is a SERVICE. If so, we can try
+  // to simplify the Service Query using the result of the other operation.
+  if (auto opt = createJoinWithService(a, b, jcs)) {
+    candidates.push_back(std::move(opt.value()));
+    return candidates;
+  }
+
   if (jcs.size() >= 2) {
     // If there are two or more join columns and we are not using the
     // TwoColumnJoin (the if part before this comment), use a multiColumnJoin.
@@ -1767,6 +1774,35 @@ auto QueryPlanner::createJoinWithHasPredicateScan(
   auto plan = makeSubtreePlan<HasPredicateScan>(
       qec, std::move(otherTree), otherTreeJoinColumn, std::move(object));
   mergeSubtreePlanIds(plan, a, b);
+  return plan;
+}
+
+// _____________________________________________________________________
+auto QueryPlanner::createJoinWithService(
+    SubtreePlan a, SubtreePlan b,
+    const std::vector<std::array<ColumnIndex, 2>>& jcs)
+    -> std::optional<SubtreePlan> {
+  auto aRootOp = std::dynamic_pointer_cast<Service>(a._qet->getRootOperation());
+  auto bRootOp = std::dynamic_pointer_cast<Service>(b._qet->getRootOperation());
+
+  // Exactly one of the two Operations can be a service.
+  if (static_cast<bool>(aRootOp) == static_cast<bool>(bRootOp)) {
+    return std::nullopt;
+  }
+
+  auto service = aRootOp ? aRootOp : bRootOp;
+  auto sibling = bRootOp ? a : b;
+
+  service->setSiblingTree(sibling._qet);
+
+  const auto& qec = service->getExecutionContext();
+
+  SubtreePlan plan =
+      jcs.size() == 1
+          ? makeSubtreePlan<Join>(qec, a._qet, b._qet, jcs[0][0], jcs[0][1])
+          : makeSubtreePlan<MultiColumnJoin>(qec, a._qet, b._qet);
+  mergeSubtreePlanIds(plan, a, b);
+
   return plan;
 }
 

src/engine/QueryPlanner.h
@@ -329,6 +329,10 @@ class QueryPlanner {
       SubtreePlan a, SubtreePlan b,
       const std::vector<std::array<ColumnIndex, 2>>& jcs);
 
+  [[nodiscard]] static std::optional<SubtreePlan> createJoinWithService(
+      SubtreePlan a, SubtreePlan b,
+      const std::vector<std::array<ColumnIndex, 2>>& jcs);
+
   [[nodiscard]] vector<SubtreePlan> getOrderByRow(
       const ParsedQuery& pq,
       const std::vector<std::vector<SubtreePlan>>& dpTab) const;

src/engine/Service.h
@@ -46,6 +46,9 @@ class Service : public Operation {
   // The function used to obtain the result from the remote endpoint.
   GetTsvFunction getTsvFunction_;
 
+  // The siblingTree, used for SERVICE clause optimization.
+  std::shared_ptr<QueryExecutionTree> siblingTree_;
+
  public:
   // Construct from parsed Service clause.
   //
@@ -54,13 +57,26 @@ class Service : public Operation {
   // but in our tests (`ServiceTest`) we use a mock function that does not
   // require a running `HttpServer`.
   Service(QueryExecutionContext* qec, parsedQuery::Service parsedServiceClause,
-          GetTsvFunction getTsvFunction = sendHttpOrHttpsRequest);
+          GetTsvFunction getTsvFunction = sendHttpOrHttpsRequest,
+          std::shared_ptr<QueryExecutionTree> siblingTree = nullptr);
+
+  // Set the siblingTree (subTree that will later be joined with the Result of
+  // the Service Operation), used to reduce the Service Queries Complexity.
+  void setSiblingTree(std::shared_ptr<QueryExecutionTree> siblingTree) {
+    siblingTree_ = siblingTree;
+  }
 
   // Methods inherited from base class `Operation`.
   std::string getDescriptor() const override;
   size_t getResultWidth() const override;
   std::vector<ColumnIndex> resultSortedOn() const override { return {}; }
   float getMultiplicity(size_t col) override;
+
+  // Getters for testing.
+  const auto& getSiblingTree() const { return siblingTree_; }
+  const auto& getGraphPatternAsString() const {
+    return parsedServiceClause_.graphPatternAsString_;
+  }
 
  private:
   uint64_t getSizeEstimateBeforeLimit() override;
@@ -81,6 +97,9 @@ class Service : public Operation {
 
   // Compute the result using `getTsvFunction_`.
   Result computeResult([[maybe_unused]] bool requestLaziness) override;
+
+  // Get a VALUES clause that contains the values of the siblingTree's result.
+  std::optional<std::string> getSiblingValuesClause() const;
 
   // Write the given TSV result to the given result object. The `I` is the width
   // of the result table.

test/ServiceTest.cpp
@@ -8,9 +8,11 @@
 #include <regex>
 
 #include "engine/Service.h"
+#include "global/RuntimeParameters.h"
 #include "parser/GraphPatternOperation.h"
 #include "util/IdTableHelpers.h"
 #include "util/IndexTestHelpers.h"
+#include "util/TripleComponentTestHelpers.h"
 #include "util/http/HttpUtils.h"
 
 // Fixture that sets up a test index and a factory for producing mocks for the
@@ -192,4 +194,100 @@ TEST_F(ServiceTest, computeResult) {
   IdTable expectedIdTable = makeIdTableFromVector(
       {{idX, idY}, {idBla, idBli}, {idBlu, idBla}, {idBli, idBlu}});
   EXPECT_EQ(result->idTable(), expectedIdTable);
+
+  // Check 5: When a siblingTree with variables common to the Service Clause is
+  // passed, the Service Operation shall use the siblings result to reduce
+  // its Query complexity by injecting them as Value Clause
+  auto iri = ad_utility::testing::iri;
+  using TC = TripleComponent;
+  auto siblingTree = std::make_shared<QueryExecutionTree>(
+      testQec,
+      std::make_shared<Values>(
+          testQec,
+          (parsedQuery::SparqlValues){
+              {Variable{"?x"}, Variable{"?y"}, Variable{"?z"}},
+              {{TC(iri("<x>")), TC(iri("<y>")), TC(iri("<z>"))},
+               {TC(iri("<x>")), TC(iri("<y>")), TC(iri("<z2>"))},
+               {TC(iri("<blu>")), TC(iri("<bla>")), TC(iri("<blo>"))}}}));
+
+  auto parsedServiceClause5 = parsedServiceClause;
+  parsedServiceClause5.graphPatternAsString_ =
+      "{ ?x <ble> ?y . ?y <is-a> ?z2 . }";
+  parsedServiceClause5.visibleVariables_.emplace_back("?z2");
+
+  std::string_view expectedSparqlQuery5 =
+      "PREFIX doof: <http://doof.org> SELECT ?x ?y ?z2 "
+      "WHERE { VALUES (?x ?y) { (<x> <y>) (<blu> <bla>) } . ?x <ble> ?y . ?y "
+      "<is-a> ?z2 . }";
+
+  Service serviceOperation5{
+      testQec, parsedServiceClause5,
+      getTsvFunctionFactory(expectedUrl, expectedSparqlQuery5,
+                            "?x\t?y\t?z2\n<x>\t<y>\t<y>\n<bla>\t<bli>\t<y>\n<"
+                            "blu>\t<bla>\t<y>\n<bli>\t<blu>\t<y>\n"),
+      siblingTree};
+  EXPECT_NO_THROW(serviceOperation5.getResult());
+
+  // Check 6: SiblingTree's rows exceed maxValue
+  const auto maxValueRowsDefault =
+      RuntimeParameters().get<"service-max-value-rows">();
+  RuntimeParameters().set<"service-max-value-rows">(0);
+  testQec->getQueryTreeCache().clearAll();
+  std::string_view expectedSparqlQuery6 =
+      "PREFIX doof: <http://doof.org> SELECT ?x ?y ?z2 "
+      "WHERE { ?x <ble> ?y . ?y <is-a> ?z2 . }";
+  Service serviceOperation6{
+      testQec, parsedServiceClause5,
+      getTsvFunctionFactory(expectedUrl, expectedSparqlQuery6,
+                            "?x\t?y\t?z2\n<x>\t<y>\t<y>\n<bla>\t<bli>\t<y>\n<"
+                            "blu>\t<bla>\t<y>\n<bli>\t<blu>\t<y>\n"),
+      siblingTree};
+  EXPECT_NO_THROW(serviceOperation6.getResult());
+  RuntimeParameters().set<"service-max-value-rows">(maxValueRowsDefault);
+}
+
+TEST_F(ServiceTest, getCacheKey) {
+  parsedQuery::Service parsedServiceClause{{Variable{"?x"}, Variable{"?y"}},
+                                           Iri{"<http://localhorst/api>"},
+                                           "PREFIX doof: <http://doof.org>",
+                                           "{ }"};
+
+  // The cacheKey of the Service Operation has to depend on the cacheKey of
+  // the siblingTree, as it might alter the Service Query.
+
+  Service service(
+      testQec, parsedServiceClause,
+      getTsvFunctionFactory(
+          "http://localhorst:80/api",
+          "PREFIX doof: <http://doof.org> SELECT ?x ?y WHERE { }",
+          "?x\t?y\n<x>\t<y>\n<bla>\t<bli>\n<blu>\t<bla>\n<bli>\t<blu>\n"));
+
+  auto ck_noSibling = service.getCacheKey();
+
+  auto iri = ad_utility::testing::iri;
+  using TC = TripleComponent;
+  auto siblingTree = std::make_shared<QueryExecutionTree>(
+      testQec,
+      std::make_shared<Values>(
+          testQec,
+          (parsedQuery::SparqlValues){
+              {Variable{"?x"}, Variable{"?y"}, Variable{"?z"}},
+              {{TC(iri("<x>")), TC(iri("<y>")), TC(iri("<z>"))},
+               {TC(iri("<blu>")), TC(iri("<bla>")), TC(iri("<blo>"))}}}));
+  service.setSiblingTree(siblingTree);
+
+  auto ck_sibling = service.getCacheKey();
+  EXPECT_NE(ck_noSibling, ck_sibling);
+
+  auto siblingTree2 = std::make_shared<QueryExecutionTree>(
+      testQec,
+      std::make_shared<Values>(
+          testQec, (parsedQuery::SparqlValues){
+                       {Variable{"?x"}, Variable{"?y"}, Variable{"?z"}},
+                       {{TC(iri("<x>")), TC(iri("<y>")), TC(iri("<z>"))}}}));
+
+  service.setSiblingTree(siblingTree2);
+
+  auto ck_changedSibling = service.getCacheKey();
+  EXPECT_NE(ck_sibling, ck_changedSibling);
 }

src/engine/Service.cpp
@@ -5,11 +5,14 @@
 #include "engine/Service.h"
 
 #include <absl/strings/str_cat.h>
+#include <absl/strings/str_join.h>
 #include <absl/strings/str_split.h>
 
 #include "engine/CallFixedSize.h"
+#include "engine/ExportQueryExecutionTrees.h"
 #include "engine/Values.h"
 #include "engine/VariableToColumnMap.h"
+#include "global/RuntimeParameters.h"
 #include "parser/TokenizerCtre.h"
 #include "parser/TurtleParser.h"
 #include "util/Exception.h"
@@ -21,10 +24,12 @@
 // ____________________________________________________________________________
 Service::Service(QueryExecutionContext* qec,
                  parsedQuery::Service parsedServiceClause,
-                 GetTsvFunction getTsvFunction)
+                 GetTsvFunction getTsvFunction,
+                 std::shared_ptr<QueryExecutionTree> siblingTree)
     : Operation{qec},
       parsedServiceClause_{std::move(parsedServiceClause)},
-      getTsvFunction_{std::move(getTsvFunction)} {}
+      getTsvFunction_{std::move(getTsvFunction)},
+      siblingTree_{std::move(siblingTree)} {}
 
 // ____________________________________________________________________________
 std::string Service::getCacheKeyImpl() const {
@@ -32,7 +37,11 @@ std::string Service::getCacheKeyImpl() const {
   // TODO: This duplicates code in GraphPatternOperation.cpp .
   os << "SERVICE " << parsedServiceClause_.serviceIri_.toSparql() << " {\n"
      << parsedServiceClause_.prologue_ << "\n"
-     << parsedServiceClause_.graphPatternAsString_ << "\n}\n";
+     << parsedServiceClause_.graphPatternAsString_ << "\n";
+  if (siblingTree_ != nullptr) {
+    os << siblingTree_->getRootOperation()->getCacheKey() << "\n";
+  }
+  os << "}\n";
   return std::move(os).str();
 }
 
@@ -91,6 +100,14 @@ Result Service::computeResult([[maybe_unused]] bool requestLaziness) {
   serviceIriString.remove_prefix(1);
   serviceIriString.remove_suffix(1);
   ad_utility::httpUtils::Url serviceUrl{serviceIriString};
+
+  // Try to simplify the Service Query using it's sibling Operation.
+  if (auto valuesClause = getSiblingValuesClause(); valuesClause.has_value()) {
+    auto openBracketPos = parsedServiceClause_.graphPatternAsString_.find('{');
+    parsedServiceClause_.graphPatternAsString_ =
+        "{\n" + valuesClause.value() + '\n' +
+        parsedServiceClause_.graphPatternAsString_.substr(openBracketPos + 1);
+  }
 
   // Construct the query to be sent to the SPARQL endpoint.
   std::string variablesForSelectClause = absl::StrJoin(
@@ -157,6 +174,66 @@ Result Service::computeResult([[maybe_unused]] bool requestLaziness) {
                   std::move(tsvResult), &idTable, &localVocab);
 
   return {std::move(idTable), resultSortedOn(), std::move(localVocab)};
+}
+
+// ____________________________________________________________________________
+std::optional<std::string> Service::getSiblingValuesClause() const {
+  if (siblingTree_ == nullptr) {
+    return std::nullopt;
+  }
+
+  const auto& siblingResult = siblingTree_->getResult();
+  if (siblingResult->idTable().size() >
+      RuntimeParameters().get<"service-max-value-rows">()) {
+    return std::nullopt;
+  }
+
+  checkCancellation();
+
+  std::vector<ColumnIndex> commonColumnIndices;
+  const auto& siblingVars = siblingTree_->getVariableColumns();
+  std::string vars = "(";
+  for (const auto& localVar : parsedServiceClause_.visibleVariables_) {
+    auto it = siblingVars.find(localVar);
+    if (it == siblingVars.end()) {
+      continue;
+    }
+    absl::StrAppend(&vars, it->first.name(), " ");
+    commonColumnIndices.push_back(it->second.columnIndex_);
+  }
+  vars.back() = ')';
+
+  checkCancellation();
+
+  ad_utility::HashSet<std::string> rowSet;
+
+  std::string values = " { ";
+  for (size_t rowIndex = 0; rowIndex < siblingResult->idTable().size();
+       ++rowIndex) {
+    std::string row = "(";
+    for (const auto& columnIdx : commonColumnIndices) {
+      const auto& optionalString = ExportQueryExecutionTrees::idToStringAndType(
+          siblingTree_->getRootOperation()->getIndex(),
+          siblingResult->idTable()(rowIndex, columnIdx),
+          siblingResult->localVocab());
+
+      if (optionalString.has_value()) {
+        absl::StrAppend(&row, optionalString.value().first, " ");
+      }
+    }
+    row.back() = ')';
+
+    if (rowSet.contains(row)) {
+      continue;
+    }
+
+    rowSet.insert(row);
+    absl::StrAppend(&values, row, " ");
+
+    checkCancellation();
+  }
+
+  return "VALUES " + vars + values + "} . ";
 }
 
 // ____________________________________________________________________________

src/engine/Values.cpp
@@ -130,8 +130,10 @@ void Values::writeValues(IdTable* idTablePtr, LocalVocab* localVocab) {
   std::vector<size_t> numLocalVocabPerColumn(idTable.numColumns());
   for (auto& row : parsedValues_._values) {
     for (size_t colIdx = 0; colIdx < idTable.numColumns(); colIdx++) {
-      TripleComponent& tc = row[colIdx];
-      Id id = std::move(tc).toValueId(getIndex().getVocab(), *localVocab);
+      const TripleComponent& tc = row[colIdx];
+      // TODO<joka921> We don't want to move, but also don't want to
+      // unconditionally copy.
+      Id id = TripleComponent{tc}.toValueId(getIndex().getVocab(), *localVocab);
       idTable(rowIdx, colIdx) = id;
       if (id.getDatatype() == Datatype::LocalVocabIndex) {
         ++numLocalVocabPerColumn[colIdx];

src/global/RuntimeParameters.h
@@ -46,7 +46,8 @@ inline auto& RuntimeParameters() {
                 30s}),
         SizeT<"lazy-index-scan-max-size-materialization">{1'000'000},
         Bool<"use-binsearch-transitive-path">{true},
-        Bool<"group-by-hash-map-enabled">{false}};
+        Bool<"group-by-hash-map-enabled">{false},
+        SizeT<"service-max-value-rows">{100}};
   }();
   return params;
 }

test/QueryPlannerTest.cpp
@@ -1062,3 +1062,29 @@ TEST(QueryPlanner, CancellationCancelsQueryPlanning) {
                                         HasSubstr("Query planning"),
                                         ad_utility::CancellationException);
 }
+
+// ___________________________________________________________________________
+TEST(QueryPlanner, JoinWithService) {
+  auto scan = h::IndexScanFromStrings;
+
+  auto sibling = scan("?x", "<is-a>", "?y");
+
+  std::string_view graphPatternAsString = "{ ?x <is-a> ?z . }";
+
+  h::expect(
+      "SELECT * WHERE {"
+      "SERVICE <https://endpoint.com> { ?x <is-a> ?z . ?y <is-a> ?a . }}",
+      h::Service(std::nullopt, "{ ?x <is-a> ?z . ?y <is-a> ?a . }"));
+
+  h::expect(
+      "SELECT * WHERE { ?x <is-a> ?y ."
+      "SERVICE <https://endpoint.com> { ?x <is-a> ?z . }}",
+      h::UnorderedJoins(sibling, h::Service(sibling, graphPatternAsString)));
+
+  h::expect(
+      "SELECT * WHERE { ?x <is-a> ?y . "
+      "SERVICE <https://endpoint.com> { ?x <is-a> ?z . ?y <is-a> ?a . }}",
+      h::MultiColumnJoin(
+          sibling,
+          h::Sort(h::Service(sibling, "{ ?x <is-a> ?z . ?y <is-a> ?a . }"))));
+}

commit 3a6a791c199fd436ac8ff6cc04b110d5bd4f4e09

Fix query planning for join with `SERVICE` clause (#1422)
There is a bug in the optimization introduced by #1341, which checks whether the sibling operand of a join with a `SERVICE` operations has a small result (and if yes, passes that result to the `SERVICE` clause as `VALUES` in order to reduce the result size of the `SERVICE` call). Instead of just adding the sibling operand to the existing `Service` operation (which led to an object that should have been `const` being changed), now a new `Service` operation that includes the sibling is created. In particular, this fixes https://qlever.cs.uni-freiburg.de/osm-planet, which worked before #1341 and now works again as it should.

src/engine/QueryPlanner.cpp
@@ -1779,28 +1779,42 @@ auto QueryPlanner::createJoinWithHasPredicateScan(
 
 // _____________________________________________________________________
 auto QueryPlanner::createJoinWithService(
-    SubtreePlan a, SubtreePlan b,
+    const SubtreePlan& a, const SubtreePlan& b,
     const std::vector<std::array<ColumnIndex, 2>>& jcs)
     -> std::optional<SubtreePlan> {
+  // We can only proceed if exactly one of the inputs is a `SERVICE`.
   auto aRootOp = std::dynamic_pointer_cast<Service>(a._qet->getRootOperation());
   auto bRootOp = std::dynamic_pointer_cast<Service>(b._qet->getRootOperation());
-
-  // Exactly one of the two Operations can be a service.
   if (static_cast<bool>(aRootOp) == static_cast<bool>(bRootOp)) {
     return std::nullopt;
   }
 
-  auto service = aRootOp ? aRootOp : bRootOp;
-  auto sibling = bRootOp ? a : b;
+  // Setup some variables that are agnostic of which of the two inputs is the
+  // serviceWithSibling clause, as these cases are completely symmetric.
+  const auto& [serviceIn, sibling, serviceIdx, siblingIdx] = [&]() {
+    // `std::tie` requires lvalue-references, that's why we define explicit
+    // variables for `0` and `1`.
+    static constexpr size_t zero = 0;
+    static constexpr size_t one = 1;
+    if (aRootOp) {
+      return std::tie(aRootOp, b, zero, one);
+    } else {
+      AD_CORRECTNESS_CHECK(bRootOp);
+      return std::tie(bRootOp, a, one, zero);
+    }
+  }();
 
-  service->setSiblingTree(sibling._qet);
-
-  const auto& qec = service->getExecutionContext();
+  auto serviceWithSibling =
+      makeSubtreePlan(serviceIn->createCopyWithSiblingTree(sibling._qet));
+  auto qec = serviceIn->getExecutionContext();
 
   SubtreePlan plan =
       jcs.size() == 1
-          ? makeSubtreePlan<Join>(qec, a._qet, b._qet, jcs[0][0], jcs[0][1])
-          : makeSubtreePlan<MultiColumnJoin>(qec, a._qet, b._qet);
+          ? makeSubtreePlan<Join>(qec, std::move(serviceWithSibling._qet),
+                                  sibling._qet, jcs[0][serviceIdx],
+                                  jcs[0][siblingIdx])
+          : makeSubtreePlan<MultiColumnJoin>(
+                qec, std::move(serviceWithSibling._qet), sibling._qet);
   mergeSubtreePlanIds(plan, a, b);
 
   return plan;

src/engine/QueryPlanner.h
@@ -330,7 +330,7 @@ class QueryPlanner {
       const std::vector<std::array<ColumnIndex, 2>>& jcs);
 
   [[nodiscard]] static std::optional<SubtreePlan> createJoinWithService(
-      SubtreePlan a, SubtreePlan b,
+      const SubtreePlan& a, const SubtreePlan& b,
       const std::vector<std::array<ColumnIndex, 2>>& jcs);
 
   [[nodiscard]] vector<SubtreePlan> getOrderByRow(

src/engine/Service.h
@@ -58,10 +58,19 @@ class Service : public Operation {
           GetResultFunction getResultFunction = sendHttpOrHttpsRequest,
           std::shared_ptr<QueryExecutionTree> siblingTree = nullptr);
 
-  // Set the siblingTree (subTree that will later be joined with the Result of
-  // the Service Operation), used to reduce the Service Queries Complexity.
-  void setSiblingTree(std::shared_ptr<QueryExecutionTree> siblingTree) {
-    siblingTree_ = siblingTree;
+  // Create a new `Service` operation, that is equal to `*this` but additionally
+  // respects the `siblingTree`. The sibling tree is a partial query that will
+  // later be joined with the result of the `Service`. If the result of the
+  // sibling is small, it will be used to constrain the SERVICE query using a
+  // `VALUES` clause.
+  [[nodiscard]] std::shared_ptr<Service> createCopyWithSiblingTree(
+      std::shared_ptr<QueryExecutionTree> siblingTree) const {
+    AD_CORRECTNESS_CHECK(siblingTree_ == nullptr);
+    // TODO<joka921> This copies the `parsedServiceClause_`. We could probably
+    // use a `shared_ptr` here to reduce the copying during QueryPlanning.
+    return std::make_shared<Service>(getExecutionContext(),
+                                     parsedServiceClause_, getResultFunction_,
+                                     std::move(siblingTree));
   }
 
   // Methods inherited from base class `Operation`.

test/ServiceTest.cpp
@@ -313,9 +313,9 @@ TEST_F(ServiceTest, getCacheKey) {
               {Variable{"?x"}, Variable{"?y"}, Variable{"?z"}},
               {{TC(iri("<x>")), TC(iri("<y>")), TC(iri("<z>"))},
                {TC(iri("<blu>")), TC(iri("<bla>")), TC(iri("<blo>"))}}}));
-  service.setSiblingTree(siblingTree);
 
-  auto ck_sibling = service.getCacheKey();
+  auto ck_sibling =
+      service.createCopyWithSiblingTree(siblingTree)->getCacheKey();
   EXPECT_NE(ck_noSibling, ck_sibling);
 
   auto siblingTree2 = std::make_shared<QueryExecutionTree>(
@@ -325,9 +325,9 @@ TEST_F(ServiceTest, getCacheKey) {
                        {Variable{"?x"}, Variable{"?y"}, Variable{"?z"}},
                        {{TC(iri("<x>")), TC(iri("<y>")), TC(iri("<z>"))}}}));
 
-  service.setSiblingTree(siblingTree2);
+  auto serviceWithSibling = service.createCopyWithSiblingTree(siblingTree2);
 
-  auto ck_changedSibling = service.getCacheKey();
+  auto ck_changedSibling = serviceWithSibling->getCacheKey();
   EXPECT_NE(ck_sibling, ck_changedSibling);
 }
 