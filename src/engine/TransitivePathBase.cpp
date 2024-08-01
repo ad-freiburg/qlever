229a284915a6f139247a980a156b298b1da70286
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2024-08-01 17:21:45 +0200

    Fix a bug in the bugfix.

--- src/engine/TransitivePathBase.cpp
+++ src/engine/TransitivePathBase.cpp
@@ -379,7 +379,7 @@ std::shared_ptr<TransitivePathBase> TransitivePathBase::bindLeftOrRightSide(
     AD_CORRECTNESS_CHECK(!p->variableColumns_.contains(variable));
     p->variableColumns_[variable] = columnIndexWithType;
   }
-  p->resultWidth_ += leftOrRightOp->getResultWidth();
+  p->resultWidth_ += leftOrRightOp->getResultWidth() - 1;
   return std::move(p);
 }
 
