be6b7b9438b6c8ab7dea1c1825ab57c17dba25d2
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2024-08-26 12:37:10 +0200

    Thought about the design and fixed it.
    Signed-off-by: Johannes Kalmbach <johannes.kalmbach@gmail.com>

--- src/index/IndexBuilderTypes.h
+++ src/index/IndexBuilderTypes.h
@@ -134,9 +134,12 @@ struct alignas(256) ItemMapManager {
   /// Construct by assigning the minimum ID that should be returned by the map.
   explicit ItemMapManager(uint64_t minId, const TripleComponentComparator* cmp,
                           ItemAlloc alloc)
-      : map_(alloc), minId_(minId), comparator_(cmp) {}
+      : map_(alloc), minId_(minId), comparator_(cmp) {
+    for (const auto& [iri, internalId] : qlever::specialIds) {
+      // TODO<joka921> Continue
+    }
 
-  /// Move the held HashMap out as soon as we are done inserting and only need
+    /// Move the held HashMap out as soon as we are done inserting and only need
   /// the actual vocabulary.
   ItemMapAndBuffer&& moveMap() && { return std::move(map_); }
 
@@ -174,6 +177,8 @@ struct alignas(256) ItemMapManager {
     return std::apply(
         [this](const auto&... els) { return std::array{getId(els)...}; }, t);
   }
+
+  ad_utility::HashMap<Id, Id> internalIdMapping_;
   ItemMapAndBuffer map_;
   uint64_t minId_ = 0;
   const TripleComponentComparator* comparator_ = nullptr;
