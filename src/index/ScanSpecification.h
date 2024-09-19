0b47e766cddff57177df7d3d2fce63b464a00d8b
Johannes Kalmbach <joka921@users.noreply.github.com>
2024-09-19 22:35:19 +0200

    Improve `ScanSpecification` + fix empty index scan bugs (#1495)
    The `ScanSpecification` class now has two additional members: a `LocalVocab` and a set of graphs. The `LocalVocab` enables that one or several of the `col0Id`, `col1Id`, `col2Id` can be new IRIs or literals (note that our comparators work fine even exactly the same IRI or literal is contained in different `LocalVocab`s). This is important for SPARQL Update. The set of graphs enables filtering of the scan result by one or severval graph IRIs.
    
    Also convert `ScanSpecificationAsTripelComponent` to `ScanSpecification` already in the `IndexScan` class (where we already have an `Index` available to convert the `TripleComponent`s to `Id`s). In a future change, we might want to get rid of `ScanSpecificationAsTripelComponent` altogether and produce a `ScanSpecification` right away.
    
    On the side, clean up the code and fix several bugs for empty results of index scan operations that only occur with lazy operations.

--- src/index/ScanSpecification.h
+++ src/index/ScanSpecification.h
@@ -20,26 +20,48 @@ class Index;
 // the knowledge graph at all. The values which are `nullopt` become variables
 // and are returned as columns in the result of the scan.
 class ScanSpecification {
- private:
+ public:
   using T = std::optional<Id>;
+  using Graphs = std::optional<ad_utility::HashSet<Id>>;
 
+ private:
   T col0Id_;
   T col1Id_;
   T col2Id_;
+  // A local vocab that is needed in case at least one of the `colXIds_` has
+  // type `LocalVocabIndex`. Note that this doesn't automatically mean that the
+  // scan result will be empty, because local vocab entries might also be
+  // created by SPARQL UPDATE requests.
+  // Note: This `localVocab` keeps the `colXIds` alive in that case. It is a
+  // serious bug, to copy the `colXIds` out of this class. The only valid usage
+  // is to compare them with other IDs as long as the `ScanSpecification` is
+  // still alive.
+  // TODO<joka921> This can be enforced by the type system, but that requires
+  // several intrusive changes in other parts of QLever.
+  std::shared_ptr<const LocalVocab> localVocab_;
+
+  // If specified (i.e. not `nullopt`) then the result of the scan only consists
+  // of triples that belong to the union of these graphs.
+  Graphs graphsToFilter_{};
   friend class ScanSpecificationAsTripleComponent;
 
   void validate() const;
 
  public:
-  ScanSpecification(T col0Id, T col1Id, T col2Id)
-      : col0Id_{col0Id}, col1Id_{col1Id}, col2Id_{col2Id} {
+  ScanSpecification(T col0Id, T col1Id, T col2Id, LocalVocab localVocab = {},
+                    Graphs graphsToFilter = std::nullopt)
+      : col0Id_{col0Id},
+        col1Id_{col1Id},
+        col2Id_{col2Id},
+        localVocab_{std::make_shared<LocalVocab>(std::move(localVocab))},
+        graphsToFilter_(std::move(graphsToFilter)) {
     validate();
   }
   const T& col0Id() const { return col0Id_; }
   const T& col1Id() const { return col1Id_; }
   const T& col2Id() const { return col2Id_; }
 
-  bool operator==(const ScanSpecification&) const = default;
+  const Graphs& graphsToFilter() const { return graphsToFilter_; }
 
   // Only used in tests.
   void setCol1Id(T col1Id) {
@@ -51,30 +73,28 @@ class ScanSpecification {
 // Same as `ScanSpecification` (see above), but stores `TripleComponent`s
 // instead of `Id`s.
 class ScanSpecificationAsTripleComponent {
+ public:
   using T = std::optional<TripleComponent>;
+  using Graphs = std::optional<ad_utility::HashSet<TripleComponent>>;
 
  private:
-  std::optional<TripleComponent> col0_;
-  std::optional<TripleComponent> col1_;
-  std::optional<TripleComponent> col2_;
+  T col0_;
+  T col1_;
+  T col2_;
+  Graphs graphsToFilter_{};
 
  public:
   // Construct from three optional `TripleComponent`s. If any of the three
   // entries is unbound (`nullopt` or of type `Variable`), then all subsequent
   // entries also have to be unbound. For example if `col0` is bound, but `col1`
   // isn't, then `col2` also has to be unbound.
-  ScanSpecificationAsTripleComponent(T col0, T col1, T col2);
+  ScanSpecificationAsTripleComponent(T col0, T col1, T col2,
+                                     Graphs graphsToFilter = std::nullopt);
 
   // Convert to a `ScanSpecification`. The `index` is used to convert the
-  // `TripleComponent` to `Id`s by looking them up in the vocabulary. Return
-  // `nullopt` if and only if one of the vocab lookup fails (then the result of
-  // the corresponding scan will be empty).
-  // TODO<joka921> Once we implement SPARQL UPDATE, we possibly also to use the
-  // `LocalVocab` of the UPDATE triples here.
-  std::optional<ScanSpecification> toScanSpecification(
-      const IndexImpl& index) const;
-  std::optional<ScanSpecification> toScanSpecification(
-      const Index& index) const;
+  // `TripleComponent` to `Id`s by looking them up in the vocabulary.
+  ScanSpecification toScanSpecification(const IndexImpl& index) const;
+  ScanSpecification toScanSpecification(const Index& index) const;
 
   // The number of columns that the corresponding index scan will have.
   size_t numColumns() const;
