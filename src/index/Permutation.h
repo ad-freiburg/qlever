d59bd43002b9d733810d20e988a9d393e2b1cfb2
Julian <14220769+Qup42@users.noreply.github.com>
2024-10-04 09:18:36 +0200

    SPARQL Update Part 2: DeltaTriples (#1429)
    `DeltaTriples` manage on `LocatedTriples` object per permutation. Insertions/Deletions of (sets of) triples are processed by `DeltaTriples`.

--- src/index/Permutation.h
+++ src/index/Permutation.h
@@ -31,6 +31,8 @@ class Permutation {
   static constexpr auto SOP = Enum::SOP;
   static constexpr auto OPS = Enum::OPS;
   static constexpr auto OSP = Enum::OSP;
+  static constexpr auto ALL = {Enum::PSO, Enum::POS, Enum::SPO,
+                               Enum::SOP, Enum::OPS, Enum::OSP};
 
   using MetaData = IndexMetaDataMmapView;
   using Allocator = ad_utility::AllocatorWithLimit<Id>;
@@ -127,6 +129,9 @@ class Permutation {
   // _______________________________________________________
   const bool& isLoaded() const { return isLoaded_; }
 
+  // _______________________________________________________
+  const MetaData& metaData() const { return meta_; }
+
  private:
   // for Log output, e.g. "POS"
   std::string readableName_;
@@ -136,7 +141,6 @@ class Permutation {
   // sorted, for example {1, 0, 2} for PSO.
   array<size_t, 3> keyOrder_;
 
-  const MetaData& metaData() const { return meta_; }
   MetaData meta_;
 
   // This member is `optional` because we initialize it in a deferred way in the
