4d58bed7a455236ce5f1ea0dd5659330a76b4308
Jonathan Zeller <zellerjonathan@web.de>
2024-10-13 00:31:46 +0200

    refactor architecture: put algorithms into separate file

---
+++ test/engine/SpatialJoinAlgorithmsTest.cpp
@@ -0,0 +1,10 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "engine/SpatialJoinAlgorithms.h"
+
+namespace {  // anonymous namespace to avoid linker problems
+    TEST(SpatialJoinAlgorithms, test) {
+        ASSERT_TRUE(true);
+    }
+}
