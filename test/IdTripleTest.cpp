f856919842dc3492d2236fe4822d6662b582edcc
Johannes Kalmbach <joka921@users.noreply.github.com>
2024-10-22 22:10:28 +0200

    Located triples with graph information (#1572)
    Triples from a SPARQL 1.1 Update operation also belong to a graph, but so far, our located triples did not contain graph information. Now each located triple holds an array of *four* `Id`s, where the first three denote the subject, predicate, and object (in the order of the permutation in which the triple is located), and the fourth denotes the graph.

--- test/IdTripleTest.cpp
+++ test/IdTripleTest.cpp
@@ -13,8 +13,8 @@
 using namespace ad_utility::testing;
 
 TEST(IdTripleTest, constructors) {
-  const std::array<Id, 3> ids{Id::makeFromInt(42), VocabId(10),
-                              Id::makeFromBool(false)};
+  const std::array<Id, 4> ids{Id::makeFromInt(42), VocabId(10),
+                              Id::makeFromBool(false), VocabId(123)};
   const std::array<Id, 2> payload{Id::makeFromDouble(3.14),
                                   Id::makeFromBool(true)};
 
@@ -34,29 +34,31 @@ TEST(IdTripleTest, constructors) {
 }
 
 TEST(IdTripleTest, permute) {
-  std::array<Id, 3> ids{VocabId(0), VocabId(1), VocabId(2)};
+  auto V = VocabId;
+  std::array<Id, 4> ids{V(0), V(1), V(2), V(3)};
   // Without a payload
   {
     IdTriple<0> idTriple{ids};
     EXPECT_THAT(idTriple.permute({1, 0, 2}),
-                testing::Eq(IdTriple{
-                    std::array<Id, 3>{VocabId(1), VocabId(0), VocabId(2)}}));
+                testing::Eq(IdTriple{std::array<Id, 4>{
+                    VocabId(1), VocabId(0), VocabId(2), VocabId(3)}}));
   }
 
   // With a payload
   {
     IdTriple<2> idTriple(ids, {IntId(10), IntId(5)});
     EXPECT_THAT(idTriple.permute({1, 0, 2}),
-                testing::Eq(IdTriple<2>({VocabId(1), VocabId(0), VocabId(2)},
-                                        {IntId(10), IntId(5)})));
+                testing::Eq(IdTriple<2>(
+                    {VocabId(1), VocabId(0), VocabId(2), VocabId(3)},
+                    {IntId(10), IntId(5)})));
   }
 }
 
 TEST(IdTripleTest, toPermutedTriple) {
   {
-    IdTriple<0> idTriple({VocabId(0), VocabId(10), VocabId(5)});
+    IdTriple<0> idTriple({VocabId(0), VocabId(10), VocabId(5), VocabId(42)});
     EXPECT_THAT(idTriple.toPermutedTriple(),
                 testing::Eq(CompressedBlockMetadata::PermutedTriple{
-                    VocabId(0), VocabId(10), VocabId(5)}));
+                    VocabId(0), VocabId(10), VocabId(5), VocabId(42)}));
   }
 }
