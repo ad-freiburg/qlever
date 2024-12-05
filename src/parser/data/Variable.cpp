eafd594df9381b596c9c49c4b236d0bb1e2c15b9
Felix Meisen <fesemeisen@outlook.de>
2024-12-05 12:49:12 +0100

    added method appendEscapedWord to escape special chars in Variables

--- src/parser/data/Variable.cpp
+++ src/parser/data/Variable.cpp
@@ -65,15 +65,7 @@ Variable Variable::getEntityScoreVariable(
     entity = std::get<Variable>(varOrEntity).name().substr(1);
   } else {
     type = "_fixedEntity_";
-    // Converts input string to unambiguous result string not containing any
-    // special characters. "_" is used as an escaping character.
-    for (char c : std::get<std::string>(varOrEntity)) {
-      if (isalpha(static_cast<unsigned char>(c))) {
-        entity += c;
-      } else {
-        absl::StrAppend(&entity, "_", std::to_string(c), "_");
-      }
-    }
+    appendEscapedWord(std::get<std::string>(varOrEntity), entity);
   }
   return Variable{
       absl::StrCat(SCORE_VARIABLE_PREFIX, name().substr(1), type, entity)};
@@ -91,13 +83,7 @@ Variable Variable::getWordScoreVariable(std::string_view word,
     type = "word_";
   }
   convertedWord = "_";
-  for (char c : word) {
-    if (isalpha(static_cast<unsigned char>(c))) {
-      convertedWord += c;
-    } else {
-      absl::StrAppend(&convertedWord, "_", std::to_string(c), "_");
-    }
-  }
+  appendEscapedWord(word, convertedWord);
   return Variable{absl::StrCat(SCORE_VARIABLE_PREFIX, type, name().substr(1),
                                convertedWord)};
 }
@@ -117,5 +103,17 @@ bool Variable::isValidVariableName(std::string_view var) {
     return remaining.empty();
   } catch (...) {
     return false;
+  }
+}
+
+// _____________________________________________________________________________
+void Variable::appendEscapedWord(std::string_view word,
+                                 std::string& target) const {
+  for (char c : word) {
+    if (isalpha(static_cast<unsigned char>(c))) {
+      target += c;
+    } else {
+      absl::StrAppend(&target, "_", std::to_string(c), "_");
+    }
   }
 }
