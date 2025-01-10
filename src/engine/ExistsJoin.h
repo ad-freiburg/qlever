52943570743cb6c9db292f6331e1b8304ab32379
Hannah Bast <bast@cs.uni-freiburg.de>
2025-01-10 03:59:43 +0100

    Made a pass over `ExistsJoin.h` and `ExistsJoin.cpp`

--- src/engine/ExistsJoin.h
+++ src/engine/ExistsJoin.h
@@ -1,13 +1,14 @@
-//  Copyright 2025, University of Freiburg,
-//                  Chair of Algorithms and Data Structures.
-//  Author: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
+// Copyright 2025, University of Freiburg
+// Chair of Algorithms and Data Structures
+// Author: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
 
 #pragma once
 
 #include "engine/Operation.h"
 #include "engine/QueryExecutionTree.h"
 
-// The implementation of the SPARQL `EXISTS` function. It takes two subtrees,
+// The implementation of an "EXISTS join", which we use to realize the semantics
+// of the SPARQL `EXISTS` function. The join takes two subtrees as input, and
 // and returns the left subtree with an additional boolean column that is `true`
 // iff at least one matching row is contained in the right subtree.
 class ExistsJoin : public Operation {
@@ -17,25 +18,28 @@ class ExistsJoin : public Operation {
   std::shared_ptr<QueryExecutionTree> right_;
   std::vector<std::array<ColumnIndex, 2>> joinColumns_;
 
-  // The variable of the added result column.
+  // The variable of the added (Boolean) result column.
   Variable existsVariable_;
 
  public:
-  // Constructor. The `existsVariable` (the variable for the added boolean
-  // column) must not yet be bound by `left`.
+  // Constructor. The `existsVariable` (the variable for the added column) must
+  // not yet be bound by `left`.
   ExistsJoin(QueryExecutionContext* qec,
              std::shared_ptr<QueryExecutionTree> left,
              std::shared_ptr<QueryExecutionTree> right,
              Variable existsVariable);
 
   // For a given subtree and a given expression, extract all the
-  // `ExistsExpressions` from the expression and add one `ExistsJoin` per
-  // `ExistsExpression` to the subtree. The left side of the `ExistsJoin` is the
-  // input subtree, the right hand side of the `ExistsJoin` as well as the
+  // `ExistsExpression`s from the expression and add one `ExistsJoin` per
+  // `ExistsExpression` to the subtree. The left side of the `ExistsJoin` is
+  // the input subtree, the right hand side of the `ExistsJoin` as well as the
   // variable to which the result is bound are extracted from the
   // `ExistsExpression`. The returned subtree can then be used to evaluate the
-  // `expression`. Note: `ExistsExpression` is a simple dummy that only reads
-  // the values of the column that is added by the `ExistsJoin`.
+  // `expression`.
+  //
+  // NOTE: `ExistsExpression` is a dummy that only reads the values of the
+  // column that is added by the `ExistsJoin`. The main work is done by the
+  // latter and not by the former.
   static std::shared_ptr<QueryExecutionTree> addExistsJoinsToSubtree(
       const sparqlExpression::SparqlExpressionPimpl& expression,
       std::shared_ptr<QueryExecutionTree> subtree, QueryExecutionContext* qec,
