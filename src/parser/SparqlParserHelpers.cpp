2ac535401083d1d8638db6616ad4394aa5c41cfb
RobinTF <83676088+RobinTF@users.noreply.github.com>
2025-02-05 18:03:11 +0100

    Implement unicode unescape before parsing

--- src/parser/SparqlParserHelpers.cpp
+++ src/parser/SparqlParserHelpers.cpp
@@ -4,6 +4,11 @@
 
 #include "SparqlParserHelpers.h"
 
+#include <unicode/unistr.h>
+
+#include <charconv>
+#include <ctre-unicode.hpp>
+
 #include "sparqlParser/generated/SparqlAutomaticLexer.h"
 
 namespace sparqlParserHelpers {
@@ -13,7 +18,8 @@ using std::string;
 ParserAndVisitor::ParserAndVisitor(
     std::string input,
     SparqlQleverVisitor::DisableSomeChecksOnlyForTesting disableSomeChecks)
-    : input_{std::move(input)}, visitor_{{}, disableSomeChecks} {
+    : input_{unescapeUnicodeSequences(std::move(input))},
+      visitor_{{}, disableSomeChecks} {
   // The default in ANTLR is to log all errors to the console and to continue
   // the parsing. We need to turn parse errors into exceptions instead to
   // propagate them to the user.
@@ -27,7 +33,36 @@ ParserAndVisitor::ParserAndVisitor(
 ParserAndVisitor::ParserAndVisitor(
     std::string input, SparqlQleverVisitor::PrefixMap prefixes,
     SparqlQleverVisitor::DisableSomeChecksOnlyForTesting disableSomeChecks)
-    : ParserAndVisitor{std::move(input), disableSomeChecks} {
+    : ParserAndVisitor{unescapeUnicodeSequences(std::move(input)),
+                       disableSomeChecks} {
   visitor_.setPrefixMapManually(std::move(prefixes));
+}
+
+// _____________________________________________________________________________
+std::string ParserAndVisitor::unescapeUnicodeSequences(std::string input) {
+  std::u8string_view utf8View{reinterpret_cast<char8_t*>(input.data()),
+                              input.size()};
+  std::string output;
+  size_t lastPos = 0;
+
+  for (auto match :
+       ctre::search_all<R"(\\U[0-9A-Fa-f]{8}|\\u[0-9A-Fa-f]{4})">(utf8View)) {
+    output += input.substr(lastPos, match.data() - (utf8View.data() + lastPos));
+    lastPos = match.data() + match.size() - utf8View.data();
+
+    auto hexValue = match.to_view();
+
+    UChar32 codePoint;
+    const char* startPointer = reinterpret_cast<const char*>(hexValue.data());
+    auto result = std::from_chars(
+        startPointer + 2, startPointer + hexValue.size(), codePoint, 16);
+    AD_CORRECTNESS_CHECK(result.ec == std::errc{});
+
+    icu::UnicodeString helper{codePoint};
+    helper.toUTF8String(output);
+  }
+
+  output += input.substr(lastPos);
+  return output;
 }
 }  // namespace sparqlParserHelpers
