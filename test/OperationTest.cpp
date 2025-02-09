612a8df15e5cf241f3a534ac0a299b25467c797e
Hannah Bast <bast@cs.uni-freiburg.de>
2025-02-09 02:49:12 +0100

    Don't use zero cost for cached subtrees
    This continues #1736, which changed the size estimate for cached subtrees
    from the exact size to the estimate size. Analogous to that, now also
    change the cost estimate for cached subtrees from zero to the normal
    cost estimate.
    
    Introduce a runtime parameter `zero-cost-for-cached-subtree` to revert
    to the old behavior if needed (the default is `false`).

--- test/OperationTest.cpp
+++ test/OperationTest.cpp
@@ -242,14 +242,23 @@ TEST(OperationTest, estimatesForCachedResults) {
 
     [[maybe_unused]] auto res = qet->getResult();
   }
-  // The result is now cached inside the static execution context, if we create
-  // the same operation again, the cost estimate is 0. The size estimate doesn't
+  // The result is now cached inside the static execution context. If we create
+  // the same operation again and `zero-cost-for-cached-subtrees` is set to
+  // `true`, the cost estimate should be zero. The size estimate does not
   // change (see the `getCostEstimate` function for details on why).
   {
+    RuntimeParameters().set<"zero-cost-for-cached-subtree">(true);
     auto qet = makeQet();
     EXPECT_EQ(qet->getCacheKey(), qet->getRootOperation()->getCacheKey());
     EXPECT_EQ(qet->getSizeEstimate(), 24u);
     EXPECT_EQ(qet->getCostEstimate(), 0u);
+  }
+  {
+    RuntimeParameters().set<"zero-cost-for-cached-subtree">(false);
+    auto qet = makeQet();
+    EXPECT_EQ(qet->getCacheKey(), qet->getRootOperation()->getCacheKey());
+    EXPECT_EQ(qet->getSizeEstimate(), 24u);
+    EXPECT_EQ(qet->getCostEstimate(), 210u);
   }
 }
 
