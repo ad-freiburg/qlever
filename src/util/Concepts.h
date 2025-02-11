3521811a37db4545b5d5f192b5a3fc9e92f01cbf
Giuseppe Picciuca <dangertauros@gmail.com>
2025-02-11 11:38:31 +0100

    Fix Streamable concept occurrences

--- src/util/Concepts.h
+++ src/util/Concepts.h
@@ -19,8 +19,6 @@ CPP_requires(is_streamable_, requires(T x, std::ostream& os)(os << x));
  * A concept to ensure objects can be formatted by std::ostream.
  * @tparam T The Type to be formatted
  */
-// TODO<joka921, gpcicciuca> turn back to `CPP_concept` and fix the remaining
-// places.
 template <typename T>
-concept Streamable = CPP_requires_ref(detail::is_streamable_, T);
+CPP_concept Streamable = CPP_requires_ref(detail::is_streamable_, T);
 }  // namespace ad_utility
