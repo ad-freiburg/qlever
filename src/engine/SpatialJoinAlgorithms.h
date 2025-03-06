c56c863af7c14b4d47d49a9c154a0c9a9c3176b0
Jonathan Zeller <zellerjonathan@web.de>
2025-03-06 23:06:50 +0100

    bugfix

--- src/engine/SpatialJoinAlgorithms.h
+++ src/engine/SpatialJoinAlgorithms.h
@@ -227,5 +227,7 @@ class SpatialJoinAlgorithms {
   size_t numFailedParsedGeometries_ = 0;
 
   // this vector stores the geometries, which have already been parsed
-  std::vector<AnyGeometry, ad_utility::AllocatorWithLimit<Value>> geometries_;
+  std::vector<AnyGeometry,
+              ad_utility::AllocatorWithLimit<BoostGeometryNamespace::Value>>
+      geometries_;
 };
