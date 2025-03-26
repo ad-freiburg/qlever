3ab50584c7366508c6242537167a0ddc16fd76fa
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2025-03-26 19:59:54 +0100

    a helper script and a small change for CMakeLists.txt
    Signed-off-by: Johannes Kalmbach <johannes.kalmbach@gmail.com>

---
+++ run-clang-cxx-17-rewriter.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+printf "Rewriting operator== automatically\n"
+SOURCE_FILES=()
+find . -not \( -path "./third_party/*" -prune \) -not \( -path "./cmake-build*" -prune \) -not \( -path "./build*" -prune \) -name \*.h -print0 -o -name \*.cpp -print0 > sourcelist
+while IFS=  read -r -d $'\0'; do
+    SOURCE_FILES+=("$REPLY")
+done <sourcelist
+
+for source in "${SOURCE_FILES[@]}" ;do
+  echo $source
+  /local/data-ssd/kalmbacj/llvm-project/llvm/cmake-build-release/bin/backport-defaulted-equality -p cmake-build-clang-18-compile-commands $source
+
+done
+sem --wait
+echo "Finished rewriting all files"
