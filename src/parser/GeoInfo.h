15b61d048d1dc331e5842e5d6a8993b577c95ff9
ullingerc <ullingec@informatik.uni-freiburg.de>
2025-04-09 17:32:52 +0200

    add test skeleton

--- src/parser/GeoInfo.h
+++ src/parser/GeoInfo.h
@@ -9,7 +9,7 @@
 #include <string>
 #include <utility>
 
-// TODO<ullingerc> Taken from libspatialjoin - reuse?
+// TODO<ullingerc> Taken from libspatialjoin - reuse from there?
 enum WKTType : uint8_t {
   NONE = 0,
   POINT = 1,
@@ -23,18 +23,22 @@ enum WKTType : uint8_t {
 
 // A geometry info object holds precomputed details on WKT literals.
 struct GeometryInfo {
-  WKTType type_;
-  std::pair<uint64_t, uint64_t> boundingBox_;
-  uint64_t centroid_;
-  double metricLength_;
-  double metricArea_;
-  uint64_t libspatialjoinOffset_;
+  uint64_t dummyAttribute_;
+
+  // TODO<ullingerc> Implement actual precomputation, f.ex.
+  // WKTType type_;
+  // std::pair<uint64_t, uint64_t> boundingBox_;
+  // uint64_t centroid_;
+  // double metricLength_;
+  // double metricArea_;
+  // uint64_t libspatialjoinOffset_;
 
   // Parse a WKT literal and precompute all attributes.
-  static GeometryInfo fromWktLiteral(const std::string_view& wkt) {
-    // TODO<ullingerc> Replace this dummy
-    return GeometryInfo{WKTType::NONE, {0, 0}, 0, 0, 0, 0};
-    // TODO<ullingerc> libspatialjoin offsets?
+  static GeometryInfo fromWktLiteral(
+      [[maybe_unused]] const std::string_view& wkt) {
+    // TODO<ullingerc> Replace this dummy with computation
+    // TODO<ullingerc> How can we retrieve libspatialjoin offsets?
+    return GeometryInfo{1};
   };
 };
 
---
+++ test/index/vocabulary/GeoVocabularyTest.cpp
@@ -0,0 +1,9 @@
+// Copyright 2025, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Christoph Ullinger <ullingec@cs.uni-freiburg.de>
+
+#include <gtest/gtest.h>
+
+// TODO<ullingerc> Unit tests
+
+TEST(GeoVocabularyTest, Test) { ; }
