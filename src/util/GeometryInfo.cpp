8f0203b44a3343bdb571ecdae6e77b23072ac366
ullingerc <ullingec@informatik.uni-freiburg.de>
2025-04-10 11:43:20 +0200

    parsing experiment

--- src/util/GeometryInfo.cpp
+++ src/util/GeometryInfo.cpp
@@ -7,17 +7,64 @@
 #include <spatialjoin/WKTParse.h>
 #include <util/geo/Geo.h>
 
+#include <cstdint>
+
 #include "parser/Literal.h"
 #include "parser/NormalizedString.h"
+#include "util/geo/Point.h"
 
+using namespace ::util::geo;
+using CoordType = double;
+using ParsedWkt =
+    std::variant<Point<CoordType>, Line<CoordType>, Polygon<CoordType>,
+                 MultiPoint<CoordType>, MultiLine<CoordType>,
+                 MultiPolygon<CoordType>, Collection<CoordType>>;
+
+// ____________________________________________________________________________
 GeometryInfo GeometryInfo::fromWktLiteral(const std::string_view& wkt) {
+  // TODO<ullingerc> Remove unneccessary string copying
   auto lit = ad_utility::triple_component::Literal::fromStringRepresentation(
       std::string(wkt));
-  auto wktLiteral = asStringViewUnsafe(lit.getContent());
+  auto wktLiteral = std::string(asStringViewUnsafe(lit.getContent()));
 
-  // TODO<ullingerc> Replace this dummy with computation
-  // TODO<ullingerc> How can we retrieve libspatialjoin offsets?
-  return GeometryInfo{
-      static_cast<uint8_t>(::util::geo::getWKTType(std::string(wktLiteral)))};
-  // util::geo::getWKTType()
+  ParsedWkt parsed;
+
+  auto type = getWKTType(wktLiteral);
+  switch (type) {
+    case WKTType::POINT: {
+      parsed = pointFromWKT<CoordType>(wktLiteral);
+      break;
+    }
+    case WKTType::LINESTRING: {
+      parsed = lineFromWKT<CoordType>(wktLiteral);
+      break;
+    }
+    case WKTType::POLYGON: {
+      parsed = polygonFromWKT<CoordType>(wktLiteral);
+      break;
+    }
+    case WKTType::MULTIPOINT: {
+      parsed = multiPointFromWKT<CoordType>(wktLiteral);
+      break;
+    }
+    case WKTType::MULTILINESTRING: {
+      parsed = multiLineFromWKT<CoordType>(wktLiteral);
+      break;
+    }
+    case WKTType::MULTIPOLYGON: {
+      parsed = multiPolygonFromWKT<CoordType>(wktLiteral);
+      break;
+    }
+    case WKTType::COLLECTION: {
+      parsed = collectionFromWKT<CoordType>(wktLiteral);
+      break;
+    }
+    case WKTType::NONE:
+      break;
+  }
+
+  auto x = std::visit([]<typename T>(T& val) { return centroid(val); }, parsed);
+  std::cout << "Centroid:" << x.getX() << " " << x.getY() << std::endl;
+
+  return GeometryInfo{type};
 }
