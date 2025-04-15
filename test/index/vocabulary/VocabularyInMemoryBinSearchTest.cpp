3bc3a6366f408fae5d461ee965ca18a1d33ef065
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2025-04-15 15:38:20 +0200

    Add unit tests.
    Signed-off-by: Johannes Kalmbach <johannes.kalmbach@gmail.com>

--- test/index/vocabulary/VocabularyInMemoryBinSearchTest.cpp
+++ test/index/vocabulary/VocabularyInMemoryBinSearchTest.cpp
@@ -41,10 +41,10 @@ class VocabularyCreator {
       if (ids.has_value()) {
         AD_CORRECTNESS_CHECK(ids.value().size() == words.size());
       }
-      size_t idx = 0;
+      uint64_t idx = 0;
       for (auto& word : words) {
         size_t actualIdx = ids.has_value() ? ids.value().at(idx) : idx;
-        writer(word, actualIdx);
+        EXPECT_EQ(writer(word, actualIdx), actualIdx);
         ++idx;
       }
       static std::atomic<unsigned> doFinish = 0;
@@ -63,9 +63,7 @@ class VocabularyCreator {
   auto createVocabularyFromDiskImpl(
       const std::vector<std::string>& words,
       std::optional<std::vector<uint64_t>> ids = std::nullopt) {
-    {
-      createVocabularyImpl(words, std::move(ids));
-    }
+    { createVocabularyImpl(words, std::move(ids)); }
     VocabularyInMemoryBinSearch vocabulary;
     vocabulary.open(vocabFilename_);
     return vocabulary;
