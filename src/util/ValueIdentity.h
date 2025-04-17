dffd4fb0df6db468566e8980f10335da630430e9
Valentino Orlandi <valentino.orlandi@partner.bmw.de>
2025-04-17 17:37:09 +0200

    Added ValueIdentity header

---
+++ src/util/ValueIdentity.h
@@ -0,0 +1,33 @@
+// Copyright 2025, Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+
+#ifndef QLEVER_SRC_UTIL_VALUEIDENTITY_H
+#define QLEVER_SRC_UTIL_VALUEIDENTITY_H
+
+// The use case of the following aliases and constants is as follows:
+// Locally (inside a function) put a `using namespace
+// ad_utility::use_alue_identity`, then use `VI<someType>` as a convenient short
+// cut for `std::value_identity<someValue>`. This can be used to make template
+// arguments to local lambdas more readable, as C++ doesn't allow for local
+// templates. For example:
+//
+// auto f1 = []<int i>() {/* use i */};
+// auto f2 = []<int i>(VI<i>) { /* use i /*};
+// f1.template operator()<1>(); // Works but is rather unreadable.
+// f2(vi<1>); // Minimal visual overhead for specifying the template parameter.
+
+namespace ad_utility::use_value_identity {
+
+template <auto V>
+struct ValueIdentity {
+  static constexpr auto value = V;
+};
+
+template <auto V>
+using VI = ValueIdentity<V>;
+
+template <auto V>
+static constexpr auto vi = VI<V>{};
+
+}  // namespace ad_utility::use_value_identity
+
+#endif  // QLEVER_SRC_UTIL_VALUEIDENTITY_H
