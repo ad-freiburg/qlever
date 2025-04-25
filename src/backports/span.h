ed5880c9b17599f6d81b6673027216946aa2edd7
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2025-04-25 16:23:53 +0200

    Add some comments and fix the compilation in C++20 mode.
    Signed-off-by: Johannes Kalmbach <johannes.kalmbach@gmail.com>

--- src/backports/span.h
+++ src/backports/span.h
@@ -6,7 +6,19 @@
 #ifndef QLEVER_SRC_BACKPORTS_SPAN_H
 #define QLEVER_SRC_BACKPORTS_SPAN_H
 
-#include "backports/cppTemplate2.h"
+// This header implements the following types and functions, all related to
+// spans:
+// - ql::span
+// - ql::as_bytes
+// - ql::as_writeable_bytes
+// - ql::dynamic_extent
+// In C++20 mode (by default) they are aliases for the corresponding types and
+// functions from the standard library, in C++17 mode (when `QLEVER_CPP_17` is
+// enabled) they are implemented using `range-v3`.
+// Note that we use a fork of range-v3 that makes the span implementations very
+// similar, but some small differences remain, In particular the deduction
+// guides for the construction from `std::array` which leads to a static extent
+// are missing.
 
 #ifdef QLEVER_CPP_17
 #include <cstddef>
@@ -21,7 +33,7 @@ span<std::byte const, ::ranges::detail::byte_size<T>(N)> as_bytes(
 }
 
 template <typename T, ::ranges::detail::span_index_t N>
-span<std::byte, ::ranges::detail::byte_size<T>(N)> as_writeable_bytes(
+span<std::byte, ::ranges::detail::byte_size<T>(N)> as_writable_bytes(
     span<T, N> s) noexcept {
   return {reinterpret_cast<std::byte*>(s.data()), s.size_bytes()};
 }
@@ -30,10 +42,10 @@ static constexpr auto dynamic_extent = ::ranges::dynamic_extent;
 }  // namespace ql
 
 #else
-#include "backports/span.h"
+#include <span>
 namespace ql {
 using std::as_bytes;
-using std::as_writeable_bytes;
+using std::as_writable_bytes;
 using std::span;
 static constexpr auto dynamic_extent = std::dynamic_extent;
 }  // namespace ql
