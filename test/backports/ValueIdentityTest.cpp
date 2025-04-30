55bcb4529d132cda0cee711f4bbc55702eb59275
Daniele Asnicar <daniele.asnicar@partner.bmw.de>
2025-04-30 11:44:27 +0200

    Add ApplyAsValueIdentity tests

---
+++ test/backports/ValueIdentityTest.cpp
@@ -0,0 +1,41 @@
+// Copyright 2025, Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+
+#include <gtest/gtest.h>
+
+#include "util/ValueIdentity.h"
+
+using namespace ad_utility;
+using namespace ad_utility::use_value_identity;
+
+TEST(ApplyAsValueIdentity, FunctorInvocation) {
+  constexpr static auto functor = [](auto... compile_time_values,
+                                     auto... runtime_values) {
+    constexpr int compile_time_values_sum =
+        (0 + ... + compile_time_values.value);
+    int runtime_sum = (0 + ... + runtime_values);
+    return compile_time_values_sum + runtime_sum;
+  };
+
+  constexpr ApplyAsValueIdentity apply{functor};
+
+  constexpr int result_single_arg = apply.operator()<5, 10, 15>(20);
+  constexpr int result_double_arg = apply.operator()<5, 10, 15>(20, 25);
+  EXPECT_EQ(result_single_arg, 50);
+  EXPECT_EQ(result_double_arg, 75);
+}
+
+TEST(ApplyAsValueIdentityTuple, FunctorInvocationWithTuple) {
+  constexpr static auto functor = [](auto runtime_tuple,
+                                     auto... compile_time_values) {
+    int runtime_sum = std::apply(
+        [](auto&&... elems) { return (0 + ... + elems); }, runtime_tuple);
+    return (compile_time_values.value + ... + runtime_sum);
+  };
+
+  constexpr ApplyAsValueIdentityTuple apply{functor};
+
+  constexpr int result_single_arg = apply.operator()<5, 10>(15);
+  constexpr int result_double_arg = apply.operator()<5, 10>(15, 20);
+  EXPECT_EQ(result_single_arg, 30);
+  EXPECT_EQ(result_double_arg, 50);
+}
