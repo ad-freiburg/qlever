09fdb4c3b12799fa5d6d60960ecb09c58a2ca5cd
RobinTF <83676088+RobinTF@users.noreply.github.com>
2025-05-27 04:58:02 +0200

    Allow each `Operation` to specify how to make sure its result is sorted (#1946)
    Introduce an interface that allows every `Operation` to specify how to make sure that its result is sorted according to a given ordering of the columns. Operations which cannot do that themselves return `std::nullopt`, which causes the operation to be wrapped inside a `Sort` operation. However, if the operation has a more efficient way to achieve the desired result, it can return an alternative execution tree instead.

--- test/SortTest.cpp
+++ test/SortTest.cpp
@@ -148,9 +148,6 @@ TEST(Sort, SimpleMemberFunctions) {
     ASSERT_EQ(0u, varColMap.at(Variable{"?0"}).columnIndex_);
     EXPECT_FALSE(s.knownEmptyResult());
     EXPECT_EQ(42.0, s.getMultiplicity(0));
-
-    EXPECT_THAT(s.getSubtree()->getRootOperation()->getCacheKey(),
-                ::testing::StartsWith("Values for testing with"));
   }
 
   {
@@ -172,6 +169,35 @@ TEST(Sort, SimpleMemberFunctions) {
     EXPECT_FALSE(s.knownEmptyResult());
     EXPECT_EQ(42.0, s.getMultiplicity(0));
     EXPECT_EQ(84.0, s.getMultiplicity(1));
+  }
+}
+
+// _____________________________________________________________________________
+TEST(Sort, checkSortedCloneIsProperlyHandled) {
+  {
+    VectorTable input{{0},   {1},       {-1},  {3},
+                      {-17}, {1230957}, {123}, {-1249867132}};
+    auto inputTable = makeIdTableFromVector(input, &Id::makeFromInt);
+    Sort sort = makeSort(std::move(inputTable), {0});
+    EXPECT_THROW(sort.makeSortedTree({0}), ad_utility::Exception);
+    EXPECT_THROW(sort.makeSortedTree({}), ad_utility::Exception);
+    // Check that we don't double sort
+    auto operation = sort.makeSortedTree({0, 1});
+    ASSERT_TRUE(operation.has_value());
+    const auto& childReference =
+        *operation.value()->getRootOperation()->getChildren().at(0);
+    EXPECT_NE(typeid(childReference), typeid(Sort));
+  }
+  {
+    VectorTable input{{0, 0}, {1, 1}};
+    auto inputTable = makeIdTableFromVector(input, &Id::makeFromInt);
+    Sort sort = makeSort(std::move(inputTable), {0, 1});
+    // Check that we don't double sort
+    auto operation = sort.makeSortedTree({1, 0});
+    ASSERT_TRUE(operation.has_value());
+    const auto& childReference =
+        *operation.value()->getRootOperation()->getChildren().at(0);
+    EXPECT_NE(typeid(childReference), typeid(Sort));
   }
 }
 
