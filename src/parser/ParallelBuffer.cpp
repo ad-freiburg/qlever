6f73d49ace2c274da2ed52235d3eb19f0297c9a7
RobinTF <83676088+RobinTF@users.noreply.github.com>
2025-06-27 14:30:21 +0200

    Start with smaller chunk size

--- src/parser/ParallelBuffer.cpp
+++ src/parser/ParallelBuffer.cpp
@@ -43,7 +43,7 @@ std::optional<size_t> ParallelBufferWithEndRegex::findRegexNearEnd(
     const BufferType& vec, const re2::RE2& regex) {
   size_t inputSize = vec.size();
   AD_CORRECTNESS_CHECK(inputSize > 0);
-  size_t chunkSize = std::min(1000UL, inputSize);
+  size_t chunkSize = std::min(1UL, inputSize);
   re2::StringPiece regexResult;
   bool match = false;
   while (true) {
