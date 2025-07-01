f8de0dacf7ad3afe5762bb901663c9ca72669ac7
ullingerc <ullingec@informatik.uni-freiburg.de>
2025-07-01 10:24:10 +0200

    add additional unit test

--- test/index/vocabulary/GeoVocabularyTest.cpp
+++ test/index/vocabulary/GeoVocabularyTest.cpp
@@ -5,6 +5,7 @@
 #include <gtest/gtest.h>
 
 #include "../../GeometryInfoTestHelpers.h"
+#include "index/Vocabulary.h"
 #include "index/vocabulary/CompressedVocabulary.h"
 #include "index/vocabulary/GeoVocabulary.h"
 #include "index/vocabulary/VocabularyInMemory.h"
@@ -14,6 +15,7 @@
 namespace {
 
 using namespace geoInfoTestHelpers;
+using namespace ad_utility;
 
 // A function to test that a GeoVocabulary can correctly insert and
 // lookup literals and precompute geometry information. This function is generic
@@ -61,12 +63,38 @@ void testGeoVocabulary() {
   geoVocab.close();
 };
 
+// _____________________________________________________________________________
 TEST(GeoVocabularyTest, GeoVocabInMem) {
   testGeoVocabulary<VocabularyInMemory>();
 }
 
+// _____________________________________________________________________________
 TEST(GeoVocabularyTest, GeoVocabInternalExternal) {
   testGeoVocabulary<CompressedVocabulary<VocabularyInternalExternal>>();
+}
+
+// _____________________________________________________________________________
+TEST(GeoVocabularyTest, VocabularyGetGeoInfoFromUnderlyingGeoVocab) {
+  // Generate test vocabulary
+  RdfsVocabulary vocabulary;
+  const VocabularyType geoSplitVocabType{
+      VocabularyType::Enum::OnDiskCompressedGeoSplit};
+  vocabulary.resetToType(geoSplitVocabType);
+  auto wordCallback = vocabulary.makeWordWriterPtr("geoVocabTest.dat");
+  auto nonGeoIdx = (*wordCallback)("<http://example.com/abc>", true);
+  auto geoIdx = (*wordCallback)(
+      "\"LINESTRING(2 2, 4 "
+      "4)\"^^<http://www.opengis.net/ont/geosparql#wktLiteral>",
+      true);
+  wordCallback->finish();
+
+  // Load test vocabulary and try to retrieve precomputed `GeometryInfo`
+  vocabulary.readFromFile("geoVocabTest.dat");
+  ASSERT_FALSE(vocabulary.getGeoInfo(VocabIndex::make(nonGeoIdx)).has_value());
+  auto gi = vocabulary.getGeoInfo(VocabIndex::make(geoIdx));
+  ASSERT_TRUE(gi.has_value());
+  GeometryInfo exp{2, {{2, 2}, {4, 4}}, {3, 3}};
+  checkGeoInfo(gi.value(), exp);
 }
 
 }  // namespace
