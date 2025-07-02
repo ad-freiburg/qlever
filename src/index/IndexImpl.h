619614a11654b88d3b86754760cb9d8fba286756
Sebastian Wieczorek <ccoecontrol@dpnotreply.users.github.com>
2025-07-02 09:32:33 +0200

    Removing commented code

--- src/index/IndexImpl.h
+++ src/index/IndexImpl.h
@@ -614,7 +614,6 @@ class IndexImpl {
   // the input).
   NumNormalAndInternal numTriples() const;
 
-  // using BlocksOfTriples = cppcoro::generator<IdTableStatic<0>>;
   using BlocksOfTriples = ad_utility::InputRangeTypeErased<IdTableStatic<0>>;
 
   // Functions to create the pairs of permutations during the index build. Each
