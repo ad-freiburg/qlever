5e1c8800b21c0873119695461fc32633ea97388b
ullingerc <ullingec@informatik.uni-freiburg.de>
2025-07-16 11:37:46 +0200

    work on tests

--- test/engine/SpatialJoinPrefilterTest.cpp
+++ test/engine/SpatialJoinPrefilterTest.cpp
@@ -31,60 +31,100 @@ using SweeperDistResult = std::vector<std::vector<double>>;
 using QET = std::shared_ptr<QueryExecutionTree>;
 using Loc = ad_utility::source_location;
 
+using ValIdToGeomName = ad_utility::HashMap<ValueId, std::string>;
+using GeomNameToValId = ad_utility::HashMap<std::string, ValueId>;
+
+using SweeperResultWithIds =
+    std::vector<std::tuple<SpatialJoinType, ValueId, ValueId, double>>;
+using GeoRelationWithIds = std::tuple<SpatialJoinType, ValueId, ValueId>;
+
+struct SweeperTestResult {
+  SweeperResultWithIds results_;
+  util::geo::I32Box boxAfterAddingLeft_;
+  util::geo::I32Box boxAfterAddingRight_;
+  size_t numElementsInSweeper_;
+  size_t numElementsSkippedByPrefilter_;
+};
+
+using TestGeometry = std::tuple<std::string, std::string, bool>;
+
+const std::vector<TestGeometry> geometries{
+    {"uni", areaUniFreiburg, true},
+    {"uni-p", pointUniFreiburg, true},
+    {"minster", areaMuenster, true},
+    {"minster-p", pointMinster, true},
+    {"gk-allee", lineSegmentGeorgesKoehlerAllee, true},
+    {"london", areaLondonEye, false},
+    {"lib", areaStatueOfLiberty, false},
+    {"eiffel", areaEiffelTower, false},
+    {"eiffel-p", pointEiffelTower, false},
+    // Special
+    {"approx-de", approximatedAreaGermany, true},
+    {"uni-separate", areaUniFreiburg, true},
+};
+
 // Helpers for testing the `LibspatialjoinAlgorithm`
 // TODO configurable for different tests
 std::string buildLibSJTestDataset(bool addApproxGermany = false,
                                   bool germanyDifferentPredicate = true,
                                   bool addSeparateUni = false) {
-  std::vector<std::tuple<std::string, std::string, bool>> geometries{
-      {"uni", areaUniFreiburg, true},
-      {"uni-p", pointUniFreiburg, true},
-      {"minster", areaMuenster, true},
-      {"minster-p", pointMinster, true},
-      {"gk-allee", lineSegmentGeorgesKoehlerAllee, true},
-      {"london", areaLondonEye, false},
-      {"lib", areaStatueOfLiberty, false},
-      {"eiffel", areaEiffelTower, false},
-      {"eiffel-p", pointEiffelTower, false},
-  };
-
   std::string kg;
   for (const auto& [name, wkt, isInGermany] : geometries) {
-    kg = absl::StrCat(
-        kg, "<", name, "> <wkt-",
-        (isInGermany && germanyDifferentPredicate ? "de" : "other"), "> ", wkt,
-        " .\n");
+    if (addApproxGermany) {
+      if (name == "approx-de") {
+        kg = absl::StrCat(kg, "<approx-de> <wkt-approx-de> ", wkt, " .\n");
+      }
+    } else if (addSeparateUni) {
+      if (name == "uni-separate") {
+        kg =
+            absl::StrCat(kg, "<uni-separate> <wkt-uni-separate> ", wkt, " .\n");
+      }
+    } else {
+      kg = absl::StrCat(
+          kg, "<", name, "> <wkt-",
+          (isInGermany && germanyDifferentPredicate ? "de" : "other"), "> ",
+          wkt, " .\n");
+    }
   }
-
-  if (addApproxGermany) {
-    kg = absl::StrCat(kg, "<approx-de> <wkt-approx-de> ",
-                      approximatedAreaGermany, " .\n");
-  }
-
-  if (addSeparateUni) {
-    kg = absl::StrCat(kg, "<uni-separate> <wkt-uni-separate> ", areaUniFreiburg,
-                      " .\n");
-  }
-
   return kg;
 }
 
+//
+std::pair<ValIdToGeomName, GeomNameToValId> resolveValIdTable(
+    QueryExecutionContext* qec) {
+  ValIdToGeomName vMap;
+  GeomNameToValId nMap;
+  for (const auto& [name, wkt, isInGermany] : geometries) {
+    VocabIndex idx;
+    if (!qec->getIndex().getVocab().getId(wkt, &idx)) {
+      // This literal is not contained in this index
+      continue;
+    }
+    auto vId = ValueId::makeFromVocabIndex(idx);
+    vMap[vId] = name;
+    nMap[name] = vId;
+  }
+  return {vMap, nMap};
+}
+
 // TODO
-sj::SweeperCfg makeSweeperCfg(SweeperResult& results,
+sj::SweeperCfg makeSweeperCfg(const LibSpatialJoinConfig& libSJConfig,
+                              SweeperResult& results,
                               SweeperDistResult& resultDists,
                               double withinDist) {
+  using enum SpatialJoinType;
   sj::SweeperCfg cfg;
   cfg.numThreads = 1;
   cfg.numCacheThreads = 1;
   cfg.geomCacheMaxSize = 10'000;
   cfg.pairStart = "";
-  cfg.sepIsect = std::string{static_cast<char>(SpatialJoinType::INTERSECTS)};
-  cfg.sepContains = std::string{static_cast<char>(SpatialJoinType::CONTAINS)};
-  cfg.sepCovers = std::string{static_cast<char>(SpatialJoinType::COVERS)};
-  cfg.sepTouches = std::string{static_cast<char>(SpatialJoinType::TOUCHES)};
-  cfg.sepEquals = std::string{static_cast<char>(SpatialJoinType::EQUALS)};
-  cfg.sepOverlaps = std::string{static_cast<char>(SpatialJoinType::OVERLAPS)};
-  cfg.sepCrosses = std::string{static_cast<char>(SpatialJoinType::CROSSES)};
+  cfg.sepIsect = std::string{static_cast<char>(INTERSECTS)};
+  cfg.sepContains = std::string{static_cast<char>(CONTAINS)};
+  cfg.sepCovers = std::string{static_cast<char>(COVERS)};
+  cfg.sepTouches = std::string{static_cast<char>(TOUCHES)};
+  cfg.sepEquals = std::string{static_cast<char>(EQUALS)};
+  cfg.sepOverlaps = std::string{static_cast<char>(OVERLAPS)};
+  cfg.sepCrosses = std::string{static_cast<char>(CROSSES)};
   cfg.pairEnd = "";
   cfg.useBoxIds = true;
   cfg.useArea = true;
@@ -95,9 +135,12 @@ sj::SweeperCfg makeSweeperCfg(SweeperResult& results,
   cfg.useInnerOuter = false;
   cfg.noGeometryChecks = false;
   cfg.withinDist = withinDist;
-  cfg.writeRelCb = [&results, &resultDists, withinDist](size_t t, const char* a,
-                                                        const char* b,
-                                                        const char* pred) {
+  cfg.writeRelCb = [&results, &resultDists, withinDist, &libSJConfig](
+                       size_t t, const char* a, const char* b,
+                       const char* pred) {
+    if (pred[0] == static_cast<char>(libSJConfig.joinType_)) {
+      // TODO ??
+    }
     results[t].push_back(
         {static_cast<SpatialJoinType>(pred[0]), std::atoi(a), std::atoi(b)});
     if (withinDist >= 0) {
@@ -110,16 +153,6 @@ sj::SweeperCfg makeSweeperCfg(SweeperResult& results,
   cfg.sweepCancellationCb = {};
   return cfg;
 }
-
-//
-struct SweeperTestResult {
-  SweeperResult results_;
-  SweeperDistResult resultDists_;
-  util::geo::I32Box boxAfterAddingLeft_;
-  util::geo::I32Box boxAfterAddingRight_;
-  size_t numElementsInSweeper_;
-  size_t numElementsSkippedByPrefilter_;
-};
 
 //
 void runParsingAndSweeper(QueryExecutionContext* qec, QET leftChild,
@@ -145,12 +178,13 @@ void runParsingAndSweeper(QueryExecutionContext* qec, QET leftChild,
   SweeperDistResult resultDists{{}};
 
   double withinDist = libSJConfig.maxDist_.value_or(-1);
-  auto sweeperCfg = makeSweeperCfg(results, resultDists, withinDist);
+  auto sweeperCfg =
+      makeSweeperCfg(libSJConfig, results, resultDists, withinDist);
   std::string sweeperPath = qec->getIndex().getOnDiskBase() + ".spatialjoin";
   sj::Sweeper sweeper(sweeperCfg, ".", "", sweeperPath.c_str());
 
   ASSERT_EQ(sweeper.numElements(), 0);
-  auto box = sjAlgo.libspatialjoinParse(
+  auto [box, countLeft] = sjAlgo.libspatialjoinParse(
       false, {prepared.idTableLeft_, prepared.leftJoinCol_}, sweeper, 1,
       std::nullopt);
   sweeper.setFilterBox(box);
@@ -158,7 +192,7 @@ void runParsingAndSweeper(QueryExecutionContext* qec, QET leftChild,
   if (usePrefilter) {
     prefilterBox = sweeper.getPaddedBoundingBox(box);
   }
-  auto boxRight = sjAlgo.libspatialjoinParse(
+  auto [boxRight, countRight] = sjAlgo.libspatialjoinParse(
       true, {prepared.idTableRight_, prepared.rightJoinCol_}, sweeper, 1,
       prefilterBox);
   sweeper.flush();
@@ -171,13 +205,34 @@ void runParsingAndSweeper(QueryExecutionContext* qec, QET leftChild,
                                     .details_["num-geoms-dropped-by-prefilter"])
           : 0;
   size_t numEl = sweeper.numElements();
-  sweeper.sweep();
-
-  if (libSJConfig.maxDist_.has_value()) {
-    ASSERT_EQ(results.size(), resultDists.size());
+  if (countLeft && countRight) {
+    sweeper.sweep();
   }
 
-  testResult = {results, resultDists, box, boxRight, numEl, numSkipped};
+  ASSERT_EQ(results.size(), 1);
+  ASSERT_EQ(resultDists.size(), 1);
+
+  if (libSJConfig.maxDist_.has_value()) {
+    ASSERT_EQ(results.at(0).size(), resultDists.at(0).size());
+  }
+
+  SweeperResultWithIds resultMatched;
+
+  for (size_t row = 0; row < results.at(0).size(); row++) {
+    double dist = 0;
+    if (libSJConfig.maxDist_.has_value()) {
+      dist = resultDists.at(0).at(row);
+    }
+
+    const auto [sjType, leftIdx, rightIdx] = results.at(0).at(row);
+    auto valIdLeft = prepared.idTableLeft_->at(leftIdx, prepared.leftJoinCol_);
+    auto valIdRight =
+        prepared.idTableRight_->at(rightIdx, prepared.rightJoinCol_);
+
+    resultMatched.emplace_back(sjType, valIdLeft, valIdRight, dist);
+  }
+
+  testResult = {resultMatched, box, boxRight, numEl, numSkipped};
 }
 
 //
@@ -203,17 +258,33 @@ void checkPrefilterBox(const util::geo::I32Box& actual,
 }
 
 //
-void checkSweeperTestResult(const SweeperTestResult& actual,
+void checkSweeperTestResult(QueryExecutionContext* qec,
+                            const SweeperTestResult& actual,
                             const SweeperTestResult& expected,
+                            bool checkPrefilterBoxes = false,
                             Loc loc = Loc::current()) {
   auto l = generateLocationTrace(loc);
 
   ASSERT_EQ(actual.results_.size(), expected.results_.size());
-  ASSERT_EQ(actual.resultDists_.size(), expected.resultDists_.size());
-  // TODO actually compare results_ and resultDists_
-
-  checkPrefilterBox(actual.boxAfterAddingLeft_, expected.boxAfterAddingLeft_);
-  checkPrefilterBox(actual.boxAfterAddingRight_, expected.boxAfterAddingRight_);
+  auto [vMap, nMap] = resolveValIdTable(qec);
+  ad_utility::HashMap<GeoRelationWithIds, double> map;
+  for (const auto& [sjType, valIdLeft, valIdRight, dist] : expected.results_) {
+    std::cout << " EXPECTED " << (int)(sjType) << " " << vMap.at(valIdLeft)
+              << " " << vMap.at(valIdRight) << " " << dist << std::endl;
+    map[{sjType, valIdLeft, valIdRight}] = dist;
+  }
+  for (const auto& [sjType, valIdLeft, valIdRight, dist] : actual.results_) {
+    std::cout << " ACTUAL " << (int)(sjType) << " " << vMap.at(valIdLeft) << " "
+              << vMap.at(valIdRight) << " " << dist << std::endl;
+    GeoRelationWithIds key{sjType, valIdLeft, valIdRight};
+    ASSERT_TRUE(map.contains(key));
+    ASSERT_NEAR(map[key], dist, 0.01);
+  }
+  if (checkPrefilterBoxes) {
+    checkPrefilterBox(actual.boxAfterAddingLeft_, expected.boxAfterAddingLeft_);
+    checkPrefilterBox(actual.boxAfterAddingRight_,
+                      expected.boxAfterAddingRight_);
+  }
 
   ASSERT_EQ(actual.numElementsInSweeper_, expected.numElementsInSweeper_);
   ASSERT_EQ(actual.numElementsSkippedByPrefilter_,
@@ -250,9 +321,14 @@ TEST(SpatialJoinTest, BoundingBoxPrefilter) {
       buildIndexScan(qec, {"?obj1", std::string{"<wkt-de>"}, "?geom1"});
   auto rightChild =
       buildIndexScan(qec, {"?obj2", std::string{"<wkt-other>"}, "?geom2"});
+  auto [vMap, nMap] = resolveValIdTable(qec);
   SweeperTestResult testResult;
   runParsingAndSweeper(qec, leftChild, rightChild, {INTERSECTS}, testResult,
                        true);
+
+  SweeperTestResult expected{{}, {}, {}, 7, 4};
+  // {{{INTERSECTS, nMap["uni"], nMap["gk-allee"], 0}}, {}, {}, 5, 4};
+  checkSweeperTestResult(qec, testResult, expected);
 
   // Now check results / resultDists
 
