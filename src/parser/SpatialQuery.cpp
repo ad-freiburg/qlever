47126661f3432777cd0c96b4aa67137493b77598
ullingerc <ullingec@informatik.uni-freiburg.de>
2025-07-17 16:33:09 +0200

    Rename `SpatialJoinConfig` to `LibSpatialJoinConfig` to avoid confusion

--- src/parser/SpatialQuery.cpp
+++ src/parser/SpatialQuery.cpp
@@ -5,7 +5,7 @@
 
 #include "parser/SpatialQuery.h"
 
-#include "engine/SpatialJoinConfig.h"
+#include "engine/LibSpatialJoinConfig.h"
 #include "parser/MagicServiceIriConstants.h"
 #include "parser/PayloadVariables.h"
 
@@ -193,8 +193,8 @@ SpatialJoinConfiguration SpatialQuery::toSpatialJoinConfiguration() const {
   // Task specification
   SpatialJoinTask task;
   if (algo == SpatialJoinAlgorithm::LIBSPATIALJOIN) {
-    task = SpatialJoinConfig{joinType.value_or(SpatialJoinType::INTERSECTS),
-                             maxDist_};
+    task = LibSpatialJoinConfig{joinType.value_or(SpatialJoinType::INTERSECTS),
+                                maxDist_};
   } else if (maxResults_.has_value()) {
     task = NearestNeighborsConfig{maxResults_.value(), maxDist_};
   } else {
