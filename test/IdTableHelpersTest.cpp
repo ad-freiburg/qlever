ddee463ff67116394989bbf12b29da39950c989a
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2025-07-24 08:57:08 +0200

    Reinstate the tests instead of deleting them.
    Signed-off-by: Johannes Kalmbach <johannes.kalmbach@gmail.com>

--- test/IdTableHelpersTest.cpp
+++ test/IdTableHelpersTest.cpp
@@ -117,6 +117,12 @@ TEST(IdTableHelpersTest, createRandomlyFilledIdTableWithoutGenerators) {
       0, 0, std::vector{JoinColumnAndBounds{0, 0, 1}}));
   ASSERT_ANY_THROW(createRandomlyFilledIdTable(
       1, 0, std::vector{JoinColumnAndBounds{0, 0, 1}}));
+  {
+    auto table = createRandomlyFilledIdTable(
+        0, 1, std::vector{JoinColumnAndBounds{0, 0, 1}});
+    EXPECT_EQ(table.numRows(), 0);
+    EXPECT_EQ(table.numColumns(), 1);
+  }
 
   // Table with out of bounds join column.
   ASSERT_ANY_THROW(
@@ -219,6 +225,12 @@ TEST(IdTableHelpersTest, createRandomlyFilledIdTableWithGenerators) {
       createRandomlyFilledIdTable(10, 10, {1}, std::function<ValueId()>{}));
 
   // Creating an empty table of size (0,0).
+  {
+    auto table = createRandomlyFilledIdTable(
+        0, 0, std::vector<std::pair<size_t, std::function<ValueId()>>>{});
+    EXPECT_EQ(table.numRows(), 0);
+    EXPECT_EQ(table.numColumns(), 0);
+  }
   ASSERT_ANY_THROW(
       createRandomlyFilledIdTable(0, 0, {}, std::function<ValueId()>{}));
 
@@ -318,6 +330,10 @@ TEST(IdTableHelpersTest, generateIdTable) {
       return entry == ad_utility::testing::VocabId(row);
     }));
   }
+
+  table = generateIdTable(0, 0, createCountUpGenerator(0));
+  EXPECT_EQ(table.numRows(), 0);
+  EXPECT_EQ(table.numColumns(), 0);
 }
 
 /*
