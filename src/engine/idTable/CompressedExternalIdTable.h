8d830a183e41ff11c79302e8b5e9daf14b55d310
Hannah Bast <bast@cs.uni-freiburg.de>
2025-07-29 03:04:54 +0200

    First steps to fix a bug in `CompressedExternalIdTable`
    The bug occurs when building an index for a dataset, where all subjects
    are blank nodes. Then the following assertion fails:
    
    `Assertion `(numBlocksPushed_ == 0) != compressAndWriteFuture_.valid()`
    
    The problem only occurs, when `STXXL_MEMORY` is not large enough.
    However, when making it too small, the index build fails even earlier
    with a different (already known) problem, namely that the setting is not
    large enough for merging.

--- src/engine/idTable/CompressedExternalIdTable.h
+++ src/engine/idTable/CompressedExternalIdTable.h
@@ -378,6 +378,10 @@ class CompressedExternalIdTableBase {
       compressAndWriteFuture_.get();
     }
     if (block.empty()) {
+      // BUGFIX: It's important to set the future, even if it does nothing
+      // (otherwise the assertion in `transformAndPushLastBlock` fails).
+      compressAndWriteFuture_ = std::async(
+          std::launch::deferred, [this]() { });
       return;
     }
     ++numBlocksPushed_;
@@ -401,6 +405,8 @@ class CompressedExternalIdTableBase {
     // from pushing a block is still in flight. If we have never pushed a block,
     // then also the future cannot be valid.
     AD_CORRECTNESS_CHECK((numBlocksPushed_ == 0) !=
+                         compressAndWriteFuture_.valid(),
+                         numBlocksPushed_, " | ",
                          compressAndWriteFuture_.valid());
     // Optimization for inputs that are smaller than the blocksize, do not use
     // the external file, but simply sort and return the single block.
