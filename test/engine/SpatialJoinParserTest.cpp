deaa9eb03a05b2a73cf732d18059545504716c86
ullingerc <ullingec@informatik.uni-freiburg.de>
2025-08-05 11:41:00 +0200

    prepare test

---
+++ test/engine/SpatialJoinParserTest.cpp
@@ -0,0 +1,16 @@
+// Copyright 2025, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Author: Christoph Ullinger <ullingec@cs.uni-freiburg.de>
+
+#include <gtest/gtest.h>
+
+#include "engine/SpatialJoinParser.h"
+
+namespace {
+using namespace ad_utility::detail::parallel_wkt_parser;
+// TODO SpatialJoinPrefilterTestHelpers
+
+// TODO
+TEST(SpatialJoinParser, Todo) { ASSERT_TRUE(true); }
+
+}  // namespace
