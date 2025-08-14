ad-freiburg/qlever

commit 256ed191d0de5e9924bcf14f49626f3213094a36

Group by now uses `IndexScan::numVariables` (#2264)
This does not change any functionality for now, but will make the `GroupBy` operation checks for optimizations more robust when columns are stripped away from an `IndexScan`

src/engine/GroupByImpl.cpp
@@ -622,7 +622,7 @@ std::optional<IdTable> GroupByImpl::computeGroupByForSingleIndexScan() const {
     return std::nullopt;
   }
 
-  if (indexScan->getResultWidth() <= 1 ||
+  if (indexScan->numVariables() <= 1 ||
       indexScan->graphsToFilter().has_value() || !_groupByVariables.empty()) {
     return std::nullopt;
   }
@@ -635,7 +635,7 @@ std::optional<IdTable> GroupByImpl::computeGroupByForSingleIndexScan() const {
 
   // Distinct counts are only supported for triples with three variables.
   bool countIsDistinct = varAndDistinctness.value().isDistinct_;
-  if (countIsDistinct && indexScan->getResultWidth() != 3) {
+  if (countIsDistinct && indexScan->numVariables() != 3) {
     return std::nullopt;
   }
 
@@ -645,7 +645,7 @@ std::optional<IdTable> GroupByImpl::computeGroupByForSingleIndexScan() const {
   if (!isVariableBoundInSubtree(var)) {
     // The variable is never bound, so its count is zero.
     table(0, 0) = Id::makeFromInt(0);
-  } else if (indexScan->getResultWidth() == 3) {
+  } else if (indexScan->numVariables() == 3) {
     if (countIsDistinct) {
       auto permutation =
           getPermutationForThreeVariableTriple(*_subtree, var, var);
@@ -786,7 +786,7 @@ GroupByImpl::getPermutationForThreeVariableTriple(
       std::dynamic_pointer_cast<const IndexScan>(tree.getRootOperation());
 
   if (!indexScan || indexScan->graphsToFilter().has_value() ||
-      indexScan->getResultWidth() != 3) {
+      indexScan->numVariables() != 3) {
     return std::nullopt;
   }
   {

commit 77a89a8b39a6b68ad31ec0e13dc0733138c776c4

Fix issue and add regression test (#2288)
The following query leads to an assertion error. This is a regression introduced by #2264. It is now fixed again. In particular, fixes #2284.
```sparql
SELECT (COUNT(DISTINCT ?g) AS ?count) WHERE {
  GRAPH ?g {
    ?s ?p ?o .
  }
}
```
NOTE: The query materializes all quads (triples + graph info) from the data, and thus only works when enough memory for that is available. Fixing this is a separate issue

src/engine/GroupByImpl.cpp
@@ -650,9 +650,11 @@ std::optional<IdTable> GroupByImpl::computeGroupByForSingleIndexScan() const {
     return std::nullopt;
   }
 
-  // Distinct counts are only supported for triples with three variables.
+  // Distinct counts are only supported for triples with three variables without
+  // a GRAPH variable.
   bool countIsDistinct = varAndDistinctness.value().isDistinct_;
-  if (countIsDistinct && indexScan->numVariables() != 3) {
+  if (countIsDistinct && (indexScan->numVariables() != 3 ||
+                          !indexScan->additionalVariables().empty())) {
     return std::nullopt;
   }
 

test/GroupByTest.cpp
@@ -2272,6 +2272,28 @@ TEST(GroupBy, nonConstantAggregationFunctions) {
   }
 }
 
+// _____________________________________________________________________________
+TEST(GroupBy, countDistinctGraph) {
+  // Regression test for https://github.com/ad-freiburg/qlever/issues/2284
+  using V = Variable;
+  auto* qec = ad_utility::testing::getQec();
+  auto subtree = ad_utility::makeExecutionTree<IndexScan>(
+      qec, Permutation::Enum::PSO,
+      SparqlTripleSimple{V{"?s"}, V{"?p"}, V{"?o"}, {{3, V{"?g"}}}});
+
+  auto expr0 = std::make_unique<VariableExpression>(Variable{"?g"});
+  auto expr1 = std::make_unique<CountExpression>(true, std::move(expr0));
+  GroupBy groupBy{qec,
+                  {},
+                  {{SparqlExpressionPimpl{std::move(expr1),
+                                          "COUNT(DISTINCT ?g) AS ?gCount"},
+                    Variable{"?gCount"}}},
+                  std::move(subtree)};
+
+  auto result = groupBy.computeResultOnlyForTesting(false);
+  EXPECT_EQ(result.idTable(), makeIdTableFromVector({{Id::makeFromInt(1)}}));
+}
+
 namespace {
 class GroupByLazyFixture : public ::testing::TestWithParam<bool> {
  protected: