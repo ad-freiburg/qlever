ad-freiburg/qlever

commit 9b8d21ee0a6af0ff9ec00f94cf4847786c38bd8b

Make export of `NaN`, `INF`, and `-INF` compliant with standard (#2306)
So far, QLever exported `nan`, `inf`, or `-inf`, which is not compliant with https://www.w3.org/TR/2004/REC-xmlschema-2-20041028/#double . Use the occasion to forego the use of `std::stringstream`. In particular, fixes #2303

Co-authored-by: Hannah Bast <bast@cs.uni-freiburg.de>

src/engine/ExportQueryExecutionTrees.cpp
@@ -7,6 +7,7 @@
 #include "ExportQueryExecutionTrees.h"
 
 #include <absl/strings/str_cat.h>
+#include <absl/strings/str_format.h>
 #include <absl/strings/str_join.h>
 #include <absl/strings/str_replace.h>
 
@@ -338,17 +339,29 @@ ExportQueryExecutionTrees::idToStringAndTypeForEncodedValue(Id id) {
       // We use the immediately invoked lambda here because putting this block
       // in braces confuses the test coverage tool.
       return [id] {
-        // Format as integer if fractional part is zero, let C++ decide
-        // otherwise.
-        std::stringstream ss;
         double d = id.getDouble();
-        double dIntPart;
-        if (std::modf(d, &dIntPart) == 0.0) {
-          ss << std::fixed << std::setprecision(0) << id.getDouble();
-        } else {
-          ss << d;
+        if (!std::isfinite(d)) {
+          // NOTE: We used `std::stringstream` before which is bad for two
+          // reasons. First, it would ouput "nan" or "inf" in lowercase, which
+          // is not legal RDF syntax. Second, creating a `std::stringstream`
+          // object is unnecessarily expensive.
+          std::string literal = [d]() {
+            if (std::isnan(d)) {
+              return "NaN";
+            }
+            AD_CORRECTNESS_CHECK(std::isinf(d));
+            return d > 0 ? "INF" : "-INF";
+          }();
+          return std::pair{std::move(literal), XSD_DOUBLE_TYPE};
         }
-        return std::pair{std::move(ss).str(), XSD_DECIMAL_TYPE};
+        double dIntPart;
+        // If the fractional part is zero, write number without decimal point.
+        // Otherwise, use `%g`, which uses fixed-size or exponential notation,
+        // whichever is more compact.
+        std::string out = std::modf(d, &dIntPart) == 0.0
+                              ? absl::StrFormat("%.0f", d)
+                              : absl::StrFormat("%g", d);
+        return std::pair{std::move(out), XSD_DECIMAL_TYPE};
       }();
     case Bool:
       return std::pair{std::string{id.getBoolLiteral()}, XSD_BOOLEAN_TYPE};
@@ -1245,6 +1258,8 @@ ExportQueryExecutionTrees::computeResultAsQLeverJSON(
     const char* i = XSD_INT_TYPE;
     const char* d = XSD_DECIMAL_TYPE;
     const char* b = XSD_BOOLEAN_TYPE;
+    // Note: If `type` is `XSD_DOUBLE_TYPE`, `literal` is always "NaN", "INF" or
+    // "-INF", which doesn't have a short form notation.
     if (type == nullptr || type == i || type == d ||
         (type == b && literal.length() > 1)) {
       return std::move(literal);

test/ExportQueryExecutionTreesTest.cpp
@@ -541,12 +541,18 @@ TEST(ExportQueryExecutionTrees, UnusedVariable) {
 // ____________________________________________________________________________
 TEST(ExportQueryExecutionTrees, Floats) {
   std::string kg =
-      "<s> <p> 42.2 . <s> <p> -42019234865.781e12 . <s> <p> "
-      "4.012934858173560e-12";
+      "<s> <p> 42.2 . <s> <p> -42019234865.781e12 ."
+      " <s> <p> 4.012934858173560e-12 ."
+      " <s> <p> \"NaN\"^^<http://www.w3.org/2001/XMLSchema#double> ."
+      " <s> <p> \"INF\"^^<http://www.w3.org/2001/XMLSchema#double> ."
+      " <s> <p> \"-INF\"^^<http://www.w3.org/2001/XMLSchema#double> .";
   std::string query = "SELECT ?o WHERE {?s ?p ?o} ORDER BY ?o";
 
   std::string expectedXml = makeXMLHeader({"o"}) +
                             R"(
+  <result>
+    <binding name="o"><literal datatype="http://www.w3.org/2001/XMLSchema#double">-INF</literal></binding>
+  </result>
   <result>
     <binding name="o"><literal datatype="http://www.w3.org/2001/XMLSchema#decimal">-42019234865780982022144</literal></binding>
   </result>
@@ -555,52 +561,91 @@ TEST(ExportQueryExecutionTrees, Floats) {
   </result>
   <result>
     <binding name="o"><literal datatype="http://www.w3.org/2001/XMLSchema#decimal">42.2</literal></binding>
+  </result>
+  <result>
+    <binding name="o"><literal datatype="http://www.w3.org/2001/XMLSchema#double">INF</literal></binding>
+  </result>
+  <result>
+    <binding name="o"><literal datatype="http://www.w3.org/2001/XMLSchema#double">NaN</literal></binding>
   </result>)" + xmlTrailer;
   TestCaseSelectQuery testCaseFloat{
-      kg, query, 3,
+      kg, query, 6,
       // TSV
       "?o\n"
+      "-INF\n"
       "-42019234865780982022144\n"
       "4.01293e-12\n"
-      "42.2\n",
+      "42.2\n"
+      "INF\n"
+      "NaN\n",
       // CSV
       "o\n"
+      "-INF\n"
       "-42019234865780982022144\n"
       "4.01293e-12\n"
-      "42.2\n",
+      "42.2\n"
+      "INF\n"
+      "NaN\n",
       makeExpectedQLeverJSON(
-          {"\"-42019234865780982022144\"^^<http://www.w3.org/2001/XMLSchema#decimal>"s,
+          {"\"-INF\"^^<http://www.w3.org/2001/XMLSchema#double>"s,
+           "\"-42019234865780982022144\"^^<http://www.w3.org/2001/XMLSchema#decimal>"s,
            "\"4.01293e-12\"^^<http://www.w3.org/2001/XMLSchema#decimal>"s,
-           "\"42.2\"^^<http://www.w3.org/2001/XMLSchema#decimal>"s}),
+           "\"42.2\"^^<http://www.w3.org/2001/XMLSchema#decimal>"s,
+           "\"INF\"^^<http://www.w3.org/2001/XMLSchema#double>"s,
+           "\"NaN\"^^<http://www.w3.org/2001/XMLSchema#double>"s}),
       makeExpectedSparqlJSON(
-          {makeJSONBinding("http://www.w3.org/2001/XMLSchema#decimal",
+          {makeJSONBinding("http://www.w3.org/2001/XMLSchema#double", "literal",
+                           "-INF"),
+           makeJSONBinding("http://www.w3.org/2001/XMLSchema#decimal",
                            "literal", "-42019234865780982022144"),
            makeJSONBinding("http://www.w3.org/2001/XMLSchema#decimal",
                            "literal", "4.01293e-12"),
            makeJSONBinding("http://www.w3.org/2001/XMLSchema#decimal",
-                           "literal", "42.2")}),
+                           "literal", "42.2"),
+           makeJSONBinding("http://www.w3.org/2001/XMLSchema#double", "literal",
+                           "INF"),
+           makeJSONBinding("http://www.w3.org/2001/XMLSchema#double", "literal",
+                           "NaN")}),
       expectedXml};
   runSelectQueryTestCase(testCaseFloat);
 
   TestCaseConstructQuery testCaseConstruct{
-      kg, "CONSTRUCT {?s ?p ?o} WHERE {?s ?p ?o} ORDER BY ?o", 3, 3,
+      kg, "CONSTRUCT {?s ?p ?o} WHERE {?s ?p ?o} ORDER BY ?o", 6, 6,
       // TSV
+      "<s>\t<p>\t\"-INF\"^^<http://www.w3.org/2001/XMLSchema#double>\n"
       "<s>\t<p>\t-42019234865780982022144\n"
       "<s>\t<p>\t4.01293e-12\n"
-      "<s>\t<p>\t42.2\n",
+      "<s>\t<p>\t42.2\n"
+      "<s>\t<p>\t\"INF\"^^<http://www.w3.org/2001/XMLSchema#double>\n"
+      "<s>\t<p>\t\"NaN\"^^<http://www.w3.org/2001/XMLSchema#double>\n",
       // CSV
+      "<s>,<p>,\"\"\"-INF\"\"^^<http://www.w3.org/2001/XMLSchema#double>\"\n"
       "<s>,<p>,-42019234865780982022144\n"
       "<s>,<p>,4.01293e-12\n"
-      "<s>,<p>,42.2\n",
+      "<s>,<p>,42.2\n"
+      "<s>,<p>,\"\"\"INF\"\"^^<http://www.w3.org/2001/XMLSchema#double>\"\n"
+      "<s>,<p>,\"\"\"NaN\"\"^^<http://www.w3.org/2001/XMLSchema#double>\"\n",
       // Turtle
+      "<s> <p> \"-INF\"^^<http://www.w3.org/2001/XMLSchema#double> .\n"
       "<s> <p> -42019234865780982022144 .\n"
       "<s> <p> 4.01293e-12 .\n"
-      "<s> <p> 42.2 .\n",
+      "<s> <p> 42.2 .\n"
+      "<s> <p> \"INF\"^^<http://www.w3.org/2001/XMLSchema#double> .\n"
+      "<s> <p> \"NaN\"^^<http://www.w3.org/2001/XMLSchema#double> .\n",
       []() {
         nlohmann::json j;
+        j.push_back(std::vector{
+            "<s>"s, "<p>"s,
+            "\"-INF\"^^<http://www.w3.org/2001/XMLSchema#double>"s});
         j.push_back(std::vector{"<s>"s, "<p>"s, "-42019234865780982022144"s});
         j.push_back(std::vector{"<s>"s, "<p>"s, "4.01293e-12"s});
         j.push_back(std::vector{"<s>"s, "<p>"s, "42.2"s});
+        j.push_back(
+            std::vector{"<s>"s, "<p>"s,
+                        "\"INF\"^^<http://www.w3.org/2001/XMLSchema#double>"s});
+        j.push_back(
+            std::vector{"<s>"s, "<p>"s,
+                        "\"NaN\"^^<http://www.w3.org/2001/XMLSchema#double>"s});
         return j;
       }()};
   runConstructQueryTestCase(testCaseConstruct);

test/RdfParserTest.cpp
@@ -40,6 +40,22 @@ auto encodedIriManager = []() -> const EncodedIriManager* {
 
 auto re2Parser = []() { return Re2Parser{encodedIriManager()}; };
 auto ctreParser = []() { return CtreParser{encodedIriManager()}; };
+
+bool isPositiveInfinity(const TripleComponent& value) {
+  if (!value.isDouble()) {
+    return false;
+  }
+  double d = value.getDouble();
+  return std::isinf(d) && d > 0;
+}
+
+bool isNegativeInfinity(const TripleComponent& value) {
+  if (!value.isDouble()) {
+    return false;
+  }
+  double d = value.getDouble();
+  return std::isinf(d) && d < 0;
+}
 }  // namespace
 
 // TODO<joka921>: Use the following abstractions and the alias `Parser` in all
@@ -243,6 +259,27 @@ TEST(RdfParserTest, rdfLiteral) {
                                                           expected[i]);
   }
 
+  auto testLiteral = [](const std::string& literal, const auto& predicate,
+                        ad_utility::source_location loc =
+                            ad_utility::source_location::current()) {
+    auto trace = generateLocationTrace(loc);
+
+    EXPECT_THAT((checkParseResult<Re2Parser, &Re2Parser::rdfLiteral>(literal))
+                    .getLastParseResult(),
+                ::testing::Truly(predicate));
+    EXPECT_THAT((checkParseResult<CtreParser, &CtreParser::rdfLiteral>(literal))
+                    .getLastParseResult(),
+                ::testing::Truly(predicate));
+  };
+
+  testLiteral(R"("NaN"^^)"s + "<" + XSD_DOUBLE_TYPE + ">",
+              [](const TripleComponent& tripleComponent) {
+                return tripleComponent.isDouble() &&
+                       std::isnan(tripleComponent.getDouble());
+              });
+  testLiteral(R"("INF"^^)"s + "<" + XSD_DOUBLE_TYPE + ">", isPositiveInfinity);
+  testLiteral(R"("-INF"^^)"s + "<" + XSD_DOUBLE_TYPE + ">", isNegativeInfinity);
+
   auto runCommonTests = [](auto p) {
     p.prefixMap_["doof"] = iri("<www.doof.org/>");
 
@@ -266,6 +303,9 @@ TEST(RdfParserTest, literalAndDatatypeToTripleComponent) {
 
   ASSERT_EQ(ladttc("42.1234", fromIri(XSD_DOUBLE_TYPE)), 42.1234);
   ASSERT_EQ(ladttc("+42.2345", fromIri(XSD_DOUBLE_TYPE)), +42.2345);
+  ASSERT_TRUE(std::isnan(ladttc("NaN", fromIri(XSD_DOUBLE_TYPE)).getDouble()));
+  ASSERT_TRUE(isPositiveInfinity(ladttc("INF", fromIri(XSD_DOUBLE_TYPE))));
+  ASSERT_TRUE(isNegativeInfinity(ladttc("-INF", fromIri(XSD_DOUBLE_TYPE))));
   ASSERT_EQ(ladttc("-142.321", fromIri(XSD_DECIMAL_TYPE)), -142.321);
   ASSERT_EQ(ladttc("-142321", fromIri(XSD_INT_TYPE)), -142321);
   ASSERT_EQ(ladttc("+144321", fromIri(XSD_INTEGER_TYPE)), +144321);

test/SparqlExpressionTest.cpp
@@ -635,8 +635,9 @@ TEST(SparqlExpression, stringOperators) {
       Ids{I(3), I(1), I(3), I(0)});
   checkStr(Ids{I(1), I(2), I(3)},
            IdOrLiteralOrIriVec{lit("1"), lit("2"), lit("3")});
-  checkStr(Ids{D(-1.0), D(1.0), D(2.34)},
-           IdOrLiteralOrIriVec{lit("-1"), lit("1"), lit("2.34")});
+  checkStr(Ids{D(-1.0), D(1.0), D(2.34), D(NAN), D(INFINITY), D(-INFINITY)},
+           IdOrLiteralOrIriVec{lit("-1"), lit("1"), lit("2.34"), lit("NaN"),
+                               lit("INF"), lit("-INF")});
   checkStr(Ids{B(true), B(false), Id::makeBoolFromZeroOrOne(true),
                Id::makeBoolFromZeroOrOne(false)},
            IdOrLiteralOrIriVec{lit("true"), lit("false"), lit("true"),

commit 184687b94fcee61b67b82fb06a36f1f38c3f404c

Fix typo introduced in #2306 (#2337)
This PR fixes the typo that slipped in when merging #2306.

src/engine/ExportQueryExecutionTrees.cpp
@@ -342,7 +342,7 @@ ExportQueryExecutionTrees::idToStringAndTypeForEncodedValue(Id id) {
         double d = id.getDouble();
         if (!std::isfinite(d)) {
           // NOTE: We used `std::stringstream` before which is bad for two
-          // reasons. First, it would ouput "nan" or "inf" in lowercase, which
+          // reasons. First, it would output "nan" or "inf" in lowercase, which
           // is not legal RDF syntax. Second, creating a `std::stringstream`
           // object is unnecessarily expensive.
           std::string literal = [d]() {