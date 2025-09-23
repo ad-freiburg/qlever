f7526a7402fff225702f671d66677758950bf041
ullingerc <ullingec@informatik.uni-freiburg.de>
2025-09-23 11:16:34 +0200

    use lighter include

--- src/parser/SpatialQuery.h
+++ src/parser/SpatialQuery.h
@@ -6,7 +6,7 @@
 #ifndef QLEVER_SRC_PARSER_SPATIALQUERY_H
 #define QLEVER_SRC_PARSER_SPATIALQUERY_H
 
-#include "engine/SpatialJoin.h"
+#include "engine/SpatialJoinConfig.h"
 #include "parser/MagicServiceQuery.h"
 #include "parser/PayloadVariables.h"
 
