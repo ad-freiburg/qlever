91c0d5da1227e2ea56f32dfa4ff6156f2c84f5fd
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2025-10-02 16:12:28 +0200

    Clean up the overloads
    Signed-off-by: Johannes Kalmbach <johannes.kalmbach@gmail.com>

--- src/backports/StartsWith.h
+++ src/backports/StartsWith.h
@@ -1,7 +1,11 @@
-//  Copyright 2024, University of Freiburg,
-//                  Chair of Algorithms and Data Structures.
-//  Author: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
-// Copyright 2025, Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+// Copyright 2025 The QLever Authors, in particular:
+//
+// 2025 Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>, UFR
+
+// UFR = University of Freiburg, Chair of Algorithms and Data Structures
+
+// You may not use this file except in compliance with the Apache 2.0 License,
+// which can be found in the `LICENSE` file at the root of the QLever project.
 
 #ifndef QLEVER_SRC_BACKPORTS_STARTSWITH_H
 #define QLEVER_SRC_BACKPORTS_STARTSWITH_H
@@ -34,20 +38,17 @@ CPP_template(typename CharT, typename Traits, typename Other)(
 }
 
 // Overload 2: starts_with(string_view, CharT)
-template <typename CharT, typename Traits>
-constexpr bool starts_with(std::basic_string_view<CharT, Traits> sv,
-                           CharT prefix) noexcept {
+CPP_template(typename CharT, typename Traits, typename Other)(
+    requires ql::concepts::convertible_to<
+        const Other&,
+        CharT>) constexpr bool starts_with(std::basic_string_view<CharT, Traits>
+                                               sv,
+                                           const Other& prefixIn) noexcept {
+  CharT prefix(prefixIn);
   return !sv.empty() && Traits::eq(sv.front(), prefix);
 }
 
-// Overload 3: starts_with(string_view, const CharT*)
-template <typename CharT, typename Traits>
-constexpr bool starts_with(std::basic_string_view<CharT, Traits> sv,
-                           const CharT* prefix) {
-  return starts_with(sv, std::basic_string_view<CharT, Traits>(prefix));
-}
-
-// Overload 4: starts_with(string, string_view)
+// Overload 3: starts_with(string, string_view)
 CPP_template(typename CharT, typename Traits, typename Allocator,
              typename Other)(
     requires ql::concepts::convertible_to<
@@ -62,19 +63,16 @@ CPP_template(typename CharT, typename Traits, typename Allocator,
   return starts_with(std::basic_string_view<CharT, Traits>(str), prefix);
 }
 
-// Overload 5: starts_with(string, CharT)
-template <typename CharT, typename Traits, typename Allocator>
-constexpr bool starts_with(
-    const std::basic_string<CharT, Traits, Allocator>& str,
-    CharT prefix) noexcept {
-  return starts_with(std::basic_string_view<CharT, Traits>(str), prefix);
-}
-
-// Overload 6: starts_with(string, const CharT*)
-template <typename CharT, typename Traits, typename Allocator>
-constexpr bool starts_with(
-    const std::basic_string<CharT, Traits, Allocator>& str,
-    const CharT* prefix) {
+// Overload 4: starts_with(string, CharT)
+CPP_template(typename CharT, typename Traits, typename Allocator,
+             typename Other)(
+    requires ql::concepts::convertible_to<
+        const Other&,
+        CharT>) constexpr bool starts_with(const std::basic_string<CharT,
+                                                                   Traits,
+                                                                   Allocator>&
+                                               str,
+                                           const Other& prefix) noexcept {
   return starts_with(std::basic_string_view<CharT, Traits>(str), prefix);
 }
 
--- test/backports/StartsWithTest.cpp
+++ test/backports/StartsWithTest.cpp
@@ -177,3 +177,65 @@ TEST_F(StartsWithTest, ConstexprSupport) {
   EXPECT_TRUE(result2);
   EXPECT_TRUE(result3);
 }
+
+// _____________________________________________________________________________
+// Tests for types convertible to string_view or char
+TEST_F(StartsWithTest, ConvertibleTypes) {
+  // Test with std::string convertible to string_view
+  std::string hello_string = "Hello";
+  std::string h_string = "H";
+
+  EXPECT_TRUE(ql::starts_with(sv, hello_string));
+  EXPECT_TRUE(ql::starts_with(str, hello_string));
+  EXPECT_FALSE(ql::starts_with(sv, std::string("World")));
+  EXPECT_FALSE(ql::starts_with(str, std::string("World")));
+
+  // Test with string literal as C-string (const char*)
+  EXPECT_TRUE(ql::starts_with(sv, "Hello"));
+  EXPECT_TRUE(ql::starts_with(str, "Hello"));
+  EXPECT_FALSE(ql::starts_with(sv, "World"));
+  EXPECT_FALSE(ql::starts_with(str, "World"));
+
+  // Test with character types convertible to char
+  char h_char = 'H';
+  int h_int = 'H';  // ASCII value of 'H'
+
+  EXPECT_TRUE(ql::starts_with(sv, h_char));
+  EXPECT_TRUE(ql::starts_with(str, h_char));
+  EXPECT_TRUE(ql::starts_with(sv, h_int));
+  EXPECT_TRUE(ql::starts_with(str, h_int));
+
+  char w_char = 'W';
+  int w_int = 'W';
+
+  EXPECT_FALSE(ql::starts_with(sv, w_char));
+  EXPECT_FALSE(ql::starts_with(str, w_char));
+  EXPECT_FALSE(ql::starts_with(sv, w_int));
+  EXPECT_FALSE(ql::starts_with(str, w_int));
+}
+
+// _____________________________________________________________________________
+// Tests for wide character convertible types
+TEST_F(StartsWithTest, WideConvertibleTypes) {
+  std::wstring wstr = L"Hello, World!";
+  std::wstring_view wsv = wstr;
+
+  // Test with std::wstring convertible to wstring_view
+  std::wstring hello_wstring = L"Hello";
+
+  EXPECT_TRUE(ql::starts_with(wsv, hello_wstring));
+  EXPECT_TRUE(ql::starts_with(wstr, hello_wstring));
+  EXPECT_FALSE(ql::starts_with(wsv, std::wstring(L"World")));
+  EXPECT_FALSE(ql::starts_with(wstr, std::wstring(L"World")));
+
+  // Test with wide character types
+  wchar_t h_wchar = L'H';
+
+  EXPECT_TRUE(ql::starts_with(wsv, h_wchar));
+  EXPECT_TRUE(ql::starts_with(wstr, h_wchar));
+
+  wchar_t w_wchar = L'W';
+
+  EXPECT_FALSE(ql::starts_with(wsv, w_wchar));
+  EXPECT_FALSE(ql::starts_with(wstr, w_wchar));
+}
