2695fabfce79e3be10421ca41eac4fd260733269
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2025-10-10 11:51:32 +0200

    Fix some minor bugs (maybe_unused + a single default-constructed lambda)
    Signed-off-by: Johannes Kalmbach <johannes.kalmbach@gmail.com>

--- src/index/CompressedRelation.cpp
+++ src/index/CompressedRelation.cpp
@@ -1696,8 +1696,9 @@ auto CompressedRelationWriter::createPermutationPair(
   };
   // TODO<joka921> Use `CALL_FIXED_SIZE`.
   ad_utility::CompressedExternalIdTableSorter<decltype(compare), 0>
-      twinRelationSorter(basename + ".twin-twinRelationSorter", numColumns,
-                         4_GB, alloc);
+      twinRelationSorter(
+          basename + ".twin-twinRelationSorter", numColumns, 4_GB, alloc,
+          ad_utility::DEFAULT_BLOCKSIZE_EXTERNAL_ID_TABLE, compare);
 
   DistinctIdCounter distinctCol1Counter;
   auto addBlockForLargeRelation = [&numBlocksCurrentRel, &writer1,
