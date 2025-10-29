5d29a98fa2b668774db08144a19e204bbf73ae39
ullingerc <ullingec@informatik.uni-freiburg.de>
2025-10-29 09:40:18 +0100

    new units

--- src/util/UnitOfMeasurement.cpp
+++ src/util/UnitOfMeasurement.cpp
@@ -23,6 +23,10 @@ double kilometerToUnit(double kilometers,
         return 1.0;
       case MILES:
         return kilometerToMile;
+      case FEET:
+        return kilometerToFeet;
+      case YARDS:
+        return kilometerToYards;
       default:
         AD_CORRECTNESS_CHECK(!isLengthUnit(unit.value()));
         AD_THROW("Unsupported unit of measurement for distance.");
@@ -52,6 +56,16 @@ double squareMeterToUnit(double squareMeters,
         return 1.0E-6;
       case SQUARE_MILES:
         return squareMeterToSquareMile;
+      case SQUARE_FEET:
+        return squareMeterToSquareFeet;
+      case SQUARE_YARDS:
+        return squareMeterToSquareYard;
+      case ACRE:
+        return squareMeterToAcre;
+      case ARE:
+        return 1.0E-2;
+      case HECTARE:
+        return 1.0E-4;
       default:
         AD_CORRECTNESS_CHECK(!isAreaUnit(unit.value()));
         AD_THROW("Unsupported unit of measurement for area.");
@@ -69,12 +83,26 @@ UnitOfMeasurement iriToUnitOfMeasurement(const std::string_view& iri) {
     return KILOMETERS;
   } else if (iri == UNIT_MILE_IRI) {
     return MILES;
+  } else if (iri == UNIT_FEET_IRI) {
+    return FEET;
+  } else if (iri == UNIT_YARDS_IRI) {
+    return YARDS;
   } else if (iri == UNIT_SQUARE_METER_IRI) {
     return SQUARE_METERS;
   } else if (iri == UNIT_SQUARE_KILOMETER_IRI) {
     return SQUARE_KILOMETERS;
   } else if (iri == UNIT_SQUARE_MILE_IRI) {
     return SQUARE_MILES;
+  } else if (iri == UNIT_SQUARE_FEET_IRI) {
+    return SQUARE_FEET;
+  } else if (iri == UNIT_SQUARE_YARDS_IRI) {
+    return SQUARE_YARDS;
+  } else if (iri == UNIT_ACRE_IRI) {
+    return ACRE;
+  } else if (iri == UNIT_ARE_IRI) {
+    return ARE;
+  } else if (iri == UNIT_HECTARE_IRI) {
+    return HECTARE;
   }
   return UNKNOWN;
 }
@@ -82,14 +110,16 @@ UnitOfMeasurement iriToUnitOfMeasurement(const std::string_view& iri) {
 // ____________________________________________________________________________
 bool isLengthUnit(UnitOfMeasurement unit) {
   using enum UnitOfMeasurement;
-  return unit == METERS || unit == KILOMETERS || unit == MILES;
+  return unit == METERS || unit == KILOMETERS || unit == MILES ||
+         unit == FEET || unit == YARDS;
 }
 
 // ____________________________________________________________________________
 bool isAreaUnit(UnitOfMeasurement unit) {
   using enum UnitOfMeasurement;
   return unit == SQUARE_METERS || unit == SQUARE_KILOMETERS ||
-         unit == SQUARE_MILES;
+         unit == SQUARE_MILES || unit == SQUARE_FEET || unit == SQUARE_YARDS ||
+         unit == ACRE || unit == ARE || unit == HECTARE;
 }
 
 }  // namespace ad_utility::detail
