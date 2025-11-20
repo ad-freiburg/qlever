e5387454f87a750a6f7693492897fe6583d7a97f
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2025-11-20 16:09:18 +0100

    Changes to `index` for the C++17 rewrite.
    Signed-off-by: Johannes Kalmbach <johannes.kalmbach@gmail.com>

--- src/index/DeltaTriples.cpp
+++ src/index/DeltaTriples.cpp
@@ -172,7 +172,9 @@ void DeltaTriples::modifyTriplesImpl(CancellationHandle cancellationHandle,
   tracer.beginTrace("rewriteLocalVocabEntries");
   rewriteLocalVocabEntriesAndBlankNodes(triples);
   tracer.endTrace("rewriteLocalVocabEntries");
+#ifndef QLEVER_CPP_17
   AD_EXPENSIVE_CHECK(ql::ranges::is_sorted(triples));
+#endif
   AD_EXPENSIVE_CHECK(std::unique(triples.begin(), triples.end()) ==
                      triples.end());
   tracer.beginTrace("removeExistingTriples");
@@ -218,8 +220,9 @@ SharedLocatedTriplesSnapshot DeltaTriples::getSnapshot() {
   // copies), hence the explicit `clone`.
   auto snapshotIndex = nextSnapshotIndex_;
   ++nextSnapshotIndex_;
-  return SharedLocatedTriplesSnapshot{std::make_shared<LocatedTriplesSnapshot>(
-      locatedTriples(), localVocab_.getLifetimeExtender(), snapshotIndex)};
+  return SharedLocatedTriplesSnapshot{
+      std::make_shared<LocatedTriplesSnapshot>(LocatedTriplesSnapshot{
+          locatedTriples(), localVocab_.getLifetimeExtender(), snapshotIndex})};
 }
 
 // ____________________________________________________________________________
