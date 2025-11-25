962cf97f59f14cdba6b60e00a7f1d7be505f57e5
C. Ullinger <ullingec@informatik.uni-freiburg.de>
2025-11-25 22:46:48 +0100

    Use `gtest` matchers for testing `GeometryInfo` and related helper classes (#2517)
    This change migrates the test helpers for checking approximate equality of `GeoPoint`, `Centroid`, `BoundingBox`, `MetricLength`, `MetricArea` and `GeometryInfo` from custom helper functions to Gtest matchers. This is more in line with the intended way of using `gtest` and it allows, for example, storing a matcher in a variable and using it multiple times. Existing tests are rewritten as part of this change to use the new matchers instead of the previously used functions.
    
    These changes are also a preparation for #2233, where this infrastructure will be extended for checking `GeoPointOrWkt` objects and a better infrastructure in `ValueGetterTestHelpers`.

---
+++ test/printers/GeometryInfoPrinters.h
@@ -0,0 +1,76 @@
+// Copyright 2025 The QLever Authors, in particular:
+//
+// 2025 Christoph Ullinger <ullingec@informatik.uni-freiburg.de>, UFR
+//
+// UFR = University of Freiburg, Chair of Algorithms and Data Structures
+
+#ifndef QLEVER_TEST_PRINTERS_GEOMETRYINFOPRINTERS_H
+#define QLEVER_TEST_PRINTERS_GEOMETRYINFOPRINTERS_H
+
+#include <ostream>
+
+#include "rdfTypes/GeometryInfo.h"
+
+// Note that the printers must be defined inside the `ad_utility` or `testing`
+// namespace such that argument-dependent lookup can find them within `gtest`.
+namespace ad_utility {
+
+// _____________________________________________________________________________
+inline void PrintTo(const Centroid& centroid, std::ostream* os) {
+  auto& s = *os;
+  s << "Centroid(" << centroid.centroid().toStringRepresentation() << ")";
+}
+
+// _____________________________________________________________________________
+inline void PrintTo(const BoundingBox& bb, std::ostream* os) {
+  auto& s = *os;
+  s << "BoundingBox(Lower Left = " << bb.lowerLeft().toStringRepresentation()
+    << ", Upper Right = " << bb.upperRight().toStringRepresentation() << ")";
+}
+
+// _____________________________________________________________________________
+inline void PrintTo(const GeometryType& gt, std::ostream* os) {
+  auto& s = *os;
+  s << "GeometryType(" << std::to_string(gt.type())
+    << ", IRI: " << gt.asIri().value_or("-") << ")";
+}
+
+// _____________________________________________________________________________
+inline void PrintTo(const NumGeometries& gt, std::ostream* os) {
+  auto& s = *os;
+  s << "NumGeometries(" << gt.numGeometries() << ")";
+}
+
+// _____________________________________________________________________________
+inline void PrintTo(const MetricLength& gt, std::ostream* os) {
+  auto& s = *os;
+  s << "MetricLength(" << gt.length() << ")";
+}
+
+// _____________________________________________________________________________
+inline void PrintTo(const MetricArea& gt, std::ostream* os) {
+  auto& s = *os;
+  s << "MetricArea(" << gt.area() << ")";
+}
+
+// _____________________________________________________________________________
+inline void PrintTo(const GeometryInfo& gi, std::ostream* os) {
+  auto& s = *os;
+  s << "GeometryInfo(";
+  PrintTo(gi.getWktType(), os);
+  s << ",";
+  PrintTo(gi.getCentroid(), os);
+  s << ",";
+  PrintTo(gi.getBoundingBox(), os);
+  s << ",";
+  PrintTo(gi.getNumGeometries(), os);
+  s << ",";
+  PrintTo(gi.getMetricLength(), os);
+  s << ",";
+  PrintTo(gi.getMetricArea(), os);
+  s << ")";
+}
+
+}  // namespace ad_utility
+
+#endif  // QLEVER_TEST_PRINTERS_GEOMETRYINFOPRINTERS_H
