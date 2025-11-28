ad-freiburg/qlever

commit 3b0af4ca86fe8da3d005b27827c1b7ea032af052

Backport more code (all over the codebase) to C++17 (#2543)
1. Replace `if constexpr` with `QL_CONSTEXPR` macro where GCC 8.3 miscompiles
2. Replace lazy range pipelines with eager evaluation using `absl::InlinedVector` for GCC 8.3
3. Explicitly implement move constructors instead of defaulting due to older Boost `noexcept` issues
4. Replace brace-initialized tuple returns with `std::make_tuple`
5. Replace `emplace_back` with `push_back({...})` for aggregate initialization
6. Replace defaulted comparison operators with explicit implementations
7. Add default constructors for types used with `bit_cast` in C++17 mode
8. Replace `std::string_view::starts_with` with `ql::starts_with`
9. Remove C++20 header `<numbers>`
10. Add missing `#include <backports/algorithm.h>` headers
11. Replace returns with brace-initialized struct by explicit struct construction
12. Change abbreviated function templates to `CPP_template` macro
13. Extract `constexpr` values from lambda captures to `static constexpr` variables
14. Replace structured binding references with universal references for GCC 8
15. Replace `typeof` with `decltype` for C++11 compliance
16. Add conditional compilation to exclude HTTP server components in C++17 builds
17. Explicitly convert string literals to `std::string_view` in tuple construction

src/libqlever/LibQLeverExample.cpp
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
   std::cout << "\x1b[1mBuilding index for input file \"" << inputFile << "\""
             << " with basename \"" << indexBasename << "\x1b[0m" << std::endl;
   qlever::IndexBuilderConfig config;
-  config.inputFiles_.emplace_back(inputFile, qlever::Filetype::Turtle);
+  config.inputFiles_.push_back({inputFile, qlever::Filetype::Turtle});
   config.baseName_ = indexBasename;
   try {
     qlever::Qlever::buildIndex(config);

test/libqlever/QleverTest.cpp
@@ -21,7 +21,7 @@ TEST(LibQlever, buildIndexAndRunQuery) {
   }
 
   IndexBuilderConfig c;
-  c.inputFiles_.emplace_back(filename, Filetype::Turtle);
+  c.inputFiles_.push_back({filename, Filetype::Turtle});
   c.baseName_ = "testIndexForLibQlever";
 
   // Test the activation of the memory limit

src/engine/AddCombinedRowToTable.h
@@ -8,6 +8,8 @@
 #ifndef QLEVER_SRC_ENGINE_ADDCOMBINEDROWTOTABLE_H
 #define QLEVER_SRC_ENGINE_ADDCOMBINEDROWTOTABLE_H
 
+#include <absl/container/inlined_vector.h>
+
 #include <array>
 #include <cstdint>
 #include <vector>
@@ -356,7 +358,9 @@ class AddCombinedRowToIdTable {
           // undefined.
           for (const auto& [targetIndex, sourceIndex] : optionalIndexBuffer_) {
             Id id = [&col, isColFromLeft, sourceIndex = sourceIndex]() {
-              if constexpr (isColFromLeft) {
+              // Note: `if constexpr` doesn't work here for QCC 8.3 for some
+              // reason which has yet to be analyzed.
+              if QL_CONSTEXPR (isColFromLeft) {
                 return col[sourceIndex];
               } else {
                 (void)col;
@@ -400,9 +404,25 @@ class AddCombinedRowToIdTable {
       // Make sure to reset `mergedVocab_` so it is in a valid state again.
       mergedVocab_ = LocalVocab{};
       // Only merge non-null vocabs.
-      auto range = currentVocabs_ | ql::views::filter(toBool) |
-                   ql::views::transform(dereference);
-      mergedVocab_.mergeWith(ql::ranges::ref_view{range});
+      // Note: On QCC 8.3, the elegant lazy range pipeline gives us trouble, so
+      // we use an `absl::InlinedVector<reference_wrapper>` instead, which can
+      // be done here as we know that we have at most two local vocabs.
+      auto rangeOfNonNullVocabs = [&]() {
+#ifndef QLEVER_REDUCED_FEATURE_SET_FOR_CPP17
+        return currentVocabs_ | ql::views::filter(toBool) |
+               ql::views::transform(dereference);
+#else
+        absl::InlinedVector<std::reference_wrapper<const LocalVocab>, 2>
+            tmpVocabs;
+        for (const auto& el : currentVocabs_) {
+          if (el != nullptr) {
+            tmpVocabs.push_back(std::cref(*el));
+          }
+        }
+        return tmpVocabs;
+#endif
+      }();
+      mergedVocab_.mergeWith(ql::ranges::ref_view{rangeOfNonNullVocabs});
     }
   }
   const IdTableView<0>& inputLeft() const {

src/index/IndexBuilderTypes.h
@@ -115,7 +115,15 @@ struct ItemMapAndBuffer {
   MonotonicBuffer buffer_;
 
   explicit ItemMapAndBuffer(ItemAlloc alloc) : map_{alloc} {}
-  ItemMapAndBuffer(ItemMapAndBuffer&&) noexcept = default;
+  // Note: For older boost versions + compilers, we unfortunately cannot default
+  // copy constructor because
+  // 1. In older boost versions, the move operations of the polymorphic
+  // allocators were not yet marked `noexcept`
+  // 2. We definitely want this move constructor to be `noexcept`.
+  // 3. GCC 8 complains if we explicitly use `noexcept = default` if the default
+  // implementation wouldn't be noexcept.
+  ItemMapAndBuffer(ItemMapAndBuffer&& rhs) noexcept
+      : map_{std::move(rhs.map_)}, buffer_{std::move(rhs.buffer_)} {}
   // We have to delete the move-assignment as it would have the wrong semantics
   // (the monotonic buffer wouldn't be moved, this is one of the oddities of the
   // `ql::pmr` types.

src/index/TextIndexBuilder.cpp
@@ -443,7 +443,7 @@ void TextIndexBuilder::calculateBlockBoundariesImpl(
     // If we are in a block where one of the fourLetterPrefixes are contained,
     // use those as the block start.
     adjustPrefixSortKey(prefixSortKey, len);
-    return std::tuple{std::move(len), std::move(prefixSortKey)};
+    return std::make_tuple(std::move(len), std::move(prefixSortKey));
   };
   auto [currentLen, prefixSortKey] =
       getLengthAndPrefixSortKey(WordVocabIndex::make(0));

src/parser/Tokenizer.h
@@ -312,7 +312,7 @@ class Tokenizer : public SkipWhitespaceAndCommentsMixin<Tokenizer> {
 
   template <size_t idx>
   std::tuple<bool, size_t, std::string_view> getNextTokenRecurse() {
-    return std::tuple(false, idx, "");
+    return std::make_tuple(false, idx, std::string_view{""});
   }
 
   template <size_t idx, TurtleTokenId fst, TurtleTokenId... ids>
@@ -326,8 +326,9 @@ class Tokenizer : public SkipWhitespaceAndCommentsMixin<Tokenizer> {
     reset(beg, dataSize);
     bool curBetter = currentSuccess && res.size() > content.size();
 
-    return std::tuple(success || currentSuccess, curBetter ? idx : unusedIdx,
-                      curBetter ? res : content);
+    return std::make_tuple(success || currentSuccess,
+                           curBetter ? idx : unusedIdx,
+                           curBetter ? res : content);
   }
 
   // If there is a prefix match with the argument, move forward the input stream

src/parser/TokenizerCtre.h
@@ -304,7 +304,7 @@ class TokenizerCtre : public SkipWhitespaceAndCommentsMixin<TokenizerCtre> {
   // left, so there's no match.
   template <size_t idx>
   std::tuple<bool, size_t, std::string_view> getNextTokenRecurse() {
-    return std::tuple(false, idx, "");
+    return std::make_tuple(false, idx, std::string_view{""});
   }
 
   /*
@@ -328,8 +328,9 @@ class TokenizerCtre : public SkipWhitespaceAndCommentsMixin<TokenizerCtre> {
     reset(beg, dataSize);
     bool curBetter = currentSuccess && res.size() > content.size();
 
-    return std::tuple(success || currentSuccess, curBetter ? idx : unusedIdx,
-                      curBetter ? res : content);
+    return std::make_tuple(success || currentSuccess,
+                           curBetter ? idx : unusedIdx,
+                           curBetter ? res : content);
   }
 
   /*

src/rdfTypes/GeometryInfo.h
@@ -95,7 +95,9 @@ struct NumGeometries {
 
   uint32_t numGeometries() const { return numGeometries_; }
 
-  constexpr bool operator==(const NumGeometries& other) const = default;
+  constexpr bool operator==(const NumGeometries& other) const {
+    return numGeometries_ == other.numGeometries_;
+  };
 };
 
 // Represents the length of the geometry in meters.
@@ -105,6 +107,10 @@ struct MetricLength {
 
  public:
   explicit MetricLength(double length);
+#ifdef QLEVER_REDUCED_FEATURE_SET_FOR_CPP17
+  // Required for `bit_cast`.
+  MetricLength() = default;
+#endif
 
   double length() const { return length_; }
 };
@@ -120,6 +126,10 @@ struct MetricArea {
 
  public:
   explicit MetricArea(double area);
+#ifdef QLEVER_REDUCED_FEATURE_SET_FOR_CPP17
+  // Required for `bit_cast`.
+  MetricArea() = default;
+#endif
 
   double area() const { return area_; };
 
@@ -177,6 +187,10 @@ class GeometryInfo {
   GeometryInfo(uint8_t wktType, const BoundingBox& boundingBox,
                Centroid centroid, NumGeometries numGeometries,
                MetricLength metricLength, MetricArea metricArea);
+#ifdef QLEVER_REDUCED_FEATURE_SET_FOR_CPP17
+  // Required for `bit_cast`.
+  GeometryInfo() = default;
+#endif
 
   GeometryInfo(const GeometryInfo& other) = default;
 

commit d7c9afd64660ee477161d41c4bf31ab0abfe8c56

Fix warning turned error in `LibQleverExample.cpp` and `QleverTest.cpp` (#2546)
This fixes a regression introduced in #2543 and ensures the build on master (and all PRs) are green again.

src/libqlever/LibQLeverExample.cpp
@@ -30,7 +30,8 @@ int main(int argc, char** argv) {
   std::cout << "\x1b[1mBuilding index for input file \"" << inputFile << "\""
             << " with basename \"" << indexBasename << "\x1b[0m" << std::endl;
   qlever::IndexBuilderConfig config;
-  config.inputFiles_.push_back({inputFile, qlever::Filetype::Turtle});
+  config.inputFiles_.push_back(
+      {inputFile, qlever::Filetype::Turtle, std::nullopt});
   config.baseName_ = indexBasename;
   try {
     qlever::Qlever::buildIndex(config);

test/libqlever/QleverTest.cpp
@@ -21,7 +21,7 @@ TEST(LibQlever, buildIndexAndRunQuery) {
   }
 
   IndexBuilderConfig c;
-  c.inputFiles_.push_back({filename, Filetype::Turtle});
+  c.inputFiles_.push_back({filename, Filetype::Turtle, std::nullopt});
   c.baseName_ = "testIndexForLibQlever";
 
   // Test the activation of the memory limit