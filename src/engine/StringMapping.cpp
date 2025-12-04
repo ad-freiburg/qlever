6ae4c0242c5d53c57d37f43968e52da10c9684f1
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2025-12-04 12:36:57 +0100

    Another set of preparations for the Binary export.
    Signed-off-by: Johannes Kalmbach <johannes.kalmbach@gmail.com>

---
+++ src/engine/StringMapping.cpp
@@ -0,0 +1,47 @@
+// Copyright 2025, University of Freiburg
+// Chair of Algorithms and Data Structures
+// Authors: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
+//          Robin Textor-Falconi <textorr@cs.uni-freiburg.de>
+
+#include "engine/StringMapping.h"
+#include "engine/ExportQueryExecutionTrees.h"
+
+#include "index/Index.h"
+
+namespace qlever::binary_export {
+
+// _____________________________________________________________________________
+std::vector<std::string> StringMapping::flush(const Index& index) {
+  LocalVocab dummy;
+  numProcessedRows_ = 0;
+  std::vector<std::string> sortedStrings;
+  sortedStrings.resize(stringMapping_.size());
+  for (auto& [oldId, newId] : stringMapping_) {
+    auto literalOrIri =
+        ExportQueryExecutionTrees::idToLiteralOrIri(index, oldId, dummy, true);
+    AD_CORRECTNESS_CHECK(literalOrIri.has_value());
+    sortedStrings[newId] =
+        std::move(literalOrIri.value().toStringRepresentation());
+  }
+  stringMapping_.clear();
+  return sortedStrings;
+}
+
+// _____________________________________________________________________________
+Id StringMapping::remapId(Id id) {
+  static constexpr std::array allowedDatatypes{
+    Datatype::VocabIndex, Datatype::LocalVocabIndex,
+    Datatype::TextRecordIndex, Datatype::WordVocabIndex};
+  AD_EXPENSIVE_CHECK(ad_utility::contains(allowedDatatypes, id.getDatatype()));
+  size_t distinctIndex = 0;
+  if (stringMapping_.contains(id)) {
+    distinctIndex = stringMapping_.at(id);
+  } else {
+    distinctIndex = stringMapping_[id] = stringMapping_.size();
+  }
+  // The shift is required to imitate the unused bits of a pointer.
+  return Id::makeFromLocalVocabIndex(
+      reinterpret_cast<LocalVocabIndex>(distinctIndex << Id::numDatatypeBits));
+}
+
+}  // namespace qlever::binary_export
---
+++ test/engine/StringMappingTest.cpp
@@ -0,0 +1,95 @@
+// Copyright 2025, University of Freiburg
+// Chair of Algorithms and Data Structures
+// Authors: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <ranges>
+#include <vector>
+
+#include "../util/IdTableHelpers.h"
+#include "../util/IndexTestHelpers.h"
+#include "engine/BinaryExport.h"
+#include "engine/StringMapping.h"
+#include "global/Id.h"
+
+using namespace qlever::binary_export;
+
+// _____________________________________________________________________________
+TEST(BinaryExport, toExportableIdTrivial) {
+  Id intId = Id::makeFromInt(42);
+  LocalVocab localVocab;
+  StringMapping mapping;
+
+  Id result = toExportableId(intId, localVocab, mapping);
+
+  EXPECT_EQ(result, intId);
+  EXPECT_TRUE(mapping.stringMapping_.empty());
+}
+
+// _____________________________________________________________________________
+TEST(StringMapping, remapId) {
+  // This is important so we have a working comparator.
+  ad_utility::testing::getQec("<a> <b> <c> .");
+  auto toMappedId = [](size_t count) {
+    return Id::makeFromLocalVocabIndex(
+        reinterpret_cast<LocalVocabIndex>(count << ValueId::numDatatypeBits));
+  };
+
+  auto binaryEq = [](Id a, Id b,
+                     ad_utility::source_location loc =
+                         AD_CURRENT_SOURCE_LOC()) {
+    auto g = generateLocationTrace(loc);
+    EXPECT_EQ(a.getBits(), b.getBits());
+  };
+
+  LocalVocabEntry testWord{
+      ad_utility::triple_component::Literal::fromStringRepresentation(
+          "\"abc\"")};
+  LocalVocabEntry duplicateWord{
+      ad_utility::triple_component::Iri::fromStringRepresentation("<b>")};
+  StringMapping mapping;
+  Id id1 = Id::makeFromVocabIndex(VocabIndex::make(1));
+  Id id2 = Id::makeFromLocalVocabIndex(&testWord);
+  Id id3 = Id::makeFromTextRecordIndex(TextRecordIndex::make(42));
+  Id id4 = Id::makeFromWordVocabIndex(WordVocabIndex::make(1010));
+  Id id5 = Id::makeFromLocalVocabIndex(&duplicateWord);
+
+  // Mapped ids start counting from zero.
+  binaryEq(mapping.remapId(id1), toMappedId(0));
+  binaryEq(mapping.remapId(id2), toMappedId(1));
+  binaryEq(mapping.remapId(id3), toMappedId(2));
+  binaryEq(mapping.remapId(id4), toMappedId(3));
+  binaryEq(mapping.remapId(id1), toMappedId(0));
+  binaryEq(mapping.remapId(id5), toMappedId(0));
+
+  EXPECT_THAT(mapping.stringMapping_,
+              ::testing::UnorderedElementsAre(
+                  ::testing::Pair(id1, 0), ::testing::Pair(id2, 1),
+                  ::testing::Pair(id3, 2), ::testing::Pair(id4, 3)));
+}
+
+// _____________________________________________________________________________
+TEST(StringMapping, needsFlush) {
+  StringMapping mapping;
+
+  // Initially should not need flush
+  EXPECT_FALSE(mapping.needsFlush());
+
+  // Add enough rows to trigger flush (needs at least one mapping entry)
+  mapping.stringMapping_[Id::makeFromInt(1)] = 0;
+  for (size_t i = 0; i < 100'000; ++i) {
+    mapping.nextRow();
+  }
+
+  EXPECT_TRUE(mapping.needsFlush());
+
+  // Test size-based flush
+  StringMapping mapping2;
+  for (size_t i = 0; i < 10'000; ++i) {
+    mapping2.stringMapping_[Id::makeFromInt(i)] = i;
+  }
+
+  EXPECT_TRUE(mapping2.needsFlush());
+}
