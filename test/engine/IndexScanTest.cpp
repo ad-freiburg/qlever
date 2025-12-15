22dff0947bf19ff5ba18809d00aa0bac3fcef885
RobinTF <83676088+RobinTF@users.noreply.github.com>
2025-12-15 13:59:34 +0100

    Add unit test for internal iri check

--- test/engine/IndexScanTest.cpp
+++ test/engine/IndexScanTest.cpp
@@ -1819,3 +1819,96 @@ TEST(IndexScanTest, StripColumnsWithPrefiltering) {
         << varsToKeep.size() << " variables";
   }
 }
+
+// _____________________________________________________________________________
+TEST(IndexScanTest, internalPrefixIsCorrectlyChosen) {
+  auto* qec = getQec();
+  const auto& index = qec->getIndex();
+  TripleComponent internalIri{ad_utility::triple_component::Iri::fromIriref(
+      makeQleverInternalIri("something"))};
+  TripleComponent languageTaggedIri{
+      ad_utility::triple_component::Iri::fromIriref("@en@<abc>")};
+  TripleComponent regularIri{
+      ad_utility::triple_component::Iri::fromIriref("<abc>")};
+  TripleComponent regularLiteral{1};
+
+  for (auto permutation : Permutation::ALL) {
+    const auto* permutationPtr = &index.getImpl().getPermutation(permutation);
+    EXPECT_EQ(IndexScan::getPermutationForTriple(
+                  permutation, index,
+                  SparqlTripleSimple{regularIri, regularIri, regularIri})
+                  .get(),
+              permutationPtr);
+    EXPECT_EQ(
+        IndexScan::getPermutationForTriple(
+            permutation, index,
+            SparqlTripleSimple{regularLiteral, regularLiteral, regularLiteral})
+            .get(),
+        permutationPtr);
+    EXPECT_EQ(
+        IndexScan::getPermutationForTriple(
+            permutation, index,
+            SparqlTripleSimple{regularLiteral, regularIri, regularLiteral})
+            .get(),
+        permutationPtr);
+  }
+
+  for (auto permutation : Permutation::INTERNAL) {
+    const auto* permutationPtr =
+        &index.getImpl().getPermutation(permutation).internalPermutation();
+    EXPECT_EQ(IndexScan::getPermutationForTriple(
+                  permutation, index,
+                  SparqlTripleSimple{internalIri, regularIri, regularIri})
+                  .get(),
+              permutationPtr);
+    EXPECT_EQ(IndexScan::getPermutationForTriple(
+                  permutation, index,
+                  SparqlTripleSimple{regularIri, internalIri, regularIri})
+                  .get(),
+              permutationPtr);
+    EXPECT_EQ(IndexScan::getPermutationForTriple(
+                  permutation, index,
+                  SparqlTripleSimple{regularIri, regularIri, internalIri})
+                  .get(),
+              permutationPtr);
+
+    EXPECT_EQ(IndexScan::getPermutationForTriple(
+                  permutation, index,
+                  SparqlTripleSimple{languageTaggedIri, regularIri, regularIri})
+                  .get(),
+              permutationPtr);
+    EXPECT_EQ(IndexScan::getPermutationForTriple(
+                  permutation, index,
+                  SparqlTripleSimple{regularIri, languageTaggedIri, regularIri})
+                  .get(),
+              permutationPtr);
+    EXPECT_EQ(IndexScan::getPermutationForTriple(
+                  permutation, index,
+                  SparqlTripleSimple{regularIri, regularIri, languageTaggedIri})
+                  .get(),
+              permutationPtr);
+  }
+
+  using enum Permutation::Enum;
+  // Unsupported configurations.
+  EXPECT_THROW(IndexScan::getPermutationForTriple(
+                   OPS, index,
+                   SparqlTripleSimple{languageTaggedIri, internalIri,
+                                      languageTaggedIri}),
+               ad_utility::Exception);
+  EXPECT_THROW(IndexScan::getPermutationForTriple(
+                   OSP, index,
+                   SparqlTripleSimple{languageTaggedIri, internalIri,
+                                      languageTaggedIri}),
+               ad_utility::Exception);
+  EXPECT_THROW(IndexScan::getPermutationForTriple(
+                   SOP, index,
+                   SparqlTripleSimple{languageTaggedIri, internalIri,
+                                      languageTaggedIri}),
+               ad_utility::Exception);
+  EXPECT_THROW(IndexScan::getPermutationForTriple(
+                   SPO, index,
+                   SparqlTripleSimple{languageTaggedIri, internalIri,
+                                      languageTaggedIri}),
+               ad_utility::Exception);
+}
