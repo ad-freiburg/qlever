89e524b99fec2866ac9d20409da2e2d6d70fd98e
RobinTF <83676088+RobinTF@users.noreply.github.com>
2025-12-25 03:25:06 +0100

    Update triples for the internal `@lang@...` predicates (#2461)
    So far, update operations never add or delete triples for the internal `@lang@...` predicates. For example, when an update operation adds the triple `wd:Q18642047 rdfs:label "Han Bing"@zh`, it's important to also add the implied "internal" triple `wd:Q18642047 @zh@rdfs:label "Han Bing"@zh`. Without the latter, a query like `SELECT * { ?subject rdfs:label ?label FILTER (LANG(?label) = "zh") }` is missing the newly added triple. This is now fixed.
    
    NOTE 1: The internal `ql:langtag` triples are still not updated. This only affects queries where literals are obtained via a variable predicate and then filtered by language, as in `SELECT * { wd:Q873 ?anything ?literal FILTER (LANG(?literal) = "zh") }`, which is rarely used. This will be addressed in a future PR
    
    NOTE 2: The check whether an internal triple is implied currently happens at a point in the code, where the update triples have already been converted to the `Id` space. Each object is then looked up in the vocabulary again, in order to determine whether it has a language tag. This will be addressed once the language tag is encoded in the `Id`

--- test/CompressedRelationsTest.cpp
+++ test/CompressedRelationsTest.cpp
@@ -26,6 +26,8 @@ Id V(int64_t index) {
   return Id::makeFromVocabIndex(VocabIndex::make(index));
 }
 
+auto I = &Id::makeFromInt;
+
 // Retrieve the corresponding `BlockMetadataRanges` value for the
 // given`CompressedBlockMetadata` vector
 auto getBlockMetadataRangesfromVec =
@@ -966,8 +968,8 @@ TEST(CompressedRelationReader, getResultSizeImpl) {
   DeltaTriplesManager& deltaTriplesManager = index.deltaTriplesManager();
   deltaTriplesManager.modify<void>([](DeltaTriples& dt) {
     auto handle = std::make_shared<ad_utility::CancellationHandle<>>();
-    dt.insertTriples(handle, {IdTriple{{V(0), V(1), V(2), V(3)}},
-                              IdTriple{{V(0), V(4), V(5), V(3)}}});
+    dt.insertTriples(handle, {IdTriple{{I(0), I(1), I(2), I(3)}},
+                              IdTriple{{I(0), I(4), I(5), I(3)}}});
   });
   auto sharedLocatedTriplesSnapshot = deltaTriplesManager.getCurrentSnapshot();
   const auto& locatedTriplesSnapshot = *sharedLocatedTriplesSnapshot;
@@ -998,13 +1000,13 @@ TEST(CompressedRelationReader, getResultSizeImpl) {
     expectResultSizes(perm, {std::nullopt, std::nullopt, std::nullopt}, 0, 2,
                       2);
   }
-  expectResultSizes(Permutation::SPO, {V(0), std::nullopt, std::nullopt}, 0, 2,
+  expectResultSizes(Permutation::SPO, {I(0), std::nullopt, std::nullopt}, 0, 2,
                     2);
   // Not all triples of the block are requested. The size estimate is truncated
   // by a factor which is a RuntimeParameter.
-  expectResultSizes(Permutation::PSO, {V(1), std::nullopt, std::nullopt}, 0, 1,
+  expectResultSizes(Permutation::PSO, {I(1), std::nullopt, std::nullopt}, 0, 1,
                     1);
-  expectResultSizes(Permutation::PSO, {V(1), V(5), std::nullopt}, 0, 1, 0);
+  expectResultSizes(Permutation::PSO, {I(1), I(5), std::nullopt}, 0, 1, 0);
 }
 
 namespace {
--- test/ExecuteUpdateTest.cpp
+++ test/ExecuteUpdateTest.cpp
@@ -120,7 +120,7 @@ TEST(ExecuteUpdate, executeUpdate) {
     // Now the actual tests.
     expectExecuteUpdate("INSERT DATA { <s> <p> <o> . }", NumTriples(1, 0, 1));
     expectExecuteUpdate("DELETE DATA { <z> <label> \"zz\"@en }",
-                        NumTriples(0, 1, 1));
+                        NumTriples(0, 1, 1, 0, 1));
     expectExecuteUpdate(
         "DELETE { ?s <is-a> ?o } INSERT { <a> <b> <c> } WHERE { ?s <is-a> ?o }",
         NumTriples(1, 2, 3));
@@ -131,7 +131,7 @@ TEST(ExecuteUpdate, executeUpdate) {
         "DELETE { ?s <is-a> ?o } INSERT { ?s <is-a> ?o } WHERE { ?s <is-a> ?o "
         "}",
         NumTriples(2, 0, 2));
-    expectExecuteUpdate("DELETE WHERE { ?s ?p ?o }", NumTriples(0, 8, 8));
+    expectExecuteUpdate("DELETE WHERE { ?s ?p ?o }", NumTriples(0, 8, 8, 0, 1));
     expectExecuteUpdateFails(
         "SELECT * WHERE { ?s ?p ?o }",
         testing::HasSubstr(
@@ -147,26 +147,27 @@ TEST(ExecuteUpdate, executeUpdate) {
         NumTriples(0, 1, 1));
     expectExecuteUpdate(
         "INSERT DATA { <a> <b> <c> }; DELETE WHERE { ?s ?p ?o }",
-        NumTriples(0, 9, 9));
+        NumTriples(0, 9, 9, 0, 1));
     expectExecuteUpdate("CLEAR SILENT GRAPH <x>", NumTriples(0, 0, 0));
-    expectExecuteUpdate("CLEAR DEFAULT", NumTriples(0, 8, 8));
+    expectExecuteUpdate("CLEAR DEFAULT", NumTriples(0, 8, 8, 0, 1));
     expectExecuteUpdate("CLEAR SILENT NAMED", NumTriples(0, 0, 0));
-    expectExecuteUpdate("CLEAR ALL", NumTriples(0, 8, 8));
+    expectExecuteUpdate("CLEAR ALL", NumTriples(0, 8, 8, 0, 1));
     expectExecuteUpdate("DROP GRAPH <x>", NumTriples(0, 0, 0));
-    expectExecuteUpdate("DROP SILENT DEFAULT", NumTriples(0, 8, 8));
+    expectExecuteUpdate("DROP SILENT DEFAULT", NumTriples(0, 8, 8, 0, 1));
     expectExecuteUpdate("DROP NAMED", NumTriples(0, 0, 0));
-    expectExecuteUpdate("DROP SILENT ALL", NumTriples(0, 8, 8));
+    expectExecuteUpdate("DROP SILENT ALL", NumTriples(0, 8, 8, 0, 1));
     expectExecuteUpdate("ADD <x> TO <x>", NumTriples(0, 0, 0));
     expectExecuteUpdate("ADD SILENT <x> TO DEFAULT", NumTriples(0, 0, 0));
-    expectExecuteUpdate("ADD DEFAULT TO <x>", NumTriples(8, 0, 8));
+    expectExecuteUpdate("ADD DEFAULT TO <x>", NumTriples(8, 0, 8, 1, 0));
     expectExecuteUpdate("ADD SILENT DEFAULT TO DEFAULT", NumTriples(0, 0, 0));
     expectExecuteUpdate("MOVE SILENT DEFAULT TO DEFAULT", NumTriples(0, 0, 0));
     expectExecuteUpdate("MOVE GRAPH <x> TO <x>", NumTriples(0, 0, 0));
-    expectExecuteUpdate("MOVE <x> TO DEFAULT", NumTriples(0, 8, 8));
-    expectExecuteUpdate("MOVE DEFAULT TO GRAPH <x>", NumTriples(8, 8, 16));
-    expectExecuteUpdate("COPY DEFAULT TO <x>", NumTriples(8, 0, 8));
+    expectExecuteUpdate("MOVE <x> TO DEFAULT", NumTriples(0, 8, 8, 0, 1));
+    expectExecuteUpdate("MOVE DEFAULT TO GRAPH <x>",
+                        NumTriples(8, 8, 16, 1, 1));
+    expectExecuteUpdate("COPY DEFAULT TO <x>", NumTriples(8, 0, 8, 1, 0));
     expectExecuteUpdate("COPY DEFAULT TO DEFAULT", NumTriples(0, 0, 0));
-    expectExecuteUpdate("COPY <x> TO DEFAULT", NumTriples(0, 8, 8));
+    expectExecuteUpdate("COPY <x> TO DEFAULT", NumTriples(0, 8, 8, 0, 1));
     expectExecuteUpdate("CREATE SILENT GRAPH <x>", NumTriples(0, 0, 0));
     expectExecuteUpdate("CREATE GRAPH <y>", NumTriples(0, 0, 0));
   }
@@ -181,29 +182,29 @@ TEST(ExecuteUpdate, executeUpdate) {
         "<u> <blub> <blah> <s> .";
     indexConfig.indexType = qlever::Filetype::NQuad;
     // That the DEFAULT graph is the union graph again causes some problems.
-    expectExecuteUpdate("CLEAR SILENT GRAPH <q>", NumTriples(0, 3, 3));
+    expectExecuteUpdate("CLEAR SILENT GRAPH <q>", NumTriples(0, 3, 3, 0, 2));
     expectExecuteUpdate("CLEAR GRAPH <a>", NumTriples(0, 0, 0));
     expectExecuteUpdate("CLEAR DEFAULT", NumTriples(0, 1, 1));
-    expectExecuteUpdate("CLEAR SILENT NAMED", NumTriples(0, 6, 6));
-    expectExecuteUpdate("CLEAR ALL", NumTriples(0, 7, 7));
-    expectExecuteUpdate("DROP GRAPH <q>", NumTriples(0, 3, 3));
+    expectExecuteUpdate("CLEAR SILENT NAMED", NumTriples(0, 6, 6, 0, 3));
+    expectExecuteUpdate("CLEAR ALL", NumTriples(0, 7, 7, 0, 3));
+    expectExecuteUpdate("DROP GRAPH <q>", NumTriples(0, 3, 3, 0, 2));
     expectExecuteUpdate("DROP SILENT GRAPH <a>", NumTriples(0, 0, 0));
     expectExecuteUpdate("DROP SILENT DEFAULT", NumTriples(0, 1, 1));
-    expectExecuteUpdate("DROP NAMED", NumTriples(0, 6, 6));
-    expectExecuteUpdate("DROP SILENT ALL", NumTriples(0, 7, 7));
+    expectExecuteUpdate("DROP NAMED", NumTriples(0, 6, 6, 0, 3));
+    expectExecuteUpdate("DROP SILENT ALL", NumTriples(0, 7, 7, 0, 3));
     expectExecuteUpdate("ADD <q> TO <q>", NumTriples(0, 0, 0));
     expectExecuteUpdate("ADD <a> TO <q>", NumTriples(0, 0, 0));
-    expectExecuteUpdate("ADD SILENT <q> TO DEFAULT", NumTriples(3, 0, 3));
+    expectExecuteUpdate("ADD SILENT <q> TO DEFAULT", NumTriples(3, 0, 3, 2, 0));
     expectExecuteUpdate("ADD DEFAULT TO <q>", NumTriples(1, 0, 1));
     expectExecuteUpdate("ADD SILENT DEFAULT TO DEFAULT", NumTriples(0, 0, 0));
     expectExecuteUpdate("MOVE SILENT DEFAULT TO DEFAULT", NumTriples(0, 0, 0));
-    expectExecuteUpdate("MOVE GRAPH <q> TO <t>", NumTriples(3, 3, 6));
-    expectExecuteUpdate("MOVE <q> TO DEFAULT", NumTriples(3, 4, 7));
+    expectExecuteUpdate("MOVE GRAPH <q> TO <t>", NumTriples(3, 3, 6, 2, 2));
+    expectExecuteUpdate("MOVE <q> TO DEFAULT", NumTriples(3, 4, 7, 2, 2));
     expectExecuteUpdate("MOVE DEFAULT TO GRAPH <t>", NumTriples(1, 1, 2));
-    expectExecuteUpdate("MOVE DEFAULT TO GRAPH <q>", NumTriples(1, 4, 5));
-    expectExecuteUpdate("COPY DEFAULT TO <q>", NumTriples(1, 3, 4));
+    expectExecuteUpdate("MOVE DEFAULT TO GRAPH <q>", NumTriples(1, 4, 5, 0, 2));
+    expectExecuteUpdate("COPY DEFAULT TO <q>", NumTriples(1, 3, 4, 0, 2));
     expectExecuteUpdate("COPY DEFAULT TO DEFAULT", NumTriples(0, 0, 0));
-    expectExecuteUpdate("COPY <q> TO DEFAULT", NumTriples(3, 1, 4));
+    expectExecuteUpdate("COPY <q> TO DEFAULT", NumTriples(3, 1, 4, 2, 0));
     expectExecuteUpdate("CREATE SILENT GRAPH <x>", NumTriples(0, 0, 0));
     expectExecuteUpdate("CREATE GRAPH <y>", NumTriples(0, 0, 0));
   }
