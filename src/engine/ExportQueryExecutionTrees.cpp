215b8acee7eb547aac8d5a774dba25ddbbf3654b
marvin <marvin.stoetzel@mailbox.org>
2026-01-05 11:32:33 +0100

    bugfix: do not compute stats after every row of the result table for a CONSTRUCT-query, move the cache stats computation and report to after we have iterated over all rows in the result table for the WHERE-clause

--- src/engine/ExportQueryExecutionTrees.cpp
+++ src/engine/ExportQueryExecutionTrees.cpp
@@ -316,21 +316,20 @@ ExportQueryExecutionTrees::constructQueryResultToTriples(
     }
     rowOffset += idTable.get().size(); // progress tracking
 
-    // Only compute and log stats when debug logging is enabled
-    if constexpr (LOGLEVEL >= INFO) {
-      auto stats = cache.getStats();
-      AD_LOG_INFO<< "Construct Query Cache stats: \n"
-                   << "var eval cache hits: " << stats.variableHits() << "\n"
-                   << "var eval cache misses: " << stats.variableMisses() << "\n"
-                   << "literal eval cache hits: " << stats.literalHits() << "\n"
-                   << "literal eval cache misses: " << stats.literalMisses() << "\n"
-                   << "iri eval cache hits: " << stats.iriHits() << "\n"
-                   << "iri eval cache misses: " << stats.iriMisses() << "\n"
-                   << "blankNode eval cache hits: " << stats.iriHits() << "\n"
-                   << "blankNode eval cache misses: " << stats.iriMisses() << "\n"
-          ;
-    }
   };
+  // Only compute and log stats when debug logging is enabled
+  if constexpr (LOGLEVEL >= INFO) {
+    auto stats = cache.getStats();
+    AD_LOG_INFO << "Construct Query Cache stats: \n"
+               << "var eval cache hits: " << stats.variableHits() << "\n"
+               << "var eval cache misses: " << stats.variableMisses() << "\n"
+               << "literal eval cache hits: " << stats.literalHits() << "\n"
+               << "literal eval cache misses: " << stats.literalMisses() << "\n"
+               << "iri eval cache hits: " << stats.iriHits() << "\n"
+               << "iri eval cache misses: " << stats.iriMisses() << "\n"
+               << "blankNode eval cache hits: " << stats.iriHits() << "\n"
+               << "blankNode eval cache misses: " << stats.iriMisses() << std::endl;
+  }
 
   // For each result from the WHERE clause, we produce up to
   // `constructTriples.size()` triples. We do not account for triples that are
