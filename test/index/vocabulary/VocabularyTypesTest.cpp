c151ecb64fd7f1b0756cfe329f84c3d9e4663876
RobinTF <83676088+RobinTF@users.noreply.github.com>
2026-01-07 10:42:48 +0100

    Fix wrong termination of application

---
+++ test/index/vocabulary/VocabularyTypesTest.cpp
@@ -0,0 +1,56 @@
+//   Copyright 2026, University of Freiburg,
+//   Chair of Algorithms and Data Structures.
+//   Author: Robin Textor-Falconi <textorr@informatik.uni-freiburg.de>
+
+#include <absl/functional/function_ref.h>
+#include <gtest/gtest.h>
+
+#include "../../util/GTestHelpers.h"
+#include "index/vocabulary/VocabularyTypes.h"
+
+// A class that executes a passed function in its constuctor.
+class Caller {
+ public:
+  explicit Caller(absl::FunctionRef<void()> f) { std::invoke(f); }
+};
+
+// A class inheriting from `WordWriterBase` that throws when initializing a
+// member.
+class WordWriterThrowing : public WordWriterBase {
+ private:
+  Caller caller_;
+
+ public:
+  WordWriterThrowing()
+      : caller_{[]() { throw std::runtime_error("Constructor failed"); }} {}
+  uint64_t operator()(std::string_view, bool) override { return 0; }
+  void finishImpl() override {}
+};
+
+// A class inheriting from `WordWriterBase` that doesn't call finish.
+class WordWriterNoFinish : public WordWriterBase {
+ public:
+  WordWriterNoFinish() {}
+  uint64_t operator()(std::string_view, bool) override { return 0; }
+  void finishImpl() override {}
+};
+
+// _____________________________________________________________________________
+TEST(VocabularyTypes, verifyWordWriterBaseDestructorBehavesAsExpected) {
+  // Test that the original exception from `WordWriterThrowing` is propagated.
+  AD_EXPECT_THROW_WITH_MESSAGE_AND_TYPE(WordWriterThrowing{},
+                                        ::testing::StrEq("Constructor failed"),
+                                        std::runtime_error);
+
+  // Test that the no finish exception is thrown when destroying a
+  // `WordWriterNoFinish`.
+  AD_EXPECT_THROW_WITH_MESSAGE_AND_TYPE(
+      WordWriterNoFinish{}, ::testing::HasSubstr("WordWriterBase::finish was"),
+      std::runtime_error);
+
+  // Test that no exception is thrown when `finish` is called.
+  EXPECT_NO_THROW({
+    WordWriterNoFinish writer;
+    writer.finish();
+  });
+}
