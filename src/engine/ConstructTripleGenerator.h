ad-freiburg/qlever

commit 45ed92917fa9e025324f27a13ef7f0c951154e57

Simplify filter of range

src/engine/ConstructTripleGenerator.h
@@ -73,10 +73,9 @@ class ConstructTripleGenerator {
             cancellationHandle_->throwIfCancelled();
             return ConstructQueryEvaluator::evaluateTriple(triple, context);
           };
-      auto filterer = [](const StringTriple& t) { return !t.isEmpty(); };
 
       return constructTriples_ | ql::views::transform(innerTransformer) |
-             ql::views::filter(filterer);
+             ql::views::filter(&StringTriple::isEmpty);
     };
     return table.view_ | ql::views::transform(outerTransformer) |
            ql::views::join;

commit 7ef8bc47d33f5513752f16eac3d42b3bb2806c18

fix bug introduced in commit 45ed9291. Non-empty StringTriples were filtered out, instead of empty StringTriples.

src/engine/ConstructTripleGenerator.h
@@ -78,7 +78,7 @@ class ConstructTripleGenerator {
       return constructTriples_ |
              ql::views::transform(
                  evaluateConstructTriplesForRowFromWhereClause) |
-             ql::views::filter(&StringTriple::isEmpty);
+             ql::views::filter(std::not_fn(&StringTriple::isEmpty));
     };
     return table.view_ | ql::views::transform(outerTransformer) |
            ql::views::join;