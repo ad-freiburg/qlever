2c0e59017fda98935fbebf3d3130dd44a53c8bff
marvin <marvin.stoetzel@mailbox.org>
2026-01-23 09:57:29 +0100

    Add comments to function declarations in ConstructQueryEvaluator.h

--- src/engine/ConstructQueryEvaluator.h
+++ src/engine/ConstructQueryEvaluator.h
@@ -18,17 +18,28 @@ class ConstructQueryEvaluator {
   using StringTriple = QueryExecutionTree::StringTriple;
 
  public:
+  // Evaluates an Iri (which is part of a CONSTRUCT triple pattern).
   static std::optional<std::string> evaluate(const Iri& iri);
 
+  // Evaluates a Literal (which is part of a CONSTRUCT triple pattern) using
+  // the position of the literal in the template triple
+  // (Literals are only allowed to be in the OBJECT position of a triple).
   static std::optional<std::string> evaluate(const Literal& literal,
                                              PositionInTriple role);
 
+  // Evaluates a BlankNode (which is part of a CONSTRUCT triple pattern) using
+  // the provided context.
   static std::optional<std::string> evaluate(
       const BlankNode& node, const ConstructQueryExportContext& context);
 
+  // Evaluates a Variable (which is part of a CONSTRUCT triple pattern) using
+  // the provided context.
   static std::optional<std::string> evaluate(
       const Variable& var, const ConstructQueryExportContext& context);
 
+  // Evaluates a GraphTerm (which is part of a CONSTRUCT triple pattern) using
+  // the provided context and the position of the GraphTerm in the template
+  // triple.
   static std::optional<std::string> evaluate(
       const GraphTerm& term, const ConstructQueryExportContext& context,
       PositionInTriple posInTriple);
