576993dc1c33b3f2c56a2cc0737dc52b9258cc19
marvin <marvin.stoetzel@mailbox.org>
2026-02-13 12:15:20 +0100

    fix comment

--- src/engine/ConstructTypes.h
+++ src/engine/ConstructTypes.h
@@ -52,7 +52,7 @@ using PreprocessedTriple = std::array<PreprocessedTerm, NUM_TRIPLE_POSITIONS>;
 
 // Result of preprocessing all CONSTRUCT template triples. Contains the
 // preprocessed triples and the unique variable column indices that need to be
-// evaluated at query time.
+// evaluated for each row of the result-table.
 struct PreprocessedConstructTemplate {
   std::vector<PreprocessedTriple> preprocessedTriples_;
   std::vector<size_t> uniqueVariableColumns_;
