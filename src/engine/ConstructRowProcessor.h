683b1ce90e341d386984aa40b2439cef74dfd85b
marvin <marvin.stoetzel@mailbox.org>
2026-02-20 09:42:09 +0100

    modernize copyright message of ConstructRowProcessor.h

--- src/engine/ConstructRowProcessor.h
+++ src/engine/ConstructRowProcessor.h
@@ -4,6 +4,9 @@
 //
 // UFR = University of Freiburg, Chair of Algorithms and Data Structures
 
+// You may not use this file except in compliance with the Apache 2.0 License,
+// which can be found in the `LICENSE` file at the root of the QLever project.
+
 #ifndef QLEVER_SRC_ENGINE_CONSTRUCTBATCHPROCESSOR_H
 #define QLEVER_SRC_ENGINE_CONSTRUCTBATCHPROCESSOR_H
 
