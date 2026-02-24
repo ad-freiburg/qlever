91bdf30f1892226be51296e8a11f560b1d479c42
Johannes Kalmbach <johannes.kalmbach@gmail.com>
2026-02-24 21:30:09 +0100

    A next try....
    Signed-off-by: Johannes Kalmbach <johannes.kalmbach@gmail.com>

--- test/index/IndexRebuilderTest.cpp
+++ test/index/IndexRebuilderTest.cpp
@@ -81,19 +81,16 @@ void deleteVocabFiles(const std::string& vocabBasename,
 TEST(IndexRebuilder, materializeEmptyLocalVocab) {
   for (const auto& typeEnum : ad_utility::VocabularyType::all()) {
     ad_utility::testing::TestIndexConfig config{"<a> <c> <e> . <g> <i> <k> ."};
-    auto type = ad_utility::VocabularyType{
-        ad_utility::VocabularyType::Enum::InMemoryCompressed};
+    auto type = ad_utility::VocabularyType{typeEnum};
     std::cerr << "testing " << type.toString() << std::endl;
     config.vocabularyType = type;
     auto oldIndex = ad_utility::testing::makeTestIndex(
         "materializeEmptyLocalVocab", std::move(config));
     std::string vocabPrefix = "/tmp/materializeEmptyLocalVocab";
     std::string vocabFileName = vocabPrefix + VOCAB_SUFFIX;
-    /*
     absl::Cleanup removeVocabFiles{[&vocabFileName, &type] {
       deleteVocabFiles(vocabFileName, type.value());
     }};
-    */
 
     auto getId = ad_utility::testing::makeGetId(oldIndex);
     auto [insertionPositions, localVocabMapping] =
@@ -115,7 +112,6 @@ TEST(IndexRebuilder, materializeEmptyLocalVocab) {
   }
 }
 
-#if false
 // _____________________________________________________________________________
 TEST(IndexRebuilder, materializeLocalVocab) {
   auto type = ad_utility::VocabularyType::random();
@@ -595,4 +591,3 @@ TEST(IndexRebuilder, serverIntegration) {
 
   threadPool.join();
 }
-#endif
