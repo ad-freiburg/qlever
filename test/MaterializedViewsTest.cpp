ad-freiburg/qlever

commit 9f1e9be7dfa4ae629590c6a5168edd71824db7ff

Don't deduplicate rows implicitly when scanning materialized views (#2708)
So far, `IndexScan` operations on materialized views deduplicated rows implicitly if the reason for the duplication was an unselected column that was the fourth column or beyond. This behavior makes sense when the fourth column stores the graph ID of a triple, but not for materialized views. It It is now deactivated for materialized views.

For example, if the dataset consists of a single triple `<a> <b> <c>`, then the materialized view written with the following query will contain two rows `<a> <b> <c> 1` and `<a> <b> <c> 2`. When reading the first three columns from that view, the result so far would have been `<a> <b> <c>` once, and is now `<a> <b> <c>` twice, as it should be.
```sparql
SELECT ?s ?p ?o ?g {
  ?s ?p ?o .
  VALUES ?g { 1 2 }
}
```

src/index/CompressedRelation.cpp
@@ -1171,8 +1171,11 @@ CompressedRelationReader::decompressAndPostprocessBlock(
         includeGraphColumn);
     hasUpdates = true;
   }
-  bool wasPostprocessed =
-      scanConfig.graphFilter_.postprocessBlock(decompressedBlock, metadata);
+  bool wasPostprocessed = false;
+  if (useGraphPostProcessing_) {
+    wasPostprocessed =
+        scanConfig.graphFilter_.postprocessBlock(decompressedBlock, metadata);
+  }
   return {std::move(decompressedBlock), wasPostprocessed, hasUpdates};
 }
 

src/index/CompressedRelation.h
@@ -697,9 +697,17 @@ class CompressedRelationReader {
   // The file that stores the actual permutations.
   ad_utility::File file_;
 
+  // This setting controls whether filtering on the graph column and
+  // deduplication of rows is performed during scanning. Deactivating this is
+  // used for materialized views where repeated rows are meaningful.
+  bool useGraphPostProcessing_;
+
  public:
-  explicit CompressedRelationReader(Allocator allocator, ad_utility::File file)
-      : allocator_{std::move(allocator)}, file_{std::move(file)} {}
+  explicit CompressedRelationReader(Allocator allocator, ad_utility::File file,
+                                    bool useGraphPostProcessing = true)
+      : allocator_{std::move(allocator)},
+        file_{std::move(file)},
+        useGraphPostProcessing_{useGraphPostProcessing} {}
 
   // Helper function that enables a comparison of a triple with an `Id` in the
   // function `getBlocksForJoin` below.  If the given triple matches `col0Id` of

test/MaterializedViewsTest.cpp
@@ -782,7 +782,7 @@ TEST_F(MaterializedViewsTestLarge, LazyScan) {
       ++numBlocks;
     }
 
-    EXPECT_EQ(numRows, 2 * numFakeSubjects_);
+    EXPECT_EQ(numRows, 20 * numFakeSubjects_);
     AD_LOG_INFO << "Lazy scan had " << numRows << " rows from " << numBlocks
                 << " block(s)" << std::endl;
 
@@ -801,6 +801,57 @@ TEST_F(MaterializedViewsTestLarge, LazyScan) {
     auto col = qet->getVariableColumn(Variable{"?cnt"});
     auto count = res->idTable().at(0, col);
     ASSERT_TRUE(count.getDatatype() == Datatype::Int);
-    EXPECT_EQ(count.getInt(), 2 * numFakeSubjects_);
+    EXPECT_EQ(count.getInt(), 20 * numFakeSubjects_);
   }
+}
+
+// _____________________________________________________________________________
+TEST_F(MaterializedViewsTest, NoDuplicateRemovalOnScan) {
+  // Test that rows from materialized views are not implicitly deduplicated when
+  // scanning. If the first three columns have duplicates (differing only in an
+  // unselected additional column), all rows should be returned.
+
+  // Write a view where each row is duplicated with two different values of
+  // the fourth column `?x`.
+  const std::string dupQuery =
+      "SELECT ?s ?p ?o ?g { ?s ?p ?o . VALUES ?g { 1 2 } }";
+  qlv().writeMaterializedView("dupView", dupQuery);
+  qlv().loadMaterializedView("dupView");
+
+  // Base case: Query the view selecting all 4 columns: we expect exactly the
+  // original result.
+  auto allColsExpected =
+      getQueryResultAsIdTable(dupQuery + " INTERNAL SORT BY ?s ?p ?o ?g");
+  auto allColsResult = getQueryResultAsIdTable(R"(
+    PREFIX view: <https://qlever.cs.uni-freiburg.de/materializedView/>
+    SELECT * {
+      SERVICE view:dupView {
+        _:config view:column-s ?s ;
+                 view:column-p ?p ;
+                 view:column-o ?o ;
+                 view:column-g ?g .
+      }
+    }
+  )");
+  EXPECT_THAT(allColsResult, matchesIdTable(allColsExpected));
+
+  // No-deduplication case: Select only the first 3 columns, but expect each of
+  // them twice.
+  auto numRowsDedup =
+      getQueryResultAsIdTable("SELECT ?s ?p ?o { ?s ?p ?o }").numRows();
+  auto threeColsExpected = getQueryResultAsIdTable(
+      "SELECT ?s ?p ?o { ?s ?p ?o . VALUES ?g { 1 2 } } "
+      "INTERNAL SORT BY ?s ?p ?o");
+  auto threeColsResult = getQueryResultAsIdTable(R"(
+    PREFIX view: <https://qlever.cs.uni-freiburg.de/materializedView/>
+    SELECT * {
+      SERVICE view:dupView {
+        _:config view:column-s ?s ;
+                 view:column-p ?p ;
+                 view:column-o ?o .
+      }
+    }
+  )");
+  EXPECT_EQ(threeColsResult.numRows(), 2 * numRowsDedup);
+  EXPECT_THAT(threeColsResult, matchesIdTable(threeColsExpected));
 }

src/engine/MaterializedViews.cpp
@@ -362,8 +362,10 @@ MaterializedView::MaterializedView(std::string onDiskBase, std::string name)
     originalQuery_ = viewInfoJson.at("query").get<std::string>();
   }
 
-  // Read permutation
-  permutation_->loadFromDisk(filename, false);
+  // Read permutation, and deactivate the graph post-processing of
+  // `CompressedRelationReader`, including row deduplication, which is not the
+  // intended behavior for materialized views.
+  permutation_->loadFromDisk(filename, false, false);
   AD_CORRECTNESS_CHECK(permutation_->isLoaded());
 }
 

src/index/Permutation.cpp
@@ -31,7 +31,8 @@ CompressedRelationReader::ScanSpecAndBlocks Permutation::getScanSpecAndBlocks(
 
 // _____________________________________________________________________
 void Permutation::loadFromDisk(const std::string& onDiskBase,
-                               bool loadInternalPermutation) {
+                               bool loadInternalPermutation,
+                               bool useGraphPostProcessing) {
   onDiskBase_ = onDiskBase;
   if (loadInternalPermutation) {
     internalPermutation_ =
@@ -56,7 +57,7 @@ void Permutation::loadFromDisk(const std::string& onDiskBase,
              e.what());
   }
   meta_.readFromFile(&file);
-  reader_.emplace(allocator_, std::move(file));
+  reader_.emplace(allocator_, std::move(file), useGraphPostProcessing);
   AD_LOG_INFO << "Registered " << readableName_
               << " permutation: " << meta_.statistics() << std::endl;
   isLoaded_ = true;

src/index/Permutation.h
@@ -74,7 +74,8 @@ class Permutation {
 
   // everything that has to be done when reading an index from disk
   void loadFromDisk(const std::string& onDiskBase,
-                    bool loadInternalPermutation = false);
+                    bool loadInternalPermutation = false,
+                    bool useGraphPostProcessing = true);
 
   // Set the original metadata for the delta triples. This also sets the
   // metadata for internal permutation if present.

commit c21e686ca36bb4d25dcddcb1332857f653d41bb8

Fix regression involving `useGraphPostProcessing_` (#2745)
This change fixes a regression from #2708, which introduced the `useGraphPostProcessing_` flag in `CompressedRelationReader`. However, setting this to `false` also deactivated the deletion of the graph column, which is necessary when it is not selected. Now the deletion of the graph column is also performed when `useGraphPostProcessing_` is set to `false`.

src/index/CompressedRelation.cpp
@@ -343,13 +343,18 @@ bool CompressedRelationReader::FilterDuplicatesAndGraphs::
 }
 
 // _____________________________________________________________________________
-bool CompressedRelationReader::FilterDuplicatesAndGraphs::postprocessBlock(
-    IdTable& block, const CompressedBlockMetadata& blockMetadata) const {
-  bool filteredByGraph = filterByGraphIfNecessary(block, blockMetadata);
+void CompressedRelationReader::FilterDuplicatesAndGraphs::
+    deleteGraphColumnIfNecessary(IdTable& block) const {
   if (deleteGraphColumn_) {
     block.deleteColumn(graphColumn_);
   }
+}
 
+// _____________________________________________________________________________
+bool CompressedRelationReader::FilterDuplicatesAndGraphs::postprocessBlock(
+    IdTable& block, const CompressedBlockMetadata& blockMetadata) const {
+  bool filteredByGraph = filterByGraphIfNecessary(block, blockMetadata);
+  deleteGraphColumnIfNecessary(block);
   bool filteredByDuplicates = filterDuplicatesIfNecessary(block, blockMetadata);
   return filteredByGraph || filteredByDuplicates;
 }
@@ -1175,6 +1180,10 @@ CompressedRelationReader::decompressAndPostprocessBlock(
   if (useGraphPostProcessing_) {
     wasPostprocessed =
         scanConfig.graphFilter_.postprocessBlock(decompressedBlock, metadata);
+  } else {
+    // If we do not use graph postprocessing, we might still need to remove the
+    // extra column.
+    scanConfig.graphFilter_.deleteGraphColumnIfNecessary(decompressedBlock);
   }
   return {std::move(decompressedBlock), wasPostprocessed, hasUpdates};
 }

src/index/CompressedRelation.h
@@ -559,6 +559,9 @@ class CompressedRelationReader {
     // disk, and if this fact can be determined by `blockMetadata` alone.
     bool canBlockBeSkipped(const CompressedBlockMetadata& blockMetadata) const;
 
+    // Delete the `graphColumn_` from `block` if `deleteGraphColumn_` is true.
+    void deleteGraphColumnIfNecessary(IdTable& block) const;
+
    private:
     // Return a lambda that returns true if `desiredGraphs_` allows the given
     // `graph` and it is not the default graph.

test/MaterializedViewsTest.cpp
@@ -859,6 +859,34 @@ TEST_F(MaterializedViewsTest, NoDuplicateRemovalOnScan) {
   )");
   EXPECT_EQ(threeColsResult.numRows(), 2 * numRowsDedup);
   EXPECT_THAT(threeColsResult, matchesIdTable(threeColsExpected));
+
+  // Graph column regression from #2708: Previously the graph column would
+  // overwrite an additional column if it was not selected.
+  {
+    qlv().writeMaterializedView("graphColRegression", R"(
+      SELECT ?a ?b ?c ?d ?e {
+        VALUES (?a ?b ?c ?d ?e) {
+          (1 2 3 4 5)
+          (11 12 13 14 15)
+        }
+      }
+    )");
+    auto res = getQueryResultAsIdTable(R"(
+    PREFIX view: <https://qlever.cs.uni-freiburg.de/materializedView/>
+      SELECT ?e {
+        SERVICE view:graphColRegression {
+          [
+            view:column-a 1 ;
+            view:column-e ?e
+          ]
+        }
+      }
+    )");
+    // If we always select the graph column, but do not read it into a dummy
+    // variable, we would get `4` instead of `5` here.
+    auto expected = getQueryResultAsIdTable("SELECT (5 AS ?e) {}");
+    EXPECT_THAT(res, matchesIdTable(expected));
+  }
 }
 
 // Example queries for testing query rewriting.