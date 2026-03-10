c4ea7ba1bdce1d8f9f943a1706fa0c97986807fe
marvin <marvin.stoetzel@mailbox.org>
2026-03-10 10:53:35 +0100

    fix copyright messages

--- test/ExportIdsTest.cpp
+++ test/ExportIdsTest.cpp
@@ -1,7 +1,8 @@
-// Copyright 2026 The QLever Authors, in particular
-// Authors: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
-//          Robin Textor-Falconi <textorr@cs.uni-freiburg.de>
-//          Hannah Bast <bast@cs.uni-freiburg.de>
+// Copyright 2026, The QLever Authors, in particular
+// Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>, UFR
+// Robin Textor-Falconi <textorr@cs.uni-freiburg.de>, UFR
+// Hannah Bast <bast@cs.uni-freiburg.de>, UFR
+// Marvin Stoetzel <stoetzem@email.uni-freiburg.de>, UFR
 // UFR = University of Freiburg, Chair of Algorithms and Data Structures
 
 // You may not use this file except in compliance with the Apache 2.0 License,
