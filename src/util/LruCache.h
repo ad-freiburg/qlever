de080042c003b0b48953f6bd80331be57ddec48f
marvin <marvin.stoetzel@mailbox.org>
2026-03-10 10:59:07 +0100

    fix copyright header

--- src/util/LruCache.h
+++ src/util/LruCache.h
@@ -1,6 +1,11 @@
-//   Copyright 2025, University of Freiburg,
-//   Chair of Algorithms and Data Structures.
-//   Author: Robin Textor-Falconi <textorr@informatik.uni-freiburg.de>
+// Copyright 2025 The QLever Authors, in particular
+// 2025 Robin Textor-Falconi <textorr@informatik.uni-freiburg.de>, UFR
+// 2026 Marvin Stoetzel <stoetzem@email.uni-freiburg.de>, UFR
+//
+// UFR = University of Freiburg, Chair of Algorithms and Data Structures
+
+// You may not use this file except in compliance with the Apache 2.0 License,
+// which can be found in the `LICENSE` file at the root of the QLever project.
 
 #ifndef QLEVER_SRC_UTIL_LRUCACHE_H
 #define QLEVER_SRC_UTIL_LRUCACHE_H
