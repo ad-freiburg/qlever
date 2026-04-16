1e4e9652631c44fa0ab70e79a946cd7f281e68ca
marvin <marvin.stoetzel@mailbox.org>
2026-04-16 08:39:01 +0200

    add comment that describes purpose of a method

--- src/engine/ConstructBatchEvaluator.h
+++ src/engine/ConstructBatchEvaluator.h
@@ -91,11 +91,7 @@ class ConstructBatchEvaluator {
       size_t idTableColumnIdx, const BatchEvaluationContext& ctx,
       const LocalVocab& localVocab, const Index& index, IdCache& idCache);
 
-  // Convert a single `Id` to its `EvaluatedTerm` string representation.
-  // Returns `std::nullopt` if the `Id` has no string representation.
-  static std::optional<EvaluatedTerm> idToEvaluatedTerm(
-      const Index& index, Id id, const LocalVocab& localVocab);
-
+  // convert the result of `ExportIds::idToStringAndType` to an `EvaluatedTerm`.
   static std::optional<EvaluatedTerm> stringAndTypeToEvaluatedTerm(
       std::optional<std::pair<std::string, const char*>> optStringAndType);
 };
