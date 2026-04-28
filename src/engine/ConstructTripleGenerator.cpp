aaaa35d4fe23766e10ae4fcd7927dda8d3609d67
marvin <marvin.stoetzel@mailbox.org>
2026-04-28 12:49:26 +0200

    drop implicit conversion by using ql::ranges::end instead of ql::ranges::size

--- src/engine/ConstructTripleGenerator.cpp
+++ src/engine/ConstructTripleGenerator.cpp
@@ -39,7 +39,7 @@ CPP_template(typename ChunkView)(requires ranges::range<ChunkView>) static std::
 
   AD_CORRECTNESS_CHECK(!ql::ranges::empty(chunkView));
   const size_t batchBegin = *ql::ranges::begin(chunkView);
-  const size_t batchEnd = batchBegin + ql::ranges::size(chunkView);
+  const size_t batchEnd = *ql::ranges::end(chunkView);
 
   BatchEvaluationContext ctx{tableWithVocab.idTable(), batchBegin, batchEnd};
 
