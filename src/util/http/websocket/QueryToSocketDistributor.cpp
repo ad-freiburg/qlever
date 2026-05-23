f88acfba74abc296deed0deff2e2547723c5fead
Johannes Kalmbach <joka921@users.noreply.github.com>
2026-05-23 22:24:51 +0200

    Change error reference to const in QueryToSocketDistributor

--- src/util/http/websocket/QueryToSocketDistributor.cpp
+++ src/util/http/websocket/QueryToSocketDistributor.cpp
@@ -24,7 +24,7 @@ net::awaitable<void> QueryToSocketDistributor::waitForUpdate() const {
   // `https://gcc.gnu.org/bugzilla/show_bug.cgi?id=124584`
   auto waitResult = co_await infiniteTimer_.async_wait(
       net::bind_executor(strand_, net::as_tuple(net::use_awaitable)));
-  auto& [error] = waitResult;
+  const auto& [error] = waitResult;
   // If this fails this means the infinite timer expired or aborted with an
   // unexpected error code. This should not happen at all
   AD_CORRECTNESS_CHECK(error == net::error::operation_aborted);
