d19210b1feb5bfeca20d31d0a2fba17072ee9dff
RobinTF <83676088+RobinTF@users.noreply.github.com>
2026-05-29 14:32:36 +0200

    Fix lazy group by slowdown

--- src/engine/GroupByImpl.cpp
+++ src/engine/GroupByImpl.cpp
@@ -51,7 +51,7 @@ class LazyGroupByRange
   bool singleIdTable_{false};
   // runtime state
   size_t inWidth_;
-  IdTable resultTable_;
+  IdTableStatic<OUT_WIDTH> resultTable_;
   std::unique_ptr<LazyGroupBy> lazyGroupBy_;
   LocalVocab currentLocalVocab_;
   std::vector<LocalVocab> storedLocalVocabs_;
@@ -119,17 +119,16 @@ class LazyGroupByRange
                      sparqlExpression::EvaluationContext& evaluationContext) {
     if (groupSplitAcrossTables_) {
       lazyGroupBy_->processBlock(evaluationContext, blockStart, blockEnd);
-      lazyGroupBy_->commitRow(resultTable_, evaluationContext,
+      IdTable dynamicResultTable = std::move(resultTable_).toDynamic();
+      lazyGroupBy_->commitRow(dynamicResultTable, evaluationContext,
                               currentGroupBlock_);
+      resultTable_ =
+          std::move(dynamicResultTable).template toStatic<OUT_WIDTH>();
       groupSplitAcrossTables_ = false;
     } else {
-      // This processes the whole block in batches if possible.
-      IdTableStatic<OUT_WIDTH> table{
-          std::move(resultTable_).template toStatic<OUT_WIDTH>()};
       parent_->template processBlock<OUT_WIDTH>(
-          table, aggregates_, evaluationContext, blockStart, blockEnd,
+          resultTable_, aggregates_, evaluationContext, blockStart, blockEnd,
           &currentLocalVocab_, groupByCols_);
-      resultTable_ = std::move(table).toDynamic();
     }
   }
 
@@ -166,8 +165,8 @@ class LazyGroupByRange
                                idTable.size());
     if (!singleIdTable_ && !resultTable_.empty()) {
       currentLocalVocab_.mergeWith(storedLocalVocabs_);
-      auto result = Result::IdTableVocabPair{std::move(resultTable_),
-                                             std::move(currentLocalVocab_)};
+      auto result = Result::IdTableVocabPair{
+          std::move(resultTable_).toDynamic(), std::move(currentLocalVocab_)};
       // Keep last local vocab for next commit, since we might write to
       // `currentLocalVocab_`, we need to clone it.
       currentLocalVocab_ = storedLocalVocabs_.back().clone();
@@ -188,14 +187,15 @@ class LazyGroupByRange
       if (groupByCols_.empty()) {
         // If we have an implicit GROUP BY, where the entire input is a
         // single group, we need to produce one result row.
-        parent_->processEmptyImplicitGroup<OUT_WIDTH>(resultTable_, aggregates_,
-                                                      &currentLocalVocab_);
-        return IdTableVocabPair{std::move(resultTable_),
+        IdTable dynamicResultTable = std::move(resultTable_).toDynamic();
+        parent_->processEmptyImplicitGroup<OUT_WIDTH>(
+            dynamicResultTable, aggregates_, &currentLocalVocab_);
+        return IdTableVocabPair{std::move(dynamicResultTable),
                                 std::move(currentLocalVocab_)};
       }
       if (singleIdTable_) {
         // Yield at least a single empty table if requested.
-        return IdTableVocabPair{std::move(resultTable_),
+        return IdTableVocabPair{std::move(resultTable_).toDynamic(),
                                 std::move(currentLocalVocab_)};
       }
       return std::nullopt;
@@ -216,10 +216,11 @@ class LazyGroupByRange
     sparqlExpression::EvaluationContext evaluationContext =
         parent_->createEvaluationContext(currentLocalVocab_, idTable);
 
-    lazyGroupBy_->commitRow(resultTable_, evaluationContext,
+    IdTable dynamicResultTable = std::move(resultTable_).toDynamic();
+    lazyGroupBy_->commitRow(dynamicResultTable, evaluationContext,
                             currentGroupBlock_);
     currentLocalVocab_.mergeWith(storedLocalVocabs_);
-    return IdTableVocabPair{std::move(resultTable_),
+    return IdTableVocabPair{std::move(dynamicResultTable),
                             std::move(currentLocalVocab_)};
   }
 };
