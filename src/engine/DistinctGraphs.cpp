d3d6ecd3768c1b22e8ba48de85c89c16c3e4fb84
mete tolga <mtlg@tuta.io>
2026-06-03 19:50:22 +0200

    All request except combining two if's and editing the `LazyScan` function are done and fixed.

--- src/engine/DistinctGraphs.cpp
+++ src/engine/DistinctGraphs.cpp
@@ -1,8 +1,16 @@
-#include "DistinctGraphs.h"
+// Copyright 2026 The QLever Authors, in particular:
+//
+// 2026 Mete tolga gonultas mg885@email.uni-freiburg.de, UFR
 
+// UFR = University of Freiburg, Chair of Algorithms and Data Structures
+
+// You may not use this file except in compliance with the Apache 2.0 License,
+// which can be found in the `LICENSE` file at the root of the QLever project.
+
+#include "engine/DistinctGraphs.h"
+
+#include <algorithm>
 #include <optional>
-
-#include "engine/IndexScan.h"
 #include "engine/Result.h"
 #include "global/Constants.h"
 #include "global/RuntimeParameters.h"
@@ -11,105 +19,89 @@
 #include "index/Permutation.h"
 #include "index/ScanSpecification.h"
 #include "util/HashSet.h"
-#include "util/Log.h"
 
+// Constructor needs qec and graph variable which isn't should be bound on the inner queries 
 DistinctGraphs::DistinctGraphs(QueryExecutionContext* qec,
                                Variable graphVariable)
     : Operation{qec}, graphVariable_{std::move(graphVariable)} {}
-
-std::vector<QueryExecutionTree*> DistinctGraphs::getChildren() { return {}; }
-
-std::string DistinctGraphs::getDescriptor() const { return "Distinct Graphs"; }
-
-size_t DistinctGraphs::getResultWidth() const { return 1; }
-
-size_t DistinctGraphs::getCostEstimate() { return 0; }
-
-float DistinctGraphs::getMultiplicity(size_t) { return 1.0f; }
-
-bool DistinctGraphs::knownEmptyResult() { return false; }
 
 std::unique_ptr<Operation> DistinctGraphs::cloneImpl() const {
   return std::make_unique<DistinctGraphs>(_executionContext, graphVariable_);
 }
 
-std::vector<ColumnIndex> DistinctGraphs::resultSortedOn() const { return {}; }
-
-std::string DistinctGraphs::getCacheKeyImpl() const {
-  return "DistinctGraphs " + graphVariable_.name();
+VariableToColumnMap DistinctGraphs::computeVariableToColumnMap() const {
+  return {{graphVariable_, makeAlwaysDefinedColumn(0)}};
 }
 
-uint64_t DistinctGraphs::getSizeEstimateBeforeLimit() { return 1; }
-
+/**
+ * @brief Implements support for SPARQL queries of the form GRAPH ?g { ... } where ?g is an unbound variable.
+ * * DistinctGraphs operation efficiently reads all distinct named graph IDs directly from 
+ * the block metadata of the index, without a full table scan where possible.
+ * * If distinct graph count <= MAX_NUM_GRAPHS_STORED_IN_BLOCK_METADATA:
+ * - Traverse through the graphInfo_;
+ * - If a new graph Id is seen, decompress and read it to prove its existence.
+ * Else:
+ * - Decompress, read, and add all graph Ids to the result.
+ *
+ * Filters out the default graph IRI from the result when the treatDefaultGraphAsNamedGraph 
+ * runtime parameter is off.
+ *
+ * @param requestLaziness If true, hints to the query engine that results can be evaluated lazily.
+ * Currently marked as [[maybe_unused]].
+ *
+ * @return A Result object containing a unique set of all matching named graph Ids.
+ */
 Result DistinctGraphs::computeResult([[maybe_unused]] bool requestLaziness) {
   const auto& permutation =
       getIndex().getImpl().getPermutation(Permutation::Enum::SPO);
-  auto blockRanges =
-      permutation.getAugmentedMetadataForPermutation(locatedTriplesState());
-  ad_utility::HashSet<Id> graphIds;
+  ad_utility::HashSet<Id::T> graphIds;
 
   using ScanSpecAndBlocks = CompressedRelationReader::ScanSpecAndBlocks;
-  ScanSpecAndBlocks scanSpec{
-      ScanSpecification{std::nullopt, std::nullopt, std::nullopt}, blockRanges};
-
-  for (const auto& blockRange : blockRanges) {  // vector<range of Blocks>
-    // metadatas of "every block in range"
-    for (const auto& metadata : blockRange) {
-      if (metadata.graphInfo_) {
-        bool hasNewGraphId = false;
-        for (const auto& id : *metadata.graphInfo_) {
-          if (!graphIds.contains(id)) {
-            hasNewGraphId = true;  // if there is at least one non-existed id in
-                                   // the graphIds
-            break;
-          }
-        }
-        if (hasNewGraphId) {  // there is at least one non-proved element in the
-                              // graph infos
-          auto lazyScanResult = permutation.lazyScan(
-              scanSpec, std::vector<CompressedBlockMetadata>{metadata},
-              std::vector<ColumnIndex>{ADDITIONAL_COLUMN_GRAPH_ID},
-              cancellationHandle_, locatedTriplesState());
-          for (auto& idTable : lazyScanResult) {
-            for (Id id : idTable.getColumn(3)) {
-              graphIds.insert(id);
-            }
-          }
-        }
-      } else {  // if there is more than MAX_GRAPH graph
+  ScanSpecAndBlocks scanSpecAndBlocks = permutation.getScanSpecAndBlocks(ScanSpecification{std::nullopt, std::nullopt, std::nullopt}, locatedTriplesState());
+  
+  for (const auto& metadata : scanSpecAndBlocks.getBlockMetadataView()){
+    if (metadata.graphInfo_) {
+      bool hasNewGraphId = ql::ranges::any_of(metadata.graphInfo_.value(), 
+        [&graphIds](Id id){return !graphIds.contains(id.getBits()); }
+      );
+      if (hasNewGraphId) {
         auto lazyScanResult = permutation.lazyScan(
-            scanSpec, std::vector<CompressedBlockMetadata>{metadata},
+            scanSpecAndBlocks, std::vector<CompressedBlockMetadata>{metadata},
             std::vector<ColumnIndex>{ADDITIONAL_COLUMN_GRAPH_ID},
             cancellationHandle_, locatedTriplesState());
         for (auto& idTable : lazyScanResult) {
           for (Id id : idTable.getColumn(3)) {
-            graphIds.insert(id);
+            graphIds.insert(id.getBits());
           }
+        }
+      }
+    } else {
+      auto lazyScanResult = permutation.lazyScan(
+          scanSpecAndBlocks, std::vector<CompressedBlockMetadata>{metadata},
+          std::vector<ColumnIndex>{ADDITIONAL_COLUMN_GRAPH_ID},
+          cancellationHandle_, locatedTriplesState());
+      for (auto& idTable : lazyScanResult) {
+        for (Id id : idTable.getColumn(3)) {
+          graphIds.insert(id.getBits());
         }
       }
     }
   }
 
-  auto treatDefaultGraphAsNamedGraph_ =
+  auto treatDefaultGraphAsNamedGraph =
       getRuntimeParameter<&RuntimeParameters::treatDefaultGraphAsNamedGraph_>();
-  if (!treatDefaultGraphAsNamedGraph_) {
+  if (!treatDefaultGraphAsNamedGraph) {
     auto defaultGraph =
         TripleComponent{
             ad_utility::triple_component::Iri::fromIriref(DEFAULT_GRAPH_IRI)}
             .toValueId(getIndex());
-    graphIds.erase(*defaultGraph);
+    graphIds.erase(defaultGraph->getBits());
   }
 
   IdTable idTable{1, getExecutionContext()->getAllocator()};
   idTable.resize(graphIds.size());
-  size_t rowIndex = 0;
-  for (Id id : graphIds) {
-    idTable[rowIndex][0] = id;
-    rowIndex++;
-  }
-  return {std::move(idTable), resultSortedOn(), LocalVocab{}};
-}
+  ql::ranges::transform(graphIds, idTable.getColumn(0).begin(),
+                        Id::fromBits);
 
-VariableToColumnMap DistinctGraphs::computeVariableToColumnMap() const {
-  return {{graphVariable_, makeAlwaysDefinedColumn(0)}};
+  return {std::move(idTable), resultSortedOn(), LocalVocab{}};
 }
