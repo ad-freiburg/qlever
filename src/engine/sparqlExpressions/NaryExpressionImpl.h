ad-freiburg/qlever

commit 576a8beb9f4b1311424fc692d845d9490a3f7e9e

Faster compilation with type-erased `StringExpression` (#2735)
The `sparqlExpression` module is expensive to compile because of its many templates. This change implements facilities to alternatively implement them using type erasure, which is cheaper to compile, but reduces the performance at runtime; a worthwhile trade-off during development and debugging. The alternative implementation can be activated via the `cmake` flag `-DCHEAPER_COMPILATION=ON`. For now, the new machinery is applied to `GeoExpressions.cpp`  and `StringExpressions.cpp`, which currently are the most expensive to compile.

Specifically, with the new flag, the compile time for build type `RelWithDebInfo` reduces from around 5 minutes to around 2 minutes on one of our AMD Ryzen 9 machines, while a query like `SELECT (SUM(?x + ?y) AS ?sum) WHERE { ?x a ?y }` becomes around 50% slower.

src/engine/sparqlExpressions/NaryExpressionImpl.h
@@ -8,14 +8,16 @@
 #define QLEVER_SRC_ENGINE_SPARQLEXPRESSIONS_NARYEXPRESSIONIMPL_H
 
 #include <absl/functional/bind_front.h>
+#include <absl/strings/str_join.h>
 
 #include "engine/sparqlExpressions/SparqlExpressionGenerators.h"
 #include "engine/sparqlExpressions/SparqlExpressionValueGetters.h"
 #include "util/CryptographicHashUtils.h"
 
 namespace sparqlExpression::detail {
+
 template <typename NaryOperation>
-class NaryExpression : public SparqlExpression {
+class NaryExpressionStronglyTyped : public SparqlExpression {
   CPP_assert(isOperation<NaryOperation>);
 
  public:
@@ -27,14 +29,15 @@ class NaryExpression : public SparqlExpression {
 
  public:
   // Construct from an array of `N` child expressions.
-  explicit NaryExpression(Children&& children);
+  explicit NaryExpressionStronglyTyped(Children&& children);
 
   // Construct from `N` child expressions. Each of the children must have a type
   // `std::unique_ptr<SubclassOfSparqlExpression>`.
   CPP_template(typename... C)(
       requires(concepts::convertible_to<C, SparqlExpression::Ptr>&&...)
-          CPP_and(sizeof...(C) == N)) explicit NaryExpression(C... children)
-      : NaryExpression{Children{std::move(children)...}} {}
+          CPP_and(sizeof...(C) ==
+                  N)) explicit NaryExpressionStronglyTyped(C... children)
+      : NaryExpressionStronglyTyped{Children{std::move(children)...}} {}
 
   // __________________________________________________________________________
   ExpressionResult evaluate(EvaluationContext* context) const override;
@@ -48,40 +51,205 @@ class NaryExpression : public SparqlExpression {
   ql::span<SparqlExpression::Ptr> childrenImpl() override;
 
   // Evaluate the `naryOperation` on the `operands` using the `context`.
-  CPP_template(typename... Operands)(
-      requires(SingleExpressionResult<Operands>&&...)) static ExpressionResult
-      evaluateOnChildrenOperands(NaryOperation naryOperation,
-                                 EvaluationContext* context,
-                                 Operands&&... operands) {
-    // Perform a more efficient calculation if a specialized function exists
-    // that matches all operands.
-    if (isAnySpecializedFunctionPossible(naryOperation._specializedFunctions,
-                                         operands...)) {
-      auto optionalResult = evaluateOnSpecializedFunctionsIfPossible(
-          naryOperation._specializedFunctions,
-          std::forward<Operands>(operands)...);
-      AD_CORRECTNESS_CHECK(optionalResult);
-      return std::move(optionalResult.value());
+  // Is deliberately a functor, s.t. we can pass it to `bind_front` etc,
+  // although the call operator is overloaded.
+  struct EvaluateOnChildOperands {
+    CPP_template(typename... Operands)(
+        requires(SingleExpressionResult<Operands>&&...)) ExpressionResult
+    operator()(NaryOperation naryOperation, EvaluationContext* context,
+               Operands&&... operands) const {
+      // Perform a more efficient calculation if a specialized function exists
+      // that matches all operands.
+      if (isAnySpecializedFunctionPossible(naryOperation._specializedFunctions,
+                                           operands...)) {
+        auto optionalResult = evaluateOnSpecializedFunctionsIfPossible(
+            naryOperation._specializedFunctions,
+            std::forward<Operands>(operands)...);
+        AD_CORRECTNESS_CHECK(optionalResult);
+        return std::move(optionalResult.value());
+      }
+
+      // We have to first determine the number of results we will produce.
+      auto targetSize = getResultSize(*context, operands...);
+
+      // The result is a constant iff all the results are constants.
+      constexpr static bool resultIsConstant =
+          (... && isConstantResult<Operands>);
+
+      // The generator for the result of the operation.
+      auto resultGenerator = applyOperation(targetSize, naryOperation, context,
+                                            AD_FWD(operands)...);
+
+      // Compute the result.
+      using ResultType = ql::ranges::range_value_t<decltype(resultGenerator)>;
+      VectorWithMemoryLimit<ResultType> result{context->_allocator};
+      result.reserve(targetSize);
+      ql::ranges::move(resultGenerator, std::back_inserter(result));
+
+      if constexpr (resultIsConstant) {
+        AD_CORRECTNESS_CHECK(result.size() == 1);
+        return std::move(result[0]);
+      } else {
+        return result;
+      }
+    }
+  };
+};
+
+// _____________________________________________________________________________
+template <typename Op>
+NaryExpressionStronglyTyped<Op>::NaryExpressionStronglyTyped(
+    Children&& children)
+    : children_{std::move(children)} {}
+
+// _____________________________________________________________________________
+
+template <typename NaryOperation>
+ExpressionResult NaryExpressionStronglyTyped<NaryOperation>::evaluate(
+    EvaluationContext* context) const {
+  auto resultsOfChildren = ad_utility::applyFunctionToEachElementOfTuple(
+      [context](const auto& child) { return child->evaluate(context); },
+      children_);
+
+  // A function that only takes several `ExpressionResult`s,
+  // and evaluates the expression.
+  auto evaluateOnChildrenResults =
+      absl::bind_front(ad_utility::visitWithVariantsAndParameters,
+                       EvaluateOnChildOperands{}, NaryOperation{}, context);
+
+  return std::apply(evaluateOnChildrenResults, std::move(resultsOfChildren));
+}
+
+// _____________________________________________________________________________
+template <typename Op>
+ql::span<SparqlExpression::Ptr>
+NaryExpressionStronglyTyped<Op>::childrenImpl() {
+  return {children_.data(), children_.size()};
+}
+
+// __________________________________________________________________________
+template <typename Op>
+[[nodiscard]] std::string NaryExpressionStronglyTyped<Op>::getCacheKey(
+    const VariableToColumnMap& varColMap) const {
+  std::string key = typeid(*this).name();
+  key += absl::StrJoin(
+      children_ | ql::views::transform([&varColMap](const auto& child) {
+        return child->getCacheKey(varColMap);
+      }),
+      "");
+  return key;
+}
+
+// ============================================================================
+// Type-erased expression support.
+// ============================================================================
+
+// Type-erased version of the `NaryExpression` class. Much cheaper to compile,
+// but also slower in the execution. It is only templated on the signature of
+// its core implementation function; all other implementation details (the
+// actual function, as well as the value getters used to create the inputs)
+// are type-erased.
+// Note: The `getCacheKey` function of this class itself will not work
+// correctly, as it doesn't account for the "same function, different value
+// getter" case. But when used as an implementation detail of the
+// `NaryExpressionTypeErased` class below, then this works, because the value
+// getters become part of the classes name/typeid.
+template <typename Ret, typename... Args>
+class NaryExpressionTypeErasedImpl : public SparqlExpression {
+ public:
+  static constexpr size_t N = sizeof...(Args);
+  using Children = std::array<SparqlExpression::Ptr, N>;
+  using Function = std::function<Ret(Args...)>;
+
+  // Type-erased `std::function` that converts the `ExpressionResult` variant
+  // into a type-erased range of `Args`s.
+  template <typename Arg>
+  using TypeErasedGetter = std::function<ad_utility::InputRangeTypeErased<Arg>(
+      ExpressionResult, EvaluationContext*, size_t)>;
+
+  // Tuple of type-erased value-getters, one for each argument of this
+  // function.
+  using Getters = std::tuple<TypeErasedGetter<Args>...>;
+
+ private:
+  Children children_;
+  Function function_;
+  Getters getters_;
+
+ public:
+  // Construct from an array of `N` child expressions, as well as the
+  // `function` and `getters`.
+  explicit NaryExpressionTypeErasedImpl(Function function, Getters getters,
+                                        Children&& children)
+      : children_{std::move(children)},
+        function_{std::move(function)},
+        getters_{std::move(getters)} {}
+
+  // __________________________________________________________________________
+  ExpressionResult evaluate(EvaluationContext* context) const override {
+    return std::apply(
+        [&](auto&&... child) {
+          return evaluateOnChildrenOperands(context,
+                                            child->evaluate(context)...);
+        },
+        children_);
+  }
+
+  // _________________________________________________________________________
+  [[nodiscard]] std::string getCacheKey(
+      const VariableToColumnMap& varColMap) const override {
+    const auto& signatureId = typeid(*this);
+    const auto& functionId = function_.target_type();
+    std::string key =
+        absl::StrCat(signatureId.name(), "_", signatureId.hash_code(), "_",
+                     functionId.name(), "_", functionId.hash_code(), "_");
+    for (const auto& child : children_) {
+      key += child->getCacheKey(varColMap);
+    }
+    return key;
+  }
+
+ private:
+  // _________________________________________________________________________
+  ql::span<SparqlExpression::Ptr> childrenImpl() override { return children_; }
+
+  // Evaluate the `naryOperation` on the `operands` using the `context`.
+  CPP_variadic_template(typename... Operands)(requires(
+      ...&& std::is_same_v<ExpressionResult, Operands>)) ExpressionResult
+      evaluateOnChildrenOperands(EvaluationContext* context,
+                                 Operands... operands) const {
+    // We have to first determine the number of results the expression will
+    // produce.
+    auto targetSize = context->size();
+    bool resultIsConstant = (... && isConstantExpressionResult(operands));
+    if (resultIsConstant) {
+      targetSize = 1;
     }
 
-    // We have to first determine the number of results we will produce.
-    auto targetSize = getResultSize(*context, operands...);
+    // A `zip_view` of the result of all the value getters applied to their
+    // respective child result.
+    auto zipper = std::apply(
+        [&](const auto&... getters) {
+          return ::ranges::views::zip(ad_utility::OwningView{
+              getters(std::move(operands), context, targetSize)}...);
+        },
+        getters_);
 
-    // The result is a constant iff all the results are constants.
-    constexpr static bool resultIsConstant =
-        (... && isConstantResult<Operands>);
-
-    // The generator for the result of the operation.
+    // Apply the `function_` on a tuple of arguments (the `zipper` above has
+    // tuples as value and reference type).
+    auto onTuple = [&](auto&& tuple) {
+      return std::apply(
+          [this](auto&&... args) { return function_(AD_FWD(args)...); },
+          AD_FWD(tuple));
+    };
     auto resultGenerator =
-        applyOperation(targetSize, naryOperation, context, AD_FWD(operands)...);
-
+        ql::views::transform(ql::ranges::ref_view(zipper), onTuple);
     // Compute the result.
-    using ResultType = ql::ranges::range_value_t<decltype(resultGenerator)>;
-    VectorWithMemoryLimit<ResultType> result{context->_allocator};
+    VectorWithMemoryLimit<std::decay_t<Ret>> result{context->_allocator};
     result.reserve(targetSize);
     ql::ranges::move(resultGenerator, std::back_inserter(result));
 
-    if constexpr (resultIsConstant) {
+    if (resultIsConstant) {
       AD_CORRECTNESS_CHECK(result.size() == 1);
       return std::move(result[0]);
     } else {
@@ -89,6 +257,83 @@ class NaryExpression : public SparqlExpression {
     }
   }
 };
+
+// ============================================================================
+// Implementation of `NaryExpressionTypeErased` using the
+// `NaryExpressionTypeErasedImpl` from above: It has the same template argument
+// as `NaryExpression`, but uses type erasure for the function and value
+// getters.
+// ============================================================================
+
+// Helper: given a Function type and a tuple of (strongly typed) function
+// getters, compute the corresponding `NaryExpressionTypeErasedImpl` type and
+// provide a helper function to create the tuple of type-erased value getters.
+template <typename Func, typename VGTuple>
+struct TypeErasedNaryHelper;
+
+template <typename Func, typename... VGs>
+struct TypeErasedNaryHelper<Func, std::tuple<VGs...>> {
+  using Res = std::invoke_result_t<Func, typename VGs::Value...>;
+  using BaseType = NaryExpressionTypeErasedImpl<Res, typename VGs::Value...>;
+  static auto makeGetters() {
+    return typename BaseType::Getters{TypeErasedValueGetter<VGs>{}...};
+  }
+};
+
+// Forward declaration, because we implement pattern matching using partial
+// specialization below.
+template <typename NaryOperation>
+class NaryExpressionTypeErased;
+
+// Partial specialization for `Operation<N, FV<Function, ValueGetters...>,
+// SpecializedFunctions...>` As that is exactly the pattern the strongly typed
+// `NaryExpression` uses. Note: The `SpezializedFunctions` (which implement more
+// efficient evaluation in some circumstances, but are not required for
+// correctness) are ignored in the type-erased case, which is only used during
+// development for cheaper compilation.
+template <size_t N, typename Function, typename... ValueGetters,
+          typename... SFs>
+class NaryExpressionTypeErased<
+    Operation<N, FunctionAndValueGetters<Function, ValueGetters...>, SFs...>>
+    : public TypeErasedNaryHelper<
+          Function, ValueGetterPack<N, std::tuple<ValueGetters...>>>::BaseType {
+  using Helper =
+      TypeErasedNaryHelper<Function,
+                           ValueGetterPack<N, std::tuple<ValueGetters...>>>;
+  using Base = Helper::BaseType;
+  using Children = std::array<SparqlExpression::Ptr, N>;
+
+ public:
+  // Construct from an array of `N` child expressions.
+  explicit NaryExpressionTypeErased(Children&& children)
+      : Base(Function{}, Helper::makeGetters(), std::move(children)) {}
+
+  // Construct from `N` child expressions.
+  CPP_template(typename... C)(
+      requires(concepts::convertible_to<C, SparqlExpression::Ptr>&&...)
+          CPP_and(sizeof...(C) ==
+                  N)) explicit NaryExpressionTypeErased(C... children)
+      : NaryExpressionTypeErased{Children{std::move(children)...}} {}
+};
+
+// A unified alias for either the type-erased or the strongly-typed expressions.
+#ifdef _QLEVER_TYPE_ERASED_EXPRESSIONS
+template <typename... Args>
+using NaryExpression = NaryExpressionTypeErased<Args...>;
+#else
+template <typename... Args>
+using NaryExpression = NaryExpressionStronglyTyped<Args...>;
+#endif
+
+// Define a class `Name` that is a strong typedef (via inheritance) from
+// `NaryExpression<N, X, ...>`.
+// The strong typedef (vs. a simple `using` declaration) is used to improve
+// compiler messages as the resulting class has a short and descriptive name.
+#define NARY_EXPRESSION(Name, N, X, ...)                                     \
+  class Name : public NaryExpression<detail::Operation<N, X, __VA_ARGS__>> { \
+    using Base = NaryExpression<Operation<N, X, __VA_ARGS__>>;               \
+    using Base::Base;                                                        \
+  };
 
 // Takes a `Function` that returns a numeric value (integral or floating point)
 // and converts it to a function, that takes the same arguments and returns the
@@ -148,64 +393,6 @@ using ad_utility::SetOfIntervals;
 
 // The types for the concrete MultiBinaryExpressions and UnaryExpressions.
 using TernaryBool = EffectiveBooleanValueGetter::Result;
-
-// _____________________________________________________________________________
-template <typename Op>
-NaryExpression<Op>::NaryExpression(Children&& children)
-    : children_{std::move(children)} {}
-
-// _____________________________________________________________________________
-
-template <typename NaryOperation>
-ExpressionResult NaryExpression<NaryOperation>::evaluate(
-    EvaluationContext* context) const {
-  auto resultsOfChildren = ad_utility::applyFunctionToEachElementOfTuple(
-      [context](const auto& child) { return child->evaluate(context); },
-      children_);
-
-  // Bind the `evaluateOnChildrenOperands` to a lambda.
-  auto evaluateOnChildOperandsAsLambda = [](auto&&... args) {
-    return evaluateOnChildrenOperands(AD_FWD(args)...);
-  };
-
-  // A function that only takes several `ExpressionResult`s,
-  // and evaluates the expression.
-  auto evaluateOnChildrenResults = absl::bind_front(
-      ad_utility::visitWithVariantsAndParameters,
-      evaluateOnChildOperandsAsLambda, NaryOperation{}, context);
-
-  return std::apply(evaluateOnChildrenResults, std::move(resultsOfChildren));
-}
-
-// _____________________________________________________________________________
-template <typename Op>
-ql::span<SparqlExpression::Ptr> NaryExpression<Op>::childrenImpl() {
-  return {children_.data(), children_.size()};
-}
-
-// __________________________________________________________________________
-template <typename Op>
-[[nodiscard]] std::string NaryExpression<Op>::getCacheKey(
-    const VariableToColumnMap& varColMap) const {
-  std::string key = typeid(*this).name();
-  key += ad_utility::lazyStrJoin(
-      children_ | ql::views::transform([&varColMap](const auto& child) {
-        return child->getCacheKey(varColMap);
-      }),
-      "");
-  return key;
-}
-
-// Define a class `Name` that is a strong typedef (via inheritance) from
-// `NaryExpresssion<N, X, ...>`. The strong typedef (vs. a simple `using`
-// declaration) is used to improve compiler messages as the resulting class has
-// a short and descriptive name.
-#define NARY_EXPRESSION(Name, N, X, ...)                                     \
-  class Name : public NaryExpression<detail::Operation<N, X, __VA_ARGS__>> { \
-    using Base = NaryExpression<Operation<N, X, __VA_ARGS__>>;               \
-    using Base::Base;                                                        \
-  };
-
 }  // namespace sparqlExpression::detail
 
 #endif  // QLEVER_SRC_ENGINE_SPARQLEXPRESSIONS_NARYEXPRESSIONIMPL_H

.github/workflows/native-build.yml
@@ -108,9 +108,10 @@ jobs:
       - name: Configure CMake
         env:
           USE_PCH: ${{ matrix.compiler-version == 21 && 'ON' || 'OFF' }}
+          CHEAPER_COMPILATION: ${{ matrix.compiler-version == 11 && 'ON' || 'OFF' }}
         # Configure CMake in a 'build' subdirectory. `CMAKE_BUILD_TYPE` is only required if you are using a single-configuration generator such as make.
         # See https://cmake.org/cmake/help/latest/variable/CMAKE_BUILD_TYPE.html?highlight=cmake_build_type
-        run: cmake -B ${{github.workspace}}/build -DCMAKE_BUILD_TYPE=${{matrix.build-type}} -DCMAKE_TOOLCHAIN_FILE="$(pwd)/toolchains/${{matrix.compiler}}${{matrix.compiler-version}}.cmake" -DADDITIONAL_COMPILER_FLAGS="${{matrix.warnings}} ${{matrix.asan-flags}} ${{matrix.ubsan-flags}}" -DUSE_PARALLEL=true -DRUN_EXPENSIVE_TESTS=${{matrix.expensive-tests}} -DENABLE_EXPENSIVE_CHECKS=true ${{matrix.additional-cmake-options}} -DADDITIONAL_LINKER_FLAGS="-B /usr/bin/mold" -DSINGLE_TEST_BINARY=ON -DUSE_PRECOMPILED_HEADERS=${{env.USE_PCH}}
+        run: cmake -B ${{github.workspace}}/build -DCMAKE_BUILD_TYPE=${{matrix.build-type}} -DCMAKE_TOOLCHAIN_FILE="$(pwd)/toolchains/${{matrix.compiler}}${{matrix.compiler-version}}.cmake" -DADDITIONAL_COMPILER_FLAGS="${{matrix.warnings}} ${{matrix.asan-flags}} ${{matrix.ubsan-flags}}" -DUSE_PARALLEL=true -DRUN_EXPENSIVE_TESTS=${{matrix.expensive-tests}} -DENABLE_EXPENSIVE_CHECKS=true ${{matrix.additional-cmake-options}} -DADDITIONAL_LINKER_FLAGS="-B /usr/bin/mold" -DSINGLE_TEST_BINARY=ON -DUSE_PRECOMPILED_HEADERS=${{env.USE_PCH}} -DCHEAPER_COMPILATION=${{env.CHEAPER_COMPILATION}}
 
       - name: Build
         # Build your program with the given configuration

CMakeLists.txt
@@ -48,6 +48,14 @@ add_compile_definitions(BOOST_ASIO_DISABLE_AWAITABLE_FRAME_RECYCLING)
 # contribute to the precompiled headers, then it might be beneficial to deactivate precompiled headers.
 option(USE_PRECOMPILED_HEADERS "Use precompiled headers to reduce compile times" ON)
 
+option(CHEAPER_COMPILATION "Use type erasure in several places, this reduces the compile time, but decreasese the performance. Can be used for local development" OFF)
+if (CHEAPER_COMPILATION)
+    add_compile_definitions(_QLEVER_TYPE_ERASED_EXPRESSIONS)
+    # The backports are cheaper to compile, as they do not use coroutines, which have an inherent compile time overhead
+    # even with debug builds.
+    add_compile_definitions(QLEVER_EXPRESSION_GENERATOR_BACKPORTS_FOR_CPP17)
+endif()
+
 # Coroutines require an additional compiler flag that is called differently
 # on clang and g++
 option(COMPILER_VERSION_CHECK_DEACTIVATED "Disable compiler version check" OFF)

src/engine/sparqlExpressions/ConditionalExpressions.cpp
@@ -32,11 +32,13 @@ struct IfImpl {
   }
 };
 
-// This macro implements an expression that evaluates the `IF()` function, but
-// will be extended below by additional member functions.
-NARY_EXPRESSION(IfExpressionImpl, 3,
-                FV<IfImpl, EffectiveBooleanValueGetter, ActualValueGetter,
-                   ActualValueGetter>);
+// This class implements an expression that evaluates the `IF()` function, but
+// will be extended below by additional member functions. It always uses
+// `NaryExpressionStronglyTyped` explicitly because `ActualValueGetter` doesn't
+// have a uniform result type, and therefore cannot be type-erased.
+using IfExpressionImpl = NaryExpressionStronglyTyped<
+    detail::Operation<3, FV<IfImpl, EffectiveBooleanValueGetter,
+                            ActualValueGetter, ActualValueGetter>>>;
 
 // The actual `IfExpression` class that adds an override for
 // `isResultAlwaysDefined`.

src/engine/sparqlExpressions/SparqlExpressionTypes.h
@@ -335,6 +335,34 @@ std::optional<ExpressionResult> evaluateOnSpecializedFunctionsIfPossible(
   return result;
 }
 
+// Implementation of the `ValueGetterPack` (see below).
+namespace valueGetterPack::detail {
+template <size_t N, typename>
+struct ValueGetterPackImpl;
+
+template <size_t N, typename... ValueGetters>
+struct ValueGetterPackImpl<N, std::tuple<ValueGetters...>> {
+  static_assert(sizeof...(ValueGetters) == 1 || N == sizeof...(ValueGetters));
+  using type = std::conditional_t<
+      sizeof...(ValueGetters) != 1, std::tuple<ValueGetters...>,
+      // `mp_repeat_c` repeats the first argument (a tuple of a single
+      // `ValueGetter` in our case) N times, and then concatenates it (into a
+      // tuple of N-times the same `ValueGetter`.
+      boost::mp11::mp_repeat_c<std::tuple<ValueGetters...>, N>>;
+};
+}  // namespace valueGetterPack::detail
+
+// In the SPARQL expression module, an N-ary operation can either specify `N`
+// different value getters (one for each argument), or a single value getter
+// (the same for each arguments). The following helper function takes the `N` as
+// well as a `std::tuple<ValueGetters...>` where either there have to be `N`
+// value getters in the tuple, or only a single value getter. The result is then
+// always a tuple of `N` value getters, (created by repeating the single value
+// getter n-times if necessary).
+template <size_t N, typename T>
+using ValueGetterPack =
+    typename valueGetterPack::detail::ValueGetterPackImpl<N, T>::type;
+
 // Class for an operation used in a `SparqlExpression`, consisting of the
 // function for computing the operation and the value getters for the operands.
 // The number of operands is fixed.
@@ -365,9 +393,7 @@ struct Operation {
  public:
   constexpr static size_t N = NumOperands;
   using Function = typename FunctionAndValueGettersT::Function;
-  using ValueGetters = std::conditional_t<
-      NV == 1, std::array<std::tuple_element_t<0, OriginalValueGetters>, N>,
-      OriginalValueGetters>;
+  using ValueGetters = ValueGetterPack<N, OriginalValueGetters>;
   Function _function;
   ValueGetters _valueGetters{};
   std::tuple<SpecializedFunctions...> _specializedFunctions{};
@@ -387,6 +413,16 @@ constexpr bool isOperation<Operation<NumOperations, Ts...>> = true;
 CPP_template(typename... Inputs)(requires(SingleExpressionResult<Inputs>&&...))
     size_t getResultSize(const EvaluationContext& context, const Inputs&...) {
   return (... && isConstantResult<Inputs>) ? 1ul : context.size();
+}
+
+// Helper to check if an `ExpressionResult` variant holds a constant.
+// Used by the type erased expression.
+inline bool isConstantExpressionResult(const ExpressionResult& res) {
+  return std::visit(
+      [](const auto& el) {
+        return isConstantResult<std::decay_t<decltype(el)>>;
+      },
+      res);
 }
 
 }  // namespace detail

commit 80a20da0278bccd4cd2cca8fc91565c777f14678

Fix `use-after-stack-return` in cheap compilation mode (#2993)
This fixes a regression introduced in #2735, which was caught by ASAN. One-line code change; the comment above it explains it.

src/engine/sparqlExpressions/NaryExpressionImpl.h
@@ -280,7 +280,11 @@ struct TypeErasedNaryHelper;
 
 template <typename Func, typename... VGs>
 struct TypeErasedNaryHelper<Func, std::tuple<VGs...>> {
-  using Res = std::invoke_result_t<Func, typename VGs::Value...>;
+  // `std::decay_t` ensures a value type, not a reference. Without this,
+  // expressions that use `ql:identity` as their functor, because the
+  // `ValueGetter`s do all the work would lead to dangling stack references.
+  // The strongly-typed path handles the same issue in `applyFunction`.
+  using Res = std::decay_t<std::invoke_result_t<Func, typename VGs::Value...>>;
   using BaseType = NaryExpressionTypeErasedImpl<Res, typename VGs::Value...>;
   static auto makeGetters() {
     return typename BaseType::Getters{TypeErasedValueGetter<VGs>{}...};