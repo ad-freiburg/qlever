d9dcce5e95d9d04d6da55c5c3108a84b4a5dd7d1
mete tolga <mtlg@tuta.io>
2026-07-01 15:44:22 +0200

    `cancellationHandle` added to `computeUniqueGraphIds` as a parameter.

--- src/index/CompressedRelation.cpp
+++ src/index/CompressedRelation.cpp
@@ -1412,7 +1412,8 @@ auto CompressedRelationReader::getFirstAndLastTripleIgnoringGraph(
 // ____________________________________________________________________________
 ad_utility::HashSet<Id::T> CompressedRelationReader::computeUniqueGraphIds(
     const CompressedRelationReader::ScanSpecAndBlocks& scanSpecAndBlocks,
-    const LocatedTriplesPerBlock& locatedTriplesPerBlock) const {
+    const LocatedTriplesPerBlock& locatedTriplesPerBlock,
+    const CancellationHandle& cancellationHandle) const {
   ad_utility::HashSet<Id::T> graphIds;
   std::array<ColumnIndex, 1> additionalColumns{ADDITIONAL_COLUMN_GRAPH_ID};
   const auto scanConfig =
@@ -1427,6 +1428,7 @@ ad_utility::HashSet<Id::T> CompressedRelationReader::computeUniqueGraphIds(
         });
     if (shouldScan) {
       auto block = readAndDecompressBlock(metadata, scanConfig);
+      cancellationHandle->throwIfCancelled();
       AD_CORRECTNESS_CHECK(block.has_value());
       for (Id id : block->block_.getColumn(ADDITIONAL_COLUMN_GRAPH_ID)) {
         graphIds.insert(id.getBits());
