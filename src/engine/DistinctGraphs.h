c490ec62d0c3435648835d50e241e005ad88c2f6
mete tolga <mtlg@tuta.io>
2026-07-01 15:16:17 +0200

    `computeUniqueGraphIds` function now takes scanSpecAndBlocks and locatedTriplesPerBlocks.

--- src/engine/DistinctGraphs.h
+++ src/engine/DistinctGraphs.h
@@ -1,6 +1,6 @@
 // Copyright 2026 The QLever Authors, in particular:
 //
-// 2026 Mete Tolga Gonultas mg885@email.uni-freiburg.de, UFR
+// 2026 Mete Tolga Gonultas <mg885@email.uni-freiburg.de>, UFR
 
 // UFR = University of Freiburg, Chair of Algorithms and Data Structures
 
