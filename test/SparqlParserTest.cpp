c61e408a8719160c29e0511151ae99e9323087a2
Johannes Kalmbach <joka921@users.noreply.github.com>
2026-07-07 17:32:44 +0200

    Update test/SparqlParserTest.cpp
    Co-authored-by: RobinTF <83676088+RobinTF@users.noreply.github.com>

--- test/SparqlParserTest.cpp
+++ test/SparqlParserTest.cpp
@@ -1694,7 +1694,7 @@ TEST(ParserTest, unionGraphAsDefaultGraphRuntimeParameter) {
 
   // The `USING` clause of a SPARQL Update is deliberately not affected by
   // this `RuntimeParameter`: an update without a `USING`/`WITH` clause will
-  // always only affect the `ql-default-graph`, even if the union graph is the
+  // always only affect the `ql:default-graph`, even if the union graph is the
   // default for queries.
   ad_utility::BlankNodeManager bnm;
   EncodedIriManager ev;
