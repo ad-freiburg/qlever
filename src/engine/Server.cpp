97ed3b366f88864d6dcc835d5147967c7125975b
Damekt <damektomas@seznam.cz>
2026-07-09 18:00:54 +0200

    Making one lambda mutable to allow move on cancellationHandle.

--- src/engine/Server.cpp
+++ src/engine/Server.cpp
@@ -624,7 +624,7 @@ CPP_template_def(typename RequestT, typename ResponseT)(
               name.value(), std::move(query.query_), query.datasetClauses_,
               std::move(cancellationHandle), timeLimit.value(), requestTimer);
         },
-        cancellationHandle);
+        std::move(cancellationHandle));
     co_await std::move(coroutine);
 
     // Construct simple response JSON.
