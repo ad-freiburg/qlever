c2c42158acce243bf0b394ff512c091a88fb69a6
Damekt <damektomas@seznam.cz>
2026-07-13 07:34:57 +0200

    Moved processUpdateImp from Server class into Qlever class.

--- src/engine/Server.h
+++ src/engine/Server.h
@@ -296,14 +296,6 @@ class Server {
           const std::weak_ptr<ad_utility::websocket::QueryHub>& queryHub,
           const RequestT& request, std::string_view operation,
           std::string_view clientIp = {});
-  // Execute an update operation. The function must have exclusive access to the
-  // DeltaTriples object.
-  UpdateMetadata processUpdateImpl(
-      const Index& index, const PlannedQuery& plannedUpdate,
-      ad_utility::SharedCancellationHandle cancellationHandle,
-      DeltaTriples& deltaTriples,
-      ad_utility::timer::TimeTracer& tracer =
-          ad_utility::timer::DEFAULT_TIME_TRACER);
 
   static json composeErrorResponseJson(
       const std::string& query, const std::string& errorMsg,
