8c8577bfe1423cdf573e7266110ec7284d77ac38
marvin <marvin.stoetzel@mailbox.org>
2026-08-03 09:50:34 +0200

    remove unused includes

--- src/engine/ExportQueryExecutionTrees.h
+++ src/engine/ExportQueryExecutionTrees.h
@@ -14,13 +14,10 @@
 #ifndef QLEVER_SRC_ENGINE_EXPORTQUERYEXECUTIONTREES_H
 #define QLEVER_SRC_ENGINE_EXPORTQUERYEXECUTIONTREES_H
 
-// #include <functional>
-
 #include "engine/ConstructTripleGenerator.h"
 #include "engine/QueryExecutionTree.h"
 #include "engine/QueryExportTypes.h"
 #include "parser/data/LimitOffsetClause.h"
-// #include "util/Algorithm.h"
 #include "util/CancellationHandle.h"
 #include "util/http/MediaTypes.h"
 #include "util/stream_generator.h"
