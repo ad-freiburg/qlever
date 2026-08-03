30a90e8ac1dec2d553e240a6b671e75d09654332
Damekt <damektomas@seznam.cz>
2026-08-03 17:55:23 +0200

    Added comment so that the UpdateExportLimit tests the indepedence of exportLimit on queries with LIMIT/OFFSET.

--- test/util/ParsedQueryTestHelpers.h
+++ test/util/ParsedQueryTestHelpers.h
@@ -13,11 +13,14 @@
 namespace ad_utility::testing {
 using namespace ad_utility::url_parser::sparqlOperation;
 
+// Create a shared `EncodedIriManager` instance used for testing.
 inline const EncodedIriManager* encodedIriManager() {
   static EncodedIriManager encodedIriManager_;
   return &encodedIriManager_;
 }
 
+// Help function that parses a SPARL query string into a typed `ParsedQuery`
+// object.
 inline auto parseQuery(std::string query,
                        const std::vector<DatasetClause>& datasets = {}) {
   return SparqlParser::parseQuery(encodedIriManager(), std::move(query),
