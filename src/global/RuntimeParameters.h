4bb37c800effdafe7e834e945bb6f4085c50afb7
marvin <marvin.stoetzel@mailbox.org>
2026-08-13 21:08:57 +0200

    chore: update copyright headers (add Marvin Stoetzel, extend to 2026)

--- src/global/RuntimeParameters.h
+++ src/global/RuntimeParameters.h
@@ -1,6 +1,9 @@
-//   Copyright 2024, University of Freiburg,
-//   Chair of Algorithms and Data Structures.
-//   Author: Robin Textor-Falconi <textorr@informatik.uni-freiburg.de>
+// Copyright 2024 - 2026, The QLever Authors, in particular:
+//
+// 2024 - 2026 Robin Textor-Falconi <textorr@informatik.uni-freiburg.de>, UFR
+// 2026        Marvin Stoetzel <stoetzem@email.uni-freiburg.de>, UFR
+//
+// UFR = University of Freiburg, Chair of Algorithms and Data Structures
 
 #ifndef QLEVER_RUNTIMEPARAMETERS_H
 #define QLEVER_RUNTIMEPARAMETERS_H
