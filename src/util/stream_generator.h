934d1bb0abb0435dd567a10d3a7348b4547f82f2
marvin <marvin.stoetzel@mailbox.org>
2026-08-13 19:54:21 +0200

    chore: update stream_generator.h copyright header (add Marvin Stoetzel)

--- src/util/stream_generator.h
+++ src/util/stream_generator.h
@@ -1,7 +1,8 @@
-// Copyright 2021 - 2025 The QLever Authors, in particular:
+// Copyright 2021 - 2026, The QLever Authors, in particular:
 //
 // 2021 Robin Textor-Falconi <textorr@cs.uni-freiburg.de>, UFR
-// 2025 Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>, UFR
+// 2025 - 2026 Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>, UFR
+// 2026        Marvin Stoetzel <stoetzem@email.uni-freiburg.de>, UFR
 
 // UFR = University of Freiburg, Chair of Algorithms and Data Structures
 
