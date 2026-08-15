f1d2dc88954ccb6465d95163edf58e1b3b235b45
mete tolga <mtlg@tuta.io>
2026-08-15 19:48:46 +0200

    fix: `GRAPH ?g` binding no longer relies on variable-name coincidence in subqueries

--- test/QueryPlannerTest.cpp
+++ test/QueryPlannerTest.cpp
@@ -2640,7 +2640,9 @@ TEST(QueryPlanner, graphVariablesWithinPattern) {
                      {Variable{internalVar(0)}}, {3})));
   h::expect(
       "SELECT ?x ?p WHERE { GRAPH ?g { { SELECT ?x ?p WHERE { ?x ?p ?g } } } }",
-      scan("?x", "?p", "?g", {}, NamedTag{}));
+      h::Bind(scan("?x", "?p", "?g", {}, NamedTag{}, {Variable{internalVar(0)}},
+                   {3}),
+              internalVar(0), Variable{"?g"}));
 }
 
 // _____________________________________________________________________________
