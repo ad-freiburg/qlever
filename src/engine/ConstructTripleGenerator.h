19d576d72352b4a76f4a29cf57da510f3e6b3657
marvin <marvin.stoetzel@mailbox.org>
2026-08-17 15:20:20 +0200

    docs: correct copyright author email

--- src/engine/ConstructTripleGenerator.h
+++ src/engine/ConstructTripleGenerator.h
@@ -1,5 +1,5 @@
 // Copyright 2026 The QLever Authors, in particular:
-// 2026 Marvin Stoetzel <marvin.stoetzel@email.uni-freiburg.de>, UFR
+// 2026 Marvin Stoetzel <stoetzem@email.uni-freiburg.de>, UFR
 // UFR = University of Freiburg, Chair of Algorithms and Data Structures
 //
 // You may not use this file except in compliance with the Apache 2.0 License,
