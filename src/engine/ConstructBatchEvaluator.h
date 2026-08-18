a43c7265a0370fa392ac895bb9ef724949ccfa1a
Marvin Stoetzel <stoetzem@email.uni-freiburg.de>
2026-08-18 12:26:41 +0200

    add todo comment

--- src/engine/ConstructBatchEvaluator.h
+++ src/engine/ConstructBatchEvaluator.h
@@ -37,6 +37,7 @@ struct BatchEvaluationResult {
   // set of evaluated columns may be sparse: some variables in the WHERE-clause
   // (in the `IdTable`) may not appear in the CONSTRUCT template and are thus
   // not evaluated.
+  // TODO<marvin7122> can this be a vector?
   ad_utility::HashMap<ColumnIndex, EvaluatedVariableValues> variablesByColumn_;
   size_t numRows_ = 0;
 
