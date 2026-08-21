a6bdac7b0a72cca11af2f12173176e40d802897b
marvin <marvin.stoetzel@mailbox.org>
2026-08-21 10:35:47 +0200

    fix: match SplitVocabulary helper constraints

--- src/index/vocabulary/SplitVocabulary.cpp
+++ src/index/vocabulary/SplitVocabulary.cpp
@@ -1,6 +1,12 @@
-// Copyright 2025 University of Freiburg
-// Chair of Algorithms and Data Structures
-// Author: Christoph Ullinger <ullingec@cs.uni-freiburg.de>
+// Copyright 2025 - 2026, The QLever Authors, in particular:
+//
+// 2025        Christoph Ullinger <ullingec@cs.uni-freiburg.de>, UFR
+// 2026        Marvin Stoetzel <stoetzem@email.uni-freiburg.de>, UFR
+//
+// UFR = University of Freiburg, Chair of Algorithms and Data Structures
+//
+// You may not use this file except in compliance with the Apache 2.0 License,
+// which can be found in the `LICENSE` file at the root of the QLever project.
 
 #include "index/vocabulary/GeoVocabulary.h"
 #include "index/vocabulary/SplitVocabularyImpl.h"
@@ -18,6 +24,9 @@ template class SplitVocabulary<GeoSplitFunc, GeoFilenameFunc,
 // _____________________________________________________________________________
 // Partition marked indices into underlying vocabulary-local index lists.
 template <typename SplitFunc, typename FilenameFunc, typename... VocabTypes>
+QL_CONCEPT_OR_NOTHING(
+    requires SplitFunctionT<SplitFunc>&& SplitFilenameFunctionT<
+        FilenameFunc, sizeof...(VocabTypes)>)
 typename SplitVocabulary<SplitFunc, FilenameFunc,
                          VocabTypes...>::IndicesByMarker
 SplitVocabulary<SplitFunc, FilenameFunc, VocabTypes...>::
