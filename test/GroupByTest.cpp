986c818b557f5abdb914806443606a6f7b8bac09
Anna Kaiser <kaisera@informatik.uni-freiburg.de>
2026-08-26 15:13:18 +0200

    wrote test for makeTreeWithStrippedColumns() for class GroupBy

--- test/GroupByTest.cpp
+++ test/GroupByTest.cpp
@@ -19,6 +19,7 @@
 #include "engine/QueryPlanner.h"
 #include "engine/Sort.h"
 #include "engine/SpatialJoinAlgorithms.h"
+#include "engine/StripColumns.h"
 #include "engine/Values.h"
 #include "engine/ValuesForTesting.h"
 #include "engine/sparqlExpressions/AggregateExpression.h"
@@ -3473,8 +3474,9 @@ TEST(GroupBy, makeTreeWithStrippedColumns) {
       qec, std::move(input), std::move(vars));
 
   // Test case 1: GroupBy keeps ?a and ?b.
-  // ?b is renamed as ?sumB
-  // makeTreeWithStrippedColumns has no additional variables.
+  // Alias: ?b results in ?sumB
+  // makeTreeWithStrippedColumns has ?a and ?sumB as variables.
+  // In this case there are less variables than groupByVariables and aliases.
   {
     Alias detAlias(
         SparqlExpressionPimpl{
@@ -3486,22 +3488,22 @@ TEST(GroupBy, makeTreeWithStrippedColumns) {
     GroupBy groupBy(qec, {Variable{"?a"}}, {detAlias}, subtree);
 
     std::optional<std::shared_ptr<QueryExecutionTree>> resultTree =
-        groupBy.makeTreeWithStrippedColumns(std::set<Variable>{});
+        groupBy.makeTreeWithStrippedColumns(
+            std::set<Variable>{Variable{"?a"}, Variable{"?sumB"}});
     ASSERT_TRUE(resultTree.has_value());
     ASSERT_TRUE((*resultTree) != nullptr);
     const VariableToColumnMap& v2cMap = (*resultTree)->getVariableColumns();
 
     EXPECT_EQ(v2cMap.size(), 2);
-    EXPECT_TRUE(v2cMap.contains(Variable{"?a"}));
-    EXPECT_FALSE(v2cMap.contains(Variable{"?b"}));
-    EXPECT_FALSE(v2cMap.contains(Variable{"?c"}));
-    EXPECT_FALSE(v2cMap.contains(Variable{"?d"}));
-    EXPECT_TRUE(v2cMap.contains(Variable{"?sumB"}));
+    EXPECT_THAT(v2cMap,
+                testing::UnorderedElementsAre(testing::Key(Variable{"?a"}),
+                                              testing::Key(Variable{"?sumB"})));
   }
 
   // Test case 2: GroupBy keeps ?b.
-  // ?b is renamed as ?sumB
-  // makeTreeWithStrippedColumns has no additional variables.
+  // Alias: ?b results in ?sumB
+  // makeTreeWithStrippedColumns has ?a, ?b and ?sumB as variables.
+  // In this case there are more variables than groupByVariables and aliases.
   {
     Alias detAlias(
         SparqlExpressionPimpl{
@@ -3513,66 +3515,92 @@ TEST(GroupBy, makeTreeWithStrippedColumns) {
     GroupBy groupBy(qec, {Variable{"?a"}, Variable{"?b"}}, {detAlias}, subtree);
 
     std::optional<std::shared_ptr<QueryExecutionTree>> resultTree =
-        groupBy.makeTreeWithStrippedColumns(std::set<Variable>{});
+        groupBy.makeTreeWithStrippedColumns(std::set<Variable>{
+            Variable{"?a"}, Variable{"?b"}, Variable{"?sumB"}});
     ASSERT_TRUE(resultTree.has_value());
     ASSERT_TRUE((*resultTree) != nullptr);
     const VariableToColumnMap& v2cMap = (*resultTree)->getVariableColumns();
 
     EXPECT_EQ(v2cMap.size(), 3);
-    EXPECT_TRUE(v2cMap.contains(Variable{"?a"}));
-    EXPECT_TRUE(v2cMap.contains(Variable{"?b"}));
-    EXPECT_FALSE(v2cMap.contains(Variable{"?c"}));
-    EXPECT_FALSE(v2cMap.contains(Variable{"?d"}));
-    EXPECT_TRUE(v2cMap.contains(Variable{"?sumB"}));
+    EXPECT_THAT(v2cMap,
+                testing::UnorderedElementsAre(testing::Key(Variable{"?a"}),
+                                              testing::Key(Variable{"?b"}),
+                                              testing::Key(Variable{"?sumB"})));
   }
 
   // Test case 3: GroupBy keeps ?a and ?c.
   // No Alias is available.
-  // makeTreeWithStrippedColumns has no additional variables.
+  // makeTreeWithStrippedColumns has ?a, ?c ?b and ?notIncluded as variables.
+  // In this case there are non valid variables.
   {
     GroupBy groupBy(qec, {Variable{"?a"}, Variable{"?c"}}, std::vector<Alias>{},
                     subtree);
 
     std::optional<std::shared_ptr<QueryExecutionTree>> resultTree =
-        groupBy.makeTreeWithStrippedColumns(std::set<Variable>{});
+        groupBy.makeTreeWithStrippedColumns(
+            std::set<Variable>{Variable{"?a"}, Variable{"?c"}, Variable{"?b"},
+                               Variable{"?notIncluded"}});
     ASSERT_TRUE(resultTree.has_value());
     ASSERT_TRUE((*resultTree) != nullptr);
     const VariableToColumnMap& v2cMap = (*resultTree)->getVariableColumns();
 
     EXPECT_EQ(v2cMap.size(), 2);
-    EXPECT_TRUE(v2cMap.contains(Variable{"?a"}));
-    EXPECT_FALSE(v2cMap.contains(Variable{"?b"}));
-    EXPECT_TRUE(v2cMap.contains(Variable{"?c"}));
-    EXPECT_FALSE(v2cMap.contains(Variable{"?d"}));
+    EXPECT_THAT(v2cMap,
+                testing::UnorderedElementsAre(testing::Key(Variable{"?a"}),
+                                              testing::Key(Variable{"?c"})));
   }
 
-  // Test case 4: GroupBy keeps ?c
+  // Test case 4: GroupBy keeps ?c and ?d
   // no aliases
-  // makeTreeWithStrippedColumns has ?d and ?noValidVariable as additional
-  // variables (these additional variables should not have any effect on the
-  // result, as groupBy is already stripped in the constructor).
+  // makeTreeWithStrippedColumns has ?d  as additional variables.
+  // In this case we have more groupByVariables than variables. Therefore check,
+  // whether additional StripColumns-Operation has been added.
   {
-    GroupBy groupBy(qec, {Variable{"?c"}}, {}, subtree);
+    GroupBy groupBy(qec, {Variable{"?c"}, Variable{"?d"}}, {}, subtree);
 
     std::optional<std::shared_ptr<QueryExecutionTree>> resultTree =
-        groupBy.makeTreeWithStrippedColumns(
-            std::set<Variable>{Variable{"?d"}, Variable{"?noValidVariable"}});
+        groupBy.makeTreeWithStrippedColumns(std::set<Variable>{Variable{"?d"}});
     ASSERT_TRUE(resultTree.has_value());
     ASSERT_TRUE((*resultTree) != nullptr);
     const VariableToColumnMap& v2cMap = (*resultTree)->getVariableColumns();
 
+    // Only ?d should be kept, as ?c is only needed for groupBy-Operation, but
+    // is not requested by parent tree
     EXPECT_EQ(v2cMap.size(), 1);
-    EXPECT_FALSE(v2cMap.contains(Variable{"?a"}));
-    EXPECT_FALSE(v2cMap.contains(Variable{"?b"}));
-    EXPECT_TRUE(v2cMap.contains(Variable{"?c"}));
-    EXPECT_FALSE(v2cMap.contains(Variable{"?d"}));
-    EXPECT_FALSE(v2cMap.contains(Variable{"?sumB"}));
+    EXPECT_TRUE(v2cMap.contains(Variable{"?d"}));
+
+    // Check whether a StripColumns operation has been added to the execution
+    // tree, because there are groupByVariables_ which are not included in the
+    // required variables of the parent tree. The only remaining variable of the
+    // StripColumns-Operation is ?d.
+    auto rootOperation = (*resultTree)->getRootOperation();
+    StripColumns* stripColumnsOperation =
+        dynamic_cast<StripColumns*>(rootOperation.get());
+    ASSERT_TRUE(stripColumnsOperation != nullptr);
+    VariableToColumnMap strColMap =
+        stripColumnsOperation->computeVariableToColumnMap();
+    EXPECT_EQ(strColMap.size(), 1);
+    EXPECT_TRUE(strColMap.contains(Variable{"?d"}));
+
+    // Check whether the GroupBy-Operation has still the same groupByVariables_
+    std::vector<QueryExecutionTree*> subtree =
+        stripColumnsOperation->getChildren();
+    ASSERT_TRUE(subtree.at(0) != nullptr);
+    auto groupByOp = subtree.at(0)->getRootOperation();
+    GroupBy* groupByOperation = dynamic_cast<GroupBy*>(groupByOp.get());
+    ASSERT_TRUE(groupByOperation != nullptr);
+    std::vector<Variable> newGroupByVariables =
+        groupByOperation->groupByVariables();
+    EXPECT_EQ(newGroupByVariables.size(), 2);
+    EXPECT_EQ(newGroupByVariables.at(0), Variable{"?c"});
+    EXPECT_EQ(newGroupByVariables.at(1), Variable{"?d"});
   }
 
-  // Test case 5: GroupBy keeps ?a, ?b, ?c.
-  // ?b is renamed as ?sumB
-  // makeTreeWithStrippedColumns has no additional variables.
-  // Do _groupByVariables and _aliases stay the same in the new Operation?
+  // Test case 5: GroupBy keeps ?a
+  // Alias: ?b results in ?sumB
+  // makeTreeWithStrippedColumns has ?a as variable.
+  // Check whether the Alias is removed, because its target is not requested by
+  // the parent-tree.
   {
     Alias detAlias(
         SparqlExpressionPimpl{
@@ -3581,32 +3609,24 @@ TEST(GroupBy, makeTreeWithStrippedColumns) {
             "SUM(?b)"},
         Variable{"?sumB"});
 
-    GroupBy groupBy(qec, {Variable{"?a"}, Variable{"?c"}}, {detAlias}, subtree);
+    GroupBy groupBy(qec, {Variable{"?a"}}, {detAlias}, subtree);
 
     std::optional<std::shared_ptr<QueryExecutionTree>> resultTree =
-        groupBy.makeTreeWithStrippedColumns(std::set<Variable>{});
+        groupBy.makeTreeWithStrippedColumns(std::set<Variable>{Variable{"?a"}});
     ASSERT_TRUE(resultTree.has_value());
     ASSERT_TRUE((*resultTree) != nullptr);
     const VariableToColumnMap& v2cMap = (*resultTree)->getVariableColumns();
 
-    EXPECT_EQ(v2cMap.size(), 3);
+    EXPECT_EQ(v2cMap.size(), 1);
     EXPECT_TRUE(v2cMap.contains(Variable{"?a"}));
-    EXPECT_FALSE(v2cMap.contains(Variable{"?b"}));
-    EXPECT_TRUE(v2cMap.contains(Variable{"?c"}));
-    EXPECT_FALSE(v2cMap.contains(Variable{"?d"}));
-    EXPECT_TRUE(v2cMap.contains(Variable{"?sumB"}));
 
+    // Check whether a StripColumns operation has been added to the execution
+    // tree, because there are groupByVariables_ which are not included in the
+    // required variables of the parent tree. The only remaining variable of the
+    // StripColumns-Operation is ?d.
     auto rootOperation = (*resultTree)->getRootOperation();
     GroupBy* groupByOperation = dynamic_cast<GroupBy*>(rootOperation.get());
     ASSERT_TRUE(groupByOperation != nullptr);
-    std::vector<Variable> newGroupByVariables =
-        groupByOperation->groupByVariables();
-    std::vector<Alias> newAliases = groupByOperation->aliases();
-
-    EXPECT_EQ(newGroupByVariables.size(), 2);
-    EXPECT_EQ(newGroupByVariables.at(0), Variable{"?a"});
-    EXPECT_EQ(newGroupByVariables.at(1), Variable{"?c"});
-    EXPECT_EQ(newAliases.size(), 1);
-    EXPECT_EQ(newAliases.at(0), detAlias);
+    ASSERT_EQ((groupByOperation->aliases()).size(), 0);
   }
 }
