5a83ddab3d46eff99695f33f812bb99e4a2aaaee
ullingerc <ullingec@informatik.uni-freiburg.de>
2026-09-01 10:22:35 +0200

    fixup: header, drop redundant include

--- test/BitUtilsTest.cpp
+++ test/BitUtilsTest.cpp
@@ -1,10 +1,14 @@
-//  Copyright 2022 - 2025, University of Freiburg,
-//  Chair of Algorithms and Data Structures.
-//  Authors: Johannes Kalmbach <kalmbach@cs.uni-freiburg.de>
-//           Christoph Ullinger <ullingec@cs.uni-freiburg.de>
+// Copyright 2022 - 2026 The QLever Authors, in particular:
+//
+// 2022 - 2025 Johannes Kalmbach <kalmbach@informatik.uni-freiburg.de>, UFR
+// 2025 - 2026 Christoph Ullinger <ullingec@informatik.uni-freiburg.de>, UFR
+//
+// UFR = University of Freiburg, Chair of Algorithms and Data Structures
+
+// You may not use this file except in compliance with the Apache 2.0 License,
+// which can be found in the `LICENSE` file at the root of the QLever project.
 
 #include <gmock/gmock.h>
-#include <gtest/gtest.h>
 
 #include "util/BitUtils.h"
 
