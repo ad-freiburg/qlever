9c1fa32c1138953d9dff0ed24e6ac74bc3fba563
ullingerc <ullingec@informatik.uni-freiburg.de>
2026-09-04 09:09:59 +0200

    Update copyright headers

--- src/engine/sparqlExpressions/GeoExpression.cpp
+++ src/engine/sparqlExpressions/GeoExpression.cpp
@@ -1,9 +1,13 @@
-// Copyright 2021 - 2025
-// University of Freiburg
-// Chair of Algorithms and Data Structures
-// Authors: Johannes Kalmbach <kalmbacj@cs.uni-freiburg.de>
-//          Hannah Bast <bast@cs.uni-freiburg.de>
-//          Christoph Ullinger <ullingec@cs.uni-freiburg.de>
+// Copyright 2021 - 2026 The QLever Authors, in particular:
+//
+// 2021 - 2025 Johannes Kalmbach <kalmbacj@cs.uni-freiburg.de>, UFR
+// 2021 - 2025 Hannah Bast <bast@cs.uni-freiburg.de>, UFR
+// 2021 - 2026 Christoph Ullinger <ullingec@informatik.uni-freiburg.de>, UFR
+//
+// UFR = University of Freiburg, Chair of Algorithms and Data Structures
+
+// You may not use this file except in compliance with the Apache 2.0 License,
+// which can be found in the `LICENSE` file at the root of the QLever project.
 
 #include "backports/type_traits.h"
 #include "engine/SpatialJoinConfig.h"
