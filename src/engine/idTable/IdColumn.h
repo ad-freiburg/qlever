c21a40b23894d85cffc3676ccb7ca08af0714f15
PasKes <pascal_kessler@hotmail.de>
2026-09-25 16:24:09 +0200

    Adding copyright header to IdColumn.h

--- src/engine/idTable/IdColumn.h
+++ src/engine/idTable/IdColumn.h
@@ -1,3 +1,12 @@
+// Copyright 2026 The QLever Authors, in particular:
+//
+// 2026 Pascal Keßler <kesslerp@informatik.uni-freiburg.de>, UFR
+//
+// UFR = University of Freiburg, Chair of Algorithms and Data Structures
+//
+// You may not use this file except in compliance with the Apache 2.0 License,
+// which can be found in the `LICENSE` file at the root of the QLever project.
+
 #ifndef QLEVER_SRC_ENGINE_IDTABLE_IDCOLUMN_H
 #define QLEVER_SRC_ENGINE_IDTABLE_IDCOLUMN_H
 
